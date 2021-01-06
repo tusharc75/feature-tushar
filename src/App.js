9d9bebfd2ca6b4eed682b51a60408f24e093a5f0
unknown <pranavchandra27@gmail.com>
2021-01-06 15:38:27 +0530

    add opportunity page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "@material-ui/core";
+import { theme } from "./constants/AppConfig";
+import Opportunities from "./pages/Opportunities";
+
 function App() {
-    return (
-        <div>Hello OMS</div>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Opportunities />
+    </ThemeProvider>
+  );
 }
 
 export default App;
