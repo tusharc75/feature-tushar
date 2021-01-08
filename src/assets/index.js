0eb344472e645d638158ecb2e36b5257b0993280
unknown <pranavchandra27@gmail.com>
2021-01-08 13:18:53 +0530

    svg icons for sidebar

---
+++ src/assets/index.js
@@ -0,0 +1,43 @@
+import activities from "./svg/activities.svg";
+import calender from "./svg/calender.svg";
+import contact from "./svg/contact.svg";
+import customer from "./svg/customer.svg";
+import dashboard from "./svg/dashboard.svg";
+import doa from "./svg/doa.svg";
+import flag from "./svg/flag.svg";
+import dollar from "./svg/dollar.svg";
+import leads from "./svg/leads.svg";
+import location from "./svg/location.svg";
+import logo from "./svg/logo.svg";
+import opportunities from "./svg/opportunities.svg";
+import pricing from "./svg/pricing.svg";
+import product from "./svg/product.svg";
+import quote_builder from "./svg/quote_builder.svg";
+import reminder from "./svg/reminder.svg";
+import suppliers from "./svg/suppliers.svg";
+import terms_conditions from "./svg/terms_conditions.svg";
+import users from "./svg/users.svg";
+import menu_icon from "./svg/menu_icon.svg";
+
+export {
+  menu_icon,
+  activities,
+  calender,
+  contact,
+  customer,
+  dashboard,
+  doa,
+  flag,
+  dollar,
+  leads,
+  location,
+  logo,
+  opportunities,
+  pricing,
+  product,
+  quote_builder,
+  reminder,
+  suppliers,
+  terms_conditions,
+  users,
+};
