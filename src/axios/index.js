0e37c51f041ddfcea96e9794062576c92188fbd6
punit <punit.soni33@gmail.com>
2021-02-27 19:56:00 +0530

    - Contact List, Create Screen Added

--- src/axios/index.js
+++ src/axios/index.js
@@ -22,18 +22,12 @@ export const UserMe = async () => {
     return data;
 };
 
-export const GetContacts = async () => {
-    const { data } = await api().get(`${BASE_URL}/contact`);
-    return data;
-};
-
-export const GetFields = async (resource, id) => {
-    const { data } = await api().get(`/sa-field?brand=${id}&resource=${resource}`);
+export const GetFields = async (resource) => {
+    const { data } = await api().get(`/field?resource=${resource}`);
     return data;
 };
 
 export const GetAccounts = async (params) => {
-    // const { data } = await api().get(`${BASE_URL}/account?brand=${brandId}`);
     let url = "/account";
     url = getSearchQuery(url, params);
     const { data } = await api().get(url);
@@ -45,6 +39,27 @@ export const RemoveAccounts = async (obj) => {
     const { data } = await api().put(url, obj);
     return data;
 };
+
+export const GetContacts = async (params) => {
+    let url = "/contact";
+    url = getSearchQuery(url, params);
+    const { data } = await api().get(url);
+    return data;
+};
+
+export const RemoveContacts = async (obj) => {
+    let url = "/contact/remove";
+    const { data } = await api().put(url, obj);
+    return data;
+};
+
+export const CreateNewContact = async (obj) => {
+    let url = "/contact";
+    const { data } = await api().post(url, obj);
+    return data;
+};
+
+
 
 export const getSearchQuery = (url, params) => {
     if (params && Object.keys(params).length > 0) {
