2c4a5567d03947baa13c5f6d5627fa778181b8e5
punit <punit.soni33@gmail.com>
2021-02-28 18:23:21 +0530

    - Fields clean on saving account solved

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://equipt-oms-v2.herokuapp.com";
+const BASE_URL = process?.env?.REACT_APP_API_URL || "https://equipt-oms-v2.herokuapp.com";
 
 const axiosAPI = () => {
     const token = localStorage.getItem("token");
