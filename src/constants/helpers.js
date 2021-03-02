0d14fd5d29acf84972dee5102db9f6f9ceed0986
punit <punit.soni33@gmail.com>
2021-03-02 20:53:53 +0530

    - New Contact On Modal Done

--- src/constants/helpers.js
+++ src/constants/helpers.js
@@ -80,3 +80,10 @@ const isEmailExist = async (email) => {
         return false;
     }
 };
+
+export const removeEmptyKeys = (obj) => {
+    Object.keys(obj).forEach(
+        (k) => !obj[k] && obj[k] !== undefined && delete obj[k]
+    )
+    return obj
+}
