55366b32cbbabf666ae4c4fd3750eaebada9080d
punit <punit.soni33@gmail.com>
2021-03-03 20:00:01 +0530

    - Reverse Merged With Master

--- src/pages/Account/CreateAccount/index.js
+++ src/pages/Account/CreateAccount/index.js
@@ -71,7 +71,6 @@ export default function CreateAccountMain(props) {
                     setLoading(false);
                 }, error => {
                     setLoading(false);
-                    debugger;
                 });
             });
         }
