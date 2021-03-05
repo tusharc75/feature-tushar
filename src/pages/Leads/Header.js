d9cfbf9d1c12e5d1cddcca20d5ffebbe57c452c5
punit <punit.soni33@gmail.com>
2021-03-05 18:43:22 +0530

    - Conflicts Resolved

--- src/pages/Leads/Header.js
+++ src/pages/Leads/Header.js
@@ -1,89 +1,89 @@
-import React from 'react'
-import SearchBox from '../../components/Helpers/SearchBox'
-import { makeStyles } from "@material-ui/core/styles";
-import { FilterList, SortByAlpha, Search, AddOutlined } from "@material-ui/icons";
-import {
-    Box,
-    Grid,
-    Select,
-    MenuItem,
-    FormControl,
-    IconButton,
-    Button,
+// import React from 'react'
+// import SearchBox from '../../components/Helpers/SearchBox'
+// import { makeStyles } from "@material-ui/core/styles";
+// import { FilterList, SortByAlpha, Search, AddOutlined } from "@material-ui/icons";
+// import {
+//     Box,
+//     Grid,
+//     Select,
+//     MenuItem,
+//     FormControl,
+//     IconButton,
+//     Button,
 
-} from "@material-ui/core";
+// } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
-    filterSide: {
-        display: "flex",
-        justifyContent: "flex-end",
-    },
-}));
+// const useStyles = makeStyles((theme) => ({
+//     filterSide: {
+//         display: "flex",
+//         justifyContent: "flex-end",
+//     },
+// }));
 
-function LeadsHeader(props) {
-    const classes = useStyles();
-    const { selectedType, onTypeChange, options, onSearch, searchVal, onCreate } = props
-    return <Grid container>
-        <Grid item xs={6}>
-            <FormControl style={{ minWidth: "170px" }}>
-                {
-                    Object.keys(options).length ? <Select
-                        style={{ width: '160px' }}
-                        displayEmpty
-                        labelId="demo-simple-select-outlined-label"
-                        inputProps={{ "aria-label": "Without label" }}
-                        id="demo-simple-select-outlined"
-                        MenuProps={{
-                            anchorOrigin: {
-                                vertical: "bottom",
-                                horizontal: "left"
-                            },
-                            getContentAnchorEl: null
-                        }}
-                        value={selectedType}
-                        onChange={onTypeChange}
-                        label="Select Type"
-                    >
-                        {
-                            Object.keys(options).map((k, index) => {
-                                return <MenuItem key={index} value={options[k]}>{k}</MenuItem>
-                            })
-                        }
-                    </Select>
-                        : null
-                }
-            </FormControl>
-        </Grid>
-        <Grid item xs={6} className={classes.filterSide}>
-            <Box component="div">
-                <Box component="span" marginX={1} />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={onCreate}
-                    startIcon={<AddOutlined />}
-                >
-                    Add
-                </Button>
-                <IconButton>
-                    <FilterList />
-                </IconButton>
-                <IconButton>
-                    <SortByAlpha />
-                </IconButton>
+// function LeadsHeader(props) {
+//     const classes = useStyles();
+//     const { selectedType, onTypeChange, options, onSearch, searchVal, onCreate } = props
+//     return <Grid container>
+//         <Grid item xs={6}>
+//             <FormControl style={{ minWidth: "170px" }}>
+//                 {
+//                     Object.keys(options).length ? <Select
+//                         style={{ width: '160px' }}
+//                         displayEmpty
+//                         labelId="demo-simple-select-outlined-label"
+//                         inputProps={{ "aria-label": "Without label" }}
+//                         id="demo-simple-select-outlined"
+//                         MenuProps={{
+//                             anchorOrigin: {
+//                                 vertical: "bottom",
+//                                 horizontal: "left"
+//                             },
+//                             getContentAnchorEl: null
+//                         }}
+//                         value={selectedType}
+//                         onChange={onTypeChange}
+//                         label="Select Type"
+//                     >
+//                         {
+//                             Object.keys(options).map((k, index) => {
+//                                 return <MenuItem key={index} value={options[k]}>{k}</MenuItem>
+//                             })
+//                         }
+//                     </Select>
+//                         : null
+//                 }
+//             </FormControl>
+//         </Grid>
+//         <Grid item xs={6} className={classes.filterSide}>
+//             <Box component="div">
+//                 <Box component="span" marginX={1} />
+//                 <Button
+//                     variant="contained"
+//                     color="primary"
+//                     onClick={onCreate}
+//                     startIcon={<AddOutlined />}
+//                 >
+//                     Add
+//                 </Button>
+//                 <IconButton>
+//                     <FilterList />
+//                 </IconButton>
+//                 <IconButton>
+//                     <SortByAlpha />
+//                 </IconButton>
 
-                <Box component="span" marginX={1} />
-                <SearchBox
-                    onSearch={onSearch}
-                    value={searchVal}
-                    style={{ paddingTop: '5px' }}
-                    size="small"
-                    placeholder="Search Leads"
-                    width='242px'
-                />
+//                 <Box component="span" marginX={1} />
+//                 <SearchBox
+//                     onSearch={onSearch}
+//                     value={searchVal}
+//                     style={{ paddingTop: '5px' }}
+//                     size="small"
+//                     placeholder="Search Leads"
+//                     width='242px'
+//                 />
 
-            </Box>
-        </Grid>
-    </Grid>
-}
-export default LeadsHeader
+//             </Box>
+//         </Grid>
+//     </Grid>
+// }
+// export default LeadsHeader
--- src/pages/Leads/index.js
+++ src/pages/Leads/index.js
@@ -1,68 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-
 import {
-  Typography,
+  Box,
+  Button,
   Grid,
-  Divider
+  Link,
+  Divider,
+  Menu,
+  MenuItem,
+  Select
 } from "@material-ui/core";
+import { Add, ExpandMore } from "@material-ui/icons";
 import { DataGrid } from "@material-ui/data-grid";
-import Header from "./Header";
-
+import CustomBreadCrumbs from './../../components/CustomBreadCrumbs';
+import routes from './../../components/Helpers/Routes';
 import Layout from "../../components/Layout";
 import Container from "../../components/Container";
-import { Link } from 'react-router-dom'
-import CreateLeadDialog from './CreateLead'
-import routes from './../../components/Helpers/Routes';
-import CustomBreadCrumbs from './../../components/CustomBreadCrumbs';
 import SearchBox from "../../components/Helpers/SearchBox";
+import CreateLeadDialog from './CreateLead'
+
 import "./style.css";
 
 const useStyles = makeStyles((theme) => ({
-  footerText: {
-    textAlign: "center",
-  },
   linksContainer: {
     display: "flex",
   },
   links: {
-    color: theme.palette.textDark
+    color: theme.palette.textDark,
   },
   linkDivider: {
     backgroundColor: theme.palette.darkBg,
     margin: "0 1rem",
-  }
+  },
 }));
 
 const LeadTypes = {
-  "All Accounts": 1,
-  "My Accounts": 2,
+  "All Leads": 1,
+  "My Leads": 2,
 };
+
 const Leads = () => {
   const classes = useStyles();
-  const theme = useTheme();
-  const [age, setAge] = useState("All");
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [loading, setLoading] = useState(false);
-
   const [searchVal, setSearchVal] = useState("");
-  const [selectedType, setselectedType] = useState(1)
-  const [isOpen, setIsOpen] = useState(false)
   const [query, setQuery] = useState({ page: 0, limit: 25 });
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedType, setSelectedType] = useState(1)
+  const [isOpen, setIsOpen] = useState(false)
 
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
-
-  const buttonProps = [
-    { title: "New Lead", bg: theme.palette.darkBg, color: "#fff" },
-    { title: "Delete Lead", bg: theme.palette.lightBg, color: "#fff" },
-  ];
-
-  const handleLeadTypeSel = (e) => {
-    setselectedType(e.target.value);
-  };
   const handleSearch = (e) => {
     if (query.page !== 1) {
       setQuery((prevState) => ({ ...prevState, page: 0 }));
@@ -70,12 +54,27 @@ const Leads = () => {
     setSearchVal(e.target.value);
   };
 
+  // ****** ACTIONS BUTTON STUFF *********
+  const openActions = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const closeActions = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLeadTypeSel = (e) => {
+    setSelectedType(e.target.value);
+  };
+
   const handleCreate = () => {
     setIsOpen(true)
   }
+
   const handleClose = () => {
     setIsOpen(false)
   }
+
   return (
     <Layout>
       <Grid container spacing={3} direction="row">
@@ -90,7 +89,7 @@ const Leads = () => {
               className={classes.links}
             >
               Import from Excel
-                            </Link>
+            </Link>
             <Divider
               orientation="vertical"
               flexItem
@@ -102,7 +101,7 @@ const Leads = () => {
               className={classes.links}
             >
               Export to Excel
-                            </Link>
+            </Link>
             <Divider
               orientation="vertical"
               flexItem
@@ -114,7 +113,7 @@ const Leads = () => {
               className={classes.links}
             >
               Download Template
-                            </Link>
+            </Link>
             <Divider
               orientation="vertical"
               flexItem
@@ -126,25 +125,89 @@ const Leads = () => {
               className={classes.links}
             >
               Email a Link
-                            </Link>
+            </Link>
           </Grid>
         </Grid>
       </Grid>
 
+      {/* Tables Begins Here */}
       <Container>
-        <Header
-          selectedType={selectedType}
-          onTypeChange={handleLeadTypeSel}
-          options={LeadTypes}
-          onSearch={handleSearch}
-          searchVal={searchVal}
-          onCreate={handleCreate}
-        />
-        <CreateLeadDialog
-          open={isOpen}
-          onClose={handleClose}
-        />
+        <Grid container justify="space-between">
+          <Grid item>
+            {
+              Object.keys(LeadTypes).length ? <Select
+                style={{ width: '160px' }}
+                displayEmpty
+                labelId="demo-simple-select-outlined-label"
+                inputProps={{ "aria-label": "Without label" }}
+                id="demo-simple-select-outlined"
+                MenuProps={{
+                  anchorOrigin: {
+                    vertical: "bottom",
+                    horizontal: "left"
+                  },
+                  getContentAnchorEl: null
+                }}
+                value={selectedType}
+                onChange={handleLeadTypeSel}
+                label="Select Type"
+              >
+                {
+                  Object.keys(LeadTypes).map((k, index) => {
+                    return <MenuItem key={index} value={LeadTypes[k]}>{k}</MenuItem>
+                  })
+                }
+              </Select>
+                : null
+            }
+          </Grid>
+
+          <Grid item>
+            <SearchBox onSearch={handleSearch} value={searchVal} size="small" />
+            <Box component="span" marginX={1} />
+
+            <Button variant="contained" color="primary" startIcon={<Add />} onClick={handleCreate}>
+              Add
+            </Button>
+
+            <Box component="span" marginX={1} />
+
+            <Button
+              variant="outlined"
+              color="default"
+              aria-controls="action-menu"
+              onClick={openActions}
+            >
+              Actions <ExpandMore />
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              keepMounted
+              getContentAnchorEl={null}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              id="action-menu"
+              open={Boolean(anchorEl)}
+              onClose={closeActions}
+            >
+              <MenuItem>Delete</MenuItem>
+            </Menu>
+          </Grid>
+        </Grid>
       </Container>
+
+      <Container styles={{ minHeight: "calc(100vh - 210px)", padding: 10 }}>
+        <div className="contact-grid-height1">
+          <DataGrid columns={[]} rows={[]} />
+        </div>
+      </Container>
+
+      <CreateLeadDialog
+        open={isOpen}
+        onClose={handleClose}
+      />
     </Layout>
   );
 };
