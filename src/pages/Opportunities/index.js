1f8a4a9c668d822f531182f8217c50bf34446c4d
swatigavhane1996@gmail.com <swatigavhane1996@gmail.com>
2021-03-09 12:40:33 +0530

    Changes added for Opprtunities listing

--- src/pages/Opportunities/index.js
+++ src/pages/Opportunities/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import {
   Box,
@@ -8,15 +8,28 @@ import {
   Divider,
   Menu,
   MenuItem,
-} from "@material-ui/core";
-import { DataGrid } from "@material-ui/data-grid";
-import { Add, ExpandMore } from "@material-ui/icons";
+  IconButton,
+  Tooltip,
+  Checkbox,
 
+} from "@material-ui/core";
+import DeleteIcon from '@material-ui/icons/Delete';
+import BlockIcon from '@material-ui/icons/Block';
+import { DataGrid, GridToolbar } from "@material-ui/data-grid";
+import { Add, ExpandMore } from "@material-ui/icons";
+import { useData } from '../../StateProvider/Provider';
 import Layout from "../../components/Layout";
 import Container from "../../components/Container";
 import SearchBox from "../../components/Helpers/SearchBox";
+import { capitalize } from '../../services/util'
+import axiosInstance from '../../axios/axiosInstance'
+import { getSearchQuery } from '../../services/util'
+import { CustomEventEmitter } from './../../axios/events';
+import Header from "./Header";
+import ConfirmationDialog from '../../components/Helpers/ConfirmationDialog'
 import "./style.css";
 
+let opportunityTimeout
 const useStyles = makeStyles((theme) => ({
   linksContainer: {
     display: "flex",
@@ -30,14 +43,100 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const OpportunityTypes = {
+  "All Opportunities": 1,
+  "My Opportunities": 2,
+};
+
 const Opportunities = () => {
   const classes = useStyles();
+  const { state: { user } } = useData();
   const [searchVal, setSearchVal] = useState("");
   const [query, setQuery] = useState({ page: 0, limit: 25 });
   const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedType, setSelectedType] = useState(1)
+  const [isOpen, setIsOpen] = useState(false)
+  const [checkAllOpprtunities, setCheckAllOpportunities] = useState(false);
+  const [dataRows, setDataRows] = useState([]);
+  const [rowCount, setRowCount] = useState(0);
+  const [renderCount, setRenderCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [deleteLoading, setDeleteLoading] = useState(false);
+  const [opportunityData, setOpportunityData] = useState([]);
+  const [isConfirmDialogVisible, setIsConformDialogVisible] = useState(false)
+  const [deleteRec, setDeleteRec] = useState({})
+  const [opportunityPermissions, setOpportunityPermissions] = useState({ isCreate: false, isRead: false, isDelete: false });
+
+  useEffect(() => {
+    const data = user?.role?.sideBar;
+
+    if (data) {
+      const hasAccountPermission = data.find(d => d.name == "Opportunity");
+      if (hasAccountPermission) {
+        setOpportunityPermissions({ isCreate: hasAccountPermission.isCreate, isRead: hasAccountPermission.isRead, isDelete: hasAccountPermission.isDelete });
+      }
+    }
+  }, [user]);
+
+  useEffect(() => {
+    let millisec = Object.keys(searchVal).length > 0 ? 600 : 5;
+    if (opportunityTimeout) {
+      clearTimeout(opportunityTimeout);
+    }
+
+    opportunityTimeout = setTimeout(() => {
+      fetchOpportunities();
+    }, millisec);
+
+  }, [searchVal]);
+
+  useEffect(() => {
+    if (renderCount > 0) {
+      fetchOpportunities();
+    } else setRenderCount((preCount) => preCount + 1);
+  }, [query, , selectedType]);
+
+  useEffect(() => {
+    let rows = opportunityData?.map((u) => {
+      let name = capitalize(u.firstName || '') + ' '
+      name = name + capitalize(u.middleName || '') + ' '
+      name = name + capitalize(u.lastName || '')
+
+      let res = {
+        ...u,
+        isChecked: false,
+        id: u._id,
+        name: name,
+        owner: u.owner?.optionLabel ? u.owner.optionLabel : '',
+      }
+      return res
+    });
+    setDataRows([...rows]);
+  }, [opportunityData])
+
+  const fetchOpportunities = async () => {
+    setLoading(true);
+    let searchParams = { ...query, filterOpportunites: selectedType }
+    searchParams = searchVal
+      ? { ...searchParams, search: searchVal }
+      : { ...searchParams };
+    let api = getSearchQuery("/opportunity", searchParams)
+    try {
+      axiosInstance()
+        .get(api).then(({ data }) => {
+          setRowCount(data.count)
+          setOpportunityData(data.data)
+          setLoading(false);
+        })
+
+    }
+    catch (err) {
+      setLoading(false);
+    }
+  }
 
   const handleSearch = (e) => {
-    if (query.page !== 1) {
+    if (query.page !== 0) {
       setQuery((prevState) => ({ ...prevState, page: 0 }));
     }
     setSearchVal(e.target.value);
@@ -51,6 +150,176 @@ const Opportunities = () => {
   const closeActions = () => {
     setAnchorEl(null);
   };
+
+  const handleOpportunityTypeSel = (e) => {
+    setSelectedType(e.target.value);
+  };
+
+  const handleCreate = () => {
+    setIsOpen(true)
+  }
+
+  const handleClose = () => {
+    setIsOpen(false)
+  }
+
+  const columns = [
+    {
+      field: "isChecked",
+      headerName: "Checkbox",
+      renderHeader: () => (
+        <Checkbox
+          color="primary"
+          checked={checkAllOpprtunities}
+          onChange={(ev) => {
+            setCheckAllOpportunities(ev.target.checked);
+            const gridData = dataRows;
+            gridData.map((d) => {
+              if (d.allowToDelete) {
+                d.isChecked = ev.target.checked;
+              }
+              return d;
+            });
+            setDataRows([...gridData]);
+          }}
+        />
+      ),
+      renderCell: (params) => (
+        params.row.allowToDelete ? <Checkbox
+          color="primary"
+          checked={params.value}
+          onChange={(ev) => {
+            updateCheckedStatus(params, ev)
+
+          }}
+        /> : <Tooltip className="cursor-stop" title="You must be the owner or collaborator of this contact to get the selection functionality">
+            <IconButton>
+              <BlockIcon fontSize="small" color="error" />
+            </IconButton>
+          </Tooltip>
+      ),
+      disableColumnMenu: true,
+      sortable: false,
+      filterable: false,
+      width: 75,
+    },
+    {
+      field: "firstName", headerName: "Name", width: 200,
+      renderCell: (params) => (
+        getFirstName(params.row)
+      )
+    },
+    { field: "title", headerName: "Title", width: 200 },
+    { field: "company", headerName: "Company", width: 200 },
+    { field: "phone", headerName: "Phone", width: 200 },
+    { field: "mobile", headerName: "Mobile", width: 200 },
+    { field: "email", headerName: "Email", width: 200 },
+    // { field: "status", headerName: "Lead Status", width: 200 },
+    { field: "owner", headerName: "Owner Alies", width: 200 },
+    {
+      field: "actions", headerName: "Actions ",
+      renderCell: (params) => (
+        <>
+          {
+            params.row.allowToDelete ?
+              <Tooltip
+                title="Delete" >
+                <IconButton
+                  aria-label="Delete"
+                  onClick={() => showConfirmBox(params.row)}
+                >
+                  <DeleteIcon
+                    fontSize="small" color="error" />
+                </IconButton>
+              </Tooltip > : null
+          }
+
+        </>
+      ), width: 200
+    }
+  ];
+
+  const showConfirmBox = (row) => {
+    setIsConformDialogVisible(true)
+    if (row && row._id) {
+      setDeleteRec(row)
+    }
+  }
+  const getFirstName = tData => {
+    return <Link className="nameLink"
+      to={`/opportunity/detail/${tData._id}`}
+    >
+      {tData.name || ''}
+    </Link>
+  }
+
+  const updateCheckedStatus = (params, ev) => {
+    const gridData = [...dataRows];
+    const indexOfRecord = gridData.findIndex(
+      (d) => d.id === params.row.id
+    );
+    gridData[indexOfRecord].isChecked = ev.target.checked;
+
+    setDataRows([...gridData]);
+
+    const checkedRecords = gridData.filter((d) => d.isChecked === true);
+
+    if (checkedRecords.length === gridData.length) {
+      setCheckAllOpportunities(true);
+    } else {
+      setCheckAllOpportunities(false);
+    }
+  }
+
+  const handlePage = (params) => {
+    if (query.page !== params.page) {
+      setQuery((prevState) => ({ ...prevState, page: params.page }));
+    }
+  };
+
+  const handlePageSize = (params) => {
+    if (params.pageSize !== query.limit) {
+      setQuery({ page: 0, limit: params.pageSize });
+    }
+  };
+
+  const handleSortModelChange = (params) => {
+    if (params?.sortModel && params.sortModel.length > 0) {
+      let temp = { ...params.sortModel[0] };
+      setQuery((prevState) => ({
+        ...prevState,
+        page: 0,
+        sortBy: temp.field,
+        orderBy: temp.sort,
+      }));
+    }
+  }
+
+  const handleDeleteOpportunity = async () => {
+    setDeleteLoading(true)
+    let recs = []
+    if (deleteRec?._id) {
+      recs.push(deleteRec?._id)
+    }
+    else {
+      dataRows.forEach(obj => {
+        if (obj.isChecked) recs.push(obj._id)
+      })
+    }
+    if (recs && recs.length > 0) {
+      axiosInstance()
+        .put(`/opportunity/remove`, { ids: [...recs] }).then(({ data }) => {
+          CustomEventEmitter.dispatch("show-toast", { type: "success", errorMsg: data.message });
+          setIsConformDialogVisible(false)
+          setDeleteLoading(false)
+          if (deleteRec) setDeleteRec({})
+          fetchOpportunities()
+        }).catch(err => {
+          setIsConformDialogVisible(false)
+          setDeleteLoading(false)
+        })
+    }
+  }
 
   return (
     <Layout>
@@ -107,48 +376,54 @@ const Opportunities = () => {
 
       {/* Tables Begins Here */}
       <Container>
-        <Grid container justify="space-between">
-          <Grid item></Grid>
-
-          <Grid item>
-            <SearchBox onSearch={handleSearch} value={searchVal} size="sm" />
-            <Box component="span" marginX={1} />
-
-            <Button variant="contained" color="primary" startIcon={<Add />}>
-              Add
-            </Button>
-
-            <Box component="span" marginX={1} />
-
-            <Button
-              variant="outlined"
-              color="default"
-              aria-controls="action-menu"
-              onClick={openActions}
-            >
-              Actions <ExpandMore />
-            </Button>
-            <Menu
-              anchorEl={anchorEl}
-              keepMounted
-              getContentAnchorEl={null}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              id="action-menu"
-              open={Boolean(anchorEl)}
-              onClose={closeActions}
-            >
-              <MenuItem>Delete</MenuItem>
-            </Menu>
-          </Grid>
-        </Grid>
+        <Header
+          selectedType={selectedType}
+          onTypeChange={handleOpportunityTypeSel}
+          options={OpportunityTypes}
+          onSearch={handleSearch}
+          searchVal={searchVal}
+          opportunityPermissions={opportunityPermissions}
+          onCreate={handleCreate}
+          showConfirmBox={showConfirmBox}
+          canDelete={dataRows.filter((d) => d.isChecked).length == 0}
+        />
       </Container>
       <Container styles={{ minHeight: "calc(100vh - 210px)", padding: 10 }}>
         <div className="contact-grid-height1">
-          <DataGrid columns={[]} rows={[]} />
+          <DataGrid
+            components={{
+              Toolbar: GridToolbar,
+            }}
+            rows={loading ? [] : dataRows}
+            columns={columns}
+            loading={loading}
+            disableSelectionOnClick
+            disableMultipleSelection
+            paginationMode="server"
+            pagination
+            onPageChange={handlePage}
+            onPageSizeChange={handlePageSize}
+            pageSize={query.limit}
+            page={query.page}
+            rowCount={rowCount}
+            rowsPerPageOptions={[25, 50, 75]}
+            onSortModelChange={handleSortModelChange}
+            density="compact"
+          />
         </div>
+        {
+          isConfirmDialogVisible ?
+            <ConfirmationDialog
+              open={isConfirmDialogVisible}
+              message={`Are you sure, you want to delete Opportunity ${deleteRec.name || ''}?`}
+              onClose={() => {
+                if (deleteRec) setDeleteRec({})
+                setIsConformDialogVisible(false)
+              }}
+              okBtnLoading={deleteLoading}
+              onOk={handleDeleteOpportunity}
+            /> : null
+        }
       </Container>
     </Layout>
   );
