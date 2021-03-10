7d1d5a0523100e33c8c188a970c9c8aad0742243
punit <punit.soni33@gmail.com>
2021-03-10 20:12:00 +0530

    - New Material library added for account hierarchy

--- src/pages/Account/AccountDetailPage.js
+++ src/pages/Account/AccountDetailPage.js
@@ -25,6 +25,7 @@ import axiosInstance from './../../axios/axiosInstance'
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import AccountHierarchy from './AccountHierarchy';
+import Chip from '@material-ui/core/Chip';
 
 const Roles = () => {
     const history = useHistory();
@@ -42,7 +43,7 @@ const Roles = () => {
     const [mainPoints, setMainPoints] = useState({})
     const [customizedRoutes, setCustomizedRoutes] = useState();
     const [currentTabIndex, setCurrentTabIndex] = useState(0);
-    const [accountHeirarchyData, setAccountHeirarchyData] = useState([]);
+    const [accountHierarchyData, setAccountHierarchyData] = useState([]);
 
     let { id } = useParams();
 
@@ -101,40 +102,68 @@ const Roles = () => {
                 const { parentHierarchy, ...rest } = data;
                 accounts.push({ ...rest, current: true });
 
-                var map = {}, node, roots = [], i;
+                let newData = [];
 
-                for (i = 0; i < accounts.length; i += 1) {
-                    map[accounts[i]._id] = i; // initialize the map
-                    accounts[i].children = []; // initialize the children
-                }
+                accounts.map(account => {
+                    const updatedAccount = {
+                        _id: account._id,
+                        accountName: `${account.accountName}`,
+                        typeOfAccount: account.typeOfAccount?.optionLabel,
+                        industry: account.industry?.optionLabel,
+                        typeOfBusiness: account.typeOfBusiness,
+                        phone: account.phone,
+                        type: "child"
+                    };
 
-                for (i = 0; i < accounts.length; i += 1) {
-                    node = accounts[i];
-                    if (node.parentAccount) {
-                        // if you have dangling branches check that map[node.parentId] exists
-                        accounts[map[node.parentAccount.optionValue]].children.push(node);
-                    } else {
-                        roots.push(node);
+                    if (account.parentAccount) {
+                        updatedAccount["parentAccountText"] = account.parentAccount.optionLabel;
+                        updatedAccount["parentAccountId"] = account.parentAccount.optionValue;
+                        updatedAccount["type"] = "parent";
                     }
-                }
-                setAccountHeirarchyData([...roots]);
+
+                    newData.push(updatedAccount);
+                })
+
+                setAccountHierarchyData([...newData]);
+
+
+                // let accounts = data.parentHierarchy;
+                // const { parentHierarchy, ...rest } = data;
+                // accounts.push({ ...rest, current: true });
+
+                // var map = {}, node, roots = [], i;
+
+                // for (i = 0; i < accounts.length; i += 1) {
+                //     map[accounts[i]._id] = i; // initialize the map
+                //     accounts[i].children = []; // initialize the children
+                // }
+
+                // for (i = 0; i < accounts.length; i += 1) {
+                //     node = accounts[i];
+                //     if (node.parentAccount) {
+                //         // if you have dangling branches check that map[node.parentId] exists
+                //         accounts[map[node.parentAccount.optionValue]].children.push(node);
+                //     } else {
+                //         roots.push(node);
+                //     }
+                // }
+                // setAccountHierarchyData([...roots]);
             } else {
-                setAccountHeirarchyData([
+                setAccountHierarchyData([
                     {
                         _id: data._id,
                         accountName: data.accountName,
-                        typeOfAccount: data.typeOfAccount,
-                        industry: data.industry,
+                        typeOfAccount: data.typeOfAccount?.optionLabel,
+                        industry: data.industry?.optionLabel,
                         typeOfBusiness: data.typeOfBusiness,
-                        parentAccount: data.parentAccount,
-                        phone: data.phone,
-                        children: []
+                        // parentAccount: data.parentAccount,
+                        phone: data.phone
                     }
                 ])
             }
 
 
-            //  setAccountHeirarchyData
+            //  setAccountHierarchyData
 
             if (accountFields.length == 0) {
                 getAccountFields()
@@ -328,8 +357,7 @@ const Roles = () => {
                     <Container className="detailPageContainer">
                         <Grid container spacing={3}>
                             <Grid item sm={8} md={8} lg={8}>
-                                <div className="detailPageDiv1"
-                                    style={{ pointerEvents: allowedToEdit ? "" : "none" }} >
+                                <div className="detailPageDiv1">
                                     {
                                         loading ? <Loader text="Fetching Data" style={{ marginTop: 100 }} /> :
                                             <>
@@ -355,7 +383,7 @@ const Roles = () => {
                                                     />
                                                 </Box>
                                                 <Box index={1} hidden={currentTabIndex !== 1}>
-                                                    <AccountHierarchy data={accountHeirarchyData} />
+                                                    <AccountHierarchy data={accountHierarchyData} currentAccountId={accountData._id} />
                                                 </Box>
                                             </>
                                     }
--- src/pages/Account/AccountHierarchy.js
+++ src/pages/Account/AccountHierarchy.js
@@ -1,118 +1,72 @@
-import React from 'react'
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import React, { forwardRef } from 'react'
+import MaterialTable from 'material-table';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 import Chip from '@material-ui/core/Chip';
-import { Link } from 'react-router-dom'
-import routes from './../../components/Helpers/Routes';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
-    // root: {
-    //     width: '100%',
-    //     overflowX: 'auto'
-    // },
-    table: {
-        minWidth: "150%"
-    }
-});
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
-
-export default function AccountHierarchy({ data }) {
-    const classes = useStyles();
-
-    const minWidth = 170;
-
-    const Row = ({ rowData, index }) => {
-        const [open, setOpen] = React.useState(true);
-
-        return <React.Fragment>
-            <TableRow index={index}>
-                <TableCell>
-                    {
-                        rowData.children && rowData.children.length > 0 ? <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
-                            {open ? <KeyboardArrowDownIcon /> : <KeyboardArrowRightIcon />}
-                        </IconButton> : <IconButton></IconButton>
-                    }
-                </TableCell>
-                <TableCell style={{ minWidth: rowData.children && rowData.children.length == 0 ? "" : minWidth }} component="th" scope="row">
-                    <Link className="accountNameLink" to={`${routes.accountDetails.path}/${rowData._id}`}>
-                        {rowData.accountName}
-                    </Link>
-                    {
-                        rowData.current ? <Chip label="Current" size="small" className="ml-2" /> : ""
-                    }
-                </TableCell>
-                <TableCell style={{ minWidth: rowData.children && rowData.children.length == 0 ? "" : minWidth }} align="center">{rowData.typeOfAccount?.optionLabel}</TableCell>
-                <TableCell style={{ minWidth: rowData.children && rowData.children.length == 0 ? "" : minWidth }} align="center">{rowData.industry?.optionLabel}</TableCell>
-                <TableCell style={{ minWidth: rowData.children && rowData.children.length == 0 ? "" : minWidth }} align="center">{rowData.typeOfBusiness}</TableCell>
-                <TableCell style={{ minWidth: rowData.children && rowData.children.length == 0 ? "" : minWidth }} align="center">{rowData.parentAccount?.optionLabel}</TableCell>
-                <TableCell style={{ minWidth: rowData.children && rowData.children.length == 0 ? "" : minWidth }} align="center">{rowData.phone}</TableCell>
-            </TableRow>
-            <TableRow>
-                {
-                    rowData.children && rowData.children.length > 0 &&
-                    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7} className="pr-0">
-                        <Collapse in={open} timeout="auto" unmountOnExit>
-                            <TableContainer component={Paper}>
-                                <Table aria-label="collapsible table" size="small">
-                                    <TableHead>
-                                        <TableRow style={{ visibility: "collapse" }}>
-                                            <TableCell />
-                                            <TableCell component="th">Account Name</TableCell>
-                                            <TableCell align="center">Type</TableCell>
-                                            <TableCell align="center">Industry</TableCell>
-                                            <TableCell align="center">Type Of Business</TableCell>
-                                            <TableCell align="center">Parent Account</TableCell>
-                                            <TableCell align="center">Phone</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {
-                                            rowData.children.map((row, index1) => {
-                                                return <Row key={index1} rowData={row} index={index1} />
-                                            })
-                                        }
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                        </Collapse>
-                    </TableCell>
-                }
-            </TableRow>
-        </React.Fragment>
-    }
+export default function AccountHierarchy({ data, currentAccountId }) {
 
     return (
-        <TableContainer component={Paper} style={{ width: "100%", overflowX: 'auto' }}>
-            <Table aria-label="collapsible table" size="small" className={classes.table}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell />
-                        <TableCell component="th">Account Name</TableCell>
-                        <TableCell align="center">Type</TableCell>
-                        <TableCell align="center">Industry</TableCell>
-                        <TableCell align="center">Type Of Business</TableCell>
-                        <TableCell align="center">Parent Account</TableCell>
-                        <TableCell align="center">Phone</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        data.map((row, index) => {
-                            return <Row key={index} rowData={row} index={index} />
-                        })
-                    }
-                </TableBody>
-            </Table>
-        </TableContainer>
-    )
+        <MaterialTable
+            icons={tableIcons}
+            data={data}
+            columns={[
+                {
+                    title: 'Account Name', field: 'accountName',
+                    width: 200,
+                    render: rowData => rowData._id == currentAccountId ?
+                        <>
+                            <span>{rowData.accountName}</span> <Chip label="Current" size="small" className="ml-2" />
+                        </> : `${rowData.accountName}`
+                },
+                { title: 'Type', field: 'typeOfAccount' },
+                { title: 'Industry', field: 'industry' },
+                { title: 'Type Of Business', field: 'typeOfBusiness' },
+                { title: 'Parent Account', field: 'parentAccountText' },
+                { title: 'Phone', field: 'phone' },
+            ]}
+            columnResizable={true}
+            parentChildData={(row, rows) => rows.find(a => a._id === row.parentAccountId)}
+            options={{
+                search: false,
+                paging: false,
+                sorting: false,
+                draggable: false,
+                padding: "dense",
+            }}
+        />
+    );
 }
