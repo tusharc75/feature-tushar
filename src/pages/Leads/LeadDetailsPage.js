a17fea2cc12f1b8c0807517c47024ff87b7dec8a
Pranav Chandra <pranavchandra27@gmail.com>
2021-03-11 15:15:19 +0530

    ui fixes

--- src/pages/Leads/LeadDetailsPage.js
+++ src/pages/Leads/LeadDetailsPage.js
@@ -44,7 +44,12 @@ const LeadDetailsPage = () => {
   const [isUpdating, setUpdating] = useState(false);
   const [allowedToEdit, setAllowedToEdit] = useState(false);
   const [customizedRoutes, setCustomizedRoutes] = useState([routes.lead]);
-  const [leadsPermissions, setLeadsPermissions] = useState({ isCreate: false, isUpdate: false, isRead: false, isDelete: false });
+  const [leadsPermissions, setLeadsPermissions] = useState({
+    isCreate: false,
+    isUpdate: false,
+    isRead: false,
+    isDelete: false,
+  });
 
   let { id } = useParams();
 
@@ -58,18 +63,17 @@ const LeadDetailsPage = () => {
     const data = user?.role?.sideBar;
 
     if (data) {
-      const hasLeadsPermission = data.find(d => d.name == "Lead");
+      const hasLeadsPermission = data.find((d) => d.name == "Lead");
       if (hasLeadsPermission) {
         setLeadsPermissions({
           isCreate: hasLeadsPermission.isCreate,
           isUpdate: hasLeadsPermission.isUpdate,
           isRead: hasLeadsPermission.isRead,
-          isDelete: hasLeadsPermission.isDelete
+          isDelete: hasLeadsPermission.isDelete,
         });
       }
     }
   }, [user]);
-
 
   const fetchLeadData = async () => {
     try {
@@ -90,7 +94,7 @@ const LeadDetailsPage = () => {
         routes.lead,
         { title: `${data.firstName} ${data.lastName}` },
       ]);
-    } catch (error) { }
+    } catch (error) {}
   };
 
   const handleMainPoints = (data) => {
@@ -239,19 +243,18 @@ const LeadDetailsPage = () => {
             showHeading={true}
           >
             <Box component="span" marginX={1} />
-            {
-              leadsPermissions.isDelete && leadData?.owner?.optionValue &&
-                user?.user?._id &&
-                leadData.owner.optionValue === user.user._id ? (
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  onClick={() => setShowConfirmBox(true)}
-                >
-                  Delete
-                </Button>
-              ) : null
-            }
+            {leadsPermissions.isDelete &&
+            leadData?.owner?.optionValue &&
+            user?.user?._id &&
+            leadData.owner.optionValue === user.user._id ? (
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={() => setShowConfirmBox(true)}
+              >
+                Delete
+              </Button>
+            ) : null}
           </CustomHeader>
         )}
         <div>
