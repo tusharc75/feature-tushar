3dead74c76d02f5707265caf3f4dc38c4eab8a7d
Pranav Chandra <pranavchandra27@gmail.com>
2021-04-08 09:37:04 +0530

    show loading while uploading

--- src/components/Helpers/FormTypes.tsx
+++ src/components/Helpers/FormTypes.tsx
@@ -4,6 +4,7 @@ import {
   Box,
   Button,
   Checkbox,
+  CircularProgress,
   FormControl,
   FormControlLabel,
   FormLabel,
@@ -721,12 +722,24 @@ const FormTypes = (props) => {
   ) : type === "imageUpload" ? (
     <Fragment>
       <Box display="flex" flexDirection="row">
-        <Box>
+        <Box position="relative">
           <Avatar
             src={values[name]}
             style={{ width: 70, height: 70 }}
             alt="org_logo"
           />
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            position="absolute"
+            top="0"
+            right="0"
+            width="100%"
+            height="100%"
+          >
+            {isUploading && <CircularProgress size={22} />}
+          </Box>
         </Box>
         <Box>
           <label htmlFor={name}>
@@ -738,6 +751,7 @@ const FormTypes = (props) => {
             >
               <AddCircleIcon />
               <input
+                disabled={isUploading}
                 id={name}
                 name={name}
                 onChange={handleUploadImage}
@@ -758,6 +772,7 @@ const FormTypes = (props) => {
     <Fragment>
       <Box display="flex" alignItems="center">
         <input
+          disabled={isUploading}
           id={name}
           name={name}
           onChange={handleUploadFile}
@@ -776,7 +791,11 @@ const FormTypes = (props) => {
         </label>
         <Box marginX={1} />
         <p className="text-truncate">
-          {values[name] ? values[name] : "No file choosen"}
+          {values[name]
+            ? values[name]
+            : isUploading
+            ? "Uploading..."
+            : "No file choosen"}
         </p>
       </Box>
     </Fragment>
