2efdf7f8063213a2f64909db961b60e18eb76522
swatigavhane1996@gmail.com <swatigavhane1996@gmail.com>
2021-04-12 19:27:35 +0530

    OMS-64-done

--- src/pages/User/UserRoles.tsx
+++ src/pages/User/UserRoles.tsx
@@ -9,9 +9,10 @@ import {
   IconButton,
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
-
 import BoxWithBorder from "../../components/BoxWithBorder";
+import { PERMISSION } from "../../constants/Roles"
 
+const rolesPermissions = [PERMISSION.superAdmin, PERMISSION.brandAdmin]
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -42,29 +43,31 @@ const UserRoles = ({ data, unassignRole }: props) => {
         <List style={{ padding: 0 }}>
           {data && data.length
             ? data.map((obj: any, i: string) => (
-                <BoxWithBorder
-                  key={i}
-                  style={{ padding: "0px", margin: "8px" }}
-                >
-                  <ListItem className={classes.list}>
-                    <ListItemText
-                      primary={<Typography> {obj.name || ""}</Typography>}
-                      secondary={obj.description || ""}
-                    />
-                    <ListItemSecondaryAction>
-                      <Tooltip title="Unassign Role">
-                        <IconButton
-                          edge="end"
-                          aria-label="delete"
-                          onClick={() => unassignRole(obj)}
-                        >
-                          <DeleteIcon color="error" />
-                        </IconButton>
-                      </Tooltip>
-                    </ListItemSecondaryAction>
-                  </ListItem>
-                </BoxWithBorder>
-              ))
+              <BoxWithBorder
+                key={i}
+                style={{ padding: "0px", margin: "8px" }}
+              >
+                <ListItem className={classes.list}>
+                  <ListItemText
+                    primary={<Typography> {obj.name || ""}</Typography>}
+                    secondary={obj.description || ""}
+                  />
+                  <ListItemSecondaryAction>
+                    <Tooltip title="Unassign Role">
+                      <IconButton
+                        edge="end"
+                        aria-label="delete"
+                        onClick={() => unassignRole(obj)}
+                        disabled={rolesPermissions.indexOf(obj?.permission) >= 0}
+                      >
+                        <DeleteIcon
+                          color={rolesPermissions.indexOf(obj?.permission) >= 0 ? "disabled" : "error"} />
+                      </IconButton>
+                    </Tooltip>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              </BoxWithBorder>
+            ))
             : null}
         </List>
       </div>
