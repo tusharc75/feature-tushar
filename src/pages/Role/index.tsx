7f5fb1da8ddd7b4bbe42fa6ded2f519bdcfe4751
Jhon Rajbongshi <30900722+jhonrajbongshi@users.noreply.github.com>
2021-04-12 14:13:36 +0530

    OMS-63-jhon

--- src/pages/Role/index.tsx
+++ src/pages/Role/index.tsx
@@ -58,7 +58,7 @@ const Roles: FC = () => {
   const toastConfig = useContext(CustomToastContext);
   const classes = useStyles();
   const {
-    state: { user },
+    state: { user, selectedEntity },
   }: any = useData();
   const [searchVal, setSearchVal] = useState("");
   const [selectedType, setSelectedType] = useState(1);
@@ -410,6 +410,7 @@ const Roles: FC = () => {
           fetchData={fetchRoles}
           roleType={selectedType}
           setToastConfig={toastConfig.setToastConfig}
+          selectedEntity={selectedEntity}
         />
       )}
       <Layout>
