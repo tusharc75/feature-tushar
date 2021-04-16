c9a8d553bd097009cbd633a7278ad0f9857d896c
Pranav Chandra <pranavchandra27@gmail.com>
2021-04-16 16:07:49 +0530

    refractor detail page

--- src/components/Shared/DetailsPage.tsx
+++ src/components/Shared/DetailsPage.tsx
@@ -146,18 +146,18 @@ const Details = (props: DetailProps) => {
     type === "imageUpload" || type === "fileUpload" ? 12 : size;
 
   const renderData = (val: any, fieldData: any) => {
+    const values = normalizeValues(val, fieldData);
+
     if (fieldData.hasOwnProperty("lookup") && fieldData.lookup) {
-      const redirectLink = (link) =>
-        val[fieldData.fieldName]
-          ? `/${kebabCase(fieldData.lookupResource)}/detail/${link}`
-          : "!#";
+      const redirectLink = (link: string) =>
+        `/${kebabCase(fieldData.lookupResource)}/detail/${link}`;
 
       if (fieldData.type === "multiSelect" || fieldData.type === "dropDown") {
         return (
           <Typography className={classes.fieldText} variant="body2">
             {Array.isArray(data[fieldData.fieldName]) ? (
               data[fieldData.fieldName].length ? (
-                data[fieldData.fieldName].map((_val) => (
+                data[fieldData.fieldName].map((_val: any) => (
                   <>
                     <MuiLink
                       component={Link}
@@ -187,15 +187,11 @@ const Details = (props: DetailProps) => {
     } else {
       return (
         <Typography
-          title={
-            normalizeValues(val, fieldData) === "_ _ _"
-              ? ""
-              : normalizeValues(val, fieldData)
-          }
+          title={values === "_ _ _" ? "" : values}
           className={classes.fieldText}
           variant="body2"
         >
-          {normalizeValues(val, fieldData)}
+          {values}
         </Typography>
       );
     }
