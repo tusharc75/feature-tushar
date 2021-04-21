dddd983f39aba6c8cb84d80db49409f20532ebd7
PRITAM <pritamchatterjee945@gmail.com>
2021-04-21 19:08:47 +0530

    Save button disable issue

--- src/components/Helpers/Button.tsx
+++ src/components/Helpers/Button.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Button, CircularProgress } from '@material-ui/core'
 import '../sidebar.scss'
+import { LocalDiningSharp } from '@material-ui/icons'
 function CustomButton(props) {
     const { loading, children, disabled, ...rest } = props
     return <Button  {...rest}
         disabled={disabled}
-        loading={loading}
     >
         {loading ? <CircularProgress
             style={{ marginRight: "8px" }}
