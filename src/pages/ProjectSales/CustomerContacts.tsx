1df252954efd87710101fde03c13f180a40bc99f
Pranav Chandra <pranavchandra27@gmail.com>
2021-05-05 09:44:30 +0530

    accounts contact filter works

--- src/pages/ProjectSales/CustomerContacts.tsx
+++ src/pages/ProjectSales/CustomerContacts.tsx
@@ -9,6 +9,7 @@ import { BiFace } from "react-icons/bi";
 import ListItem from "@material-ui/core/ListItem/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import { ListItemText } from "@material-ui/core";
+import { Fragment } from "react";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -45,51 +46,49 @@ function RelatedContacts({ contacts, accountId, accountName, contactRoute }) {
     <>
       {contacts && contacts.length ? (
         <>
-          {contacts.map((obj, index) => {
-            return (
-              <>
-                <Card key={index}>
-                  <CardContent className="detailListing">
-                    <Grid container className="detailCardHeader">
-                      <Grid item xs={12} sm={12}>
-                        <Link
-                          className={`f_size`}
-                          to={`/${contactRoute}/detail/${obj._id}`}
-                        >
-                          {`${obj.firstName || ""}  ${obj.lastName || ""}`}
-                        </Link>
-                      </Grid>
+          {contacts.map((obj, index) => (
+            <Fragment key={index}>
+              <Card>
+                <CardContent className="detailListing">
+                  <Grid container className="detailCardHeader">
+                    <Grid item xs={12} sm={12}>
+                      <Link
+                        className={`f_size`}
+                        to={`/${contactRoute}/detail/${obj._id}`}
+                      >
+                        {`${obj.firstName || ""}  ${obj.lastName || ""}`}
+                      </Link>
                     </Grid>
-                    <Grid container>
-                      <Grid item xs={12} sm={6}>
-                        {accountName ? (
-                          <DisplayData
-                            label="Account"
-                            value={accountName}
-                            icon={<BsPerson size={20} />}
-                          />
-                        ) : (
-                          ""
-                        )}
-                      </Grid>
-                      <Grid item xs={12} sm={6}>
-                        {obj?.title ? (
-                          <DisplayData
-                            label="Title"
-                            value={obj.title || ""}
-                            icon={<BiFace size={20} />}
-                          />
-                        ) : (
-                          ""
-                        )}
-                      </Grid>
+                  </Grid>
+                  <Grid container>
+                    <Grid item xs={12} sm={6}>
+                      {accountName ? (
+                        <DisplayData
+                          label="Account"
+                          value={accountName}
+                          icon={<BsPerson size={20} />}
+                        />
+                      ) : (
+                        ""
+                      )}
                     </Grid>
-                  </CardContent>
-                </Card>
-                <Box margin={1} />
-              </>
-            );
-          })}
+                    <Grid item xs={12} sm={6}>
+                      {obj?.title ? (
+                        <DisplayData
+                          label="Title"
+                          value={obj.title || ""}
+                          icon={<BiFace size={20} />}
+                        />
+                      ) : (
+                        ""
+                      )}
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </Card>
+              <Box margin={1} />
+            </Fragment>
+          ))}
           <Box margin={1} />
           <Box
             className="btn-view gap-1"
@@ -104,7 +103,7 @@ function RelatedContacts({ contacts, accountId, accountName, contactRoute }) {
               })
             }
           >
-            <FaEye /> View All &#8599;
+            <FaEye /> View All
           </Box>
         </>
       ) : null}
