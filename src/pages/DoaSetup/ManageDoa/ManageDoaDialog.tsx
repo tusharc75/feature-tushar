d5c4014ea6988e74d5d7239594485bcae0708c37
Jhon Rajbongshi <30900722+jhonrajbongshi@users.noreply.github.com>
2021-05-05 17:23:40 +0530

    OMS-159 test

--- src/pages/DoaSetup/ManageDoa/ManageDoaDialog.tsx
+++ src/pages/DoaSetup/ManageDoa/ManageDoaDialog.tsx
@@ -320,7 +320,15 @@ const DoaDialog = ({ userSelected, onSuccess, userList, doa, doaCurrency, open, 
                                         type="submit"
                                         disabled={currency === ""
                                             || values.users.filter(item => item.name === "" || item.name === undefined || item.id == "" || item.id === undefined).length > 0
-                                            || JSON.stringify(values.users) === JSON.stringify(users) && currency === doaCurrency
+                                            || JSON.stringify(values.users.map((item) => {
+                                                return {
+                                                  id: item?.id,
+                                                  name: item?.name,
+                                                  firstName: item?.firstName,
+                                                  lastName: item?.lastName,
+                                                  amount: Number(item.amount),
+                                                };
+                                              })) === JSON.stringify(users) && currency === doaCurrency
                                           
                                         }
                                         onClick={() => {
