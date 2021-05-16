7db05933958ba47179561a76f09072bf97e77293
Jhon Rajbongshi <30900722+jhonrajbongshi@users.noreply.github.com>
2021-05-16 10:13:01 +0530

    OMS-187

--- src/axios/axios.tsx
+++ src/axios/axios.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
-const BASE_URL = process?.env?.REACT_APP_API_URL 
-// || "https://oms-backend.vebholic.com";
+const BASE_URL = process?.env?.REACT_APP_API_URL || "https://oms-backend.vebholic.com";
 
 const axiosAPI = () => {
     const token = localStorage.getItem("token");
