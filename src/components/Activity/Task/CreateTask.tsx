ac207a4e0842df8f6248187f7cbca40e9b339339
Pranav Chandra <pranavchandra27@gmail.com>
2021-05-17 16:41:36 +0530

    remove comments from events

--- src/components/Activity/Task/CreateTask.tsx
+++ src/components/Activity/Task/CreateTask.tsx
@@ -35,6 +35,7 @@ import { SubTask } from "./SubTask";
 import CustomDialogHeader from "../../../components/CustomDialog/CustomDialogHeader";
 import CustomDialogContent from "../../../components/CustomDialog/CustomDialogContent";
 import CustomDialogFooter from "../../../components/CustomDialog/CustomDialogFooter";
+import { useData } from "../../../StateProvider/Provider";
 
 const TaskSchema = Yup.object().shape({
   name: Yup.string().required("please enter task name"),
@@ -45,6 +46,9 @@ const TaskSchema = Yup.object().shape({
 });
 
 export const CreateTask = ({ relatedTo, taskId, handleClose, status }) => {
+  const {
+    state: { user: user },
+  } = useData();
   const [id, setId] = useState(taskId);
   const [initialValues, setInitialValues] = useState(null);
   const [openAddSub, setOpenAddSub] = useState(false);
@@ -68,7 +72,7 @@ export const CreateTask = ({ relatedTo, taskId, handleClose, status }) => {
         description: "",
         status: status || "To Do",
         assignee: "",
-        reporter: "",
+        reporter: user._id,
         startDate: new Date(),
         dueDate: new Date(),
       });
@@ -246,6 +250,7 @@ export const CreateTask = ({ relatedTo, taskId, handleClose, status }) => {
                         </Box>
                         <Box pt={1}>
                           <UserDropdown
+                            disabled
                             name="reporter"
                             label="Reporter"
                             errors={errors}
