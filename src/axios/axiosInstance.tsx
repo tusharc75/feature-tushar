a8ddc806b7ad9e0e6239d9f97e80a20f3ce4ec74
Jhon Rajbongshi <30900722+jhonrajbongshi@users.noreply.github.com>
2021-05-19 12:16:53 +0530

    axios instance

--- src/axios/axiosInstance.tsx
+++ src/axios/axiosInstance.tsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export default (history = null) => {
-    //const baseURL = process?.env?.REACT_APP_API_URL || "https://oms-backend.vebholic.com";
-    const baseURL="http://localhost:4000";
+    const baseURL = process?.env?.REACT_APP_API_URL || "https://oms-backend.vebholic.com";
     let headers: any = {};
 
     if (localStorage.token) {
