5d8196d685a91e68ef61ef4be7890cffda1eb976
jinal <jinal@vebholic.com>
2021-05-20 10:13:56 +0530

    Reverse marfe

--- src/components/Helpers/CustomRenderCell.tsx
+++ src/components/Helpers/CustomRenderCell.tsx
@@ -4,14 +4,12 @@ import CopyToClipboard from './CopyToClipboard'
 
 export default function CustomRenderCell({ value, isCopyToClipboard = false }) {
     const [inHover, setHover] = useState(false);
-    return <span onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}>
-        {value || <NoDataCell />}
-        { inHover && isCopyToClipboard && value ? <CopyToClipboard textToCopy={value} /> : null}
-    </span>
-
-
-    // return <> {value || <NoDataCell />}
-    //         {isCopyToClipboard && value ? <CopyToClipboard textToCopy={value} /> : null}
-    //     </>
+    // return <span onMouseEnter={() => setHover(true)}
+    //     onMouseLeave={() => setHover(false)}>
+    //     {value || <NoDataCell />}
+    //     { inHover && isCopyToClipboard && value ? <CopyToClipboard textToCopy={value} /> : null}
+    // </span>
+    return <> {value || <NoDataCell />}
+        {isCopyToClipboard && value ? <CopyToClipboard textToCopy={value} /> : null}
+    </>
 }
