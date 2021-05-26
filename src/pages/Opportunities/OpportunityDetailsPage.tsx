89b12e98f05132d4f2f5e6c4a7ded1a1d0209e3c
Jhon Rajbongshi <30900722+jhonrajbongshi@users.noreply.github.com>
2021-05-26 20:04:59 +0530

    OMS-291 opp

--- src/pages/Opportunities/OpportunityDetailsPage.tsx
+++ src/pages/Opportunities/OpportunityDetailsPage.tsx
@@ -21,13 +21,14 @@ import {
   stepsToIgnoreManualCompleteForOpportunity, supplierContact, customerContact,
   getObjKeysWithValues, opportunityProcessFieldName, formatAmountWithCurrency
 } from "../../constants/helpers";
-import { opportunity } from '../../constants/helpers'
+import { opportunity, sidebarResource } from '../../constants/helpers'
 import CustomSteps from "../../components/CustomSteps/CustomSteps";
 import OpportunityContacts from "./OpportunityContacts";
 import AssignContactsDialog from "./AssignContactsDialog";
 import MessageDialog from "../../components/Helpers/MessageDialog";
 import AssignSupplierContactsDialog from './AssignSupplierContactsDialog'
 import { BsCheckAll } from "react-icons/bs";
+import ProjectInAccordion from "../../components/ProjectInAccordion/ProjectInAccordion";
 
 const recordsPerLine = 3;
 function OpportunityDetailsPage() {
@@ -86,6 +87,8 @@ function OpportunityDetailsPage() {
   });
 
   const { opportunityResource, opportunityApi } = opportunity
+  const [projectSales, setProjectSales] = useState([]);
+  const [typeCreateProjectSalesDialog, setTypeCreateProjectSalesDialog] = useState([{ id: id, type: opportunity.opportunityResource }]);
 
   useEffect(() => {
     if (permissions) {
@@ -96,6 +99,7 @@ function OpportunityDetailsPage() {
   useEffect(() => {
     if (id) {
       fetchOpportunityData();
+      fetchRelatedData();
     }
   }, [id]);
 
@@ -131,6 +135,12 @@ function OpportunityDetailsPage() {
           handleMainPoints(data);
           setHeadingLbl(data.opportunityName);
 
+          if (data?.customerAccountName?.optionValue) {
+            setTypeCreateProjectSalesDialog((prevState) => [...prevState, { id: data?.customerAccountName?.optionValue, type: "customerAccount" }])
+          }
+          if (data?.supplierAccountName?.optionValue) {
+            setTypeCreateProjectSalesDialog((prevState) => [...prevState, { id: data?.supplierAccountName?.optionValue, type: "supplierAccount" }])
+          }
           setAllowedToEdit([...data.collaborator ?? [], data.owner].some(
             (d) => d?.optionValue === user?.user?._id
           ))
@@ -181,6 +191,26 @@ function OpportunityDetailsPage() {
         });
     }
   }
+
+  const fetchRelatedData = () => {
+    axiosInstance()
+      .get(`${opportunityApi}/related/${id}`)
+      .then(({ data: { data } }) => {
+        setProjectSales(
+          data[sidebarResource.projectSales] &&
+            data[sidebarResource.projectSales][
+            sidebarResource[opportunity.opportunityResource].replaceAll(" ", "_")
+            ]
+            ? data[sidebarResource.projectSales][
+            sidebarResource[opportunity.opportunityResource].replaceAll(" ", "_")
+            ]
+            : []
+        );
+      })
+      .catch((error) => {
+        toastConfig.setToastConfig(error);
+      });
+  };
 
   const fetchSupplierContactData = (showDialog, useAccountList = false, accountList = []) => {
     let ids = []
@@ -503,34 +533,42 @@ function OpportunityDetailsPage() {
                     <div className="p-3">
                       {
                         opportunityData && <OpportunityContacts
-                            contacts={_.cloneDeep(opportunityData?.staticData?.supplierContacts)}
-                            title="Supplier Contacts"
-                            contactApi={supplierContact.contactApi}
-                            isExpanded={expanded.supplierContacts}
-                            onAddContact={() => {
-                              fetchSupplierContactData(true);
-                            }}
-                            onSetExpanded={() => {
-                              setExpanded({ ...expanded, supplierContacts: !expanded.supplierContacts })
-                            }}
-                            recordsPerLine={recordsPerLine}
-                          />
+                          contacts={_.cloneDeep(opportunityData?.staticData?.supplierContacts)}
+                          title="Supplier Contacts"
+                          contactApi={supplierContact.contactApi}
+                          isExpanded={expanded.supplierContacts}
+                          onAddContact={() => {
+                            fetchSupplierContactData(true);
+                          }}
+                          onSetExpanded={() => {
+                            setExpanded({ ...expanded, supplierContacts: !expanded.supplierContacts })
+                          }}
+                          recordsPerLine={recordsPerLine}
+                        />
                       }
                       {
                         opportunityData && <OpportunityContacts
-                            contacts={_.cloneDeep(opportunityData?.staticData?.customerContacts)}
-                            title="Customer Contacts"
-                            isExpanded={expanded["customerContacts"]}
-                            contactApi={customerContact.contactApi}
-                            onAddContact={() => {
-                              fetchCustomerContactData(true);
-                            }}
-                            onSetExpanded={() => {
-                              setExpanded({ ...expanded, customerContacts: !expanded.customerContacts })
-                            }}
-                            recordsPerLine={recordsPerLine}
-                          />
+                          contacts={_.cloneDeep(opportunityData?.staticData?.customerContacts)}
+                          title="Customer Contacts"
+                          isExpanded={expanded["customerContacts"]}
+                          contactApi={customerContact.contactApi}
+                          onAddContact={() => {
+                            fetchCustomerContactData(true);
+                          }}
+                          onSetExpanded={() => {
+                            setExpanded({ ...expanded, customerContacts: !expanded.customerContacts })
+                          }}
+                          recordsPerLine={recordsPerLine}
+                        />
                       }
+                      {permissions?.projectSales?.isRead && (
+                        <ProjectInAccordion
+                          recordsPerLine={3}
+                          projectSales={projectSales}
+                          type={typeCreateProjectSalesDialog}
+                          fetchData={fetchRelatedData}
+                        />
+                      )}
                     </div>
                   </TabPanel>
                   <TabPanel value={currentTabIndex} index={1}>
