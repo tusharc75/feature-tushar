9609d9583c1e5c52f9d6a328603bf2ceb54c4421
Pranav Chandra <pranavchandra27@gmail.com>
2021-05-26 11:15:23 +0530

    board changes and calender date issue reslove

--- src/components/Activity/Report/Board/BoardList.tsx
+++ src/components/Activity/Report/Board/BoardList.tsx
@@ -84,7 +84,10 @@ export const BoardList = ({
         <Box
           p={1}
           style={{
-            opacity: isCreateButton ? 1 : 0,
+            opacity:
+              isCreateButton || (status === "To Do" && subActivity.length === 0)
+                ? 1
+                : 0,
           }}
         >
           <Button startIcon={<Add />} onClick={() => setOpenDialog(true)}>
