26b9a424fa986d412932c4af47692f4c51ec7db3
PRITAM <pritamchatterjee945@gmail.com>
2021-05-27 13:41:09 +0530

    Routes handled

--- src/App.tsx
+++ src/App.tsx
@@ -312,7 +312,7 @@ function App() {
           <PrivateRoute exact path="/attachment">
             <Attachments />
           </PrivateRoute>
-          <PrivateRoute exact path="/:type">
+          <PrivateRoute  path="/:type">
             <Activity />
           </PrivateRoute>
           <PrivateRoute exact path="/calendar">
--- src/components/Activity/Helpers/ViewAll.tsx
+++ src/components/Activity/Helpers/ViewAll.tsx
@@ -6,6 +6,6 @@ import { FaEye } from 'react-icons/fa';
 export const ViewAll = ({ type, relatedTo }) => {
     let filter = relatedTo.filter((_relatedTo) => _relatedTo.access === true);
     return filter.length > 0 ? <Box className="btn-view gap-1" p={1} display="flex"  justifyContent="center" alignItems="center">
-           <FaEye /> <Link to={"/activity/" + type + "?referenceType=" + filter[0].type + "&referenceId=" + filter[0].referenceId} >View All</Link>
+           <FaEye /> <Link to={"/" + type + "?referenceType=" + filter[0].type + "&referenceId=" + filter[0].referenceId} >View All</Link>
     </Box> : null
 }
