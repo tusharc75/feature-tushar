c154cd7ad0d1d481fa0919fe66e2685424ab71e7
Swati Gavhane <Swati@vebholic.com>
2021-05-27 17:34:22 +0530

    OMS-267 activities redirect issue resolved

--- src/constants/helpers.tsx
+++ src/constants/helpers.tsx
@@ -90,6 +90,11 @@ export const sidebarResource = {
   lead: "Lead",
   opportunity: "Opportunity",
   projectSales: "Project Sales",
+  task: "Task",
+  note: "Note",
+  email: "Email",
+  attachment: "Attachment",
+  case: "Case",
 };
 
 export const lead = {
