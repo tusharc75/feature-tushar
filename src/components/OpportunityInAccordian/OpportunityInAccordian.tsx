a5481bd40972f47d6c3f78af690e65f95a6da263
punit <punit.soni33@gmail.com>
2021-05-28 12:12:09 +0530

    - opportunity, contact filter issue fixed from account details screen

--- src/components/OpportunityInAccordian/OpportunityInAccordian.tsx
+++ src/components/OpportunityInAccordian/OpportunityInAccordian.tsx
@@ -248,7 +248,7 @@ export default function OpportunityInAccordian({
               history.push(`/opportunity`, {
               accountId: accountId,
               accountName: accountName,
-              resource: `${resource}Name`,
+              resource: `${resource}`,
             })
           }
            p={1} display="flex" justifyContent="center" alignItems="center">
