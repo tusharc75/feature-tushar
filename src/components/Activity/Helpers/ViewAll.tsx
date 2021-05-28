785e672402291a8c61fa5a1b686b68a20ac434e2
Pranav Chandra <pranavchandra27@gmail.com>
2021-05-28 15:53:57 +0530

    redirect to calender for activities

--- src/components/Activity/Helpers/ViewAll.tsx
+++ src/components/Activity/Helpers/ViewAll.tsx
@@ -1,12 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import Box from '@material-ui/core/Box';
 import { Link } from "react-router-dom";
-import { FaEye } from 'react-icons/fa';
+import { FaEye } from "react-icons/fa";
 
 export const ViewAll = ({ type, relatedTo }) => {
-    let filter = relatedTo.filter((_relatedTo) => _relatedTo.access === true);
-    return filter.length > 0 ? <Link to={"/" + type + "?referenceType=" + filter[0].type + "&referenceId=" + filter[0].referenceId} >
-        <span className="btn-view gap-1" style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: 6, paddingBottom: 6 }}>
-            <FaEye /> View All</span>
-    </Link> : null
-}
+  let filter = relatedTo.filter((_relatedTo) => _relatedTo.access === true);
+  return filter.length > 0 ? (
+    <Link
+      to={
+        `${type === "event" ? "/calendar?" : "/"}` +
+        `${type === "event" ? "type=event" : type}` +
+        `${type === "event" ? "&" : "?"}referenceType=` +
+        filter[0].type +
+        "&referenceId=" +
+        filter[0].referenceId
+      }
+    >
+      <span
+        className="btn-view gap-1"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 6,
+          paddingBottom: 6,
+        }}
+      >
+        <FaEye /> View All
+      </span>
+    </Link>
+  ) : null;
+};
