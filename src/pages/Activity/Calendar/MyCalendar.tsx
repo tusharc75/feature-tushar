1bf6960ce7a4d3a5e792112c4486b5f4551144b0
jinal <jinal@vebholic.com>
2021-05-28 09:08:41 +0530

    calender ui changes

--- src/pages/Activity/Calendar/MyCalendar.tsx
+++ src/pages/Activity/Calendar/MyCalendar.tsx
@@ -2,6 +2,7 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./calender.module.scss";
 
 const localizer = momentLocalizer(moment);
 
@@ -11,6 +12,10 @@ type Props = {
   type: string;
 };
 
+const formats = {
+  weekdayFormat: (date, culture, localizer) => localizer.format(date, 'dddd', culture),
+}
+
 const MyCalendar = (props: Props) => {
   const { activities, setActivityData, type } = props;
   return (
@@ -19,6 +24,7 @@ const MyCalendar = (props: Props) => {
       defaultView="month"
       events={activities}
       localizer={localizer}
+      formats={formats}
       style={{ height: "100vh" }}
       popup={true}
       onSelectEvent={(event: any) => {
