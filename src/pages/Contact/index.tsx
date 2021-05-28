fb582426d9c6bf29ac32889cd1d5892d3a320e20
punit <punit.soni33@gmail.com>
2021-05-28 16:35:34 +0530

    - sortModel code changed

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -652,8 +652,8 @@ export default function Contact(props) {
                 // checkboxSelection: true,
                 floatingFilterComponentParams: { suppressFilterButton: true }
               }}
-              onSortChanged={(e) => {
-                dispatch({ type: "sort", sorting: e.api.getSortModel() })
+              onSortChanged={() => {
+                dispatch({ type: "sort", sorting: columnApi.getColumnState().filter(d => ["asc", "desc"].some(s => s === d.sort)) });
               }}
               onFilterChanged={(e) => {
                 dispatch({ type: "filter", filters: e.api.getFilterModel() });
