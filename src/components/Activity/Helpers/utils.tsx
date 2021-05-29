23b0b4725199e18b14b6312f5a4fb69b314512a4
Pranav Chandra <pranavchandra27@gmail.com>
2021-05-29 11:25:19 +0530

    custom colors for resources in activity

---
+++ src/components/Activity/Helpers/utils.tsx
@@ -0,0 +1,10 @@
+export const resActivityColors = {
+  customerContact: "#F57C00",
+  customerAccount: "#E65100",
+  supplierContact: "#FF9800",
+  supplierAccount: "#FF6E40",
+  lead: "#BF360C",
+  opportunity: "#3949AB",
+  user: "#990033",
+  my: "#990033",
+};
