aceecaa7781007f5b8c06de0ff5e0b4a039d867b
punit <punit.soni33@gmail.com>
2021-06-02 18:35:13 +0530

    - Custom grid code updated - browser versions updated in package.json browserlist

--- src/components/Helpers/DataGridHelpers/CustomDataGridToolbar.tsx
+++ src/components/Helpers/DataGridHelpers/CustomDataGridToolbar.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import {
     GridToolbarContainer,
-    GridToolbarExport,
-    GridColumnsToolbarButton,
-    GridDensitySelector,
-    GridFilterToolbarButton
+    // GridToolbarExport,
+    // GridColumnsToolbarButton,
+    // GridDensitySelector,
+    // GridFilterToolbarButton
 } from '@material-ui/data-grid';
 
 export default function CustomDataGridToolbar() {
     return (
         <React.Fragment>
             <GridToolbarContainer>
-                <GridColumnsToolbarButton />
+                {/* <GridColumnsToolbarButton />
                 <GridFilterToolbarButton />
-                <GridDensitySelector />
+                <GridDensitySelector /> */}
                 {/* <GridToolbarExport /> */}
             </GridToolbarContainer>
         </React.Fragment>
