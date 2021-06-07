1119a5c1bff580fed5a43178f935ecd665a53758
Swati Gavhane <Swati@vebholic.com>
2021-06-07 17:31:29 +0530

    OMS-246 changes added

--- src/components/Helpers/ActivityLoader.tsx
+++ src/components/Helpers/ActivityLoader.tsx
@@ -1,33 +1,11 @@
 import { Box } from "@material-ui/core";
-import { Skeleton } from "@material-ui/lab";
+import { CircularProgress } from "@material-ui/core"
 
 const ActivityLoader = () => {
   return (
     <>
-      <Box className="activity">
-        <Box display="flex" mb={1}>
-          <Skeleton variant="text" width="60px" />
-          <Box mx={1} />
-          <Skeleton variant="text" width="100px" />
-        </Box>
-        <Skeleton
-          style={{ borderRadius: "16px" }}
-          width="200px"
-          height="40px"
-        />
-      </Box>
-      <Box mx={1} />
-      <Box className="activity">
-        <Box display="flex" mb={1}>
-          <Skeleton variant="text" width="60px" />
-          <Box mx={1} />
-          <Skeleton variant="text" width="100px" />
-        </Box>
-        <Skeleton
-          style={{ borderRadius: "18px" }}
-          width="200px"
-          height="40px"
-        />
+      <Box className="activity" justifyContent="center" display="flex">
+        <CircularProgress size={22} className="mr-1" /> <strong>Fetching Data</strong>
       </Box>
     </>
   );
