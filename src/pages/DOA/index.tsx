fbfd990373e5adc9d13f50071622d07ec6c3a8c1
Pranav Chandra <pranavchandra27@gmail.com>
2021-06-08 21:32:28 +0530

    change grid and refractor code

--- src/pages/DOA/index.tsx
+++ src/pages/DOA/index.tsx
@@ -1,144 +1,271 @@
-import React, { useState, useEffect, Fragment, useContext } from "react";
-import Grid from '@material-ui/core/Grid';
+import React, { useState, useEffect, useContext, useReducer } from "react";
+import Grid from "@material-ui/core/Grid";
 import Layout from "../../components/Layout";
-import Button from '@material-ui/core/Button';
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import CustomBreadCrumbs from "../../components/CustomBreadCrumbs";
-import { DataGrid } from "@material-ui/data-grid";
-import DataGridCustomToolbar from "../../components/Helpers/DataGridCustomToolbar";
-import AddIcon from "@material-ui/icons/Add";
-import Tooltip from "@material-ui/core/Tooltip";
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
 import NoDataCell from "../../components/Helpers/NoDataCell";
-import { Link } from 'react-router-dom'
 import { CustomToastContext } from "../../StateProvider/CustomToastContext/CustomToastContext";
 import axiosInstance from "../../axios/axiosInstance";
 import moment from "moment";
-import CustomDataGridNoDataFound from "../../components/Helpers/CustomDataGridNoDataFound";
-import { GiAbstract055 } from 'react-icons/gi';
-import ConfirmationDialog from '../../components/Helpers/ConfirmationDialog'
+import { GiAbstract055 } from "react-icons/gi";
 import CustomContainer from "../../components/CustomContainer";
-import EditIcon from '@material-ui/icons/Edit';
-import { dateFormat } from "../../constants/helpers"
+import { dateFormat, gridPageSizes } from "../../constants/helpers";
+import CustomAgGrid from "../../components/AgGridComponents/CustomAgGrid";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return {
+        ...state,
+        loading: action.loading,
+      };
+
+    case "initialize":
+      return {
+        ...state,
+        dataRows: action.data,
+        rowCount: action.count,
+        loading: false,
+      };
+
+    case "selection":
+      return {
+        ...state,
+        selectedRecords: action.selectedRecords,
+      };
+
+    case "update":
+      return {
+        ...state,
+        dataRows: action.data,
+        loading: false,
+      };
+
+    case "filter":
+      return {
+        ...state,
+        loading: true,
+        filters: action.filters,
+        page: 0,
+      };
+
+    case "sort":
+      return {
+        ...state,
+        sorting: action.sorting,
+        loading: true,
+      };
+
+    case "search":
+      return {
+        ...state,
+        search: action.search,
+        loading: true,
+      };
+
+    case "pageChange":
+      return {
+        ...state,
+        page: action.page,
+      };
+
+    case "pageSizeChange":
+      return {
+        ...state,
+        limit: action.limit,
+        page: 0,
+        loading: true,
+      };
+
+    case "complete":
+      return {
+        ...state,
+        loading: false,
+      };
+
+    default:
+      break;
+  }
+
+  return state;
+}
+
+const intialState = {
+  dataRows: [],
+  rowCount: 0,
+  loading: false,
+  page: 0,
+  limit: 25,
+  pageSizes: gridPageSizes,
+  search: "",
+  filters: {},
+  sorting: [],
+  selectedRecords: [],
+};
 
 const DOARequest = () => {
+  const toastConfig = useContext(CustomToastContext);
+  const history = useHistory();
+  const [productBuilder, setProductBuilder] = useState([]);
+  const [gridApi, setGridApi] = useState(null);
 
-    const toastConfig = useContext(CustomToastContext)
-    const history = useHistory();
-    const [loading, setLoading] = useState(true);
-    const [isCreate, setIsCreate] = useState(false);
-    const [productBuilder, setProductBuilder] = useState([]);
-    const [showDeleteConfirmBox, setShowDeleteConfirmBox] = useState(false)
-    const [deleteRecord, setDeleteRecord] = useState(null)
-    const [replied, setReplied] = useState(false);
+  const [state, dispatch] = useReducer(reducer, intialState);
+  const {
+    dataRows,
+    rowCount,
+    loading,
+    page,
+    limit,
+    pageSizes,
+    search,
+    filters,
+    sorting,
+    selectedRecords,
+  } = state;
 
-    useEffect(() => {
-        fetchProductBuilder();
-    }, []);
+  const [columns, setColumns] = useState([
+    {
+      field: "name",
+      headerName: "Name",
+      show: true,
+      disabled: true,
+      cellRenderer: "nameRenderer",
+    },
+    {
+      field: "quotedBy",
+      headerName: "Quoted By",
+      show: true,
+      disabled: true,
+      cellRenderer: "quotedByRenderer",
+    },
+    {
+      field: "requestedBy",
+      headerName: "Requested By",
+      show: true,
+      cellRenderer: "requestedByRendered",
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      show: true,
+    },
+  ]);
 
-    const fetchProductBuilder = () => {
-        setLoading(true)
-        axiosInstance().get(`/doa-request`).then(({ data: { data } }) => {
-            setProductBuilder(data);
-            setLoading(false)
-        }).catch((error) => {
-            toastConfig.setToastConfig(error);
-        });
-    };
+  useEffect(() => {
+    fetchProductBuilder();
+  }, []);
 
+  const NameRenderer = (params) => (
+    <Link
+      className="link"
+      to={`/doa-request/${params.data.id}`}
+      title={params.value}
+    >
+      {params.value}
+    </Link>
+  );
 
+  const QuotedByRenderer = (params) => (
+    <>
+      {params.value ? (
+        <Link
+          className="link"
+          to={`/user/detail/${params.data.quoteById}`}
+          title={params.value}
+        >
+          {params.value}
+        </Link>
+      ) : (
+        <NoDataCell />
+      )}
+    </>
+  );
 
-    const columns = [
-        {
-            field: "name",
-            headerName: "Name",
-            width: 300,
-            renderCell: (params) => (
-                <Link className="link" to={`/doa-request/${params.row.id}`} >
-                    {params.row.DOAName}
-                </Link>
-            )
-        },
-        {
-            field: "Quoted By",
-            headerName: "QuotedBy",
-            width: 200,
-            disableColumnMenu: true,
-            sortable: false,
-            filterable: false,
-            renderCell: (params) => (
-                <>
-                    {params.row.QuotedBy.firstName}
-                </>
-            )
-        },
-        {
-            field: "RequestedBy",
-            headerName: "Requested By",
-            width: 300,
-            disableColumnMenu: true,
-            sortable: false,
-            filterable: false,
-            renderCell: (params) => params?.row && params?.row?.RequestedBy ? (<h5 className="createBy">
-                {params.row.RequestedBy.firstName}
-                <span
-                    className="updatedAtTime badge-date"
-                    title={`${params.row.RequestedBy.firstName} • ${moment(
-                        params.row.RequestedBy.date.slice(0, 10)
-                    ).format(dateFormat)}`}
-                >
-                    {moment(params.row.RequestedBy.date.slice(0, 10)).format(
-                        dateFormat
-                    )}
-                </span>
-            </h5>) : <NoDataCell />
-        },
-        {
-            field: "status",
-            headerName: "Status",
-            width: 200,
-            renderCell: (params) => (
-                <>
-                    {params.row.status}
-                </>
-            )
-        }
-    ];
+  const RequestedByRenderer = (params) => (
+    <>
+      {params.value ? (
+        <Link
+          className="link"
+          to={`/user/detail/${params.data.requestedById}`}
+          title={params.value}
+        >
+          {params.value}
+        </Link>
+      ) : (
+        <NoDataCell />
+      )}
+    </>
+  );
 
+  const frameworkComponents = {
+    nameRenderer: NameRenderer,
+    requestedByRendered: RequestedByRenderer,
+    quotedByRenderer: QuotedByRenderer,
+  };
 
-    return (<Layout>
-        <Grid container className="headerbox">
-            <Grid item md={12} sm={12} xs={12}>
-                <CustomBreadCrumbs routes={[{ title: 'DOA Requests' }]} />
-            </Grid>
+  const fetchProductBuilder = () => {
+    dispatch({ type: "loading", loading: true });
+
+    if (gridApi) {
+      gridApi.setRowData([]);
+      gridApi.showLoadingOverlay();
+    }
+
+    axiosInstance()
+      .get(`/doa-request`)
+      .then(({ data: { data } }) => {
+        let rows = data.map((doa) => ({
+          ...doa,
+          name: doa.DOAName,
+          quotedBy: doa.QuotedBy.firstName,
+          quoteById: doa.QuotedBy.id,
+          requestedBy: doa.RequestedBy.firstName,
+          requestedById: doa.RequestedBy.id,
+        }));
+        dispatch({ type: "initialize", data: rows, count: data.length });
+        setProductBuilder(data);
+      })
+      .catch((error) => {
+        toastConfig.setToastConfig(error);
+        dispatch({ type: "loading", loading: false });
+      });
+  };
+
+  return (
+    <Layout>
+      <Grid container className="headerbox">
+        <Grid item md={12} sm={12} xs={12}>
+          <CustomBreadCrumbs routes={[{ title: "DOA Requests" }]} />
         </Grid>
-        <CustomContainer>
-            <div className="header-panel">
-                <Grid container>
-                    <Grid item xs={6} className="d-flex align-items-center gap-1">
-                        <GiAbstract055 /> <span className="listingHeader">{'DOA Requests'}</span>
-                    </Grid>
-                </Grid>
-            </div>
-            <div className="listing-grid">
-                <DataGrid
-                    components={{
-                        Toolbar: DataGridCustomToolbar,
-                        NoRowsOverlay: CustomDataGridNoDataFound,
-                    }}
-                    loading={loading}
-                    rows={productBuilder}
-                    disableSelectionOnClick
-                    disableMultipleSelection
-                    columns={columns}
-                    pageSize={25}
-                    density="compact"
-                />
-            </div>
-        </CustomContainer>
+      </Grid>
+      <CustomContainer>
+        <div className="header-panel">
+          <Grid container>
+            <Grid item xs={6} className="d-flex align-items-center gap-1">
+              <GiAbstract055 />{" "}
+              <span className="listingHeader">{"DOA Requests"}</span>
+            </Grid>
+          </Grid>
+        </div>
+        <div className="listing-grid">
+          <CustomAgGrid
+            columns={columns}
+            dataRows={dataRows}
+            frameworkComponents={frameworkComponents}
+            setGridApi={setGridApi}
+            dispatch={dispatch}
+            rowCount={rowCount}
+            limit={limit}
+            pageSizes={pageSizes}
+            page={page}
+            actionWidth={150}
+            allowSelection={false}
+            allowAction={false}
+          />
+        </div>
+      </CustomContainer>
     </Layout>
-    );
-}
+  );
+};
 
 export default DOARequest;
