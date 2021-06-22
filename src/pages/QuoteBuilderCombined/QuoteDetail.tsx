2c61bd03c1e458e3c88bd6738f7876022e54765e
punit <punit.soni33@gmail.com>
2021-06-22 13:56:38 +0530

    - Code Updated

--- src/pages/QuoteBuilderCombined/QuoteDetail.tsx
+++ src/pages/QuoteBuilderCombined/QuoteDetail.tsx
@@ -468,8 +468,8 @@ function QuoteDetail() {
             title={params.value}
             className="text-truncate link"
             onClick={() => {
-              setcurrentVersion(params.value);
               setTabValue(2);
+              setcurrentVersion(params.value);
               // setShowVersionsDialog(false);
             }}
           >
@@ -484,8 +484,8 @@ function QuoteDetail() {
             title={params.value}
             className="text-truncate link"
             onClick={() => {
-              setcurrentVersion(params.row.versionNumber);
               setTabValue(2);
+              setcurrentVersion(params.row.versionNumber);
               // setShowVersionsDialog(false);
             }}
           >
