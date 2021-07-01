387d35663074dd1a0ea37c875cbbf4875f9d9382
Pranav Chandra <pranavchandra27@gmail.com>
2021-07-01 16:38:57 +0530

    fix product builder grid issue

--- src/components/productBuilder/index.tsx
+++ src/components/productBuilder/index.tsx
@@ -51,8 +51,9 @@ const ProductBuilder = (props) => {
     stage,
     currency,
     isPriceBuilder,
-    createdBy,
+    hasPermission,
     permissions,
+    fromQuote,
   } = props;
 
   const toastConfig = useContext(CustomToastContext);
@@ -89,49 +90,60 @@ const ProductBuilder = (props) => {
     fetchProduct(productBuilderId);
   }, [productBuilderId]);
 
-  const ActionsRenderer = (params) => (
-    <>
-      {permissions.isUpdate && (
-        <Tooltip title="Clone">
-          <IconButton
-            size="small"
-            aria-label="Clone"
-            onClick={() => {
-              setProductData(params.data);
-              setIsClone(true);
-            }}
-          >
-            <FileCopyIcon fontSize="small" color="primary" />
-          </IconButton>
-        </Tooltip>
-      )}
-      {permissions.isUpdate && (
-        <Tooltip title="Edit">
-          <IconButton
-            aria-label="Edit"
-            onClick={() => {
-              setProductData(params.data);
-            }}
-          >
-            <EditIcon fontSize="small" color="primary" />
-          </IconButton>
-        </Tooltip>
-      )}
-      {permissions.isUpdate && (
-        <Tooltip title="Delete">
-          <IconButton
-            aria-label="Delete"
-            onClick={() => {
-              setDeleteRecord(params.data);
-              setShowDeleteConfirmBox(true);
-            }}
-          >
-            <DeleteIcon fontSize="small" color="error" />
-          </IconButton>
-        </Tooltip>
-      )}
-    </>
-  );
+  const ActionsRenderer = (params) => {
+    const permission =
+      permissions?.isUpdate && fromQuote
+        ? hasPermission
+          ? true
+          : false
+        : true;
+
+    return (
+      <>
+        <IconButton
+          disabled={permission ? false : true}
+          size="small"
+          aria-label="Clone"
+          onClick={() => {
+            setProductData(params.data);
+            setIsClone(true);
+          }}
+        >
+          <FileCopyIcon
+            fontSize="small"
+            color={permission ? "primary" : "disabled"}
+          />
+        </IconButton>
+
+        <IconButton
+          disabled={permission ? false : true}
+          aria-label="Edit"
+          onClick={() => {
+            setProductData(params.data);
+          }}
+        >
+          <EditIcon
+            fontSize="small"
+            color={permission ? "primary" : "disabled"}
+          />
+        </IconButton>
+
+        <IconButton
+          disabled={permission ? false : true}
+          aria-label="Delete"
+          onClick={() => {
+            setDeleteRecord(params.data);
+            setShowDeleteConfirmBox(true);
+          }}
+        >
+          <DeleteIcon
+            fontSize="small"
+            color={permission ? "error" : "disabled"}
+          />
+        </IconButton>
+      </>
+    );
+  };
 
   const ProductNameRenderer = (params) => (
     <>
