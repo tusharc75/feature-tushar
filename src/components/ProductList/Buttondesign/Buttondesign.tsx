6e5b454cf60b60c4bb3f66391fd17e4d7419a901
harshil1509 <harshils@iitbhilai.ac.in>
2021-07-01 21:45:07 +0530

    changes in frequently bought component

--- src/components/ProductList/Buttondesign/Buttondesign.tsx
+++ src/components/ProductList/Buttondesign/Buttondesign.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
-    root: {
-      background: 'linear-gradient(90deg, #DD5052 4.81%, #DD9044 100%)',
-      border: 0,
-      borderRadius: 3,
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      color: 'white',
-      height: 48,
-      padding: '0 30px',
-    },
-  });
+  root: {
+    background: "linear-gradient(90deg, #DD5052 4.81%, #DD9044 100%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+  },
+});
 
-export default function ButtonDesign({description}){
-    const classes = useStyles();
-    return (
-        <Button className={classes.root}>{description}</Button>
-    )
+export default function ButtonDesign({ title }) {
+  const classes = useStyles();
+  return <Button className={classes.root}>{title}</Button>;
 }
--- src/components/ProductList/FrequentlyBought/FrequentlyBought.tsx
+++ src/components/ProductList/FrequentlyBought/FrequentlyBought.tsx
@@ -6,13 +6,27 @@ import { IconButton } from "@material-ui/core";
 import ButtonDesign from "../Buttondesign/Buttondesign";
 
 function FrequentlyBought() {
-  const [selected, setSelected] = useState({
-    item1: false,
-    item2: false,
-  });
+  const [count, setCount] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      select: false,
+      itemDesc:
+        "Frac Tree, 7 1/16 Run, 3 1/16 wing, 10,000 psi, Temperature ClassU, Material Class DD",
+      itemPrice: 3000,
+    },
+    {
+      id: 2,
+      select: false,
+      itemDesc:
+        "Frac Tree, 7 1/16 Run, 3 1/16 wing, 10,000 psi, Temperature ClassU, Material Class DD",
+      itemPrice: 3000,
+    },
+  ]);
 
   function countTrue4obj(obj) {
-    var count = 0;
+    let count = 0;
     for (var p in obj) {
       if (obj.hasOwnProperty(p) && obj[p] === true) {
         count++;
@@ -21,12 +35,23 @@ function FrequentlyBought() {
     return count;
   }
 
-  const handleChange = (event) => {
-    selected[event.target.name] = !selected[event.target.name];
-    setSelected({ ...selected });
-  };
+  const handleChange = (event, index) => {
+    items[index]["select"] = !items[index]["select"];
 
-  const FracImage = "https://freepngimg.com/thumb/disney_pluto/32386-8-pluto-transparent.png";
+    setItems([...items]);
+    let total = 0;
+    let cost = 0;
+    items.map((item) => {
+      total = total + countTrue4obj(item);
+      if (item.select == true) {
+        cost = cost + item.itemPrice;
+      }
+    });
+    setTotalPrice(cost);
+    setCount(total);
+  };
+  const FracImage =
+    "https://freepngimg.com/thumb/disney_pluto/32386-8-pluto-transparent.png";
   return (
     <div className={styles.outerbox}>
       <h1 className="text-align-center">Frequently Bought Together</h1>
@@ -49,67 +74,46 @@ function FrequentlyBought() {
               Frac Tree, 7 1/16 Run, 3 1/16 wing, 10,000 psi, Temperature Class
               U, Material Class DD
             </p>
-            <h2 className={styles.card__price}>$2,099.00</h2>
+            <h2 className={styles.card__price}>Rs. 3000 /day</h2>
           </div>
         </div>
-        <div className={styles.card}>
-          <div className={styles.card__checkbox}>
-            <Checkbox
-             size="small"
-              color="primary"
-              inputProps={{ "aria-label": "secondary checkbox" }}
-              name="item1"
-              value={selected.item1}
-              onChange={handleChange}
-            />
+        {items.map((item, index) => (
+          <div key={item.id} className={styles.card}>
+            <div className={styles.card__checkbox}>
+              <Checkbox
+                color="primary"
+                inputProps={{ "aria-label": "secondary checkbox" }}
+                name="select"
+                value={items[index].select}
+                onChange={(e) => handleChange(e, index)}
+              />
+            </div>
+            <img src={FracImage} className={styles.card__img} />
+            <div className={styles.card__body}>
+              <p className={styles.card__desc}>{item.itemDesc}</p>
+              <h2 className={styles.card__price}>Rs. {item.itemPrice} /day</h2>
+            </div>
           </div>
-          <img src={FracImage} className={styles.card__img} />
-          <div className={styles.card__body}>
-            <p className={styles.card__desc}>
-              Frac Tree, 7 1/16 Run, 3 1/16 wing, 10,000 psi, Temperature Class
-              U, Material Class DD
-            </p>
-            <h2 className={styles.card__price}>$2,099.00</h2>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.card__checkbox}>
-            <Checkbox
-              size="small"
-              color="primary"
-              inputProps={{ "aria-label": "secondary checkbox" }}
-              name="item2"
-              value={selected.item2}
-              onChange={handleChange}
-            />
-          </div>
-          <img src={FracImage} className={styles.card__img} />
-          <div className={styles.card__body}>
-            <p className={styles.card__desc}>
-              Frac Tree, 7 1/16 Run, 3 1/16 wing, 10,000 psi, Temperature Class
-              U, Material Class DD
-            </p>
-            <h2 className={styles.card__price}>$2,099.00</h2>
-          </div>
-        </div>
+        ))}
+
         <div className={styles.card}>
           <div className={styles.card__pricesummary}>
             <h3 className={styles.card__pricesummaryheading}>Price Summary</h3>
             <hr />
             <div className={styles.card__summary}>
               <p>Main Product Selected</p>
-              <h3>$2,099.00 / day</h3>
+              <h3>Rs. 3000 /day</h3>
             </div>
             <div className={styles.card__summary}>
-              <p>{countTrue4obj(selected)} Addon Selected</p>
-              <h3>$3,848.00 / day</h3>
+              <p>{count} Addon Selected</p>
+              <h3>Rs. {totalPrice} /day</h3>
             </div>
             <hr />
             <div className={styles.card__total}>
               <h3>Total</h3>
-              <h3>$5947.00 / day</h3>
+              <h3>Rs. {totalPrice + 3000} /day</h3>
             </div>
-            <ButtonDesign description="Rent All" />
+            <ButtonDesign title="Rent All" />
           </div>
         </div>
       </div>
