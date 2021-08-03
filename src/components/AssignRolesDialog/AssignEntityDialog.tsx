a0960e49cbbbb2a6520a10c14efb447e878acd65
PRITAM <pritamchatterjee945@gmail.com>
2021-08-03 11:31:45 +0530

    OMS-1155 formatted

--- src/components/AssignRolesDialog/AssignEntityDialog.tsx
+++ src/components/AssignRolesDialog/AssignEntityDialog.tsx
@@ -165,23 +165,23 @@ const AssignEntityDialog = ({
     setSearch(value);
     let resultData = [];
     let resultRole = [];
-    if (activeStep === 0){
-    resultData = dataConst.filter((data) => {
-      if (type === "entity") {
-        return data.address?.toLowerCase().search(value.toLowerCase()) != -1 || data.entityName?.toLowerCase().search(value.toLowerCase()) != -1
-      }
-      else {
-        return data.concatedName?.toLowerCase().search(value.toLowerCase()) != -1 || data.email?.toLowerCase().search(value.toLowerCase()) != -1;
-      }
-    });
-    setData(resultData)
-  }else{
-    resultRole = roleConst.filter((data) => {
-      return data.name.toLowerCase().search(value.toLowerCase()) != -1 || data.description.toLowerCase().search(value.toLowerCase()) != -1;
-    });
-    setRole(resultRole)
-  }
-  
+    if (activeStep === 0) {
+      resultData = dataConst.filter((data) => {
+        if (type === "entity") {
+          return data.address?.toLowerCase().search(value.toLowerCase()) != -1 || data.entityName?.toLowerCase().search(value.toLowerCase()) != -1
+        }
+        else {
+          return data.concatedName?.toLowerCase().search(value.toLowerCase()) != -1 || data.email?.toLowerCase().search(value.toLowerCase()) != -1;
+        }
+      });
+      setData(resultData)
+    } else {
+      resultRole = roleConst.filter((data) => {
+        return data.name.toLowerCase().search(value.toLowerCase()) != -1 || data.description.toLowerCase().search(value.toLowerCase()) != -1;
+      });
+      setRole(resultRole)
+    }
+
   };
 
 
