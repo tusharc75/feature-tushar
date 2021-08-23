aad9ce19e58b75e19696da222fd1e639d42173ff
Jhon Rajbongshi <jhon@vebholic.com>
2021-08-23 13:35:22 +0530

    fix(live data in product): live data

--- src/components/ProductList/ProductCard/ProductCard.tsx
+++ src/components/ProductList/ProductCard/ProductCard.tsx
@@ -41,14 +41,14 @@ const ProductCard = (props: { product: any, onAddItem: any }) => {
           else {
             history.push(`product/details/${product._id}`)
           }
-        }}>{`${product.productName}, ${product.productCategory.optionLabel} `}</h4>
+        }}>{`${product?.productName}, ${product?.productCategory ? product.productCategory.optionLabel : ""} `}</h4>
         <div><Rating name="size-small" value={product.rating} readOnly size="small" /></div>
         <Box display="flex" justifyContent="space-between" alignItems="center">
           <div>
             {(product.mrp && parseInt(product.mrp) !== 0) &&
               <>
-                <span className={styles.amount}>{formatAmountWithCurrency(product.currency, calculateNetPrice(parseInt(product.mrp), parseInt(product.discount))).fullFormatAmount}</span>
-                {(parseInt(product.discount) !== 0) && <span className={styles.amount_stricked}>{formatAmountWithCurrency(product.currency, product.mrp).fullFormatAmount}</span>}
+                <span className={styles.amount}>{formatAmountWithCurrency(product.currency, product.mrp).fullFormatAmount}</span>
+                {(product.discount && parseInt(product.discount) !== 0) && <span className={styles.amount_stricked}>{formatAmountWithCurrency(product.currency, product.mrp).fullFormatAmount}</span>}
               </>
             }
           </div>
