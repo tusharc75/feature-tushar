4f85560dcf27a2b3737448c578777b6dd5c3eff9
Swati Gavhane <Swati@vebholic.com>
2021-09-01 16:11:58 +0530

    feat: -OMS-711 added confrim cancel Dialog on cancel or close click

--- src/components/Activity/Note/CreateNote.tsx
+++ src/components/Activity/Note/CreateNote.tsx
@@ -24,6 +24,7 @@ import ImageAttachments from "../Email/ImageAttachments";
 import ImagePreview from "../Email/ImagePreview";
 import { displayDate } from "../../../constants/helpers"
 import TinyMce from "../../../components/TinyMCE"
+import ConfirmCancelDialog from "../../../components/ConfirmCancelDialog"
 
 const NoteSchema = Yup.object().shape({
     name: Yup.string()
@@ -67,6 +68,7 @@ export const CreateNote = ({ relatedTo, noteId, handleClose, handleDialogClose }
     const [imageSource, setImageSource] = useState(null);
     const [open, setOpen] = useState(false)
     const [uploadingImageOrFileProgress, setUploadingImageOrFileProgress] = useState(0)
+    const [showConfirmDialog, setShowConfirmDialog] = useState(false)
 
     useEffect(() => {
         fetchNoteDetail();
@@ -322,7 +324,10 @@ export const CreateNote = ({ relatedTo, noteId, handleClose, handleDialogClose }
                     </Form>
                 </CustomDialogContent>
                 <CustomDialogFooter>
-                    <Button size="small" type="button" color="primary" onClick={handleDialogClose}>Cancel</Button>
+                    <Button size="small" type="button" color="primary"
+                        onClick={() => {
+                            setShowConfirmDialog(true)
+                        }}>Cancel</Button>
                     <Button size="small" type="button" color="primary" variant="contained"
                         onClick={() => {
 
@@ -335,6 +340,26 @@ export const CreateNote = ({ relatedTo, noteId, handleClose, handleDialogClose }
                         }}
                         disabled={uploadingImageOrFileProgress > 0}>Save</Button>
                 </CustomDialogFooter>
+                {
+                    showConfirmDialog ?
+                        <ConfirmCancelDialog
+                            open={showConfirmDialog}
+                            onSave={() => {
+                                setShowConfirmDialog(false)
+                                submitForm();
+                                if (Object.keys(errors).length) {
+                                    Object.keys(errors).map(k => {
+                                        setFieldTouched(k, true)
+                                    })
+                                }
+                                else submitForm()
+                            }}
+                            onClose={() => {
+                                setShowConfirmDialog(false)
+                                handleClose()
+                            }}
+                        /> : null
+                }
                 {
                     open ?
                         <ImagePreview
