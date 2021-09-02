0ba3743e3ebc2e048ab0356de652941a6b43d16c
Jhon Rajbongshi <jhon@vebholic.com>
2021-09-02 12:07:18 +0530

    fix(search doa req): search doa req

--- src/pages/QuoteBuilderCombined/QuoteDetail/QuoteProcess/index.tsx
+++ src/pages/QuoteBuilderCombined/QuoteDetail/QuoteProcess/index.tsx
@@ -241,7 +241,6 @@ export default function QuoteProcess(props) {
             .catch((error) => {
                 //   toastConfig.setToastConfig(error);
             });
-
     }, [currentVersion])
 
     useEffect(() => {
@@ -624,7 +623,7 @@ export default function QuoteProcess(props) {
     };
 
     const fetchDoaLimit = () => {
-        if (quoteData) {
+       if (quoteData) {
             axiosInstance()
                 .post("doa-request/limit", { user: quoteData?.createdBy?.user?._id })
                 .then(({ data: { data } }) => {
@@ -1516,7 +1515,9 @@ export default function QuoteProcess(props) {
                                 >
                                     Download
                                 </Button>
-                                {((permissions[qbResource]?.isUpdate && permissions?.quotePdfTmeplate?.isUpdate) || (user?.user?._id === quoteData?.owner?.optionValue || quoteData?.collaborator?.some(d => d === user?.user?._id))) &&
+                                {(permissions[qbResource]?.isUpdate && permissions?.quotePdfTemplate.isUpdate && 
+                                (user?.user?._id === quoteData?.owner?.optionValue || quoteData?.collaborator?.some(d => d === user?.user?._id)) && 
+                                (user?.user?._id === quoteData?.pDFTemplate?.owner || quoteData?.pDFTemplate?.collaborator?.some(d => d === user?.user?._id))) &&
                                     <Button
                                         onClick={() => {
                                             quoteData?.pDFTemplate.optionValue && history.push(`/quote-pdf-template/detail/${quoteData.pDFTemplate.optionValue}`, {
