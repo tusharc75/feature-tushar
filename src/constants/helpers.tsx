67012e05baac0a347f2458032bb43c5ae8e53846
Pranav Chandra <pranavchandra27@gmail.com>
2021-09-03 15:31:14 +0530

    refactor(addresses): change address to warehouse
    Change addresses to warehouse
    
    OMS-1562

--- src/constants/helpers.tsx
+++ src/constants/helpers.tsx
@@ -89,7 +89,7 @@ export const formFieldNames = {
 }
 
 export const sidebarResource = {
-  address: "Address",
+  warehouse: "Warehouse",
   brand: "Brand",
   role: "Role",
   product: "Product",
@@ -137,7 +137,7 @@ export const sidebarResource = {
 };
 
 export const RESOURCE_LABEL = {
-  address: "Addresses",
+  warehouse: "Warehouse",
   account: 'Supplier Accounts',
   customerAccount: 'Customer Accounts',
   user: 'Users',
