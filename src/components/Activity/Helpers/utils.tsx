cff3ed5b6fb65e975ca52996883355b8d2ea401a
Romit Sadaria <romit.sadaria@gmail.com>
2021-09-06 22:56:19 +0530

    fix: product export and activity filter

--- src/components/Activity/Helpers/utils.tsx
+++ src/components/Activity/Helpers/utils.tsx
@@ -6,7 +6,7 @@ export const resActivityColors = {
   lead: "#BF360C",
   opportunity: "#3949AB",
   user: "#990033",
-  quotes: "#CC33CC",
+  quote: "#CC33CC",
   projectSales: "#003333",
   my: "#990033",
 };
