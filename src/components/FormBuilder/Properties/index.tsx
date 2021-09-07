0c9b608d615ec05086dab653c2f18c855cf55d38
punit <punit.soni33@gmail.com>
2021-09-07 18:38:32 +0530

    feat: - Scroll bar coming in Product Template, in the dialog when we click Edit Properties
    - Scroll bar coming in Product Template, in the dialog when we click Edit Properties

--- src/components/FormBuilder/Properties/index.tsx
+++ src/components/FormBuilder/Properties/index.tsx
@@ -374,6 +374,7 @@ export const Properties = ({ module, handleClose, fieldData, sectionId, section,
       fullScreen={isMobile || isTablet}
       TransitionComponent={CustomDialogTransition}
       aria-labelledby="customized-dialog-title"
+      className="properties_dialog_height"
       open={true}
       fullWidth
       onClose={(e, reason) => {
