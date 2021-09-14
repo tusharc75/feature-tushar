49b90c7554caa2b9d36299915a4d76521b8187e3
Pranav Chandra <pranavchandra27@gmail.com>
2021-09-14 11:28:59 +0530

    fix(helpers): mobile validation change

--- src/constants/helpers.tsx
+++ src/constants/helpers.tsx
@@ -476,7 +476,7 @@ export const yupSchema = (fields: any[], validEmail = true) => {
         ? string()
           .min(10, "Mobile number is too short")
           .required(`${input.fieldLabel} is required`)
-        : string();
+        : string().min(10, "Mobile number is too short");
     } else if (input.type === "multiSelect") {
       schema[input.fieldName] = input.required
         ? array().required(`${input.fieldLabel} is required`)
