b7526b902fa553f07f206205b374fae248f84d14
Punit - Vebholic <punit@vebholic.com>
2021-09-17 18:08:13 +0530

    feat: - OMS-1589 When logged in user unassiging in own selected entity then causing error

--- src/axios/axiosInstance.tsx
+++ src/axios/axiosInstance.tsx
@@ -89,6 +89,8 @@ export default (history = null, passedHeaders = null) => {
             }
 
             if (error.response.data && error.response.data.code && Object.values(ERROR_CODE).some(s => s === error.response.data.code)) {
+                localStorage.removeItem("selectedEntity");
+
                 if (window.confirm((`${error.response.data.error}\n\nPress Ok to redirect to home\nPress Cancel to stay here`))) {
                     //@ts-ignore
                     window.location = "/";
