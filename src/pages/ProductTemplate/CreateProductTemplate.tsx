0c5d9bf4488d80163c9bb0f66d6fa3a8f2eeee40
Jujar Singh <jujar@vebholic.com>
2021-09-24 15:27:02 +0530

    style: oMS-1841 Homepage design changed

--- src/pages/ProductTemplate/CreateProductTemplate.tsx
+++ src/pages/ProductTemplate/CreateProductTemplate.tsx
@@ -359,7 +359,7 @@ const ProductTemplate = () => {
                                             />
                                         </Box>
                                     </Grid>
-                                    <Grid item xs={12} sm={3}>
+                                    <Grid item xs={12} sm={4}>
                                         {!values["isStandard"] && <Autocomplete
                                             disabled={!hasPermissionToUpdate}
                                             options={productCategory}
@@ -388,7 +388,7 @@ const ProductTemplate = () => {
                                             )}
                                         />}
                                     </Grid>
-                                    <Grid item xs={12} sm={5} container justify="flex-end">
+                                    <Grid item xs={12} sm={4} container justify="flex-end">
                                         <HistoryButton onClick={() => setShowHistory(true)} />
                                         <Box>
                                             {(productTemplatePermissions.isCreate || productTemplatePermissions.isUpdate) &&
@@ -411,7 +411,7 @@ const ProductTemplate = () => {
                                     </Grid>
                                 </Grid>
                                 <Grid container spacing={1}>
-                                    <Grid item xs={12} sm={3}>
+                                    <Grid item xs={12} sm={4}>
                                         {<Autocomplete
                                             disabled={!hasPermissionToUpdate}
                                             multiple
@@ -441,7 +441,7 @@ const ProductTemplate = () => {
                                             )}
                                         />}
                                     </Grid>
-                                    <Grid item xs={12} sm={3}>
+                                    <Grid item xs={12} sm={4}>
                                         {<Autocomplete
                                             disabled={!hasPermissionToUpdate}
                                             getOptionLabel={(option: any) => (option ? option?.concatedName : "")}
@@ -473,7 +473,7 @@ const ProductTemplate = () => {
                                             )}
                                         />}
                                     </Grid>
-                                    <Grid item xs={12} sm={3}>
+                                    <Grid item xs={12} sm={4}>
                                         {<Autocomplete
                                             disabled={!hasPermissionToUpdate}
                                             multiple
