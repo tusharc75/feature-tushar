2a6c62b7c887ab316176e83bb16fbb7bd2fc3400
PRITAM <pritamchatterjee945@gmail.com>
2021-09-24 17:27:34 +0530

    feat(Dashboard):list item added on hover
    OMS-1841

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,167 +1,174 @@
-import {useEffect, useState, Fragment} from 'react';
-import {Container, Grid, Paper, Box, Typography, Button} from '@material-ui/core';
-import {Link} from 'react-router-dom';
-import {useData} from '../../StateProvider/Provider';
-import {kebabCase} from 'lodash';
+import { useEffect, useState, Fragment } from 'react';
+import { Container, Grid, Paper, Box, Typography, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { useData } from '../../StateProvider/Provider';
+import { kebabCase } from 'lodash';
 import styles from "./Dashboard.module.scss";
 import crmImage from "../../assets/eQuip_t-homepage-design-trs.-in-png.png";
-import {SiCivicrm} from 'react-icons/si';
+import { SiCivicrm } from 'react-icons/si';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import PhoneIcon from '@material-ui/icons/Phone';
-import {color} from "chart.js/helpers";
-import {classNames} from "react-easy-crop/helpers";
-import {MdNavigateNext, MdLocalActivity} from 'react-icons/md';
-import {RiAccountPinCircleFill, RiFolderSettingsFill} from 'react-icons/ri';
-import {BsCalendarFill} from 'react-icons/bs';
-import {FaRegistered} from 'react-icons/fa';
+import { color } from "chart.js/helpers";
+import { classNames } from "react-easy-crop/helpers";
+import { MdNavigateNext, MdLocalActivity } from 'react-icons/md';
+import { RiAccountPinCircleFill, RiFolderSettingsFill } from 'react-icons/ri';
+import { BsCalendarFill } from 'react-icons/bs';
+import { FaRegistered } from 'react-icons/fa';
 
 function Dashboard() {
-    const {
-        state: {user}
-    } = useData();
-    const [sections, setSections] = useState([]);
+  const {
+    state: { user }
+  } = useData();
+  const [sections, setSections] = useState([]);
 
-    useEffect(() => {
-        const arr = [];
-        let allData = user && [...user?.role.sideBar];
-        if (user?.role?.selectedEntity) {
-            allData = [...allData, ...user?.role?.selectedEntity?.resource];
-        }
+  useEffect(() => {
+    const arr = [];
+    let allData = user && [...user?.role.sideBar];
+    if (user?.role?.selectedEntity) {
+      allData = [...allData, ...user?.role?.selectedEntity?.resource];
+    }
 
-        allData?.forEach((u) => {
-            !arr.includes(u.sectionName) && arr.push(u.sectionName);
-        });
-        const data = arr.map((sec) => {
-            const list = allData?.filter((u) => sec === u.sectionName && u.isRead);
+    allData?.forEach((u) => {
+      !arr.includes(u.sectionName) && arr.push(u.sectionName);
+    });
+    const data = arr.map((sec) => {
+      const list = allData?.filter((u) => sec === u.sectionName && u.isRead);
 
-            let icon = <PhoneIcon/>;
-            let heading = "";
-            let text = "";
+      let icon = <PhoneIcon />;
+      let heading = "";
+      let text = "";
 
 
-            switch (sec) {
-                case "CRM +":
-                    icon = <SiCivicrm size={32}/>;
-                    text = "Convert leads and close sales deals faster.";
+      switch (sec) {
+        case "CRM +":
+          icon = <SiCivicrm size={32} />;
+          text = "Convert leads and close sales deals faster.";
 
-                    break;
+          break;
 
-                case "Accounts":
-                    icon = <RiAccountPinCircleFill size={32}/>;
-                    text = "Powerful financial platform to grow more your business."
-                    break;
+        case "Accounts":
+          icon = <RiAccountPinCircleFill size={32} />;
+          text = "Powerful financial platform to grow more your business."
+          break;
 
-                case "Activities":
-                    icon = <MdLocalActivity size={32}/>;
-                    text = "We Need to Talk About Budgeting. It’s a Necessary Evil."
-                    break;
+        case "Activities":
+          icon = <MdLocalActivity size={32} />;
+          text = "We Need to Talk About Budgeting. It’s a Necessary Evil."
+          break;
 
-                case "Product Setup":
-                    icon = <RiFolderSettingsFill size={32}/>;
-                    text = "We Need to Talk About Budgeting. It’s a Necessary Evil."
-                    break;
+        case "Product Setup":
+          icon = <RiFolderSettingsFill size={32} />;
+          text = "We Need to Talk About Budgeting. It’s a Necessary Evil."
+          break;
 
-                case "Admin Portal":
-                    icon = <BsCalendarFill size={32} style={{padding: "4px"}}/>;
+        case "Admin Portal":
+          icon = <BsCalendarFill size={32} style={{ padding: "4px" }} />;
 
-                    text = "We Need to Talk About Budgeting. It’s a Necessary Evil."
-                    break;
+          text = "We Need to Talk About Budgeting. It’s a Necessary Evil."
+          break;
 
-                case "Rentals":
-                    icon = <FaRegistered size={32}/>;
-                    text = "We Need to Talk About Budgeting. It’s a Necessary Evil."
-                    break;
-            }
+        case "Rentals":
+          icon = <FaRegistered size={32} />;
+          text = "We Need to Talk About Budgeting. It’s a Necessary Evil."
+          break;
+      }
 
-            return {
-                icon: icon,
-                text: text,
-                head: sec,
-                items: list
-            };
+      return {
+        icon: icon,
+        text: text,
+        head: sec,
+        items: list
+      };
 
-        });
-        setSections(data);
-    }, [user]);
+    });
+    setSections(data);
+  }, [user]);
 
-    const handleRoutes = (item) => {
-        return `/${kebabCase(item.name)}`;
+  const handleRoutes = (item) => {
+    return `/${kebabCase(item.name)}`;
 
-        //  Use below code to handle special route cases
-        // switch (item.name) {
+    //  Use below code to handle special route cases
+    // switch (item.name) {
 
-        //   case "T&Cs":
-        //     return "/terms-conditions";
+    //   case "T&Cs":
+    //     return "/terms-conditions";
 
-        //   default:
-        //     return `/${kebabCase(item.name)}`;
-        // }
-    };
+    //   default:
+    //     return `/${kebabCase(item.name)}`;
+    // }
+  };
 
-    return (
-        <Fragment>
-            <Container>
-                <Grid className={styles.dashboard_layout}>
-                    <div className={styles.first_box}>
-                        <h1>
-                            Raising resiliency in a rapidly transforming business environment</h1>
-                        <p>Managing your customer leads, opportunities, complex projects, and quotes just got easier</p>
-                        <img src={crmImage} alt="Logo" className={styles.set_crm_image}/>
-                    </div>
-                    <div>
-                        <Box marginY={2}>
-                            <Grid container spacing={1}>
-                                {sections.map((section) => {
-                                    return section.items.length > 0 ? (
-                                        <Grid key={section.head} item xs={12} sm={6} md={4}>
-                                            <Paper className={styles.box_layout}>
-                                                <Box padding={2}>
-                                                    <Box textAlign="center" marginBottom={2}>
-                                                        <p className={styles.set_icon}>{section.icon}</p>
-                                                        <h2 className={styles.headline}>{section.head}</h2>
-                                                        <p>{section.text}</p>
-                                                        <div className={styles.dropdown}>
-                                                            <Button className={styles.list}>
-                                                                View all<MdNavigateNext/>
-                                                                <div className={styles.dropdown_content}>
-                                                                    <a href="#">Link 1</a>
-                                                                    <a href="#">Link 2</a>
-                                                                    <a href="#">Link 3</a>
-                                                                </div>
-                                                            </Button>
-                                                        </div>
-                                                    </Box>
-                                                    {/*<Box height="150px" style={{ overflowY: 'auto' }}>*/}
-                                                    {/*  {section.items.map((item) => (*/}
-                                                    {/*    <>*/}
-                                                    {/*      <Box marginY={1} key={item.name} component="div">*/}
-                                                    {/*        <Typography paragraph>*/}
-                                                    {/*          <Link to={handleRoutes(item)}>{item.resourceLabel || item.name}</Link>*/}
-                                                    {/*        </Typography>*/}
-                                                    {/*      </Box>*/}
-                                                    {/*      /!*Only show product list if environment is local || development*!/*/}
-                                                    {/*      {['local', 'development'].includes(process.env.REACT_APP_ENV) && item.name === 'Product' && (*/}
-                                                    {/*        <Box marginY={1} key={item.name} component="div">*/}
-                                                    {/*          <Typography paragraph>*/}
-                                                    {/*            <Link to={`/product-list`}>Product List</Link>*/}
-                                                    {/*          </Typography>*/}
-                                                    {/*        </Box>*/}
-                                                    {/*      )}*/}
-                                                    {/*    </>*/}
-                                                    {/*  ))}*/}
-                                                    {/*</Box>*/}
-                                                </Box>
-                                            </Paper>
-                                        </Grid>
-                                    ) : null;
-                                })}
-                            </Grid>
+  return (
+    <Fragment>
+      <Container>
+        <Grid className={styles.dashboard_layout}>
+          <div className={styles.first_box}>
+            <h1>
+              Raising resiliency in a rapidly transforming business environment</h1>
+            <p>Managing your customer leads, opportunities, complex projects, and quotes just got easier</p>
+            <img src={crmImage} alt="Logo" className={styles.set_crm_image} />
+          </div>
+          <div>
+            <Box marginY={2}>
+              <Grid container spacing={1}>
+                {sections.map((section) => {
+                  return section.items.length > 0 ? (
+                    <Grid key={section.head} item xs={12} sm={6} md={4}>
+                      <Paper className={styles.box_layout}>
+                        <Box padding={2}>
+                          <Box textAlign="center" marginBottom={2}>
+                            <p className={styles.set_icon}>{section.icon}</p>
+                            <h2 className={styles.headline}>{section.head}</h2>
+                            <p>{section.text}</p>
+                            <div className={styles.dropdown}>
+                              <Button className={styles.list}>
+                                View all<MdNavigateNext />
+                                <div className={styles.dropdown_content}>
+                                  {
+                                    section.items.map((item) => (
+                                      <Typography>
+                                        <Link to={handleRoutes(item)}>{item.resourceLabel || item.name}</Link>
+                                      </Typography>
+                                    ))
+                                  }
+                                  {/* <a href="#">Link 1</a>
+                                  <a href="#">Link 2</a>
+                                  <a href="#">Link 3</a> */}
+                                </div>
+                              </Button>
+                            </div>
+                          </Box>
+                          {/*<Box height="150px" style={{ overflowY: 'auto' }}>*/}
+                          {/*  {section.items.map((item) => (*/}
+                          {/*    <>*/}
+                          {/*      <Box marginY={1} key={item.name} component="div">*/}
+                          {/*        <Typography paragraph>*/}
+                          {/*          <Link to={handleRoutes(item)}>{item.resourceLabel || item.name}</Link>*/}
+                          {/*        </Typography>*/}
+                          {/*      </Box>*/}
+                          {/*      /!*Only show product list if environment is local || development*!/*/}
+                          {/*      {['local', 'development'].includes(process.env.REACT_APP_ENV) && item.name === 'Product' && (*/}
+                          {/*        <Box marginY={1} key={item.name} component="div">*/}
+                          {/*          <Typography paragraph>*/}
+                          {/*            <Link to={`/product-list`}>Product List</Link>*/}
+                          {/*          </Typography>*/}
+                          {/*        </Box>*/}
+                          {/*      )}*/}
+                          {/*    </>*/}
+                          {/*  ))}*/}
+                          {/*</Box>*/}
                         </Box>
-                    </div>
-                </Grid>
-            </Container>
-        </Fragment>
-    );
+                      </Paper>
+                    </Grid>
+                  ) : null;
+                })}
+              </Grid>
+            </Box>
+          </div>
+        </Grid>
+      </Container>
+    </Fragment>
+  );
 }
 
 export default Dashboard;
