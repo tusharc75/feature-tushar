c19f20e55d6115a8d584046276f91a55e48af635
PRITAM <pritamchatterjee945@gmail.com>
2021-10-06 16:11:12 +0530

    fix(Contact):newly created account binding issue
    OAT-4

--- src/pages/Contact/ManageContact/index.tsx
+++ src/pages/Contact/ManageContact/index.tsx
@@ -30,7 +30,8 @@ export default function ManageContactDialog(props) {
     collaborators,
     owners,
     fromProject,
-    isClone = false
+    isClone = false,
+    isAccountFieldDisable = false
   } = props;
   const { accountApi, accountResource } = account;
   const {
@@ -177,7 +178,7 @@ export default function ManageContactDialog(props) {
 
   const handleDialogClose = () => {
     setShowAccountDialog(false);
-    onClose()
+    // onClose()
   };
 
   const handleGetAddedAccount = ({ data }) => {
@@ -225,6 +226,7 @@ export default function ManageContactDialog(props) {
         formValues={formValues}
         handleValuesChange={handleValuesChange}
         isClone={isClone}
+        isAccountFieldDisable={isAccountFieldDisable}
       />
       {showAccountDialog ? (
         <ManageAccountDialog
