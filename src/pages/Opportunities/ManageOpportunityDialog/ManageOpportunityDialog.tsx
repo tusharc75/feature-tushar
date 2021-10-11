01393a4d85e523ce9cbcd3ed8bfc8eaa4bf9d1e3
punit <punit.soni33@gmail.com>
2021-10-11 18:30:52 +0530

    feat: - Rental management history api fixed, euro currency issue fixed

--- src/pages/Opportunities/ManageOpportunityDialog/ManageOpportunityDialog.tsx
+++ src/pages/Opportunities/ManageOpportunityDialog/ManageOpportunityDialog.tsx
@@ -781,15 +781,15 @@ export default function ManageOpportunityDialog({
                                         <Grid
                                           item
                                           xs={
-                                            permissions.marketSegment.isCreate ? 10
+                                            permissions.marketsegment?.isCreate ? 10
                                               : 11
                                           }
                                           sm={
-                                            permissions.marketSegment.isCreate ? 10
+                                            permissions.marketsegment?.isCreate ? 10
                                               : 11
                                           }
                                           md={
-                                            permissions.marketSegment.isCreate ? 10
+                                            permissions.marketsegment?.isCreate ? 10
                                               : 11
                                           }
                                         >
@@ -835,7 +835,7 @@ export default function ManageOpportunityDialog({
                                         {
                                           // permissions.productCategory
                                           //     .isCreate
-                                          permissions.marketSegment.isCreate && (
+                                          permissions.marketsegment?.isCreate && (
                                             <Grid item xs={1} sm={1} md={1}>
                                               <Tooltip
                                                 title="Add Market Segment"
@@ -870,15 +870,15 @@ export default function ManageOpportunityDialog({
                                           <Grid
                                             item
                                             xs={
-                                              permissions.marketSegment.isCreate ? 10
+                                              permissions.marketsegment?.isCreate ? 10
                                                 : 11
                                             }
                                             sm={
-                                              permissions.marketSegment.isCreate ? 10
+                                              permissions.marketsegment?.isCreate ? 10
                                                 : 11
                                             }
                                             md={
-                                              permissions.marketSegment.isCreate ? 10
+                                              permissions.marketsegment?.isCreate ? 10
                                                 : 11
                                             }
                                           >
@@ -919,7 +919,7 @@ export default function ManageOpportunityDialog({
                                             />
                                           </Grid>
                                           {
-                                            permissions.marketSegment.isCreate && (
+                                            permissions.marketsegment?.isCreate && (
                                               <Grid item xs={1} sm={1} md={1}>
                                                 <Tooltip
                                                   title="Add Sub Market Segment"
--- src/pages/RentalManagement/RentalManagementDetailsPage.tsx
+++ src/pages/RentalManagement/RentalManagementDetailsPage.tsx
@@ -712,7 +712,7 @@ const RentalManagementDetailsPage = () => {
                       <div>
                         <Activity
                           resourceId={rentalManagementData._id}
-                          resource={routes.rentalManagement}
+                          resource={rentalManagement.resource}
                           restrictedAddActivities={
                             permissions &&
                               permissions["rentalManagement"] &&
