4e3b2ecb777ce028fb796ee227f507fbbff82c9e
PRITAM <pritamchatterjee945@gmail.com>
2021-10-13 12:48:14 +0530

    fix(axiosInstance):entity delete issue handled
    OMS-1893

--- src/axios/axiosInstance.tsx
+++ src/axios/axiosInstance.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { backendApi } from './../config';
+import { SET_SELECTED_ENTITY, SET_USER } from '../StateProvider/actionTypes';
 
 const ERROR_CODE = {
     permissionError: '1001',
@@ -93,12 +94,14 @@ export default (history = null, passedHeaders = null) => {
             }
 
             if (error.response.data && error.response.data.code && Object.values(ERROR_CODE).some(s => s === error.response.data.code)) {
-                localStorage.removeItem("selectedEntity");
+                
 
                 if (window.confirm((`${error.response.data.error}\n\nPress Ok to redirect to home\nPress Cancel to stay here`))) {
+                    localStorage.removeItem("selectedEntity");
                     //@ts-ignore
                     window.location = "/";
                 }
+                
             } else if (error.response.data && error.response.data.code && error.response.data.code === "1005") {
                 return new Promise((resolve, reject) => {
                     reject({ open: true, type: "notFoundError", message: "" });
