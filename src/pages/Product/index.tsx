633e5ad7181a5877be2f8dc4cfd4eb7fac5095d7
Romit Sadaria <romit.sadaria@gmail.com>
2021-10-19 10:00:36 +0530

    fix: bug fix in product list

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -106,7 +106,7 @@ const Product = () => {
                         entityId: firstEntity?.optionValue,
                         productCategoryChipColor: u.productCategory?.chipColour,
                         restEntity: restEntity,
-                        serializedProduct: u.serializedProduct.toString()
+                        serializedProduct: u.serializedProduct && u.serializedProduct.toString()
                     }
                     for (let col in res) {
                         if (res[col] && res[col].optionLabel) {
