3d525cc87d56eff66aaf521ebe1e756569b6180c
Jhon Rajbongshi <jhon@vebholic.com>
2021-10-19 11:43:26 +0530

    feat: product grid in quote details issue

--- src/components/productBuilder/index.tsx
+++ src/components/productBuilder/index.tsx
@@ -198,7 +198,7 @@ const ProductBuilder = (props) => {
           entity: firstEntity?.optionLabel,
           entityId: firstEntity?.optionValue,
           restEntity: restEntity,
-          serializedProduct: u.serializedProduct.toString()
+          serializedProduct: u.serializedProduct && u.serializedProduct.toString()
         }
         for (let col in res) {
           if (res[col] && res[col].optionLabel) {
