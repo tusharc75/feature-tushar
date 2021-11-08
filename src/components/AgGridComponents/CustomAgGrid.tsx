06bf54839f4715d4d0e1e00b78490b8451284277
Punit Soni <punit.soni33@gmail.com>
2021-11-08 20:18:25 +0530

    feat: - Keeping selected records on page change, filter is done

--- src/components/AgGridComponents/CustomAgGrid.tsx
+++ src/components/AgGridComponents/CustomAgGrid.tsx
@@ -144,15 +144,31 @@ export default function CustomAgGrid({
   const [, setColumns] = useState(columns);
   const [columnApi, setColumnApi] = useState(null);
 
-  const [clientSideGridApi, setClientSideGridApi] = useState(null);
+  const [currentGridApi, setCurrentGridApi] = useState(null);
   const enableRowDrag = columns.some((d) => d.rowDrag);
 
   //  If you want to do something once grid binding done
   const onGridReady = (params) => {
     setGridApi(params.api);
     setColumnApi(params.columnApi);
-    setClientSideGridApi(params.api);
+    setCurrentGridApi(params.api);
     if (handleGridReady) handleGridReady(params);
+
+    if (!isClientSideGrid) {
+
+      try {
+        let oldSelectedRecords = localStorage.getItem(`${renderedFrom}_selected`) ? JSON.parse(localStorage.getItem(`${renderedFrom}_selected`)) : []
+        if (oldSelectedRecords.length > 0) {
+          params.api.forEachNode(function (node) {
+            node.setSelected(
+              oldSelectedRecords.some((o) => o === node.data._id)
+            );
+          });
+        }
+      } catch (ex) {
+        console.error("Error in getting selected records from local storage")
+      }
+    }
   };
 
   useEffect(() => {
@@ -163,6 +179,21 @@ export default function CustomAgGrid({
         setTimeout(() => {
           columnApi.setColumnState(columnState);
         }, 50)
+      }
+
+      if (!isClientSideGrid && currentGridApi) {
+        try {
+          let oldSelectedRecords = localStorage.getItem(`${renderedFrom}_selected`) ? JSON.parse(localStorage.getItem(`${renderedFrom}_selected`)) : []
+          if (oldSelectedRecords.length > 0) {
+            currentGridApi.forEachNode(function (node) {
+              node.setSelected(
+                oldSelectedRecords.some((o) => o === node.data._id)
+              );
+            });
+          }
+        } catch (ex) {
+          console.error("Error in getting selected records from local storage")
+        }
       }
     }
   }, [columnApi, loading])
@@ -182,15 +213,15 @@ export default function CustomAgGrid({
   }
 
   useEffect(() => {
-    if (clientSideGridApi && selectedRecords.length) {
-      clientSideGridApi.forEachNode(function (node) {
+    if (currentGridApi && selectedRecords.length) {
+      currentGridApi.forEachNode(function (node) {
         node.setSelected(
           selectedRecords.some((o) => o._id === node.data._id)
         );
       });
     }
 
-  }, [clientSideGridApi, selectedRecords])
+  }, [currentGridApi, selectedRecords])
 
   var customFilterParams = {
     filterOptions: ['contains'],
@@ -349,10 +380,10 @@ export default function CustomAgGrid({
               }}
               onFilterChanged={(e) => {
                 if (isClientSideGrid) {
-                  clientSideGridApi.paginationGoToPage(0);
+                  currentGridApi.paginationGoToPage(0);
                   dispatch({
                     type: 'count',
-                    count: clientSideGridApi.getModel().rootNode.allChildrenCount
+                    count: currentGridApi.getModel().rootNode.allChildrenCount
                   });
                   dispatch({ type: 'pageChange', page: 0 });
                 } else {
@@ -366,8 +397,7 @@ export default function CustomAgGrid({
               // loadingOverlayComponentParams={{
               //     loadingMessage: 'Loading...',
               // }}
-              animateRows={enableRowDrag ?? false
-              }
+              animateRows={enableRowDrag ?? false}
               suppressAnimationFrame={!enableRowDrag}
               suppressMaintainUnsortedOrder={true}
               rowBuffer={limit}
@@ -380,12 +410,38 @@ export default function CustomAgGrid({
 
               suppressRowClickSelection={true}
               rowSelection={'multiple'}
+              onRowSelected={(event) => {
+                if (!isClientSideGrid && event.rowIndex !== null && event.node.isSelected() === false) {
+                  try {
+                    let oldSelectedRecords = localStorage.getItem(`${renderedFrom}_selected`) ? JSON.parse(localStorage.getItem(`${renderedFrom}_selected`)) : []
+                    if (oldSelectedRecords.length > 0) {
+                      localStorage.setItem(`${renderedFrom}_selected`, JSON.stringify(oldSelectedRecords.filter(f => f !== (event.data._id ?? event.data.id))));
+                    }
+                  } catch (ex) {
+                    console.error("Error in getting / storing selected records")
+                  }
+                }
+              }}
               onSelectionChanged={(event: any) => {
                 if (onSelection) onSelection(event.api.getSelectedRows());
                 dispatch({
                   type: 'selection',
                   selectedRecords: event.api.getSelectedRows()
                 });
+
+                if (renderedFrom) {
+                  try {
+                    let oldSelectedRecords = localStorage.getItem(`${renderedFrom}_selected`) ? JSON.parse(localStorage.getItem(`${renderedFrom}_selected`)) : []
+                    if (oldSelectedRecords.length > 0) {
+                      const uniqueRecords = [...new Set([...oldSelectedRecords, ...event.api.getSelectedRows().map(m => m._id)])]
+                      localStorage.setItem(`${renderedFrom}_selected`, JSON.stringify(uniqueRecords));
+                    } else {
+                      localStorage.setItem(`${renderedFrom}_selected`, JSON.stringify(event.api.getSelectedRows().map(m => m._id)))
+                    }
+                  } catch (ex) {
+                    console.error("Error in getting / storing selected records")
+                  }
+                }
               }}
               onRowDragEnd={(event: any) => {
                 if (onRowDragEnd) {
@@ -432,17 +488,17 @@ export default function CustomAgGrid({
               onPageChange={(event, newPage) => {
                 dispatch({ type: 'pageChange', page: newPage });
 
-                if (clientSideGridApi) {
-                  clientSideGridApi.paginationGoToPage(newPage);
+                if (currentGridApi) {
+                  currentGridApi.paginationGoToPage(newPage);
                 }
               }}
               rowsPerPage={limit}
               onRowsPerPageChange={(event) => {
                 dispatch({ type: 'pageSizeChange', limit: event.target.value });
 
-                if (clientSideGridApi) {
-                  clientSideGridApi.paginationGoToPage(0);
-                  clientSideGridApi.paginationSetPageSize(event.target.value);
+                if (currentGridApi) {
+                  currentGridApi.paginationGoToPage(0);
+                  currentGridApi.paginationSetPageSize(event.target.value);
                 }
               }}
               rowsPerPageOptions={pageSizes}
