8403ad5a1371085942a696a9496b811857e58d7f
jinal <jinal@vebholic.com>
2021-11-11 21:16:28 +0530

    feat: extra character removed
    removed

--- src/components/SwipableListComponents/CustomSwipableList.tsx
+++ src/components/SwipableListComponents/CustomSwipableList.tsx
@@ -179,7 +179,7 @@ export default function CustomSwipableList({
                                                 <div key={index} className="ml-2 my-1">
                                                     <div className="swipe-card-additional-details-inner">
                                                         <span style={{ color: "#337FFB" }} className="d-flex align-items-center">{a.icon}</span>
-                                                        <h5 className="text-truncate" style={{ paddingTop: "2px", fontWeight: 500 }}>{d[a.field]} asdasda sdasdasd asd asda sdasd asdasdad</h5>
+                                                        <h5 className="text-truncate" style={{ paddingTop: "2px", fontWeight: 500 }}>{d[a.field]}</h5>
                                                     </div>
                                                 </div>
                                             ))
