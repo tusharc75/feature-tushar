43d6e782a5b271744ef939a57346bbe35c5c93a5
PRITAM <pritamchatterjee945@gmail.com>
2021-11-22 16:05:32 +0530

    fix(Notification):Date format issue in notification fixed
    OAT-361

--- src/constants/helpers.tsx
+++ src/constants/helpers.tsx
@@ -647,7 +647,7 @@ export const initializeDropdownById = (field, fieldName, id) => {
 };
 export const dateFormat = localStorage.getItem("dateFormat") ?? "MM/DD/YYYY";
 export const dateTimeFormat = localStorage.getItem("dateTimeFormat") ?? "MM/DD/YYYY hh:mm A";
-export const cardDateFormat = localStorage.getItem("cardDateFormat") ?? "MMM,DD YYYY";
+export const cardDateFormat = localStorage.getItem("cardDateFormat") ?? "MM,DD YYYY";
 
 export const dateFormatForInputControl = localStorage.getItem("dateFormatForInputControl") ?? "MM/dd/yyyy";
 // export const dateTimeFormat = "MM/dd/yyyy hh:mm A"
