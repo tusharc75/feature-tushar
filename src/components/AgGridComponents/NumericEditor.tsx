0d8e1e4b2880483fa872a8957d3f28df072a6ca8
Romit S <romit@vebholic.com>
2021-11-23 22:37:35 +0530

    fix: formula infinity issue and editeble field

--- src/components/AgGridComponents/NumericEditor.tsx
+++ src/components/AgGridComponents/NumericEditor.tsx
@@ -5,6 +5,9 @@ import React, {
     useRef,
     useState,
 } from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 const KEY_BACKSPACE = 8;
 const KEY_DELETE = 46;
@@ -134,6 +137,7 @@ export default forwardRef((props: any, ref) => {
         };
     });
 
+    console.log(props)
     return (<React.Fragment>
         <input
             ref={refInput}
@@ -144,6 +148,12 @@ export default forwardRef((props: any, ref) => {
             //onKeyDown={(event) => onKeyDown(event)}
             style={{ width: '100%' }}
         />
+        <IconButton size="small" color="primary" onClick={() => props.stopEditing()} >
+            <CheckCircleOutlineIcon fontSize="small" />
+        </IconButton>
+        <IconButton size="small" color="primary" onClick={() => props.stopEditing()} >
+            <HighlightOffIcon fontSize="small" />
+        </IconButton>
     </React.Fragment>
     );
 });
