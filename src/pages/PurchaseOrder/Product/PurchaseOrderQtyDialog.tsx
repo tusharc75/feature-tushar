e2a3b31f7572ebf642a0f3f0d0c34f6cbae6910b
Jhon Rajbongshi <jhonrajbongshi12345@gmail.com>
2021-12-01 13:05:28 +0530

    fix: pO-352

--- src/pages/PurchaseOrder/Product/PurchaseOrderQtyDialog.tsx
+++ src/pages/PurchaseOrder/Product/PurchaseOrderQtyDialog.tsx
@@ -40,9 +40,10 @@ interface PurchaseOrderQtyDialogProps {
   currency: string;
   onSubmit: VoidFunction | any;
   productData?: object | any;
+  bulkEdit?: boolean | any;
 }
 
-const PurchaseOrderQtyDialog: FC<PurchaseOrderQtyDialogProps> = ({ onClose, currency, onSubmit, productData }) => {
+const PurchaseOrderQtyDialog: FC<PurchaseOrderQtyDialogProps> = ({ onClose, currency, onSubmit, productData, bulkEdit }) => {
 
   const [initialData, setInitialData] = useState({ fields: [], values: {} });
   const [fields, setFields] = useState([]);
@@ -55,7 +56,7 @@ const PurchaseOrderQtyDialog: FC<PurchaseOrderQtyDialogProps> = ({ onClose, curr
       //Update Unit As Product Start
       poFields.filter((_f) => {
         if (["unit", "umo"].includes(_f.fieldName.toLowerCase())) {
-          if (productData?.productDetail?.unit || productData?.productDetail?.umo) {
+          if (!bulkEdit && (productData?.productDetail?.unit || productData?.productDetail?.umo)) {
             let unitOption = productData?.productDetail?.unit || productData?.productDetail?.umo
             if (unitOption) {
               let newUnitOptions = unitOption?.map((item, index) => {
@@ -73,7 +74,7 @@ const PurchaseOrderQtyDialog: FC<PurchaseOrderQtyDialogProps> = ({ onClose, curr
       //End
       setInitialData({
         fields: poFields,
-        values: getObjKeysWithValues(productData, poFields),
+        values: getObjKeysWithValues(!bulkEdit ? productData : productData[0], poFields),
       });
       EvaluteproductFields(poFields);
     })
@@ -91,7 +92,12 @@ const PurchaseOrderQtyDialog: FC<PurchaseOrderQtyDialogProps> = ({ onClose, curr
 
   const handleSubmit = (values) => {
     let returnData = []
-    returnData = [{ ...values, _id: productData._id, productId: productData.productId }]
+    if (bulkEdit) {
+      returnData = productData.map(d => { return ({ ...values, _id: d._id || d.productDetail._id, productId: d.productId }) })
+    }
+    else {
+      returnData = [{ ...values, _id: productData._id || productData.productDetail._id, productId: productData.productId }]
+    }
     onSubmit(returnData)
   };
 
@@ -118,7 +124,7 @@ const PurchaseOrderQtyDialog: FC<PurchaseOrderQtyDialogProps> = ({ onClose, curr
         }) => (
           <Fragment>
             <CustomDialogHeader
-              title={"Edit"}
+              title={bulkEdit ? "Edit" : `Edit ${productData.productName || ""}`}
               onClose={() => {
                 onClose()
               }}
