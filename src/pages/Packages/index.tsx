70eb65305f50d9de4f25a2e491770ce0a610a4b8
Pranav Chandra <pranavchandra27@gmail.com>
2021-12-01 17:18:10 +0530

    fix(loadingticket): date issue

--- src/pages/Packages/index.tsx
+++ src/pages/Packages/index.tsx
@@ -575,7 +575,7 @@ const PackageList = () => {
                     {singlePackageDelete.show ? (
                         <ConfirmationDialog
                             open={singlePackageDelete.show}
-                            message={`Are you sure you want to delete this Package?`}
+                            message={`Are you sure you want to delete this Package: ${deleteRecord?.packageName} ?`}
                             onClose={() =>
                                 setSinglePackageDelete({
                                     id: null,
