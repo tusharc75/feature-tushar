6ff3cb15da07d2663599f522f65a8755bf3e613a
Pranav Chandra <pranavchandra27@gmail.com>
2021-12-02 20:28:31 +0530

    feat(transferasset): fix and changes
    Changes in transfer asset
    
    PO-428

--- src/pages/TransferAssets/ManageTransferAsset.tsx
+++ src/pages/TransferAssets/ManageTransferAsset.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, Fragment, useContext } from 'react';
+import { useState, useEffect, Fragment, useContext, useRef, FC } from 'react';
 import Button from '@material-ui/core/Button';
 import { Formik, Form } from 'formik';
 import CustomDialogHeader from '../../components/CustomDialog/CustomDialogHeader';
@@ -17,12 +17,25 @@ import { Box, Grid } from '@material-ui/core';
 import FormTypes from '../../components/Helpers/FormTypes';
 import ConfirmCancelDialog from '../../components/ConfirmCancelDialog';
 
-const ManageTransferAsset = ({ isClone = false, transferAssetId = null, onClose, onSuccess, number = "" }) => {
+interface Props {
+  isClone?: boolean;
+  transferAssetId?: any;
+  onClose?: any;
+  onSuccess?: any;
+  number?: string;
+  isEditable?: boolean;
+  isMainInfoEditable?: boolean;
+}
+
+const ManageTransferAsset: FC<Props> = (props) => {
+  const { isClone = false, transferAssetId = null, onClose, onSuccess, number = "", isEditable = false, isMainInfoEditable = false } = props
   const toastConfig = useContext(CustomToastContext);
+  const initialRender = useRef(true)
   const [isSubmitting, setSubmitting] = useState(false);
   const [initialData, setInitialData] = useState({ fields: [], values: {} });
   const [allFields, setAllFields] = useState([]);
   const [showConfirmDialog, setShowConfirmDialog] = useState(false);
+  const [formValues, setFormValues] = useState(null)
 
   const [fullScreen, setFullScreen] = useState(isMobile || isTablet);
 
@@ -66,6 +79,49 @@ const ManageTransferAsset = ({ isClone = false, transferAssetId = null, onClose,
         toastConfig.setToastConfig(error);
       });
   }, [transferAssetId]);
+
+
+  useEffect(() => {
+    if (initialRender.current) {
+      initialRender.current = false
+    } else {
+      let fields = initialData.fields;
+
+      if (fields.length > 0) {
+        fields = fields.map(field => {
+
+          const sectionFields = field.sectionFields.map(_f => {
+            if (formValues && formValues.transferType === "Internal") {
+              if (_f.fieldName === "transferToPlant" || _f.fieldName === "plantShipTo") {
+                _f.required = true
+              }
+            }
+            if (formValues && formValues.transferType === "External Customer") {
+              if (_f.fieldName === "transferToCustomer" || _f.fieldName === "customerShipTo") {
+                _f.required = true
+              }
+
+            }
+            if (formValues && formValues.transferType === "External Supplier") {
+              if (_f.fieldName === "transferToSupplier" || _f.fieldName === "supplierShipTo") {
+                _f.required = true
+              }
+            }
+            return _f
+          })
+          return {
+            ...field,
+            sectionFields
+          }
+
+        })
+      }
+
+      setInitialData({ ...initialData, fields })
+
+    }
+  }, [formValues])
+
 
   const handleSubmit = (values) => {
     setSubmitting(true);
@@ -117,8 +173,8 @@ const ManageTransferAsset = ({ isClone = false, transferAssetId = null, onClose,
       fullWidth
     >
       {initialData.fields.length ? (
-        <Formik initialValues={initialData.values} validationSchema={yupSchema(allFields)} onSubmit={handleSubmit}>
-          {({ values, errors, touched, setFieldValue, submitForm, setFieldError }) => (
+        <Formik innerRef={(ref) => { if (ref) { setFormValues(ref.values) } }} initialValues={initialData.values} validationSchema={yupSchema(allFields)} onSubmit={handleSubmit}>
+          {({ values, errors, touched, setFieldValue, submitForm, setErrors }) => (
             <Fragment>
               <CustomDialogHeader
                 title={transferAssetId ? (isClone ? 'Clone' : `Update ${RESOURCE_LABEL.transferAsset} (${number})`) : 'Create ' + RESOURCE_LABEL.transferAsset}
@@ -141,13 +197,13 @@ const ManageTransferAsset = ({ isClone = false, transferAssetId = null, onClose,
                         <Box marginY={2}>
                           <Grid spacing={3} container>
                             {form.sectionFields.map((field, index2) =>
-                              field.fieldName === 'transferToPlant' ? (
-                                values?.transferType.includes('Internal') ? (
+                              field.fieldName === 'transferToPlant' || field.fieldName === 'plantShipTo' ? (
+                                values?.transferType.includes('Internal') && (
                                   <Grid key={index2} item xs={12} sm={6} md={6}>
                                     <FormTypes
                                       isNew={Boolean(transferAssetId)}
                                       {...field}
-                                      disabled={Boolean(transferAssetId) && field.disableOnEdit}
+                                      disabled={Boolean(transferAssetId) && (isMainInfoEditable || field.disableOnEdit)}
                                       values={values}
                                       errors={{ ...errors }}
                                       touched={touched}
@@ -166,14 +222,14 @@ const ManageTransferAsset = ({ isClone = false, transferAssetId = null, onClose,
                                       size="small"
                                     />
                                   </Grid>
-                                ) : null
-                              ) : field.fieldName === 'transferToSupplier' ? (
-                                values?.transferType.includes('Supplier') ? (
+                                )
+                              ) : field.fieldName === 'transferToSupplier' || field.fieldName === 'supplierShipTo' ? (
+                                values?.transferType.includes('Supplier') && (
                                   <Grid key={index2} item xs={12} sm={6} md={6}>
                                     <FormTypes
                                       isNew={Boolean(transferAssetId)}
                                       {...field}
-                                      disabled={Boolean(transferAssetId) && field.disableOnEdit}
+                                      disabled={Boolean(transferAssetId) && (isMainInfoEditable || field.disableOnEdit)}
                                       values={values}
                                       errors={errors}
                                       touched={touched}
@@ -192,14 +248,14 @@ const ManageTransferAsset = ({ isClone = false, transferAssetId = null, onClose,
                                       size="small"
                                     />
                                   </Grid>
-                                ) : null
-                              ) : field.fieldName === 'transferToCustomer' ? (
-                                values?.transferType.includes('Customer') ? (
+                                )
+                              ) : field.fieldName === 'transferToCustomer' || field.fieldName === 'customerShipTo' ? (
+                                values?.transferType.includes('Customer') && (
                                   <Grid key={index2} item xs={12} sm={6} md={6}>
                                     <FormTypes
                                       isNew={Boolean(transferAssetId)}
                                       {...field}
-                                      disabled={Boolean(transferAssetId) && field.disableOnEdit}
+                                      disabled={Boolean(transferAssetId) && (isMainInfoEditable || field.disableOnEdit)}
                                       values={values}
                                       errors={{ ...errors }}
                                       touched={touched}
@@ -218,13 +274,13 @@ const ManageTransferAsset = ({ isClone = false, transferAssetId = null, onClose,
                                       size="small"
                                     />
                                   </Grid>
-                                ) : null
+                                )
                               ) : field.fieldName === 'transferType' ? (
                                 <Grid key={index2} item xs={12} sm={6} md={6}>
                                   <FormTypes
                                     isNew={Boolean(transferAssetId)}
                                     {...field}
-                                    disabled={Boolean(transferAssetId)}
+                                    disabled={Boolean(transferAssetId) && (isMainInfoEditable || field.disableOnEdit)}
                                     values={values}
                                     errors={errors}
                                     touched={touched}
@@ -238,14 +294,38 @@ const ManageTransferAsset = ({ isClone = false, transferAssetId = null, onClose,
                                     tooltipMessage={field?.tooltipMessage}
                                     size="small"
                                     onChange={(e, val) => {
-                                      if (val?.optionLabel.includes('Supplier')) {
-                                        setFieldError("transferToSupplier", 'Transfer To Supplier is required');
-                                      } else if (val?.optionLabel.includes('Internal')) {
-                                        setFieldError("transferToPlant", 'Transfer To Plant is required');
+                                      if (val && val.optionLabel === 'External Supplier') {
+                                        setErrors({ ...errors, "transferToSupplier": 'Transfer To Supplier is required' });
+                                      } else if (val && val.optionLabel === 'Internal') {
+                                        setErrors({ ...errors, "transferToPlant": 'Transfer To Plant is required' });
                                       } else {
-                                        setFieldError("transferToCustomer", 'Transfer To Customer is required');
+                                        setErrors({ ...errors, "transferToCustomer": 'Transfer To Customer is required' });
                                       }
                                       setFieldValue(field.fieldName, val?.optionValue ?? "");
+                                    }}
+                                  />
+                                </Grid>
+                              ) : field.fieldName === 'transferFromPlant' ? (
+                                <Grid key={index2} item xs={12} sm={6} md={6}>
+                                  <FormTypes
+                                    isNew={Boolean(transferAssetId)}
+                                    {...field}
+                                    disabled={Boolean(transferAssetId) && (isEditable || field.disableOnEdit)}
+                                    values={values}
+                                    errors={errors}
+                                    touched={touched}
+                                    label={field.fieldLabel}
+                                    name={field.fieldName}
+                                    type={field.type}
+                                    options={field.option}
+                                    required={field.required}
+                                    fullWidth
+                                    isTooltip={field?.isTooltip || false}
+                                    tooltipMessage={field?.tooltipMessage}
+                                    size="small"
+                                    setFieldValue={(name, value) => {
+                                      // handleValuesChange({ [name]: value })
+                                      setFieldValue(name, value);
                                     }}
                                   />
                                 </Grid>
@@ -254,7 +334,7 @@ const ManageTransferAsset = ({ isClone = false, transferAssetId = null, onClose,
                                   <FormTypes
                                     isNew={Boolean(transferAssetId)}
                                     {...field}
-                                    disabled={Boolean(transferAssetId) && field.disableOnEdit}
+                                    disabled={Boolean(transferAssetId) && field.disableOnEdit || (field.fieldName === "status" && true)}
                                     values={values}
                                     errors={errors}
                                     touched={touched}
