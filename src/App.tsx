5e3a4e67976bc087770b58956bee758ae4a402d8
Gagandeep Singh <gagandeep@vebholic.com>
2021-12-08 11:15:30 +0530

    fix(azure instance): bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -185,22 +185,6 @@ function App() {
     }
   }, [mappedEntities]);
 
-  useEffect(() => {
-    (async () => {
-      try {
-        axiosInstance()
-          .get(`user/meta-grid/${user?._id}`)
-          .then(({ data: { data } }) => {
-            let tempMetaData = JSON.stringify(data?.gridMetaData);
-            localStorage.setItem('gridMetaData', tempMetaData);
-            dispatch({ type: SET_GRID_METADATA, payload: data?.gridMetaData });
-          });
-      } catch (e) {
-        console.log(e);
-      }
-    })();
-  }, [user]);
-
   const getNotification = async () => {
     if (localStorage.getItem('token')) {
       await axiosInstance()
