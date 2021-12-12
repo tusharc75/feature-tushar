4908607bd7db5c0d799d764b832846ba6ba50311
unknown <romit@vebholic.com>
2021-12-12 21:14:54 +0530

    fix: rental bug fix

--- src/pages/RentalManagement/Invoice/index.tsx
+++ src/pages/RentalManagement/Invoice/index.tsx
@@ -1,28 +1,244 @@
-import { useState, useEffect, useContext, useMemo, Fragment } from "react";
-import Box from "@material-ui/core/Box/Box";
-import CommonSkeleton from "../../../components/Helpers/CommonSkeleton";
-import NoDataCell from "../../../components/Helpers/NoDataCell";
-import routes from "../../../components/Helpers/Routes";
-import Grid from "@material-ui/core/Grid/Grid";
-import { Button, Chip, IconButton } from "@material-ui/core";
-import { Delete } from "@material-ui/icons";
-import axiosInstance from "../../../axios/axiosInstance";
-import { CustomToastContext } from "../../../StateProvider/CustomToastContext/CustomToastContext";
-import { dateFormat, formatAmountWithCurrency, rentalManagement, sidebarResource, treeToFlatArray, generateUniqueId } from "../../../constants/helpers";
-import moment from "moment";
-import ConfirmationDialog from "../../../components/Helpers/ConfirmationDialog";
-import CustomReactTable from "../../../components/CustomReactTable/CustomReactTable";
-import ManagePurchaseOrder from "../../PurchaseOrder/ManagePurchaseOrder";
-import { CURReplaceByCurrencySingle } from "../../../constants/formulaUtility";
 
-const Invoice = ({ rentalManagementData, isTabletScreen, isSmallScreen, setNextStep, showActivity, currencySymbol }) => {
+import Box from "@material-ui/core/Box/Box";
+import React, { useState, useEffect, useReducer, useContext } from "react";
+import CommonSkeleton from "../../../components/Helpers/CommonSkeleton";
+import CustomAgGrid, { intialState, reducer } from "../../../components/AgGridComponents/CustomAgGrid";
+import { CommonRenderer, DateRenderer, } from "../../../components/AgGridComponents/CustomAgGridCellRenderers";
+import Grid from "@material-ui/core/Grid/Grid";
+import { Button, Dialog, IconButton } from "@material-ui/core";
+import { CustomToastContext } from "../../../StateProvider/CustomToastContext/CustomToastContext";
+import { CustomDialogTransition, customerContact, gridLoadingTimeout, purchaseOrder, rentalManagement, sidebarResource } from "../../../constants/helpers";
+import { useData } from "../../../StateProvider/Provider";
+import axiosInstance from "../../../axios/axiosInstance";
+import { CreateEmail } from "../../../components/Activity/Email/CreateEmail";
+import { isMobile, isTablet } from "react-device-detect";
+import { AiFillFilePdf } from "react-icons/ai";
+import routes from "../../../components/Helpers/Routes";
+import CustomSwipableList from "../../../components/SwipableListComponents/CustomSwipableList";
+import { BiPurchaseTagAlt, MdEmail } from "react-icons/all";
+import { CURReplaceByCurrencySingle } from "../../../constants/formulaUtility";
+import { getColumnData, getStaticFields, getFrameworkComponents, genrateColoum } from "../../../constants/columns"
+import { prepareDataForGrid } from "../../../constants/helpers";
+import CustomAgGridEditable from "../../../components/AgGridComponents/CustomAgGridEditable";
+import { Link } from "react-router-dom";
+import { startCase } from "lodash";
+
+
+const Invoice = ({ rentalManagementData, setNextStep, fetchRentalData }) => {
 
   const toastConfig = useContext(CustomToastContext);
+  const { state: { user, permissions } }: any = useData();
 
+  const [sendEmail, setSendEmail] = useState(false);
+  const [fullScreen, setFullScreen] = useState(isMobile || isTablet);
+  const [userEmails, setUserEmails] = useState({ to: [], cc: [] });
+  const [generatingPdfFile, setGeneratingFile] = useState(false);
 
-  return (<Fragment>
+  const [gridApi, setGridApi] = useState(null);
+  const [state, dispatch] = useReducer(reducer, intialState);
+  const { dataRows, rowCount, loading, page, limit, pageSizes, selectedRecords } = state;
+  const [columns, setColumns] = useState([
+    { field: "type", headerName: "Type", show: true, disabled: true, cellRenderer: "commonRenderer" },
+    { field: "description", headerName: "Description", show: true, disabled: true, cellRenderer: "commonRenderer" }
+  ])
+  const [frameWorkComponent, setFrameWorkComponent] = useState(null)
 
-  </Fragment>
+  const NameRenderer = (params) => (
+    <Link
+      className="link"
+      title={params.value}
+      to={`${routes.productDetail.path}/${params.data.productId}`}
+    >
+      {params.value}
+    </Link>
+  );
+
+  useEffect(() => {
+    axiosInstance().get("/field/child?resource=Rental Management Product").then(({ data: { data } }) => {
+      let fields = CURReplaceByCurrencySingle(data, rentalManagementData.currency)
+      axiosInstance().get("/field/child?resource=Rental Management Cost").then(({ data: { data } }) => {
+        fields = [...fields, ...CURReplaceByCurrencySingle(data, rentalManagementData.currency)]
+        let rendererNames = [];
+        genrateColoum(fields, columns, rendererNames, false);
+        let tempFrameworkComponent = getFrameworkComponents(rendererNames, true)
+        tempFrameworkComponent = {
+          commonRenderer: CommonRenderer,
+          nameRenderer: NameRenderer,
+          ...tempFrameworkComponent,
+        }
+        setFrameWorkComponent({ ...tempFrameworkComponent })
+        setColumns([...columns])
+        fetchData()
+      })
+    })
+  }, []);
+
+  const fetchData = () => {
+    let combinedData: any = []
+    axiosInstance().get(`${rentalManagement.rentalManagementApi}/productpackage/${rentalManagementData._id}`).then(({ data: { data } }) => {
+      data?.material?.forEach((item) => {
+        if (!item.parentId) {
+          item.description = `${item.type === "product" ? item.productDetail?.productName : item.packageDetail?.packageDescription}`
+          item.type = startCase(item.type);
+          combinedData.push(item);
+        }
+      });
+      axiosInstance().get(`${rentalManagement.api}/additionalcost/${rentalManagementData._id}`).then(({ data: { data } }) => {
+        data?.forEach((e) => {
+          e.type = "Service";
+        })
+        combinedData = [...combinedData, ...data];
+        let rows = combinedData?.map((item) => {
+          let res: any = {
+            ...prepareDataForGrid(item),
+          };
+          return res;
+        });
+        dispatch({ type: "initialize", data: rows, count: rows.length });
+        dispatch({ type: "loading", loading: false });
+      }).catch((error) => {
+        toastConfig.setToastConfig(error);
+        dispatch({ type: "loading", loading: false });
+      });
+    }).catch((error) => {
+      dispatch({ type: "loading", loading: false });
+      toastConfig.setToastConfig(error)
+    });
+  };
+
+  const handleGenerateInvoice = () => {
+    axiosInstance().patch(`${rentalManagement.rentalManagementApi}/generate-invoice/${rentalManagementData._id}`, {}).then(() => {
+      toastConfig.setToastConfig({ open: true, type: "success", message: "Invoice Generated Successfully" })
+      fetchRentalData()
+    })
+      .catch((error) => {
+        toastConfig.setToastConfig(error);
+      });
+  };
+
+  const onSendEmailSuccess = () => {
+
+  };
+
+  return (<>
+    <Box display="flex" justifyContent="space-between" m={1}>
+      <Box display="flex" alignItems="center">
+        {permissions?.rentalManagement?.isRead && (
+          <Button
+            variant="outlined"
+            color="primary"
+            type="button"
+            size="small"
+            startIcon={isMobile ? '' : <AiFillFilePdf />}
+            onClick={() => { }}
+          >
+            {isMobile ? <AiFillFilePdf size={22} /> : "Preview"}
+          </Button>
+        )}
+        <Box mx={1} />
+        {permissions?.rentalManagement?.isRead && (
+          <Button
+            variant="outlined"
+            color="primary"
+            type="button"
+            size="small"
+            startIcon={isMobile ? '' : <AiFillFilePdf />}
+            onClick={() => { }}
+          >
+            {isMobile ? <AiFillFilePdf size={22} /> : "Download"}
+          </Button>
+        )}
+        <Box mx={1} />
+        {permissions?.rentalManagement?.isRead && <Button
+          variant={isMobile ? "outlined" : "contained"}
+          color="primary"
+          size="small"
+          onClick={() => {
+            setSendEmail(true)
+          }}
+        >
+          {isMobile ? <MdEmail size={22} /> : `Send Email`}
+        </Button>}
+      </Box>
+      <Box display="flex" justifyContent="flex-end" p="4px">
+        <Box mx={1} />
+        <Button
+          variant={isMobile ? "outlined" : "contained"}
+          color="primary"
+          size="small"
+          disabled={rentalManagementData?.status === "Invoiced"}
+          onClick={handleGenerateInvoice}
+        >
+          {isMobile ? <BiPurchaseTagAlt size={22} /> : `Generate Invoice`}
+        </Button>
+      </Box>
+    </Box>
+    <Grid item xs={12} md={12} sm={12} className="mt-3">
+      {columns && frameWorkComponent ?
+        <CustomAgGridEditable
+          columns={columns}
+          dataRows={dataRows}
+          frameworkComponents={frameWorkComponent}
+          setGridApi={setGridApi}
+          dispatch={dispatch}
+          rowCount={rowCount}
+          limit={limit}
+          pageSizes={pageSizes}
+          page={page}
+          allowAction={false}
+          loading={loading}
+          allowSelection={false}
+          isClientSideGrid={true}
+          renderedFrom="rentalManagmentInvoicePage"
+          refreshGrid={fetchData}
+          fromPurchaseOrderGrid={true}
+          onCellValueChanged={(row) => {
+          }}
+          currency={rentalManagementData?.currency?.toLowerCase()}
+        />
+        : <Box p={2} height={500} bgcolor="white"><CommonSkeleton lenArray={[...Array(10).keys()]} /></Box>
+      }
+    </Grid>
+    {sendEmail && (
+      <Dialog
+        open={sendEmail}
+        fullScreen={fullScreen || isMobile || isTablet}
+        TransitionComponent={CustomDialogTransition}
+        aria-labelledby="customized-dialog-title"
+        maxWidth="md"
+        onClose={() => {
+          setSendEmail(false);
+          setFullScreen(false);
+        }}
+        fullWidth
+      >
+        <CreateEmail
+          generatingFile={generatingPdfFile}
+          handleClose={() => {
+            setSendEmail(false);
+            setFullScreen(false);
+          }}
+          fetchData={onSendEmailSuccess}
+          id={rentalManagementData._id}
+          showESign={true}
+          isQuoteBuilder={true}
+          options={userEmails?.to}
+          cc={userEmails?.cc ?? []}
+          emailId={null}
+          qouteBuilderAttachments={[]}
+          subject={`${user?.user?.brandName ?? 'Brand'} Invoice - ${rentalManagementData?.rentalJobName ?? ''}`}
+          fromQuote={true}
+          isMinimized={!fullScreen}
+          onMinimizeMaximize={() => {
+            setFullScreen((prevState) => !prevState);
+          }}
+          showManimizeMaximize={true}
+          fromPurchaseOrder={true}
+        />
+      </Dialog>
+    )}
+  </>
   );
 }
+
 export default Invoice;
