c5ab2afb3f278d01a58729453c7766545f603d22
PRITAM <pritamchatterjee945@gmail.com>
2022-01-01 17:07:15 +0530

    fix(ProductInventoryDetail):Page break issue fixed

--- src/pages/ProductInventory/ProductInventoryDetailsPage.tsx
+++ src/pages/ProductInventory/ProductInventoryDetailsPage.tsx
@@ -130,7 +130,7 @@ const ProductInventoryDetailsPage = () => {
                 <Link className="link" title={params.value} to={`${routes.transferAssetDetail.path}/${params.data.referenceId}`}>
                   {params.value}
                 </Link>
-                :  params.data.type.lowerCase().includes("purchase") ? 
+                :  params.data.type.toLowerCase().includes("purchase") ? 
                 <Link className="link" title={params.value} to={`${routes.purchaseOrderDetail.path}/${params.data.referenceId}`}>
                 {params.value}
               </Link>
