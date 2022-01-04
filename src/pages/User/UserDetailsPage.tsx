5b6c9000b22076a7ce5fde7d2d000bc615ed8947
Jujar Singh <jujar@vebholic.com>
2022-01-04 13:49:46 +0530

    style: pO-479 changes in project sales and many more

--- src/pages/User/UserDetailsPage.tsx
+++ src/pages/User/UserDetailsPage.tsx
@@ -1193,7 +1193,7 @@ const UserDetailsPage = () => {
                 <IoIosArrowDropleft className="icon" />
               </span>} */}
 
-            <Paper className="fixedRightPanel">
+            <Paper className={`${!isSmallScreen ? 'fixedRightPanel' : null}`}>
               {!isSmallScreen && <span className={`${showActivity ? 'activityHide' : 'activityShow'} cursor-pointer`} onClick={handleActivityHideShow}>
                 {showActivity ? <IoIosArrowDropright className="icon" /> : <IoIosArrowDropleft className="icon" />}
               </span>}
