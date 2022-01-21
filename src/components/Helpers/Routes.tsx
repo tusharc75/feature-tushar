c4c4d8fbc70240d926d23083eb752a2d8b526a1e
PRITAM <pritamchatterjee945@gmail.com>
2022-01-21 19:53:54 +0530

    feat(Project Sales): Name changes to Target Project List
    OAT-515

--- src/components/Helpers/Routes.tsx
+++ src/components/Helpers/Routes.tsx
@@ -130,11 +130,11 @@ const routes = {
     path: "/profile",
   },
   projectSales: {
-    title: storedRoutes ? storedRoutes.projectStrategy?.title : RESOURCE_LABEL.projectStrategy,
+    title: RESOURCE_LABEL.projectStrategy,
     path: "/project-sales",
   },
   projectSalesDetail: {
-    title: storedRoutes ? storedRoutes.projectStrategy?.title : RESOURCE_LABEL.projectStrategy,
+    title: RESOURCE_LABEL.projectStrategy,
     path: "/project-sales/detail",
   },
   priceTemplate: {
