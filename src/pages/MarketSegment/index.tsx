a92b75de50863a3ce7a858e1ffbb1e1d1fb79d31
sunanda35 <sunandasamanta60@gmail.com>
2022-01-24 15:08:09 +0530

    feat: market segment selected export OAT-519

--- src/pages/MarketSegment/index.tsx
+++ src/pages/MarketSegment/index.tsx
@@ -290,6 +290,14 @@ const MarketSegment = () => {
             afterImportCompleted={() => {
               fetchMarketSegment();
             }}
+            isExportAllOrSomeFeature={true}
+            total={rowCount}
+            recordsToExport={selectedRecords.length}
+            ids={selectedRecords.length ? selectedRecords.map((obj) => obj._id) : []}
+            onExportToExcelSuccess={() => {
+              if (gridApi) gridApi.deselectAll();
+              else fetchMarketSegment();
+            }}
           />
         </Grid>
       </Grid>
