58bb7c9eed4082137a06e165fb71f89cdb1a849b
PRITAM <pritamchatterjee945@gmail.com>
2022-01-31 18:16:59 +0530

    feat(Quote):Pre select Quote Pdf template dropdown
    OAT-580

--- src/pages/QuoteBuilderCombined/ManageQuote/ManageQuoteDialog.tsx
+++ src/pages/QuoteBuilderCombined/ManageQuote/ManageQuoteDialog.tsx
@@ -91,7 +91,6 @@ export default function ManageQuoteDialog({
   const {
     state: { user, selectedEntity, permissions },
   }: any = useData();
-
   const [entityData, setEntityData] = useState({
     fields: [],
     initialValues: {},
@@ -493,6 +492,7 @@ export default function ManageQuoteDialog({
         } else {
           if (isNew) {
             const selectedEntityDetails = user?.entity?.find(d => d?._id === selectedEntity)
+            const defaultQuotePdfTemplateId = user.user?.quotePDFTemplate ?? '';
 
             if (selectedEntityDetails) {
               initialData["currency"] = selectedEntityDetails.currency || "";
@@ -502,6 +502,7 @@ export default function ManageQuoteDialog({
                 )?.symbolNative
               );
             }
+            initialData["pDFTemplate"] = defaultQuotePdfTemplateId;
           }
         }
 
