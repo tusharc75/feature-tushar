d41ea47980afd4a3a1304b500111e8557599c7ff
unknown <romit@vebholic.com>
2022-02-09 18:55:57 +0530

    fix: bug fix

--- src/pages/Activity/Attachments/index.tsx
+++ src/pages/Activity/Attachments/index.tsx
@@ -562,7 +562,6 @@ if(resourceOptions[i] === "Project Sales"){
                     filter={filter}
                     chip={{ size: 'small' }}
                     activityName="attachment"
-                    //permissions={permissions}
                   />
                 </Grid>
 
