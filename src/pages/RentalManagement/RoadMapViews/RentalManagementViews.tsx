6d69703a1f4f07206f40d61eb6d2343bf8f0f888
sunanda35 <sunandasamanta60@gmail.com>
2022-02-10 15:42:09 +0530

    fix: rentalJob views page roadmap little fix PO-1091

--- src/pages/RentalManagement/RoadMapViews/RentalManagementViews.tsx
+++ src/pages/RentalManagement/RoadMapViews/RentalManagementViews.tsx
@@ -23,7 +23,7 @@ const RentalManagementViews = (props) => {
           className: 'dark-node',
           sourcePosition: 'right',
           data: { label: rentalName ?? rentalName },
-          position: { x: 0, y: 20 }
+          position: { x: 0, y: 70 }
         }
       ];
       var flowEdge: any[] = [];
@@ -34,14 +34,23 @@ const RentalManagementViews = (props) => {
           sourcePosition: 'right',
           targetPosition: 'left',
           type: 'default',
-          data: { label: item.productDetail.productName },
-          position: { x: 300, y: index * 20 }
+          data: {
+            label: (
+              <>
+                {item.productDetail.productName}
+                <br />
+                type: {item.type}
+              </>
+            )
+          },
+          position: { x: 300, y: index * 80 }
         });
         flowEdge.push({
           id: `edge-${item._id}`,
           source: `${rentalId}`,
           arrowHeadType: 'arrow',
-          target: `${item._id}`
+          target: `${item._id}`,
+          animated: true
         });
       });
       response?.data?.data?.inventory?.map((item: any, index) => {
@@ -51,14 +60,14 @@ const RentalManagementViews = (props) => {
           targetPosition: 'left',
           type: 'default',
           data: { label: item.inventoryDetail.assetNumber },
-          position: { x: 600, y: index * 40 }
+          position: { x: 600, y: index * 80 }
         });
         flowEdge.push({
           id: `edge-${item.inventoryDetail.assetNumber}`,
           source: `${item._id}`,
           arrowHeadType: 'arrow',
-          target: `${item.inventoryDetail.assetNumber}`
-          //   animated: true
+          target: `${item.inventoryDetail.assetNumber}`,
+          animated: true
         });
       });
       const loadingTicketData = await axiosInstance().get(
@@ -70,8 +79,18 @@ const RentalManagementViews = (props) => {
           sourcePosition: 'right',
           targetPosition: 'left',
           type: 'default',
-          data: { label: item.ticketName },
-          position: { x: 900, y: index * 40 }
+          data: {
+            label: (
+              <div style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                {item.ticketName}
+                <br />
+                type: {item.ticketType} Ticket
+                <br />
+                status: {item.status}
+              </div>
+            )
+          },
+          position: { x: 900, y: index * 80 }
         });
 
         item.productInventory?.map((product: any, productIndex) => {
@@ -81,21 +100,21 @@ const RentalManagementViews = (props) => {
             targetPosition: 'left',
             type: 'default',
             data: { label: product.optionLabel },
-            position: { x: 1200, y: productIndex * 40 }
+            position: { x: 1200, y: productIndex * 80 }
           });
           flowEdge.push({
             id: `edge-${item._id}`,
             source: `${product.optionLabel}`,
             arrowHeadType: 'arrow',
-            target: `${item._id}`
-            //   animated: true
+            target: `${item._id}`,
+            animated: true
           });
           flowEdge.push({
             id: `edge-${product.optionValue}`,
             source: `${item._id}`,
             arrowHeadType: 'arrow',
-            target: `${product.optionValue}`
-            //   animated: true
+            target: `${product.optionValue}`,
+            animated: true
           });
         });
       });
@@ -108,16 +127,26 @@ const RentalManagementViews = (props) => {
           sourcePosition: 'right',
           targetPosition: 'left',
           type: 'output',
-          data: { label: item.ticketName },
-          position: { x: 1400, y: index * 40 }
+          data: {
+            label: (
+              <div style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                {item.ticketName}
+                <br />
+                type: {item.ticketType} Ticket
+                <br />
+                status: {item.status}
+              </div>
+            )
+          },
+          position: { x: 1400, y: index * 80 }
         });
         item.productInventory?.map((product: any) => {
           flowEdge.push({
             id: `edge-${product.optionValue}`,
             source: `${product.optionValue}`,
             arrowHeadType: 'arrow',
-            target: `${item._id}`
-            //   animated: true
+            target: `${item._id}`,
+            animated: true
           });
         });
       });
@@ -136,118 +165,6 @@ const RentalManagementViews = (props) => {
       ]);
     }
   };
-
-  //   const initialElements: Elements<any> | any = [
-  //     {
-  //       id: 'horizontal-1',
-  //       type: 'input',
-  //       className: 'dark-node',
-  //       sourcePosition: 'right',
-  //       data: { label: rentalName },
-  //       position: { x: 0, y: 80 }
-  //     },
-  //     {
-  //       id: 'horizontal-2',
-  //       sourcePosition: 'right',
-  //       targetPosition: 'left',
-  //       data: { label: 'A Node' },
-  //       position: { x: 250, y: 0 }
-  //     },
-  //     {
-  //       id: 'horizontal-3',
-  //       sourcePosition: 'right',
-  //       targetPosition: 'left',
-  //       data: { label: 'Node 3' },
-  //       position: { x: 250, y: 160 }
-  //     },
-  //     {
-  //       id: 'horizontal-4',
-  //       sourcePosition: 'right',
-  //       targetPosition: 'left',
-  //       data: { label: 'Node 4' },
-  //       position: { x: 500, y: 0 }
-  //     },
-  //     {
-  //       id: 'horizontal-5',
-  //       sourcePosition: 'right',
-  //       targetPosition: 'left',
-  //       data: { label: 'Node 5' },
-  //       position: { x: 500, y: 100 }
-  //     },
-  //     {
-  //       id: 'horizontal-6',
-  //       sourcePosition: 'right',
-  //       targetPosition: 'left',
-  //       data: { label: 'Node 6' },
-  //       position: { x: 500, y: 230 }
-  //     },
-  //     {
-  //       id: 'horizontal-7',
-  //       sourcePosition: 'right',
-  //       targetPosition: 'left',
-  //       data: { label: 'Node 7' },
-  //       position: { x: 750, y: 50 }
-  //     },
-  //     {
-  //       id: 'horizontal-8',
-  //       source: 'right',
-  //       sourcePosition: 'right',
-  //       targetPosition: 'left',
-  //       data: { label: 'Node 8' },
-  //       position: { x: 750, y: 300 }
-  //     },
-  //     {
-  //       id: 'horizontal-e1-2',
-  //       source: 'horizontal-1',
-  //       type: 'default',
-  //       target: 'horizontal-2'
-  //       // animated: true
-  //     },
-  //     {
-  //       id: 'horizontal-e1-3',
-  //       source: 'horizontal-1',
-  //       type: 'default',
-  //       target: 'horizontal-3'
-  //       // animated: true
-  //     },
-  //     {
-  //       id: 'horizontal-e1-4',
-  //       source: 'horizontal-2',
-  //       type: 'default',
-  //       target: 'horizontal-4'
-  //       // label: "edge label"
-  //       // animated: true
-  //     },
-  //     {
-  //       id: 'horizontal-e3-5',
-  //       source: 'horizontal-3',
-  //       type: 'default',
-  //       target: 'horizontal-5'
-  //       // animated: true
-  //     },
-  //     {
-  //       id: 'horizontal-e3-6',
-  //       source: 'horizontal-3',
-  //       type: 'default',
-  //       target: 'horizontal-6'
-  //       // animated: true
-  //     },
-  //     {
-  //       id: 'horizontal-e5-7',
-  //       source: 'horizontal-5',
-  //       type: 'default',
-  //       target: 'horizontal-7'
-  //       // animated: true
-  //     },
-  //     {
-  //       id: 'horizontal-e6-8',
-  //       source: 'horizontal-5',
-  //       type: 'default',
-  //       target: 'horizontal-8'
-  //       // animated: true
-  //     }
-  //   ];
-  // write a style for the node
 
   const onLoad = (reactFlowInstance) => {
     reactFlowInstance.fitView({ padding: 0.25 });
