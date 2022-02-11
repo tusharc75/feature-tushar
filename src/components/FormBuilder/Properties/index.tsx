c025e0e1ae3f46664bbd5b93e1b3c58e0c8118ef
unknown <romit@vebholic.com>
2022-02-11 14:34:57 +0530

    fix: bug fix

--- src/components/FormBuilder/Properties/index.tsx
+++ src/components/FormBuilder/Properties/index.tsx
@@ -69,8 +69,7 @@ const LookupResource = [
   { name: 'Repair Job', value: 'Repair Job' },
   { name: 'Sales Order', value: 'Sales Order' },
   { name: 'Transfer Asset', value: 'Transfer Asset' },
-  { name: 'Address', value: 'Address' },
-  { name: 'Sublease', value: 'Sublease' }
+  { name: 'Address', value: 'Address' }
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export const Properties = ({ module, handleClose, fieldData, sectionId, section, setSection, extraFields, isCalculativeField }) => {
