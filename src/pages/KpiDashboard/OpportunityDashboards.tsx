4db6cdc46082135b61f2008ef35b9b0f378558fd
PRITAM <pritamchatterjee945@gmail.com>
2022-02-11 10:34:49 +0530

    feat(Dashboard):Quotes by Sales rep added
    OAT-621

--- src/pages/KpiDashboard/OpportunityDashboards.tsx
+++ src/pages/KpiDashboard/OpportunityDashboards.tsx
@@ -75,7 +75,7 @@ const OpportunityDashboards = (props) => {
       }
     }
     axiosInstance()
-      .get(`/dashboard/opportunities/sales-rep${url}`)
+      .get(`/dashboard/quote/sales-rep${url}`)
       .then(({ data: { data } }) => {
         const labels = [];
         const datasets = [];
@@ -472,7 +472,7 @@ const OpportunityDashboards = (props) => {
                 Filters
               </Button>
               <Box textAlign="center">
-                <Typography variant="h6">{statusText[opp2Status]} Opportunities by Sales Rep</Typography>
+                <Typography variant="h6">{statusText[opp2Status]} Quotes by Sales Rep</Typography>
                 {oppSalesRep.labels.length > 0
                   ? <Chart type="pie" data={oppSalesRep} />
                   : <Box minHeight={515}>
