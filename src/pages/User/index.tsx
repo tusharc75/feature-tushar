898954c1f98f86cc798303ff4d4bdd5598a33d92
Avisekh999 <avisekhgurung8578@gmail.comgit config --global user.email avisekhgurung8578@gmail.com>
2022-02-16 17:41:26 +0530

    feat(zone): zone
    zone

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -178,7 +178,7 @@ const User: FC = () => {
       </p>
     ) : (
       <>
-        <Tooltip
+        {/* <Tooltip
           className={permissions.user.isCreate ? "" : "cursor-stop"}
           title={permissions.user.isCreate ? "Clone" : "You do not have permission to clone/create"} >
           <IconButton
@@ -190,7 +190,7 @@ const User: FC = () => {
           >
             <FileCopyIcon fontSize="small" color="primary" />
           </IconButton>
-        </Tooltip>
+        </Tooltip> */}
         {permissions.user.isDelete ? (
 
           params.data.isBrandAdmin ? (
