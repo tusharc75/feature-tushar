2722c3db42fc5f8cadcf1b4b9f2c09557ff3647d
Avisekh999 <avisekhgurung8578@gmail.comgit config --global user.email avisekhgurung8578@gmail.com>
2022-02-19 02:31:11 +0530

    feat(feat/avi): feat/avi
    feat/avi

--- src/pages/zone/CreateZip.tsx
+++ src/pages/zone/CreateZip.tsx
@@ -9,6 +9,7 @@ import axiosInstance from '../../axios/axiosInstance';
 import { CustomToastContext } from '../../StateProvider/CustomToastContext/CustomToastContext';
 import CustomButton from '../../components/Helpers/CustomButton';
 import routes from '../../components/Helpers/Routes';
+import {TextField} from "@material-ui/core";
 import { isMobile, isTablet } from 'react-device-detect';
 import { CustomDialogTransition, isFieldNotTouched } from './../../constants/helpers';
 import InputField from '../../components/Helpers/InputField';
@@ -27,57 +28,13 @@ const CreateZip = (props) => {
   const [fullScreen, setFullScreen] = useState(isMobile || isTablet);
   const [saveClick, setSaveClick] = useState(false);
   const [cloneHeading, setCloneHeading] = useState('')
+  const [value , setValue] = useState('')
 
-  useEffect(() => {
-    axiosInstance()
-      .get('/field?resource=Zone')
-      .then(({ data: { data } }) => {
-        const fieldsDataForCreate = data.filter((obj) => obj.isCreate).map((d: any) => d.fieldData);
-        const fieldsDataForUpdate = data.filter((obj) => obj.isUpdate).map((d: any) => d.fieldData);
 
-        if (zoneId) {
-          axiosInstance()
-            .get(`/zone/` + zoneId + `/zip`)
-            .then(({ data: { data } }) => {
-              let tempOptionArray = fieldsDataForUpdate.find((d) => d.fieldName === 'name').option;
-              fieldsDataForUpdate.find((d) => d.fieldName === 'name').option = tempOptionArray.filter(
-                (data) => data.optionValue !== zoneId
-              );
-              const { name, ...rest } = data;
-              setCloneHeading(name)
-              if (isClone) {
-                setInitialData({
-                  fields: fieldsDataForUpdate,
-                  values: getObjKeysWithValues(data, fieldsDataForUpdate)
-                });
-                setFormValues(getObjKeysWithValues(data, fieldsDataForUpdate));
-              } else {
-                setInitialData({
-                  fields: fieldsDataForUpdate,
-                  values: getObjKeysWithValues(data, fieldsDataForUpdate)
-                });
-                setFormValues(getObjKeysWithValues(data, fieldsDataForUpdate));
-              }
-            })
-            .catch((error) => {
-              toastConfig.setToastConfig(error);
-            });
-        } else {
-          setInitialData({
-            fields: fieldsDataForCreate,
-            values: getObjKeys('', fieldsDataForCreate)
-          });
-          setFormValues(getObjKeys('', fieldsDataForCreate));
-        }
-      })
-      .catch((error) => {
-        toastConfig.setToastConfig(error);
-      });
-  }, [zoneId]);
+  
+  const handleSubmit = () => {
 
-  const handleSubmit = (values) => {
-
-    let newValues = { zoneZips: [values.name] };
+    let newValues = { zoneZips: [value] };
     setSaveClick(true);
     axiosInstance()
       .post(`/zone/${zoneId}/zip`, newValues)
@@ -97,12 +54,7 @@ const CreateZip = (props) => {
       });
   };
 
-  const handleValuesChange = (data) => {
-    setFormValues((prevState) => ({
-      ...prevState,
-      ...data
-    }));
-  };
+
 
   return (
     <Dialog
@@ -118,110 +70,53 @@ const CreateZip = (props) => {
       }}
       fullWidth
     >
-      {initialData && initialData.fields.length ? (
-        <Formik
-          enableReinitialize={true}
-          initialValues={initialData.values}
-          validationSchema={yupSchema(initialData.fields)}
-          validateOnMount
-          onSubmit={handleSubmit}
-        >
-          {({ values, errors, touched, setFieldValue, submitForm }) => (
-            <Fragment>
-              <CustomDialogHeader
-                title={
-                  isClone
-                    ? `Clone - ${cloneHeading}`
-                    : zoneId
-                      ? !isUpdateDisabled
-                        ? 'Create ZipCode'
-                        : values['zipCode']
-                      : 'Create ' + 'ZipCode'
-                }
-                onClose={() => {
-                  if (
-                    isFieldNotTouched(
-                      {
-                        initialValues: initialData.values,
-                        fields: initialData.fields
-                      },
-                      values
-                    )
-                  )
-                    onClose();
-                  else setShowConfirmDialog(true);
-                }}
-                isMinimized={!fullScreen}
-                onMinimizeMaximize={() => {
-                  setFullScreen((prevState) => !prevState);
-                }}
-                showManimizeMaximize={true}
-              ></CustomDialogHeader>
-              <CustomDialogContent>
-                <Form autoComplete="off" autoCorrect="off" noValidate>
-                  <InputField
-                    disabled={isUpdateDisabled}
-                    errors={errors}
-                    values={values}
-                    setFieldValue={(name, value) => {
-                      handleValuesChange({ [name]: value });
-                      setFieldValue(name, value);
-                    }}
-                    touched={touched}
-                    fieldsData={initialData.fields}
-                    size="small"
-                    fullWidth
-                  />
-                </Form>
-              </CustomDialogContent>
-              <CustomDialogFooter>
+      <CustomDialogHeader
+        title={
+            `Create Zip Code`
+        }
+        onClose={() => {
+         onClose();
+        }}
+        isMinimized={!fullScreen}
+        onMinimizeMaximize={() => {
+          setFullScreen((prevState) => !prevState);
+        }}
+        showManimizeMaximize={true}
+        ></CustomDialogHeader>
+      <CustomDialogContent>
+        <TextField 
+        variant='outlined'
+        label='Zip Code'
+        name='Zip Code'
+        value={value}
+        onChange= {(e) => {
+          setValue(e.target.value)
+         
+        }}
+
+        />
+      </CustomDialogContent>
+         <CustomDialogFooter>
                 <Button
                   size="small"
                   color="primary"
                   onClick={() => {
-                    if (
-                      isFieldNotTouched(
-                        {
-                          initialValues: initialData.values,
-                          fields: initialData.fields
-                        },
-                        values
-                      )
-                    )
-                      onClose();
-                    else setShowConfirmDialog(true);
+                   
+                     
+                    onClose();
                   }}
                 >
                   {isUpdateDisabled ? 'Close' : 'Cancel'}
                 </Button>
                 {!isUpdateDisabled && (
-                  <CustomButton loading={loading} variant="contained" color="primary" type="submit" disabled={saveClick} onClick={submitForm}>
+                  <CustomButton loading={loading} variant="contained" color="primary" type="submit" disabled={saveClick} onClick={handleSubmit}>
                     {' '}
                     Save
                   </CustomButton>
                 )}
               </CustomDialogFooter>
-              {showConfirmDialog ? (
-                <ConfirmCancelDialog
-                  open={showConfirmDialog}
-                  onSave={() => {
-                    setShowConfirmDialog(false);
-                    submitForm();
-                  }}
-                  onClose={() => {
-                    setShowConfirmDialog(false);
-                    onClose();
-                  }}
-                />
-              ) : null}
-            </Fragment>
-          )}
-        </Formik>
-      ) : (
-        <Box p={2} height={500} bgcolor="white">
-          <CommonSkeleton lenArray={[...Array(10).keys()]} />
-        </Box>
-      )}
+              
+     
     </Dialog>
   );
 };
