058e7b07723376bd389f1f8905dc20f73aa811c6
Gagandeep Singh <gagandeep@vebholic.com>
2022-03-02 13:47:14 +0530

    fix(App.tsx): Change flow for hard refresh

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -260,7 +260,7 @@ const Login = () => {
                       </Box>
                       <Box width={260} className="mt-2">
                         <Button disabled={isSubmitting} fullWidth variant="contained" color="secondary" type="submit" onClick={submitForm}>
-                          {isSubmitting ? <CircularProgress size={22} /> : 'Login'}
+                          {isSubmitting ? <CircularProgress size={22} /> : 'Loginnnn'}
                         </Button>
 
                         <Box className="mt-2">
