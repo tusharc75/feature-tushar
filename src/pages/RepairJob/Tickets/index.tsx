6c751a71c89ea71bef3d1e146eb4a9d5e9dbb249
sunanda35 <sunandasamanta60@gmail.com>
2022-03-02 16:09:33 +0530

    fix: repairJob Ticket page access from direct url giving error issue fixed

--- src/pages/RepairJob/Tickets/index.tsx
+++ src/pages/RepairJob/Tickets/index.tsx
@@ -4,13 +4,7 @@ import TypewiseTickets from '../../../components/DeliveryTicket/TypewiseTickets'
 const renderedFrom = 'repairJobTickets';
 
 const Tickets = ({ repairJobData }) => {
-    return (
-        <TypewiseTickets
-            refrenceType={DELIVERY_TICKET_REFRENCE_TYPE.repairJob}
-            refrenceId={repairJobData._id}
-            renderedFrom={renderedFrom}
-        />
-    );
+  return <TypewiseTickets refrenceType={DELIVERY_TICKET_REFRENCE_TYPE.repairJob} refrenceId={repairJobData?._id} renderedFrom={renderedFrom} />;
 };
 
 export default Tickets;
