a7e41880c2085c59ec8d035ebfe24dac706db927
Jhon Rajbongshi <jhonrajbongshi12345@gmail.com>
2022-03-11 14:25:23 +0530

    feat: react table in invoice of rental job

--- src/pages/RentalManagement/RentalManagementDetailsPage.tsx
+++ src/pages/RentalManagement/RentalManagementDetailsPage.tsx
@@ -483,8 +483,13 @@ const RentalManagementDetailsPage = () => {
                         setNextStep={setNextStep}
                         fetchRentalData={fetchRentalManagementData}
                         updateJobStatus={updateJobStatus}
+                        isSmallScreen={isSmallScreen}
+                        isTabletScreen={isTabletScreen}
                         statusOptions={statusOptions}
                         renderedFrom={`${renderedFrom}_grid-5`}
+                        showActivity={showActivity}
+                        currencySymbol={currencySymbol}
+                        stepFullScreen={stepFullScreen}
                       />
                     )}
                   </ContentFullScreen>
