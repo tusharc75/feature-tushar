2333cb4902d189b7af4d30233d388128e6311b23
Punit Soni <punit.soni33@gmail.com>
2022-03-15 18:55:55 +0530

    feat: - Build failed issue fixed

--- src/pages/RentalManagement/ReceivingTicket/newindex.tsx
+++ src/pages/RentalManagement/ReceivingTicket/newindex.tsx
@@ -615,6 +615,8 @@ const ReceivingTicket = ({ currentStep, rentalManagementData, fetchRentalData, s
             if ([INVENTORY_STATUS.scrap, INVENTORY_STATUS.lost].includes(rowData?.status)) return "error";
             return "";
           }}
+          renderedFrom="rental_management_receiving_ticket"
+          isClientSideGrid={true}
         />
         : <Box p={2} height={500} bgcolor="white"><CommonSkeleton lenArray={[...Array(10).keys()]} /></Box>
       }
