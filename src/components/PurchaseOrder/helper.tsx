9da9bae2b4e7faea7e3bc8141974234d829f9322
unknown <romit@vebholic.com>
2022-03-16 13:26:06 +0530

    fix: bug fix

---
+++ src/components/PurchaseOrder/helper.tsx
@@ -0,0 +1,39 @@
+
+import { CHILD_RESOURCE } from '../../constants/helpers';
+import axiosInstance from '../../axios/axiosInstance';
+import { CURReplaceByCurrencySingle } from '../../constants/formulaUtility';
+
+export const fetch_po_product_fields = async (currency) => {
+    var data;
+    const response = await axiosInstance().get(`/field/child?resource=${CHILD_RESOURCE.purchaseOrderProduct}`);
+    data = response?.data?.data;
+    data = CURReplaceByCurrencySingle(data, currency);
+    var isRateRequired = false
+    data.forEach(element => {
+        if (element.fieldName === "price" && element.required) {
+            isRateRequired = true;
+        }
+    });
+    if (!isRateRequired) {
+        data = data.filter((e) => e.sectionName !== "Pricing Information")
+    }
+    return data;
+}
+
+export const fetch_po_service_fields = async (currency) => {
+    var data;
+    const response = await axiosInstance().get(`/field/child?resource=${CHILD_RESOURCE.purchaseOrderService}`);
+    data = response?.data?.data;
+    data = CURReplaceByCurrencySingle(data, currency);
+    var isRateRequired = false
+    data.forEach(element => {
+        if (element.fieldName === "price" && element.required) {
+            isRateRequired = true;
+        }
+    });
+    if (!isRateRequired) {
+        data = data.filter((e) => e.sectionName !== "Pricing Information")
+    }
+    return data;
+}
+
