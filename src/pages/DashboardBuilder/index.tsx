fac258249bcfe3d88eae1bc5dc77edbdf5ab5d25
pc <pranavchandra27@gmail.com>
2022-03-16 12:36:47 +0530

    fix(dashboard): dashboard bulder ui start

--- src/pages/DashboardBuilder/index.tsx
+++ src/pages/DashboardBuilder/index.tsx
@@ -1,42 +1,22 @@
 import React from 'react';
-import { Grid, Box, Divider, Paper, ThemeOptions, IconButton } from '@material-ui/core';
-import { Delete, Edit } from '@material-ui/icons';
+import { Grid, Box, Divider, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { MdDashboardCustomize } from 'react-icons/md';
 
 import CustomBreadCrumbs from 'src/components/CustomBreadCrumbs';
 import Builder from './Builder';
-import { FormData } from './builderHelpers';
+import { IFormDataType } from './builderHelpers';
+import View from './View';
 
-const useClasses = makeStyles((theme: ThemeOptions) => ({
+const useClasses = makeStyles(() => ({
   root: {
     height: 'calc(80vh + 20px)'
-  },
-  chartViews: {
-    overflowY: 'auto',
-    height: 'calc(80vh + 10px)'
-  },
-  paper: {
-    padding: '16px',
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    width: '100%',
-    height: '200px',
-
-    '&:hover': {
-      icons: {
-        display: 'block'
-      }
-    }
-  },
-  icons: {
-    display: 'none'
   }
 }));
 
 const DashboardBuilder = () => {
   const classes = useClasses();
-  const [formData, setFormData] = React.useState([]);
+  const [formData, setFormData] = React.useState<IFormDataType[]>([]);
 
   return (
     <div>
@@ -46,14 +26,17 @@ const DashboardBuilder = () => {
         </Grid>
       </Grid>
       <div className="main-container">
-        <div className="header-panel">
-          <Box display={'flex'} alignItems="center" py={"6px"}>
+        <Box className="header-panel" display="flex" py={'6px'} justifyContent="space-between">
+          <Box display={'flex'} alignItems="center" >
             <MdDashboardCustomize size={22} className="headerLogo" />
             <Box ml={1}>
               <span className="listingHeader">Dashboard Builder</span>
             </Box>
           </Box>
-        </div>
+          <Box py={'6px'}>
+            <Button color='primary' variant='contained' size='small' disableRipple>Save</Button>
+          </Box>
+        </Box>
         <Divider />
         <Box bgcolor="#f5f5f5" p={1}>
           <Grid container spacing={2} className={classes.root}>
@@ -61,25 +44,7 @@ const DashboardBuilder = () => {
               <Builder formData={formData} setFormData={setFormData} />
             </Grid>
             <Grid item xs={12} sm={8}>
-              <Grid container spacing={1} className={classes.chartViews}>
-                {formData.length > 0 &&
-                  formData.map((form: FormData, index) => (
-                    <Grid item xs={form.column} key={form.chartTitle + ' ' + index}>
-                      <Paper className={classes.paper}>
-                        <p>{form.chartTitle}</p>
-                        <p>col = {form.column}</p>
-                        <Box mt={2} className={classes.icons}>
-                          <IconButton size="small">
-                            <Edit />
-                          </IconButton>
-                          <IconButton size="small">
-                            <Delete />
-                          </IconButton>
-                        </Box>
-                      </Paper>
-                    </Grid>
-                  ))}
-              </Grid>
+              <View formData={formData} setFormData={setFormData} />
             </Grid>
           </Grid>
         </Box>
