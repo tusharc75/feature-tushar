ebf619e54caa533830acba9251bc5397615c51f2
Punit Soni <punit.soni33@gmail.com>
2022-03-31 14:57:49 +0530

    feat: - Api calls stopped on offline mode

--- src/axios/axiosInstance.tsx
+++ src/axios/axiosInstance.tsx
@@ -67,67 +67,69 @@ export default (history = null, passedHeaders = null) => {
         new Promise((resolve, reject) => {
             resolve(response);
         }), (error) => {
-            if (error.request.responseType === 'blob' && error.response.data.type.toLowerCase().indexOf('json') != -1) {
-                return new Promise(async (resolve, reject) => {
-                    const bufferArray = await error.response.data.text()
-                    const err = JSON.parse(bufferArray);
-                    reject({ open: true, type: "error", message: err.error });
-                })
-            }
-            if (error.request.responseType === "arraybuffer") {
-                const enc = new TextDecoder("utf-8");
-                const data = enc.decode(error.response.data);
-                const err = JSON.parse(data);
-                return new Promise((resolve, reject) => reject({ open: true, type: "error", message: err.error, }));
-            }
-
-            // if (error.message == "Network Error") {
-            //     if (navigator.onLine) {
-            //         return new Promise((resolve, reject) => {
-            //             reject({ open: true, type: "error", message: "Api Not Working" });
-            //         })
-            //     }
-            // }
-
-            if (!error.response) {
-                return new Promise((resolve, reject) => {
-                    reject({ open: true, type: "error", message: error.response.data.error });
-                })
-            }
-
-            if (error.response.data && error.response.data.code && Object.values(ERROR_CODE).some(s => s === error.response.data.code)) {
-                if (window.confirm((`${error.response.data.error}\n\nPress Ok to redirect to home\nPress Cancel to stay here`))) {
-                    localStorage.removeItem("selectedEntity");
-                    //@ts-ignore
-                    window.location = "/";
-                }
-            } else if (error.response.status === 511) {
-                localStorage.clear();
-                //@ts-ignore
-                window.location = "/";
-            } else if (error.response.data && error.response.data.code && error.response.data.code === "1005") {
-                return new Promise((resolve, reject) => {
-                    reject({ open: true, type: "notFoundError", message: "" });
-                });
-            }
-            else {
-                if (error.response.status === 401) {
-                    clearTokenAndRedirectToHome();
-                    return new Promise((resolve, reject) => {
-                        reject({ open: true, type: "error", message: error.response.data.error || error.response.data.message });
-                    });
-
-                }
-                else if (error.response.status === 511) {
-                    clearTokenAndRedirectToHome();
-                }
-                else {
-                    return new Promise((resolve, reject) => {
-                        reject({ open: true, type: "error", message: error.response.data.error || error.response.data.message, data: error.response.data });
+            if (navigator.onLine) {
+                
+                if (error.request.responseType === 'blob' && error.response.data.type.toLowerCase().indexOf('json') != -1) {
+                    return new Promise(async (resolve, reject) => {
+                        const bufferArray = await error.response.data.text()
+                        const err = JSON.parse(bufferArray);
+                        reject({ open: true, type: "error", message: err.error });
                     })
                 }
-            }
+                if (error.request.responseType === "arraybuffer") {
+                    const enc = new TextDecoder("utf-8");
+                    const data = enc.decode(error.response.data);
+                    const err = JSON.parse(data);
+                    return new Promise((resolve, reject) => reject({ open: true, type: "error", message: err.error, }));
+                }
 
+                // if (error.message == "Network Error") {
+                //     if (navigator.onLine) {
+                //         return new Promise((resolve, reject) => {
+                //             reject({ open: true, type: "error", message: "Api Not Working" });
+                //         })
+                //     }
+                // }
+
+                if (!error.response) {
+                    return new Promise((resolve, reject) => {
+                        reject({ open: true, type: "error", message: error.response.data.error });
+                    })
+                }
+
+                if (error.response.data && error.response.data.code && Object.values(ERROR_CODE).some(s => s === error.response.data.code)) {
+                    if (window.confirm((`${error.response.data.error}\n\nPress Ok to redirect to home\nPress Cancel to stay here`))) {
+                        localStorage.removeItem("selectedEntity");
+                        //@ts-ignore
+                        window.location = "/";
+                    }
+                } else if (error.response.status === 511) {
+                    localStorage.clear();
+                    //@ts-ignore
+                    window.location = "/";
+                } else if (error.response.data && error.response.data.code && error.response.data.code === "1005") {
+                    return new Promise((resolve, reject) => {
+                        reject({ open: true, type: "notFoundError", message: "" });
+                    });
+                }
+                else {
+                    if (error.response.status === 401) {
+                        clearTokenAndRedirectToHome();
+                        return new Promise((resolve, reject) => {
+                            reject({ open: true, type: "error", message: error.response.data.error || error.response.data.message });
+                        });
+
+                    }
+                    else if (error.response.status === 511) {
+                        clearTokenAndRedirectToHome();
+                    }
+                    else {
+                        return new Promise((resolve, reject) => {
+                            reject({ open: true, type: "error", message: error.response.data.error || error.response.data.message, data: error.response.data });
+                        })
+                    }
+                }
+            }
             // reject(error);
         }
     );
