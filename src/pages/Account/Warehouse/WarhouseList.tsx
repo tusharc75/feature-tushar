e1e14af56acb85b73152a8b67d90d01df0b3996c
Pranav <pranavchandra27@gmail.com>
2022-04-11 16:38:13 +0530

    fix(warehouselist): remove console

--- src/pages/Account/Warehouse/WarhouseList.tsx
+++ src/pages/Account/Warehouse/WarhouseList.tsx
@@ -56,7 +56,6 @@ const WarhouseList = ({api, isCustomer=false, addWarehouse, onClose, isAddingWar
         }
         const queryString = getQueryString();
         axiosInstance().get(`${api}${queryString}`).then(({ data: { data, count } }) => {
-            console.log(data)
             let rows = data.map((u) => {
                 let finalObject = prepareDataForGrid(isCustomer ? u?.warehouseDetail : u);
                 finalObject["isChecked"] = false;
