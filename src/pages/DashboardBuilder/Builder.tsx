9add3e0d7b295e51b2558b1359d8946a38d74323
Pranav <pranavchandra27@gmail.com>
2022-05-27 15:26:43 +0530

    fix(dashboard master): fix

--- src/pages/DashboardBuilder/Builder.tsx
+++ src/pages/DashboardBuilder/Builder.tsx
@@ -43,9 +43,9 @@ const Builder = (props: Props) => {
   }, [selectedData]);
 
   React.useEffect(() => {
-    if (!formValues.kpi && !formValues.filters.map((k) => k.key).includes('status')) return;
+    if (!formValues.kpi?.kpi && !formValues.filters.map((k) => k.key).includes('status')) return;
 
-    setFormValues((prevState) => ({ ...prevState, statusOptions: statuses[formValues.kpi.kpi] }));
+    setFormValues((prevState) => ({ ...prevState, statusOptions: statuses[formValues.kpi?.kpi] }));
   }, [formValues.kpi, formValues.filters]);
 
   const fetchKpis = () => {
