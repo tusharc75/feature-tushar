56c069027d626506777462e4315a79d04cff3911
unknown <romit@vebholic.com>
2022-06-03 14:18:20 +0530

    fix: bug fix

--- src/pages/ProductInventory/AddRemove/index.tsx
+++ src/pages/ProductInventory/AddRemove/index.tsx
@@ -36,7 +36,7 @@ const AddRemove = ({ handleClose, handleSuccess, product, type, warehouse }) => 
 
     const fetchData = () => {
         setLoadingData(true)
-        axiosInstance().get(`${productInventory.api}/serial-number/${product[0]._id}/${warehouse}`)
+        axiosInstance().get(`${productInventory.api}/serial-number/${product[0]._id}?warehouse=${warehouse}`)
             .then(({ data: { data } }) => {
                 if (!data || data.lenght === 0) return
                 setSerialNumbers(data)
