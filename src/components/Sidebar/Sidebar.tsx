35c8f41a88fb770f591b33b77dbab234c8e50098
dhrubobarman <dhrubobarman847@gmail.com>
2022-06-06 12:55:24 +0530

    fix: fix style issue for iphones and mobile devices

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -25,73 +25,8 @@ import { AiOutlineDatabase, AiOutlineFileText } from 'react-icons/ai';
 import { FaRegUser } from 'react-icons/fa';
 
 import { AccountCircle } from '@material-ui/icons';
+import useStyles from './style';
 import routes from '../Helpers/Routes';
-const drawerWidth = 240;
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex'
-  },
-
-  hide: { display: 'none' },
-
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap'
-  },
-  drawerOpen: {
-    overflowY: 'auto',
-
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  drawerClose: {
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    width: '48px',
-    [theme.breakpoints.down('sm')]: {
-      width: 0,
-      borderRight: 'none !important'
-    }
-  },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0),
-    borderBottom: '2px solid #f5f8f9'
-  },
-  menuIcon: {
-    width: 22
-  },
-  drawerIcon: {
-    width: 18
-  },
-  heading: {
-    fontWeight: 'normal',
-    marginLeft: theme.spacing(2)
-  },
-  nested: {
-    paddingLeft: theme.spacing(4)
-  },
-  sidebarUser: {
-    padding: '1.5rem 1rem 1rem',
-    background: '#fff',
-    color: '#153d77',
-    textAlign: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    height: '8rem'
-  }
-}));
 
 function SideBar({ toggleDrawer, setToggleDrawer, location }) {
   const [itemToAddActiveClass, setItemToAddActiveClass] = useState(NaN);
