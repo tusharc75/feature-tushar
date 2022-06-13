cb3b40e856a253e506cc308ce51f909207abac0b
Pranav <pranavchandra27@gmail.com>
2022-06-13 13:31:47 +0530

    fix(po): fix

--- src/pages/PurchaseOrder/ReceivingAsset/CreateSerializedAsset.tsx
+++ src/pages/PurchaseOrder/ReceivingAsset/CreateSerializedAsset.tsx
@@ -198,7 +198,7 @@ const CreateSerializedAsset = ({ purchaseOrderID, onClose, onSuccess, title, pro
                                                             <div>
                                                                 {(values.seriaizedAsset.map((data, index) => (
                                                                     <Box key={index} border={'1px solid #dddddd'} borderRadius={4} mb={2} p={2}  pt={0}>
-                                                                       
+                                                                       {data?.serializedProduct &&
                                                                         <Box my={1} display="flex" justifyContent="flex-end">
                                                                             <Box mr={2}>
                                                                                 <Typography className="link cursor-pointer" style={{ color: 'var(--primary)' }} onClick={() => handleExportField(data)}>
@@ -213,7 +213,7 @@ const CreateSerializedAsset = ({ purchaseOrderID, onClose, onSuccess, title, pro
                                                                                 </Typography>
                                                                                 </label>
                                                                             </Box>
-                                                                        </Box>
+                                                                        </Box>}
                                                                         
                                                                         <Grid
                                                                             container
