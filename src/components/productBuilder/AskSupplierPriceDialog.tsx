8c425300ff71fd59fe03ddb131f27aeb279bc6be
Jhon Rajbongshi <jhonrajbongshi12345@gmail.com>
2022-07-06 16:36:02 +0530

    fix: ask supplier price changes

--- src/components/productBuilder/AskSupplierPriceDialog.tsx
+++ src/components/productBuilder/AskSupplierPriceDialog.tsx
@@ -210,7 +210,7 @@ const AskSupplierPriceDialog = (props) => {
                 open={askSupplierPriceDialog}
                 disableBackdropClick={true}
             >
-                <CustomDialogHeader title={from != "SupplierAskPrice" ? "Reject Supplier Price" : "Ask Supplier Price"} onClose={() => {
+                <CustomDialogHeader title={from != "SupplierAskPrice" ? "Ask Supplier Price" : "Reject Supplier Price"} onClose={() => {
                     setAskSupplierPriceDialog(false)
                 }}
                     isMinimized={!fullScreen}
