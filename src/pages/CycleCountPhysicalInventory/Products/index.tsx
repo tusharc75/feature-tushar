1fb84b3043832bb91c0c68275b9398ee73b519ac
unknown <romit@vebholic.com>
2022-07-13 14:34:48 +0530

    fix: bug fix

---
+++ src/pages/CycleCountPhysicalInventory/Products/index.tsx
@@ -0,0 +1,133 @@
+import React, { useContext, useState } from 'react';
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Dialog,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField
+} from '@material-ui/core';
+import CustomDialogContent from 'src/components/CustomDialog/CustomDialogContent';
+import CustomDialogHeader from 'src/components/CustomDialog/CustomDialogHeader';
+import { cycleCountPhysicalInventory } from 'src/constants/helpers';
+import axiosInstance from 'src/axios/axiosInstance';
+import { CustomToastContext } from 'src/StateProvider/CustomToastContext/CustomToastContext';
+import CustomDialogFooter from 'src/components/CustomDialog/CustomDialogFooter';
+
+const Products = ({ handleClose, handleSucess, _id, products, warehouse }) => {
+
+  const toastConfig = useContext(CustomToastContext);
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [tableData, setTableData] = useState([...products]);
+
+  const handleSubmit = async (e) => {
+    const data = {
+      _id: _id,
+      products: tableData?.map((d) => {
+        return {
+          ...d,
+          product: d?.product?.optionValue
+        };
+      })
+    };
+    setIsSubmitting(true);
+    axiosInstance()
+      .put(`${cycleCountPhysicalInventory.api}`, data)
+      .then(({ data }) => {
+        setIsSubmitting(false);
+        toastConfig.setToastConfig({
+          open: true,
+          type: 'success',
+          message: data.message
+        });
+        handleSucess();
+      })
+      .catch((err) => {
+        setIsSubmitting(false);
+        toastConfig.setToastConfig(err);
+      });
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, d: any) => {
+    const { name, value } = e.target;
+    setTableData((prevState) =>
+      prevState.map((data: any) => {
+        if (d?.product?.optionValue === data?.product?.optionValue) {
+          data['qty'] = parseInt(value) == NaN ? 0 : parseInt(value);
+        }
+        return data;
+      })
+    );
+  };
+
+  return (<Dialog
+    open={true}
+    onClose={handleClose}
+    fullScreen>
+    <CustomDialogHeader
+      title={`Enter Physical Inventory - ${warehouse}`}
+      onClose={handleClose}
+      showRequiredLabel={false}
+    />
+    <CustomDialogContent>
+      <Box display="flex" flexDirection="column">
+        <TableContainer component={Paper}>
+          <Table aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <TableCell>#</TableCell>
+                <TableCell align="left">Product</TableCell>
+                <TableCell align="left">Qty</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {tableData.map((data: any, idx) => (
+                <TableRow key={data.id}>
+                  <TableCell component="th" scope="row">
+                    {idx + 1}
+                  </TableCell>
+                  <TableCell align="left">{data?.product['optionLabel']}</TableCell>
+                  <TableCell align="left">
+                    <TextField
+                      size="small"
+                      variant="outlined"
+                      placeholder="Qty"
+                      value={data['qty']}
+                      autoComplete="off"
+                      name="Qty"
+                      type={'number'}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e, data)}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </CustomDialogContent>
+    <CustomDialogFooter>
+      <Button
+        type="submit"
+        variant="contained"
+        size="small"
+        color="primary"
+        endIcon={isSubmitting && <CircularProgress size={18} />}
+        onClick={handleSubmit}
+        disabled={tableData?.find((d) => d.qty === null)}
+      >
+        Save
+      </Button>
+    </CustomDialogFooter>
+  </Dialog>
+  );
+};
+
+export default Products;
--- src/pages/CycleCountPhysicalInventory/index.tsx
+++ src/pages/CycleCountPhysicalInventory/index.tsx
@@ -1,6 +1,5 @@
-import { Grid, IconButton } from '@material-ui/core';
-import DynamicFeedIcon from '@material-ui/icons/DynamicFeed';
 import React, { Fragment, useEffect, useReducer, useState } from 'react';
+import { Grid, IconButton } from '@material-ui/core';
 import { FaRecycle } from 'react-icons/fa';
 import axiosInstance from 'src/axios/axiosInstance';
 import CustomAgGrid, { intialState, reducer } from 'src/components/AgGridComponents/CustomAgGrid';
@@ -8,97 +7,118 @@ import CustomBreadCrumbs from 'src/components/CustomBreadCrumbs';
 import CustomContainer from 'src/components/CustomContainer';
 import routes from 'src/components/Helpers/Routes';
 import { cycleCountPhysicalInventory } from 'src/constants/helpers';
-import UpdateQuantity from './UpdateQty';
+import Products from './Products';
+import { prepareDataForGrid } from 'src/constants/helpers';
+import { gridLoadingTimeout, getLocalStorageArrayData } from '../../constants/helpers';
+import { camelCase } from 'lodash';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import HtmlTooltip from 'src/components/CustomTooltipTitle';
+import { getStaticFields, staticFrameworkRender } from '../../constants/useColumns';
 
-export default function CycleCountPInventory() {
+const CycleCountPInventory = () => {
+
+  const renderedFrom = camelCase(`${routes.cycleCountPhysicalInventory.title}`);
+  const localStorageSelectedRecords = `${renderedFrom}_selected`;
+
   const [state, dispatch] = useReducer(reducer, intialState);
   const [gridApi, setGridApi] = useState(null);
-  const [frameWorkComponent, setFrameWorkComponent] = useState({});
-  const [updateQtyDialog, setUpdateQtyDialog] = useState(false);
-  const [refId, setRefId] = useState('');
-  const [products, setProducts] = useState([]);
 
-  const [dataRows, setDataRows] = useState([]);
-  const [rowCount, setRowCount] = useState(0);
+  const [productDialog, setProductDialog] = useState({ open: false, _id: "", products: [], warehouse: "" });
+  const { loading, page, limit, pageSizes, rowCount, dataRows, search, filters, sorting, selectedRecords, appendRows } = state;
 
-  const { loading, page, limit, pageSizes, search, filters, sorting, selectedRecords, appendRows } = state;
+  // useEffect(() => {
+  //   axiosInstance().post(cycleCountPhysicalInventory.api, {
+  //     warehouse: "620dd7e82e39b606d4945dd5",
+  //     productCategory: "61c04af226046a1c809136a3",
+  //     inventoryCycle: "62cd24c922cd4949f864b526",
+  //   })
+  //     .then((response) => {
+  //     })
+  //     .catch((error) => {
+  //       dispatch({ type: 'loading', loading: false });
+  //     });
+  // }, []);
+
   useEffect(() => {
-    fetchCycleCountDetermination();
-  }, [intialState]);
+    fetchData();
+  }, []);
 
-  const fetchCycleCountDetermination = async () => {
+  const fetchData = async () => {
     dispatch({ type: 'loading', loading: true });
-    axiosInstance()
-      .get(cycleCountPhysicalInventory.api)
-      .then((response) => {
-        setDataRows(response.data.data);
-        setRowCount(response.data.data.length);
-        dispatch({ type: 'loading', loading: false });
+    axiosInstance().get(cycleCountPhysicalInventory.api)
+      .then(({ data: { data } }) => {
+        let rows = data?.map((u) => {
+          let finalObject = prepareDataForGrid(u);
+          return {
+            ...finalObject
+          };
+        });
+        dispatch({
+          type: 'initialize',
+          data: rows,
+          count: rows?.length,
+          selectedRecords: rows.filter((f) => f.isChecked === true)
+        });
+        dispatch({ type: 'initialize', data: rows, count: rows?.length });
+        setTimeout(() => {
+          dispatch({ type: 'loading', loading: false });
+        }, gridLoadingTimeout);
       })
       .catch((error) => {
         dispatch({ type: 'loading', loading: false });
       });
   };
+
   const columns = [
     {
-      field: 'cycleCode',
+      field: 'inventoryCycle',
       headerName: 'Cycle Code',
-      width: 200,
-      cellRenderer: 'NameRenderer'
+      cellRenderer: 'commonRenderer'
     },
     {
-      field: 'productCategory.optionLabel',
+      field: 'productCategory',
       headerName: 'Product Category',
-      width: 200,
-      cellRenderer: 'NameRenderer'
+      cellRenderer: 'commonRenderer'
     },
     {
-      field: 'warehouse.optionLabel',
+      field: 'warehouse',
       headerName: 'Warehouse',
-      width: 200,
-      cellRenderer: 'NameRenderer'
+      cellRenderer: 'commonRenderer'
     },
     {
-      field: 'user.optionLabel',
+      field: 'status',
+      headerName: 'Status',
+      cellRenderer: 'commonRenderer'
+    },
+    {
+      field: 'user',
       headerName: 'User',
-      width: 200,
-      cellRenderer: 'NameRenderer'
+      cellRenderer: 'commonRenderer'
     },
-    {
-      field: 'brand.optionLabel',
-      headerName: 'Brand',
-      width: 200,
-      cellRenderer: 'NameRenderer'
-    },
-    {
-      field: 'brand',
-      headerName: 'Action',
-      width: 200,
-      cellRenderer: 'actionRenderer'
-    }
+    ...getStaticFields()
   ];
 
-  const ActionRenderer = (params) => {
+  const ActionsRenderer = (params) => {
     return (
-      <>
+      <HtmlTooltip title="Products">
         <IconButton
           size="small"
           color="inherit"
           onClick={() => {
-            setUpdateQtyDialog(true);
-            setRefId(params?.data?._id || '');
-            setProducts(params?.data?.products || []);
-            console.log(updateQtyDialog);
+            setProductDialog({ open: true, _id: params?.data?._id, products: params?.data?.products, warehouse: params?.data?.warehouse })
           }}
         >
-          <DynamicFeedIcon color="secondary" fontSize="small" />
+          <VisibilityIcon color="secondary" fontSize="small" />
         </IconButton>
-      </>
+      </HtmlTooltip>
     );
   };
+
   const frameworkComponents = {
-    actionRenderer: ActionRenderer
+    actionsRenderer: ActionsRenderer,
+    ...staticFrameworkRender
   };
+
   return (
     <Fragment>
       <Grid container className="headerbox">
@@ -128,17 +148,31 @@ export default function CycleCountPInventory() {
           limit={limit}
           pageSizes={pageSizes}
           page={page}
-          allowAction={false}
+          allowAction={true}
           actionWidth={150}
           loading={loading}
           allowSelection={false}
-          //   renderedFrom={leadResource}
-          refreshGrid={fetchCycleCountDetermination}
+          renderedFrom={renderedFrom}
+          refreshGrid={fetchData}
         />
-        {updateQtyDialog && (
-          <UpdateQuantity closeDialog={() => setUpdateQtyDialog(false)} openDialog={updateQtyDialog} refId={refId} products={products} />
+        {productDialog.open && (
+          <Products
+            _id={productDialog._id}
+            handleClose={() => {
+              setProductDialog({ open: false, _id: "", products: [], warehouse: "" })
+            }}
+            handleSucess={() => {
+              fetchData()
+              setProductDialog({ open: false, _id: "", products: [], warehouse: "" })
+            }}
+            products={productDialog.products}
+            warehouse={productDialog.warehouse}
+          />
         )}
       </CustomContainer>
     </Fragment>
   );
 }
+
+export default CycleCountPInventory;
+
