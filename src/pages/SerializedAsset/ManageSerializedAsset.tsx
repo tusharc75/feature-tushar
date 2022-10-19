d879695480bf2b4c9c11d48b58408e45ea166494
unknown <romit@vebholic.com>
2022-10-19 13:22:57 +0530

    fix: bug fix

--- src/pages/SerializedAsset/ManageSerializedAsset.tsx
+++ src/pages/SerializedAsset/ManageSerializedAsset.tsx
@@ -348,7 +348,7 @@ const ManageSerializedAsset = ({ isClone = false, productInventoryId = null, onC
                                           <FormTypes
                                             isNew={Boolean(productInventoryId)}
                                             {...field}
-                                            disabled={Boolean(productInventoryId) && field.disableOnEdit && !isClone && field.isUneditable}
+                                            disabled={Boolean(productInventoryId) && !isClone ? field.disableOnEdit || field.isUneditable : field.isUneditable}
                                             fieldData={field}
                                             values={values}
                                             errors={errors}
@@ -377,10 +377,10 @@ const ManageSerializedAsset = ({ isClone = false, productInventoryId = null, onC
                                                 onClick={() => {
                                                   setPlantsOpen({ open: true, isClone: false });
                                                 }}
-                                                disabled={Boolean(productInventoryId) && field.disableOnEdit && !isClone && field.isUneditable}
+                                                disabled={Boolean(productInventoryId) && !isClone ? field.disableOnEdit || field.isUneditable : field.isUneditable}
                                                 size="small"
                                               >
-                                                <AddIcon color={Boolean(productInventoryId) && field.disableOnEdit && !isClone ? 'disabled' : 'primary'} />
+                                                <AddIcon color={Boolean(productInventoryId) && !isClone ? field.disableOnEdit || field.isUneditable ? 'disabled' : 'primary' : field.isUneditable ? 'disabled' : 'primary'} />
                                               </IconButton>
                                             </Tooltip>
                                           </Box>}
