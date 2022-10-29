807d039c596118b0dcbfff52e26b048ce21c5bbd
unknown <romit@vebholic.com>
2022-10-29 13:58:41 +0530

    fix: bug fix

--- src/constants/helpers.tsx
+++ src/constants/helpers.tsx
@@ -75,7 +75,7 @@ export const rentalManagementSteps = [
 export const transferInventorySteps = ['Add Products', 'Serialized Assets', 'Loading Ticket'];
 export const subleaseSteps = ['Add Products', 'Start Sublease', 'End Sublease'];
 export const bulkAssetCreationSteps = ['Add Products', 'Serialized Asset'];
-export const repairOrderSteps = ['Add Products', 'Work Order', 'Quotation', 'Invoice'];
+export const repairOrderSteps = ['Add Assets', 'Work Order', 'Quotation', 'Invoice'];
 
 export const accountTemplateFileName = 'Accounts-Template.xlsx';
 export const accountImportErrorFileName = 'Accounts-Errors.xlsx';
