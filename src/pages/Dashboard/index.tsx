bb65709d35084b6595a5d644e610cd8ef3e0a116
dhrubobarman <dhrubobarman847@gmail.com>
2022-11-03 18:35:04 +0530

    fix: added check for Product category in sidebar

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,10 +27,13 @@ function Dashboard() {
   const [sections, setSections] = useState([]);
   const [search, setSearch] = useState('');
   const [filteredData, setFilteredData] = useState([]);
+  const [availableResources, setAvailableResources] = useState([null]);
+  console.log(availableResources);
 
   useEffect(() => {
     let arr = [];
     let allData = [];
+
     // let allData = user && [...user?.role.sideBar];
     let entityData;
     if (user?.entity && user.entity.length) {
@@ -46,6 +49,7 @@ function Dashboard() {
       u['resourceLabelLowerCase'] = u.resourceLabel?.toLowerCase() ?? u.name?.toLowerCase();
       !arr.includes(u.sectionName) && arr.push(u.sectionName);
     });
+    setAvailableResources(allData);
 
     var data = arr.map((sec) => {
       const list = allData?.filter((u) => {
@@ -280,7 +284,9 @@ function Dashboard() {
           </div>
           <div className={styles.links}>
             <Link to={'/product'}>Product Master</Link>
-            <Link to={'/product-category'}>Product Categories</Link>
+            {availableResources?.findIndex((item) => item?.isRead && !item?.isHidden && item.name === 'Product Category') !== -1 && (
+              <Link to={'/product-category'}>Product Categories</Link>
+            )}
           </div>
         </div>
       </div>
