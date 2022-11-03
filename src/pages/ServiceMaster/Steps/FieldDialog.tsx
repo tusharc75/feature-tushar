c931c2d5385d213829ded5e1116115d29560f822
Pranav <pranavchandra27@gmail.com>
2022-11-03 17:16:39 +0530

    fix(workorder): changes add step

--- src/pages/ServiceMaster/Steps/FieldDialog.tsx
+++ src/pages/ServiceMaster/Steps/FieldDialog.tsx
@@ -11,7 +11,7 @@ import { FormBuilder } from '../../../components/FormBuilder';
 import { uniq, map } from 'lodash';
 import { CustomDialogTransition } from '../../../constants/helpers';
 
-const FieldDialog = ({ handleClose, handleSucess, serviceId, steps, stepIds }) => {
+const FieldDialog = ({ handleClose, handleSucess, serviceId, steps, stepIds, reference='' }) => {
 
   const toastConfig = React.useContext(CustomToastContext);
   const [isSubmitting, setSubmitting] = React.useState(false);
@@ -57,6 +57,10 @@ const FieldDialog = ({ handleClose, handleSucess, serviceId, steps, stepIds }) =
         data.push(_field_data);
       });
     });
+    if(reference === "workOrder") {
+      handleSucess(data)
+      return
+    }
     axiosInstance().post(`${serviceMaster.api}/fields/${serviceId}`, { stepIds: stepIds, fields: data }).then(({ data }) => {
       handleSucess()
       toastConfig.setToastConfig({
