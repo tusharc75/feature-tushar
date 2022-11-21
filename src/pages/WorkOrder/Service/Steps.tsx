b8b7d6fbce5f095ae42868b2225c432908a26ea5
dhrubobarman <dhrubobarman847@gmail.com>
2022-11-21 12:45:32 +0530

    feat: added time in steps

--- src/pages/WorkOrder/Service/Steps.tsx
+++ src/pages/WorkOrder/Service/Steps.tsx
@@ -24,6 +24,7 @@ import ConfirmationDialog from 'src/components/Helpers/ConfirmationDialog';
 import CommonSkeleton from 'src/components/Helpers/CommonSkeleton';
 import { isEqual } from 'lodash';
 import StepFieldsDialog from './StepFieldsDialog';
+import moment from 'moment';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -388,6 +389,7 @@ const Service = ({
         <div className={classes.root}>
           {serviceDetails?.steps?.map((step, index) => {
             const { stepData, isStepValid } = getFields(step);
+
             return (
               <Box
                 key={step._id}
@@ -436,7 +438,19 @@ const Service = ({
                       </Typography>
                     </Box>
                   </Box>
-                  <Box sx={{ justifyContent: 'flex-end', paddingLeft: '10px', paddingTop: '10px', marginLeft: 'auto' }}>
+                  <Box sx={{ justifyContent: 'flex-end', paddingLeft: '10px', paddingTop: '10px', marginLeft: 'auto', display: 'flex' }}>
+                    {stepData?.status === 'start' && (
+                      <div style={{ marginRight: '8px' }}>
+                        <Typography>Started: {moment(stepData?.startDate).fromNow()}</Typography>
+                      </div>
+                    )}
+                    {stepData?.status === 'end' && (
+                      <div>
+                        <Typography>
+                          Duration: {moment.duration(moment(stepData?.endDate).diff(moment(stepData?.startDate), 'seconds'), 'seconds').humanize()}
+                        </Typography>
+                      </div>
+                    )}
                     {!stepData?.status ? (
                       <Box>
                         <Button
