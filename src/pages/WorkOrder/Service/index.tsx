c799ab9c0fd6b42e477d42b268ca0204b143224b
dhrubobarman <dhrubobarman847@gmail.com>
2022-11-24 16:44:27 +0530

    fix: changed prework postwork icons in services

--- src/pages/WorkOrder/Service/index.tsx
+++ src/pages/WorkOrder/Service/index.tsx
@@ -40,6 +40,7 @@ import RotateRightOutlinedIcon from '@material-ui/icons/RotateRightOutlined';
 import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
 import moment from 'moment';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import { PreWorkIcon, PostWorkIcon } from 'src/assets/svg/svgIcons';
 
 function convertMsToTime(milliseconds) {
   milliseconds = Math.abs(milliseconds);
@@ -609,11 +610,15 @@ const Service = ({ workOrderId, allowedToEdit, workOrderData }) => {
                                         <Box ml={1}>
                                           {data?.preWork ? (
                                             <HtmlTooltip title="Pre Work Service">
-                                              <RotateLeftOutlinedIcon fontSize="small" />
+                                              <span>
+                                                <PreWorkIcon style={{ verticalAlign: 'middle' }} />
+                                              </span>
                                             </HtmlTooltip>
                                           ) : (
                                             <HtmlTooltip title="Post Work Service">
-                                              <RotateRightOutlinedIcon fontSize="small" />
+                                              <span>
+                                                <PostWorkIcon style={{ verticalAlign: 'middle' }} />
+                                              </span>
                                             </HtmlTooltip>
                                           )}
                                         </Box>
@@ -748,11 +753,15 @@ const Service = ({ workOrderId, allowedToEdit, workOrderData }) => {
                                     {data?.type === 'service' &&
                                       (data?.preWork ? (
                                         <HtmlTooltip title="Pre Work Service">
-                                          <RotateLeftOutlinedIcon fontSize="small" style={{ width: '15px', height: '15px' }} />
+                                          <span>
+                                            <PreWorkIcon style={{ verticalAlign: 'middle' }} />
+                                          </span>
                                         </HtmlTooltip>
                                       ) : (
                                         <HtmlTooltip title="Post Work Service">
-                                          <RotateRightOutlinedIcon fontSize="small" />
+                                          <span>
+                                            <PostWorkIcon style={{ verticalAlign: 'middle' }} />
+                                          </span>
                                         </HtmlTooltip>
                                       ))}
                                     {data?.type === 'service' && data?.assignedUsers?.length > 0 && (
