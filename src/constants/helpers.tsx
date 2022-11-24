9053dfc6a3116687e0266d1821061f69e39d1f0a
unknown <romit@vebholic.com>
2022-11-24 12:21:51 +0530

    fix: bug fix

--- src/constants/helpers.tsx
+++ src/constants/helpers.tsx
@@ -900,7 +900,7 @@ export const UnCamelCase = (str) => {
     .replace(/([a-z])([A-Z])/g, '$1 $2')
     .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
     .replace(/^./, function (str) {
-      return str.toUpperCase();
+      return str?.toUpperCase();
     });
 };
 
