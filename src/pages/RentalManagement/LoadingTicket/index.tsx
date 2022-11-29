7cdd811d4fdd46a55a934bc17b4fa60a6287d357
Pranav <pranavchandra27@gmail.com>
2022-11-29 17:33:22 +0530

    fix(rental job): tooltip for seperate plant assets

--- src/pages/RentalManagement/LoadingTicket/index.tsx
+++ src/pages/RentalManagement/LoadingTicket/index.tsx
@@ -44,6 +44,7 @@ import { groupBy, uniq, map } from 'lodash';
 import { objectStore, findOne } from '../../../constants/indexdbhelper';
 import HtmlTooltip from '../../../components/CustomTooltipTitle';
 import InfoIcon from '@material-ui/icons/Info';
+import HelpIcon from '@material-ui/icons/HelpOutline';
 import ShowNonSerializeAssets from '../SerializedAsset/ShowNonSerializeAssets';
 import { ExpandMore } from '@material-ui/icons';
 
@@ -292,6 +293,13 @@ const LoadingTicket = ({
       >
         {params.value}
       </Link>
+      {params?.data?.warehouseId && params?.data?.warehouseId !== rentalManagementData?.warehouse?.optionValue && (
+        <HtmlTooltip title="This asset will be shipped separately">
+          <IconButton size="small">
+            <HelpIcon fontSize="small" color="primary" />
+          </IconButton>
+        </HtmlTooltip>
+      )}
       {params?.data?.nonSerializeAsset && params?.data?.nonSerializeAsset?.length > 0 && (
         <Box ml={1}>
           <HtmlTooltip title={`Non-${routes.serializedAsset.title}`}>
@@ -318,12 +326,7 @@ const LoadingTicket = ({
     </Link>
   );
 
-  const ParentNameRenderer = (params) =>
-    params.data?.parentId ? (
-      <span>{params?.data?.parentName}</span>
-    ) : (
-      <NoDataCell />
-    );
+  const ParentNameRenderer = (params) => (params.data?.parentId ? <span>{params?.data?.parentName}</span> : <NoDataCell />);
 
   const frameworkComponents = {
     ticketRenderer: TicketRenderer,
@@ -362,7 +365,7 @@ const LoadingTicket = ({
   const columns = [
     {
       field: 'assetNumber',
-      headerName: "Details",
+      headerName: 'Details',
       show: true,
       disabled: true,
       cellRenderer: 'inventoryRenderer',
@@ -626,7 +629,7 @@ const LoadingTicket = ({
               </Menu>
               <Box mx={1} />
               {selectedRecords.length &&
-                selectedRecords?.filter((f) => f.hasOwnProperty('loadingTicketId') && f?.loadingTicketStatus === DELIVERY_TICKET_STATUS.new)?.length ===
+              selectedRecords?.filter((f) => f.hasOwnProperty('loadingTicketId') && f?.loadingTicketStatus === DELIVERY_TICKET_STATUS.new)?.length ===
                 selectedRecords?.length ? (
                 <Fragment>
                   <Tooltip title="Remove Assets From Loading Ticket(s)">
@@ -704,7 +707,7 @@ const LoadingTicket = ({
               owerCollaboratorInitialsOrImages="owerCollaboratorInitialsOrImages"
               onCreate={false}
               showClone={false}
-              onClone={() => { }}
+              onClone={() => {}}
               renderedFrom={renderedFrom}
             />
           ) : (
