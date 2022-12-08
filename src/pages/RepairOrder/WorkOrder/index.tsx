47e0e32553640500d7ed2cdd9201515ab2479e00
Jhon Rajbongshi <jhonrajbongshi12345@gmail.com>
2022-12-08 12:20:31 +0530

    fix: as-86

--- src/pages/RepairOrder/WorkOrder/index.tsx
+++ src/pages/RepairOrder/WorkOrder/index.tsx
@@ -81,27 +81,25 @@ const WorkOrder = ({
             </Box>
             <Chip
               className="ml-1"
-              label={`${
-                row.original.type === 'service'
+              label={`${row.original.type === 'service'
                   ? 'Service'
                   : row.original.type === 'product'
-                  ? 'Product'
-                  : row.original.type === 'serializedAsset'
-                  ? 'Asset'
-                  : 'Package'
-              }`}
+                    ? 'Product'
+                    : row.original.type === 'serializedAsset'
+                      ? 'Asset'
+                      : 'Package'
+                }`}
               size="small"
               color="primary"
               onClick={() => {
                 window.open(
-                  `${
-                    row.original.type === 'service'
-                      ? routes.serviceMasterDetail.path
-                      : row.original.type === 'product'
+                  `${row.original.type === 'service'
+                    ? routes.serviceMasterDetail.path
+                    : row.original.type === 'product'
                       ? routes.productDetail.path
                       : row.original.type === 'serializedAsset'
-                      ? routes.serializedAsset.path
-                      : routes.packagesDetail.path
+                        ? routes.serializedAsset.path
+                        : routes.packagesDetail.path
                   }/detail/${row.original.materialId}`
                 );
               }}
@@ -148,11 +146,11 @@ const WorkOrder = ({
         Header: 'Assigned Users',
         Cell: ({ row }) =>
           row?.original['assignedUsers'] && row?.original['assignedUsers']?.length ? (
-            row?.original['assignedUsers'].map((e, i)=>{
-              return (i === row?.original['assignedUsers'].length - 1 ) ?
-                 <a className="link text-truncate" target="_blank" href={`${routes.userDetail.path}/${e.optionValue}`}>{e?.optionLabel}</a> : 
-                 <a className="link text-truncate" target="_blank" href={`${routes.userDetail.path}/${e.optionValue}`}>{e?.optionLabel}, </a>
-              })
+            row?.original['assignedUsers'].map((e, i) => {
+              return (i === row?.original['assignedUsers'].length - 1) ?
+                <a className="link text-truncate" target="_blank" href={`${routes.userDetail.path}/${e.optionValue}`}>{e?.optionLabel}</a> :
+                <a className="link text-truncate" target="_blank" href={`${routes.userDetail.path}/${e.optionValue}`}>{e?.optionLabel}, </a>
+            })
           ) : (
             <NoDataCell />
           )
@@ -185,7 +183,7 @@ const WorkOrder = ({
         canDrag: false,
         Cell: ({ row }) => {
           return (
-            row?.original?.type === 'service' && (
+            (row?.original?.type === 'service' || (row?.original?.type === 'package' && row?.original?.packageDetail?.packageType === "Service")) && (
               <>
                 <IconButton
                   disabled={!allowedToDelete}
@@ -250,15 +248,14 @@ const WorkOrder = ({
     createWorkorderService(rows);
     rows.forEach((parent, i) => {
       parent.srno = i + 1;
-      parent.detail = `${
-        parent.type === 'service'
+      parent.detail = `${parent.type === 'service'
           ? parent?.serviceDetail?.serviceName
           : parent.type === 'product'
-          ? parent?.productDetail?.productName
-          : parent.type === 'serializedAsset'
-          ? parent?.serializedAsset?.assetNumber
-          : parent?.packageDetail?.packageName
-      }`;
+            ? parent?.productDetail?.productName
+            : parent.type === 'serializedAsset'
+              ? parent?.serializedAsset?.assetNumber
+              : parent?.packageDetail?.packageName
+        }`;
       parent.qty = parent.qty;
       parent.status = parent?.workOrder?.status;
       parent.workOrderNumber = parent?.workOrder?.workOrderNumber;
@@ -306,10 +303,10 @@ const WorkOrder = ({
         _subRow.type === 'service'
           ? _subRow?.serviceDetail?.serviceName
           : _subRow.type === 'product'
-          ? _subRow?.productDetail?.productName
-          : _subRow.type === 'serializedAsset'
-          ? _subRow?.serializedAsset?.assetNumber
-          : _subRow?.packageDetail?.packageName;
+            ? _subRow?.productDetail?.productName
+            : _subRow.type === 'serializedAsset'
+              ? _subRow?.serializedAsset?.assetNumber
+              : _subRow?.packageDetail?.packageName;
       _subRow.qtyDisplay = `${parent.qtyDisplay * _subRow.qty}`;
       _subRow.preWork = _subRow.type === 'service' ? _subRow?.serviceDetail?.preWork : false;
       _subRow.workOrder = parent?.workOrder;
@@ -476,12 +473,12 @@ const WorkOrder = ({
                 stepFullScreen
                   ? '100%'
                   : isTabletScreen
-                  ? 'calc(100vw)'
-                  : isSmallScreen
-                  ? 'calc(100vw)'
-                  : showActivity
-                  ? '100%'
-                  : 'calc(100vw - 103px)'
+                    ? 'calc(100vw)'
+                    : isSmallScreen
+                      ? 'calc(100vw)'
+                      : showActivity
+                        ? '100%'
+                        : 'calc(100vw - 103px)'
               }
               height={stepFullScreen ? 'calc(100vh - 150px)' : 'calc(100vh - 345px)'}
             >
