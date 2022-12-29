c074cf6ffe074c81cf8dc87b85a79d2f22c9ce0b
unknown <romit@vebholic.com>
2022-12-29 11:05:29 +0530

    fix: bug fix

--- src/constants/helpers.tsx
+++ src/constants/helpers.tsx
@@ -203,7 +203,9 @@ export const sidebarResource = {
   repairOrder: 'Repair Order',
   workOrder: 'Work Order',
   workOrderSupervisor: 'Work Order Supervisor',
-  workOrderTechnician: 'Work Order Technician'
+  workOrderTechnician: 'Work Order Technician',
+  freqentlyAskedQuestion: 'Frequently Asked Question',
+  blog: 'Blog'
 };
 
 export const resourceNames = {
@@ -274,7 +276,9 @@ export const resourceNames = {
   repairOrder: 'Repair Order',
   workOrder: 'Work Order',
   workOrderSupervisor: 'Work Order Supervisor',
-  workOrderTechnician: 'Work Order Technician'
+  workOrderTechnician: 'Work Order Technician',
+  freqentlyAskedQuestion: 'Frequently Asked Question',
+  blog: 'Blog'
 };
 
 export const primaryFields = {
