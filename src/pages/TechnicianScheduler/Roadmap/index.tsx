df73cf92ba47c954907f658e171762cf736ae878
sunanda35 <sunandasamanta60@gmail.com>
2023-01-19 13:49:30 +0530

    refactor

---
+++ src/pages/TechnicianScheduler/Roadmap/index.tsx
@@ -0,0 +1,186 @@
+import React, { memo, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Typography, Box, Button, ButtonGroup } from '@material-ui/core';
+import { Map } from '@material-ui/icons';
+import moment from 'moment';
+
+// import { GetRoadmap } from '../../../../axios/activity';
+
+// import Calander from './Calander';
+// import ActivityList from './ActivityList';
+// import CalanderList from './CalanderList';
+// import Loader from '../../../../components/Loader';
+import { isMobile, isTablet } from 'react-device-detect';
+import ActivityList from 'src/components/Activity/Report/Roadmap/ActivityList';
+import Calander from 'src/components/Activity/Report/Roadmap/Calander';
+import CalanderList from 'src/components/Activity/Report/Roadmap/CalanderList';
+import Loader from 'src/components/Loader';
+
+function Roadmap({ type, filter }) {
+  const scrollRef = React.useRef(null);
+  const executeScroll = () => {
+    var pageElement = document.getElementById('dayLiner');
+    var LeftPos = pageElement.offsetLeft;
+    document.getElementById('scrollDayLiner').scrollLeft = LeftPos - 200;
+    // if (scrollRef.current) {
+    //     scrollRef.current.scrollIntoView({ inline: "center" })
+    // }
+  };
+
+  const [calendarType, setCalendarType] = useState('week');
+  const [activity, setActivity] = useState(null);
+  const [treeList, setTreeList] = useState(null);
+
+  useEffect(() => {
+    fetchRoadmap();
+  }, [filter]);
+
+  const fetchRoadmap = async () => {
+    // await GetRoadmap(type, JSON.stringify(filter))
+    //   .then(({ data }) => {
+    setActivity([]);
+    setTreeList([]);
+    //     executeScroll();
+    //   })
+    //   .catch((err) => {});
+  };
+
+  let height = window.innerHeight - 250;
+  const today = new Date();
+  let startDate = moment(today).subtract(365, 'days');
+  let endDate = moment(today).add(365, 'days');
+  console.log('startdate', startDate);
+  console.log('enddate', endDate);
+  let totalDay = endDate.diff(startDate, 'days');
+
+  var dayPixel = 0;
+  if (calendarType === 'month') {
+    dayPixel = 8.5;
+  } else if (calendarType === 'week') {
+    dayPixel = 35;
+  } else {
+    dayPixel = 3;
+  }
+
+  const handelChangeCalendarType = async (type) => {
+    setCalendarType(type);
+    setTimeout(() => executeScroll(), 500);
+  };
+
+  const taskScroolRef = React.useRef(null);
+  const onscroll = (event) => {
+    var target = event.nativeEvent.target;
+    taskScroolRef.current.scrollTop = target.scrollTop;
+  };
+
+  const [expanded, setExpanded] = React.useState([]);
+  const [selected, setSelected] = React.useState([]);
+
+  const handleToggle = (event, nodeIds) => {
+    setExpanded(nodeIds);
+  };
+
+  const handleSelect = (event, nodeIds) => {
+    setSelected(nodeIds);
+  };
+
+  return activity ? (
+    <Box bgcolor="white">
+      <Box border={1} borderColor="grey.300" display="flex" height={height} style={{ position: 'relative' }}>
+        <Box display="flex" width="100%" height="100%" style={{ position: 'absolute' }}>
+          <Box minWidth={isMobile && !isTablet ? 110 : 300} border={1} borderColor="grey.300" style={{ position: 'relative', overflow: 'hidden' }}>
+            <Box height={60} bgcolor="grey.200" display="flex" style={{ position: 'sticky', top: 0, zIndex: 1 }}>
+              <Box p={2} display="flex" alignItems="center">
+                <Map />
+                <Box mr={1} />
+                <Typography variant="body1" display="block">
+                  Roadmap
+                </Typography>
+              </Box>
+            </Box>
+            <div
+              ref={taskScroolRef}
+              style={{
+                position: 'relative',
+                width: '100%',
+                height: '100%',
+                overflow: 'hidden'
+              }}
+            >
+              <Box>
+                <ActivityList
+                  fetchRoadmap={fetchRoadmap}
+                  type={type}
+                  activity={activity}
+                  treeList={treeList}
+                  expanded={expanded}
+                  selected={selected}
+                  handleToggle={handleToggle}
+                  handleSelect={handleSelect}
+                />
+                <Box height={20}></Box>
+              </Box>
+            </div>
+          </Box>
+          <Box id="scrollDayLiner" onScroll={onscroll} border={1} borderColor="grey.300" style={{ position: 'relative', overflow: 'auto' }}>
+            <Calander calendarType={calendarType} dayPixel={dayPixel} startDate={startDate} endDate={endDate} />
+
+            <Box width="100%" height="100%" style={{ position: 'absolute', zIndex: 1 }}>
+              <Box style={{ position: 'absolute', width: totalDay * dayPixel }}>
+                <CalanderList
+                  fetchRoadmap={fetchRoadmap}
+                  type={type}
+                  activity={activity}
+                  expanded={expanded}
+                  selected={selected}
+                  handleSelect={handleSelect}
+                  startDate={startDate}
+                  endDate={endDate}
+                  totalDay={totalDay}
+                  calendarType={calendarType}
+                />
+              </Box>
+            </Box>
+            <Box width={totalDay * dayPixel} height={'100%'} style={{ position: 'sticky', top: 0, bottom: 0 }}>
+              <div ref={scrollRef}>
+                <Box
+                  id="dayLiner"
+                  height={'100%'}
+                  style={{
+                    position: 'absolute',
+                    left: (100 * moment().diff(startDate, 'days')) / totalDay + '%',
+                    width: dayPixel
+                  }}
+                >
+                  <Box style={{ margin: 'auto' }} width={2} border={2} borderColor="secondary.main" height={'100%'}></Box>
+                </Box>
+              </div>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+      <Box display="flex" justifyContent="flex-end" className="mt-2">
+        <ButtonGroup disableElevation color="primary">
+          <Button size="small" variant={calendarType === 'week' ? 'contained' : 'outlined'} onClick={() => handelChangeCalendarType('week')}>
+            Weeks
+          </Button>
+          <Button size="small" variant={calendarType === 'month' ? 'contained' : 'outlined'} onClick={() => handelChangeCalendarType('month')}>
+            Months
+          </Button>
+          <Button size="small" variant={calendarType === 'quater' ? 'contained' : 'outlined'} onClick={() => handelChangeCalendarType('quater')}>
+            Quaters
+          </Button>
+        </ButtonGroup>
+      </Box>
+    </Box>
+  ) : (
+    <Loader text="" />
+  );
+}
+
+Roadmap.propTypes = {
+  type: PropTypes.any,
+  filter: PropTypes.any,
+  activityId: PropTypes.any
+};
+export default memo(Roadmap);
--- src/pages/TechnicianScheduler/index.tsx
+++ src/pages/TechnicianScheduler/index.tsx
@@ -1,7 +1,90 @@
-import React from 'react';
+import { Box, Grid, makeStyles, Paper, TextField } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
+import React, { Fragment, useState } from 'react';
+import { isMobile, isTablet } from 'react-device-detect';
+import CustomBreadCrumbs from 'src/components/CustomBreadCrumbs';
+import CustomContainer from 'src/components/CustomContainer';
+import routes from 'src/components/Helpers/Routes';
+import Roadmap from './Roadmap';
 
-function index() {
-  return <div>index</div>;
+const capitalize = (string) => {
+  return string?.charAt(0)?.toUpperCase() + string?.slice(1);
+};
+
+const useStyles = makeStyles((theme) => ({
+  activityContainer: {
+    padding: '0 10px 10px'
+  },
+  activityHeader: {
+    background: '#dfdfdf',
+    margin: '6px 6px',
+    borderRadius: '6px',
+    '& .MuiGrid-spacing-xs-1': {
+      width: 'calc(100% + 14px)'
+    }
+  }
+}));
+
+function TechnicianScheduler() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const classes = useStyles();
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [filter, setFilter] = useState({ view: '', resource: '' });
+
+  return (
+    <Fragment>
+      <Grid container className="headerbox">
+        <Grid item xs={12}>
+          <CustomBreadCrumbs routes={[{ title: capitalize(routes.technicianScheduler.title) }]} />
+        </Grid>
+      </Grid>
+      <CustomContainer>
+        <Fragment>
+          <Box p={1}>
+            <Grid container xs={12} md={12} sm={12} spacing={2}>
+              <Grid item xs={12} md={6} sm={6}>
+                <Autocomplete
+                  size="small"
+                  fullWidth
+                  freeSolo
+                  options={['Technician View', 'Order View']}
+                  getOptionLabel={(option) => option}
+                  value={filter.view || ''}
+                  onChange={(event, newValue) => {
+                    setFilter({ ...filter, view: newValue });
+                  }}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Select View" size="small" variant="outlined" className={isMobile ? 'serchBox' : ''} />
+                  )}
+                />
+              </Grid>
+              {filter?.view === 'Order View' && (
+                <Grid item xs={12} md={6} sm={6}>
+                  <Autocomplete
+                    size="small"
+                    fullWidth
+                    freeSolo
+                    options={['Service Order', 'Work Order']}
+                    getOptionLabel={(option: any) => option}
+                    value={filter.resource || ''}
+                    onChange={(event, newValue) => {
+                      setFilter({ ...filter, resource: newValue });
+                    }}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Select Resource" size="small" variant="outlined" className={isMobile ? 'serchBox' : ''} />
+                    )}
+                  />
+                </Grid>
+              )}
+            </Grid>
+          </Box>
+          <Box className={classes.activityContainer}>
+            <Roadmap type={null} filter={filter} />
+          </Box>
+        </Fragment>
+      </CustomContainer>
+    </Fragment>
+  );
 }
 
-export default index;
+export default TechnicianScheduler;
