ebf4ea3ccafa70104d292a17916ef518764b5cd3
unknown <romit@vebholic.com>
2023-01-31 18:38:21 +0530

    fix: bug fix

--- src/components/Shared/DetailsPage.tsx
+++ src/components/Shared/DetailsPage.tsx
@@ -173,16 +173,16 @@ const Details = (props: DetailProps) => {
         typeof values[input.fieldName] === 'string'
           ? values[input.fieldName]
           : filterOptions.length
-          ? filterOptions.map((d) => d.optionLabel).join(', ')
-          : '';
+            ? filterOptions.map((d) => d.optionLabel).join(', ')
+            : '';
       text = value ? value : '-';
     } else if (input.type === 'freeStyleMultiSelect') {
       const value =
         values[input.fieldName].length && Array.isArray(values[input.fieldName])
           ? values[input.fieldName].map((d) => d).join(', ')
           : typeof values[input.fieldName] === 'string'
-          ? values[input.fieldName]
-          : '';
+            ? values[input.fieldName]
+            : '';
 
       text = value ? value : '-';
     } else if (input.type === 'dropDown') {
@@ -240,10 +240,10 @@ const Details = (props: DetailProps) => {
   const renderData = (val: any, fieldData: any) => {
     const value = normalizeValues(val, fieldData);
     if (
-      fieldData.hasOwnProperty('lookup') &&
-      fieldData.lookup &&
-      permissions[camelCase(fieldData.lookupResource)]?.isRead &&
-      !unlinkFields.includes(fieldData.lookupResource)
+      fieldData?.hasOwnProperty('lookup') &&
+      fieldData?.lookup &&
+      permissions && permissions[camelCase(fieldData?.lookupResource)]?.isRead &&
+      !unlinkFields.includes(fieldData?.lookupResource)
     ) {
       if (fieldData.type === 'multiSelect' || fieldData.type === 'dropDown') {
         return (
@@ -254,13 +254,13 @@ const Details = (props: DetailProps) => {
                   <React.Fragment key={_val.optionValue}>
                     <Link
                       to={`/${kebabCase(fieldData.lookupResource)}/detail/${_val.optionValue}`}
-                      // onMouseEnter={(e) =>
-                      //   getPopoverData(
-                      //     e,
-                      //     fieldData.lookupResource,
-                      //     _val.optionValue
-                      //   )
-                      // }
+                    // onMouseEnter={(e) =>
+                    //   getPopoverData(
+                    //     e,
+                    //     fieldData.lookupResource,
+                    //     _val.optionValue
+                    //   )
+                    // }
                     >
                       <span className={`text-truncate link`}>
                         {_val.optionLabel}
@@ -275,13 +275,13 @@ const Details = (props: DetailProps) => {
             ) : data[fieldData.fieldName] ? (
               <Link
                 to={`/${kebabCase(fieldData.lookupResource)}/detail/${val[fieldData.fieldName]}`}
-                // onMouseEnter={(e) =>
-                //   getPopoverData(
-                //     e,
-                //     fieldData.lookupResource,
-                //     val[fieldData.fieldName]
-                //   )
-                // }
+              // onMouseEnter={(e) =>
+              //   getPopoverData(
+              //     e,
+              //     fieldData.lookupResource,
+              //     val[fieldData.fieldName]
+              //   )
+              // }
               >
                 <span className={`text-truncate link`}>
                   {data[fieldData.fieldName].optionLabel}
@@ -388,36 +388,36 @@ const Details = (props: DetailProps) => {
                               {renderData(initialVals, field.fieldData)}
                               {field.fieldData.type === 'fileUpload'
                                 ? initialVals[field.fieldData.fieldName] &&
-                                  (isDownloading ? (
-                                    <Box display="flex" alignItems="center">
-                                      {downloadProgress === 100 ? 'Downloaded' : 'Downloading'}
+                                (isDownloading ? (
+                                  <Box display="flex" alignItems="center">
+                                    {downloadProgress === 100 ? 'Downloaded' : 'Downloading'}
 
-                                      <Box marginLeft={1} position="relative" display="inline-flex">
-                                        <CircularProgress size={30} variant="determinate" value={downloadProgress} />
-                                        <Box
-                                          top={0}
-                                          left={0}
-                                          bottom={0}
-                                          right={0}
-                                          position="absolute"
-                                          display="flex"
-                                          alignItems="center"
-                                          justifyContent="center"
-                                        >
-                                          <Typography variant="caption" component="div" color="textSecondary">{`${downloadProgress}%`}</Typography>
-                                        </Box>
+                                    <Box marginLeft={1} position="relative" display="inline-flex">
+                                      <CircularProgress size={30} variant="determinate" value={downloadProgress} />
+                                      <Box
+                                        top={0}
+                                        left={0}
+                                        bottom={0}
+                                        right={0}
+                                        position="absolute"
+                                        display="flex"
+                                        alignItems="center"
+                                        justifyContent="center"
+                                      >
+                                        <Typography variant="caption" component="div" color="textSecondary">{`${downloadProgress}%`}</Typography>
                                       </Box>
                                     </Box>
-                                  ) : (
-                                    <IconButton
-                                      title={`Download ${initialVals[field.fieldData.fieldName]}`}
-                                      disabled={isDownloading}
-                                      size="small"
-                                      onClick={() => downloadFile(normalizeValues(initialVals, field.fieldData))}
-                                    >
-                                      <GetApp />
-                                    </IconButton>
-                                  ))
+                                  </Box>
+                                ) : (
+                                  <IconButton
+                                    title={`Download ${initialVals[field.fieldData.fieldName]}`}
+                                    disabled={isDownloading}
+                                    size="small"
+                                    onClick={() => downloadFile(normalizeValues(initialVals, field.fieldData))}
+                                  >
+                                    <GetApp />
+                                  </IconButton>
+                                ))
                                 : null}{' '}
                             </Box>
                           )}
--- src/pages/IrtTicket/Approver/index.tsx
+++ src/pages/IrtTicket/Approver/index.tsx
@@ -7,12 +7,14 @@ import PersonIcon from '@material-ui/icons/Person';
 import AssignUserDialog from './AssignUserDialog';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { CustomToastContext } from 'src/StateProvider/CustomToastContext/CustomToastContext';
+import moment from 'moment';
 
 
 const Approver = ({ irtTicketData }) => {
   const [approver, setAapprover] = useState(null);
   const [openDialog, setOpenDialog] = useState(false);
   const toastConfig = useContext(CustomToastContext);
+  const [selected, setSelected] = useState(null);
 
   useEffect(() => {
     fetchData();
@@ -28,14 +30,14 @@ const Approver = ({ irtTicketData }) => {
 
   const handleDelete = (id) => {
     axiosInstance()
-    .put(`${routes?.irtTicket?.path}/approver/remove/${irtTicketData?._id}`,{ids:[id]})
+      .put(`${routes?.irtTicket?.path}/approver/remove/${irtTicketData?._id}`, { ids: [id] })
       .then(({ data: { data } }) => {
-       fetchData()
-       toastConfig.setToastConfig({
-        open: true,
-        type: 'success',
-        message: 'Deleted Successfully'
-      });
+        fetchData()
+        toastConfig.setToastConfig({
+          open: true,
+          type: 'success',
+          message: 'Deleted Successfully'
+        });
       })
       .catch((error) => {
         toastConfig.setToastConfig(error);
@@ -73,27 +75,30 @@ const Approver = ({ irtTicketData }) => {
                       borderWidth: '1px',
                       borderStyle: 'solid',
                       backgroundColor: 'white',
-                      borderColor: 'rgb(224, 224, 224)',
+                      borderColor: selected?._id === item?._id ? '#329592' : 'rgb(224, 224, 224)',
                       cursor: 'pointer',
                       transition: '.3s'
                     }}
                     p={2}
-                    onClick={() => {}}
+                    onClick={() => {
+                      setSelected(item)
+                    }}
                   >
-                    <Box display="flex" flexDirection="row">
+                    <Box display="flex">
                       <Box>
                         <PersonIcon />
                       </Box>
                       <Box ml={2}>
                         <Typography>{item?.user?.optionLabel}</Typography>
                       </Box>
-                      <Box ml={2}>
+                      <Box ml={2} flexGrow={1}>
                         <Chip color="primary" label={item?.status} />
                       </Box>
-                      <Box ml={2}>
+                      <Box ml={2} >
                         <Tooltip title="Delete">
                           <IconButton
                             aria-label="Delete"
+                            size="small"
                             onClick={() => {
                               handleDelete(item?._id)
                             }}
@@ -114,7 +119,24 @@ const Approver = ({ irtTicketData }) => {
           </Grid>
         </Grid>
         <Grid item xs={12} sm={7} md={8} lg={9}>
-          <Box textAlign="center"></Box>
+          {selected &&
+            <Box
+              p={2}
+              style={{
+                borderWidth: '1px',
+                borderStyle: 'solid',
+                borderColor: 'rgb(224, 224, 224)',
+              }}>
+              {selected?.logs?.map((item, index) => (
+                <Box key={index}>
+                  <Typography variant="body1">{item?.detail}</Typography>
+                  <Typography variant="body2">{moment(item?.date).format('MMM DD YYYY hh:mm A')}</Typography>
+                  <br />
+                  <hr />
+                  <br />
+                </Box>
+              ))}
+            </Box>}
         </Grid>
       </Grid>
       {openDialog && <AssignUserDialog handleClose={() => setOpenDialog(false)} onSuccess={() => fetchData()} id={irtTicketData?._id} />}
