305bdc1c8beec02b721c44dc8f3b7ef0cf033902
Avisekh999 <avisekhgurung8578@gmail.com>
2023-02-13 15:49:45 +0530

    fix(productionorder fix): productionOrder fix
    ProductionOrder fix

--- src/pages/ProductionOrder/Material/index.tsx
+++ src/pages/ProductionOrder/Material/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext, Fragment } from 'react';
-import { Grid, Box, Button, IconButton, MenuItem, Menu } from '@material-ui/core';
+import { Box, Button, IconButton, Menu, MenuItem } from '@material-ui/core';
 import axiosInstance from '../../../axios/axiosInstance';
 import routes from '../../../components/Helpers/Routes';
 import { useData } from '../../../StateProvider/Provider';
@@ -7,50 +7,53 @@ import CommonSkeleton from '../../../components/Helpers/CommonSkeleton';
 import { CustomToastContext } from '../../../StateProvider/CustomToastContext/CustomToastContext';
 import HtmlTooltip from '../../../components/CustomTooltipTitle';
 import CustomReactTable from '../../../components/CustomReactTable/CustomReactTable';
-import Add from '@material-ui/icons/Add';
-import ConfirmationDialog from '../../../components/Helpers/ConfirmationDialog';
+import NoDataCell from '../../../components/Helpers/NoDataCell';
 import DeleteIcon from '@material-ui/icons/Delete';
+import { productionOrder } from '../../../constants/helpers';
+import ConfirmationDialog from '../../../components/Helpers/ConfirmationDialog';
 import { isMobile } from 'react-device-detect';
 import { ExpandMore } from '@material-ui/icons';
 import { startCase } from 'lodash';
 import { calculateRowsField } from 'src/components/RentalManagment/helper';
 import MaterialDialog from './MaterialDialog';
+import Add from '@material-ui/icons/Add';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import AssignProductDialog from 'src/components/AssignRolesDialog/AssignProductDialog';
 import AssignPackageDialog from 'src/components/AssignRolesDialog/AssignPackageDialog';
-import { flattenArray, genrateCustomTableColumns } from 'src/constants/columns';
-import OpenInNewIcon from '@material-ui/icons/OpenInNew';
+import { flattenArray } from 'src/constants/columns';
 
-const Material = ({ salesOrderData, renderedFrom, allowedToEdit }) => {
+const Material = ({
+  productionOrderData,
+  setNextStep,
+  renderedFrom,
+  stepFullScreen,
+  allowedToEdit,
+  allowedToDelete
+}) => {
   const toastConfig = useContext(CustomToastContext);
   const {
     state: { user, permissions }
   }: any = useData();
 
   const [isUpdating, setUpdating] = useState(false);
-  const [materialEdit, setMaterialEdit] = useState({ open: false, data: null, bulkedit: false, showSaveAndNext: false });
   const [addDialog, setAddDialog] = useState({ open: false, type: '', parentId: null });
-  const [selectedRecords, setSelectedRecords] = useState([]);
   const [deleteData, setDeleteData] = useState(null);
   const [isDeleting, setDeleting] = useState(false);
+  const [materialEdit, setMaterialEdit] = useState({ open: false, data: null, bulkedit: false, showSaveAndNext: false });
+  const [anchorEl, setAnchorEl] = useState(null);
   const [columns, setColumns] = useState(null);
   const [rowsData, setRowsData] = useState(null);
+  const [selectedRecords, setSelectedRecords] = useState([]);
   const [allFields, setAllFields] = useState([]);
-  const [anchorEl, setAnchorEl] = useState(null);
   const [addAnchorEl, setAddAnchorEl] = useState(null);
 
   useEffect(() => {
     fetchFields();
-  }, []);
+  }, [productionOrderData]);
 
   const fetchFields = async () => {
-    var data = fieldsToShow || [];
-    setAllFields(JSON.parse(JSON.stringify(data)));
-    const newColumns = genrateCustomTableColumns(data, '', '');
-    let qtyIndex = newColumns.findIndex((d) => d.accessor === 'qty');
-    if (qtyIndex > -1) {
-      newColumns[qtyIndex].accessor = 'qtyDisplay';
-    }
-    let coloum: any = [
+    setColumns(null);
+    const coloum: any = [
       {
         accessor: 'index',
         Header: 'Index',
@@ -64,108 +67,136 @@ const Material = ({ salesOrderData, renderedFrom, allowedToEdit }) => {
       {
         accessor: 'type',
         Header: 'Type',
+        disableFilters: true,
         sticky: isMobile ? 'none' : 'left',
-        Cell: ({ row }) => (
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <p>{`${startCase(row.original?.type)} `}</p>
-          </div>
-        )
+        width: 200,
+        Cell: ({ row }) => (row.original['type'] ? <p>{`${startCase(row.original?.type)} `}</p> : <NoDataCell />)
       },
       {
         accessor: 'detail',
-        Header: 'Detail',
+        Header: ' Details',
         minWidth: 300,
         width: 300,
         sticky: isMobile ? 'none' : 'left',
         Cell: ({ row, rows }) => (
           <div style={{ display: 'flex', alignItems: 'center' }}>
-            {allowedToEdit ? (
-              <p
-                onClick={() => {
-                  setMaterialEdit({
-                    open: true,
-                    data: row.original,
-                    bulkedit: false,
-                    showSaveAndNext: row?.index < rows?.filter((e) => e?.depth === 0)?.length - 1 && row?.depth === 0 ? true : false
-                  });
-                }}
-                className="link text-truncate"
-                title={row.original?.detail}
-              >
-                {row.original?.detail}
-              </p>
-            ) : (
-              <p className="text-truncate">{row.original?.detail}</p>
-            )}
-            {allowedToEdit && (
-              <>
-                <Box ml={1}>
-                  <span>({row.original?.subRows?.length})</span>
-                </Box>
-                <Box ml={1}>
-                  <HtmlTooltip title="Add Product">
-                    <IconButton
-                      onClick={() => {
-                        setAddDialog({ open: true, type: 'product', parentId: row.original?._id });
-                      }}
-                      size="small"
-                    >
-                      <Add fontSize="small" color="primary" />
-                    </IconButton>
-                  </HtmlTooltip>
-                </Box>
-              </>
-            )}
-            <Box ml={1}>
-              <IconButton
-                size="small"
-                onClick={() => {
-                  if (row.original.type === 'product') {
-                    window.open(`${routes.productDetail.path}/${row.original.materialId}`);
-                  } else {
-                    window.open(`${routes.packagesDetail.path}/${row.original.materialId}`);
-                  }
-                }}
-              >
-                <OpenInNewIcon fontSize="small" color="primary" />
-              </IconButton>
-            </Box>
-          </div>
+          {allowedToEdit ? (
+            <p
+              onClick={() => {
+                setMaterialEdit({
+                  open: true,
+                  data: row.original,
+                  bulkedit: false,
+                  showSaveAndNext: row?.index < rows?.filter((e) => e?.depth === 0)?.length - 1 && row?.depth === 0 ? true : false
+                });
+              }}
+              className="link text-truncate"
+              title={row.original?.detail}
+            >
+              {row.original?.detail}
+            </p>
+          ) : (
+            <p className="text-truncate">{row.original?.detail}</p>
+          )}
+          {allowedToEdit && (
+            <>
+              <Box ml={1}>
+                <span>({row.original?.subRows?.length})</span>
+              </Box>
+              <Box ml={1}>
+                <HtmlTooltip title="Add Product">
+                  <IconButton
+                    onClick={() => {
+                      setAddDialog({ open: true, type: 'product', parentId: row.original?._id });
+                    }}
+                    size="small"
+                  >
+                    <Add fontSize="small" color="primary" />
+                  </IconButton>
+                </HtmlTooltip>
+              </Box>
+            </>
+          )}
+          <Box ml={1}>
+            <IconButton
+              size="small"
+              onClick={() => {
+                if (row.original.type === 'product') {
+                  window.open(`${routes.productDetail.path}/${row.original.materialId}`);
+                } else {
+                  window.open(`${routes.packagesDetail.path}/${row.original.materialId}`);
+                }
+              }}
+            >
+              <OpenInNewIcon fontSize="small" color="primary" />
+            </IconButton>
+          </Box>
+        </div>
         )
+      },
+      {
+        accessor: 'qty',
+        Header: 'Qty',
+        width: 200,
+        Cell: ({ row }) => {
+          return row.original['qty'] ? <p className="text-truncate">{row.original.qty}</p> : <NoDataCell />;
+        },
+        editable: true
+      },
+      {
+        accessor: 'unit',
+        Header: 'Unit',
+        width: 200,
+        Cell: ({ row }) => {
+          return row.original['unit'] ? <p className="text-truncate">{row.original.unit}</p> : <NoDataCell />;
+        }
       }
     ];
-    coloum = [...coloum, ...newColumns];
     coloum.push({
       accessor: 'action',
-      Header: 'Action',
-      minWidth: 100,
-      width: 100,
+      Header: '',
+      minWidth: 70,
+      width: 70,
       sticky: 'right',
       disableFilters: true,
       canDrag: false,
-      Cell: ({ row }) =>
-        !row.original.hideSelection && (
-          <Grid container spacing={1}>
+      Cell: ({ row }) => (
+        <>
+          <HtmlTooltip title={allowedToDelete && row.original?.allowedToDelete ? 'Asset is already assigned' : 'Delete'}>
             <IconButton
               size="small"
               aria-label="Details"
               onClick={() => {
-                const obj: any = [{ id: row.original._id, type: row.original?.type, materialId: row.original?.materialId }];
+                const obj: any = [row.original._id];
                 setDeleteData(obj);
               }}
+              disabled={allowedToDelete && row.original?.allowedToDelete}
             >
-              <DeleteIcon fontSize="small" color="error" />
+              <DeleteIcon fontSize="small" color={allowedToDelete && row.original?.allowedToDelete ? 'disabled' : 'error'} />
             </IconButton>
-          </Grid>
-        )
+          </HtmlTooltip>
+        </>
+      )
+    });
+    coloum.forEach((element) => {
+      if (element.accessor === 'qty') {
+        element['Footer'] = (info) => {
+          const qtyTotal = info.rows
+            .filter((f) => f.original.parentId === null && f.values.hasOwnProperty(element.accessor) && !isNaN(f.values[element.accessor]))
+            .reduce((sum, row) => row.values[element.accessor] + sum, 0);
+          return <>{qtyTotal}</>;
+        };
+      }
     });
     setColumns(coloum);
     fetchData();
   };
 
   const fetchData = async () => {
+    setNextStep(false);
+
     var data: any = [];
-    const response = await axiosInstance().get(`${routes.demandOrder.path}/material/${salesOrderData._id}`);
+    const response = await axiosInstance().get(`${productionOrder.api}/material/${productionOrderData._id}`);
     data = response?.data?.data;
     let rows = data.material.filter((e) => e.parentId === null);
     rows.forEach((parent, i) => {
@@ -184,6 +215,16 @@ const Material = ({ salesOrderData, renderedFrom, allowedToEdit }) => {
       parent.qtyDisplay = parent.qty;
       parent.subRows = generateNestedData(data.material, parent);
     });
+
+    if (rows.length !== 0) {
+      if (rows.filter((_rows) => _rows.isValid === false).length > 0) {
+        setNextStep(false);
+      } else {
+        setNextStep(true);
+      }
+    } else {
+      setNextStep(false);
+    }
     setRowsData(rows);
     setSelectedRecords([]);
   };
@@ -215,7 +256,7 @@ const Material = ({ salesOrderData, renderedFrom, allowedToEdit }) => {
       material.push(element);
     });
     axiosInstance()
-      .post(`${routes?.demandOrder?.path}/material/${salesOrderData._id}`, { material })
+      .post(`${productionOrder.api}/material/${productionOrderData._id}`, { material })
       .then(({ data }) => {
         setAddDialog({ open: false, type: '', parentId: null });
         toastConfig.setToastConfig({
@@ -231,6 +272,37 @@ const Material = ({ salesOrderData, renderedFrom, allowedToEdit }) => {
       });
   };
 
+  const handleDelete = (rows) => {
+    setDeleting(true);
+    axiosInstance()
+      .put(`${productionOrder.api}/material/${productionOrderData?._id}/delete`, { ids: rows })
+      .then(({ data }) => {
+        setDeleting(false);
+        toastConfig.setToastConfig({
+          open: true,
+          type: 'success',
+          message: data.message
+        });
+        fetchData();
+        setDeleteData(null);
+      })
+      .catch((error) => {
+        setDeleting(false);
+        toastConfig.setToastConfig(error);
+        setDeleteData(null);
+      });
+  };
+
+  const onSaveInlineEdit = async (inputField, updatedData) => {
+    const rowData = flattenArray(rowsData)?.find((d) => d._id === updatedData._id);
+    if (inputField.hasOwnProperty('qtyDisplay')) {
+      inputField['qty'] = inputField['qtyDisplay'];
+    }
+    let rows: any = [{ ...rowData, ...updatedData }];
+    rows = await calculateRowsField(flattenArray(rowsData), inputField, allFields, updatedData);
+    handleSaveData(rows);
+  };
+
   const handleSaveData = async (rows: any, saveAndNext = false) => {
     setUpdating(true);
     rows.forEach((element) => {
@@ -244,7 +316,7 @@ const Material = ({ salesOrderData, renderedFrom, allowedToEdit }) => {
       delete element.subRows;
     });
     axiosInstance()
-      .put(`${routes.demandOrder.path}/material/${salesOrderData._id}`, { material: rows })
+      .put(`${productionOrder.api}/material/${productionOrderData._id}`, { material: rows })
       .then(({ data }) => {
         setUpdating(false);
         fetchData();
@@ -267,39 +339,6 @@ const Material = ({ salesOrderData, renderedFrom, allowedToEdit }) => {
       });
   };
 
-
-  const onSaveInlineEdit = async (inputField, updatedData) => {
-    const rowData = flattenArray(rowsData)?.find((d) => d._id === updatedData._id);
-    if (inputField.hasOwnProperty('qtyDisplay')) {
-      inputField['qty'] = inputField['qtyDisplay'];
-    }
-    let rows: any = [{ ...rowData, ...updatedData }];
-    rows = await calculateRowsField(flattenArray(rowsData), inputField, allFields, updatedData);
-    handleSaveData(rows);
-  };
-
-  const handleDelete = (rows) => {
-    setDeleting(true);
-    axiosInstance()
-      .put(`${routes.demandOrder.path}/material/${salesOrderData?._id}/delete`, { ids: rows })
-      .then(({ data }) => {
-        setDeleting(false);
-        toastConfig.setToastConfig({
-          open: true,
-          type: 'success',
-          message: data.message
-        });
-        fetchData();
-        setDeleteData(null);
-      })
-      .catch((error) => {
-        setDeleting(false);
-        toastConfig.setToastConfig(error);
-        setDeleteData(null);
-      });
-  };
-
-
   const openActions = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -316,9 +355,10 @@ const Material = ({ salesOrderData, renderedFrom, allowedToEdit }) => {
     setAddAnchorEl(null);
   };
 
+
   return (
     <Fragment>
-        {allowedToEdit &&
+      {allowedToEdit &&
         <Box display="flex" justifyContent="space-between" m={1}>
           <Box display="flex" alignItems="center">
             <Button
@@ -411,11 +451,11 @@ const Material = ({ salesOrderData, renderedFrom, allowedToEdit }) => {
           </Box>
         </Box>
       }
-      {columns && rowsData ? (
+        {columns && rowsData ? (
         <>
           <Box p="6px" zIndex={5} width={'100%'}>
             <CustomReactTable
-              height={'calc(100vh - 345px)'}
+              height={stepFullScreen ? 'calc(100vh - 150px)' : 'calc(100vh - 395px)'}
               columns={columns}
               data={rowsData}
               setWholeRowsCellColor={(rowData) => (!rowData.isValid ? '' : '')}
@@ -444,22 +484,9 @@ const Material = ({ salesOrderData, renderedFrom, allowedToEdit }) => {
           okBtnLoading={isDeleting}
         />
       )}
-      {materialEdit.open && (
-        <MaterialDialog
-          onClose={() => {
-            setMaterialEdit({ open: false, data: null, bulkedit: false, showSaveAndNext: false });
-          }}
-          materialData={materialEdit.data}
-          salesOrderData={salesOrderData}
-          handleUpdate={handleSaveData}
-          loadingEdit={isUpdating}
-          bulkEdit={materialEdit.bulkedit}
-          showSaveAndNext={materialEdit.showSaveAndNext}
-        />
-      )}
       {addDialog.open && addDialog.type === 'product' && (
         <AssignProductDialog
-          reference="demandOrder"
+          reference="productionOrder"
           serialized={null}
           productsDialogOpen={addDialog.open}
           productId={null}
@@ -473,7 +500,7 @@ const Material = ({ salesOrderData, renderedFrom, allowedToEdit }) => {
       )}
        {addDialog.open && addDialog.type === 'package' && (
         <AssignPackageDialog
-          referenceType="demandOrder"
+          referenceType="productionOrder"
           handleClose={() => setAddDialog({ open: false, type: '', parentId: null })}
           ids={[]}
           onSuccess={(rows) => {
@@ -482,55 +509,21 @@ const Material = ({ salesOrderData, renderedFrom, allowedToEdit }) => {
           packageType={null}
         />
       )}
+      {materialEdit.open && (
+        <MaterialDialog
+        onClose={() => {
+          setMaterialEdit({ open: false, data: null, bulkedit: false, showSaveAndNext: false });
+        }}
+        materialData={materialEdit.data}
+        productionOrderData={productionOrderData}
+        handleUpdate={handleSaveData}
+        loadingEdit={isUpdating}
+        bulkEdit={materialEdit.bulkedit}
+        showSaveAndNext={materialEdit.showSaveAndNext}
+        />
+      )}
     </Fragment>
   );
 };
-
-const fieldsToShow = [
-  {
-    _id: '630dbe1e9ec41861052354a3',
-    fieldLabel: 'Qty',
-    type: 'decimal',
-    option: [],
-    required: false,
-    isTooltip: false,
-    tooltipMessage: '',
-    editAble: true,
-    order: 2,
-    decimalPlaces: 2,
-    sectionName: 'Quantity Information',
-    fieldName: 'qty',
-    resource: 'Sales Order Product',
-    brand: '630dbe1e9ec418610523529c',
-    createdBy: {
-      user: '61b84437885fdf02d9104cb0',
-      date: '2022-08-30T07:37:02.237Z'
-    }
-  },
-  {
-    _id: '630dbe1e9ec41861052354a4',
-    fieldLabel: 'Unit',
-    type: 'dropDown',
-    option: [
-      {
-        optionLabel: 'Option 1',
-        optionValue: 'Option 1'
-      }
-    ],
-    required: false,
-    isTooltip: false,
-    tooltipMessage: '',
-    editAble: true,
-    order: 1,
-    sectionName: 'Quantity Information',
-    fieldName: 'unit',
-    resource: 'Sales Order Product',
-    brand: '630dbe1e9ec418610523529c',
-    createdBy: {
-      user: '61b84437885fdf02d9104cb0',
-      date: '2022-08-30T07:37:02.237Z'
-    }
-  }
-];
 
 export default Material;
