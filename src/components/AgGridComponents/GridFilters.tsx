d13fd45dff765a555662ce4c2c286309291025ef
dhrubobarman <dhrubobarman847@gmail.com>
2023-02-15 14:57:38 +0530

    fix: filter

--- src/components/AgGridComponents/GridFilters.tsx
+++ src/components/AgGridComponents/GridFilters.tsx
@@ -25,6 +25,9 @@ import CustomDialogFooter from '../CustomDialog/CustomDialogFooter';
 import axiosInstance from 'src/axios/axiosInstance';
 import { CustomToastContext } from 'src/StateProvider/CustomToastContext/CustomToastContext';
 import FormTypes from '../Helpers/FormTypes';
+import { dateTimeFormat } from 'src/constants/helpers';
+import moment from 'moment';
+import FormTypesSimple, { inputTypes } from 'src/components/Helpers/FromTypesSimple';
 import CommonSkeleton from '../Helpers/CommonSkeleton';
 
 const Transition = React.forwardRef(function Transition(
@@ -39,9 +42,10 @@ const Transition = React.forwardRef(function Transition(
 const GridFilters = ({ currentGridApi, columnApi, columns, tableSource, open, setOpen, resource }) => {
   const [isSaveFilterOpen, setIsSaveFilterOpen] = useState(false);
 
+  const [allColumns, setAllColumns] = useState(null);
   const [savedFilters, setSavedFilters] = useState(null);
   const [selectedFilter, setSelectedFilter] = useState(null);
-  const [colsWithFilterValue, setColsWithFilterValue] = useState(null);
+  const [colsWithFilterValue, setColsWithFilterValue] = useState([]);
   const [filterName, setFilterName] = useState(null);
   const [isFieldsDisabled, setIsFieldsDisabled] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
@@ -56,8 +60,8 @@ const GridFilters = ({ currentGridApi, columnApi, columns, tableSource, open, se
 
   // <<<<<<<<<<<<<<<<<<<<<<<<< HELPER FUNCTIONS >>>>>>>>>>>>>>>>>>>>>>>>>
   const handleClose = () => {
-    setOpen(false);
     // applyFilter();
+    setOpen(false);
   };
   const handleSaveDialogOpen = () => {
     if (isEditing) {
@@ -69,42 +73,28 @@ const GridFilters = ({ currentGridApi, columnApi, columns, tableSource, open, se
   const handleSaveDialogClose = () => {
     setIsSaveFilterOpen(false);
   };
-  const createEmptyColWithFilterValue = () => {
-    return columns?.map((item) => ({ name: item.field, title: item.headerName, value: '' }));
-  };
-  const updateColWithFilterValue = (name, value) => {
-    const data = colsWithFilterValue?.map((item) => {
-      if (item.name === name) return { ...item, value: value };
-      else return item;
-    });
-    setColsWithFilterValue(data);
-    return data;
-  };
+
+  // <<<<<<<<<<<<<<<<<<<<<<<<< HELPER FUNCTIONS >>>>>>>>>>>>>>>>>>>>>>>>>
+
   useEffect(() => {
     fetchGridColumns();
     fetchGridFilters();
   }, []);
 
+  useEffect(() => {
+    setColsWithFilterValue(selectedFilter?.filterValue.colsWithFilterValue || []);
+  }, [selectedFilter]);
+
   const fetchGridColumns = () => {
-    // const {
-    //   data: { data }
-    // }: any = await axiosInstance().get(`/field?resource=${resourceStartCase}`);
     axiosInstance()
       .get(`/field?resource=${resource}`)
       .then((columns) => {
-        setColsWithFilterValue(columns?.data?.data || []);
+        setAllColumns(columns?.data?.data || []);
       })
       .catch((err) => {
         toastConfig.setToastConfig(err);
       });
   };
-
-  useEffect(() => {
-    // const allCol = createEmptyColWithFilterValue();
-    // setColsWithFilterValue(allCol);
-    // setSavedFilters(() => getFiltersFromLocalStorage());
-    // fetchGridFilters();
-  }, [columns]);
 
   const fetchGridFilters = () => {
     axiosInstance()
@@ -117,13 +107,13 @@ const GridFilters = ({ currentGridApi, columnApi, columns, tableSource, open, se
         toastConfig.setToastConfig(err);
       });
   };
+
   const disableButtonandFields = () => {
     if (isEditing) {
       setIsFieldsDisabled(false);
       return;
     }
     if (!isEditing && !selectedFilter) {
-      // setColsWithFilterValue(() => createEmptyColWithFilterValue());
       setIsFieldsDisabled(false);
       return;
     }
@@ -132,56 +122,116 @@ const GridFilters = ({ currentGridApi, columnApi, columns, tableSource, open, se
       return;
     }
   };
+
   useEffect(() => {
     disableButtonandFields();
   }, [selectedFilter, isEditing]);
-  // const updateColumnValue = (name, value) => {
-  //   console.log(name, value);
-  //   setFilterValue({
-  //     ...filterValue,
-  //     name: value
-  //   });
-  //   // console.log(filterValue);
-  //   // setColsWithFilterValue((prev) => {
-  //   //   const newList = [...prev];
-  //   //   newList[index].value = value;
-  //   //   return newList;
-  //   // });
-  // };
+
+  // <<<<<<<<<<<<<<<<<<<<<<<<< FORM FUNCTIONS >>>>>>>>>>>>>>>>>>>>>>>>>
+  const formOnChange = (event, data, index) => {
+    const prevData = [...colsWithFilterValue];
+    const indexOfData = prevData.findIndex((item) => Object.keys(item)[0] === Object.keys(data)[0]);
+    const dataKey = Object.keys(data)[0];
+    if (indexOfData !== -1) {
+      if (!data[dataKey] || data[dataKey] === '') {
+        prevData.splice(indexOfData, 1);
+      } else {
+        prevData[indexOfData] = data;
+      }
+    } else {
+      prevData.push(data);
+    }
+    setColsWithFilterValue(prevData);
+  };
+
+  const getFieldValue = (fieldName) => {
+    const prevData = [...colsWithFilterValue];
+    const indexOfData = prevData.findIndex((item) => Object.keys(item)[0] === fieldName);
+    if (indexOfData === -1) {
+      return null;
+    }
+    const key = Object.keys(prevData[indexOfData])[0];
+    const data = prevData[indexOfData];
+    return data[key];
+  };
 
   // <<<<<<<<<<<<<<<<<<<<<<<<< FILTER FUNCTIONS >>>>>>>>>>>>>>>>>>>>>>>>>
-  // const createFilterModel = () => {
-  //   const filterModel = {};
-  //   colsWithFilterValue?.forEach((col) => {
-  //     if (col.value !== '') {
-  //       const key = col.name;
-  //       filterModel[key] = {
-  //         filterType: 'text',
-  //         type: 'contains',
-  //         filter: col.value
-  //       };
-  //     }
-  //   });
-  //   return {};
-  // };
+  const createFilterModel = () => {
+    const columns = [...colsWithFilterValue];
+    const filterModel = {};
+
+    columns.forEach((col) => {
+      const key = Object.keys(col)[0];
+
+      if (col.type === inputTypes.singleLine) {
+        filterModel[key] = {
+          filterType: 'text',
+          type: 'contains',
+          filter: col[key]
+        };
+      }
+      if (col.type === inputTypes.dropDown) {
+        filterModel[key] = {
+          filterType: 'text',
+          type: 'contains',
+          filter: col[key]?.optionLabel
+        };
+      }
+      if (col.type === inputTypes.dateTime) {
+        if (key.toLocaleLowerCase().includes('start')) {
+          filterModel[key] = {
+            filterType: 'text',
+            type: 'greaterThan',
+            filter: moment(col[key]).format(dateTimeFormat)
+          };
+        } else if (key.toLocaleLowerCase().includes('end')) {
+          filterModel[key] = {
+            filterTo: null,
+            type: 'lessThan',
+            filter: moment(col[key]).format(dateTimeFormat)
+          };
+        } else {
+          filterModel[key] = {
+            filterType: 'text',
+            type: 'greaterThan',
+            filter: moment(col[key]).format(dateTimeFormat)
+          };
+        }
+      }
+      if (col.type === inputTypes.currency) {
+        filterModel[key] = {
+          filterType: 'text',
+          type: 'contains',
+          filter: col[key]?.currencyCode
+        };
+      }
+      if (col.type === inputTypes.multiSelect) {
+        filterModel[key] = {
+          filterType: 'set',
+          values: col[key]?.map((item) => item.optionLabel)
+        };
+      }
+    });
+    return filterModel;
+  };
+
   const applyFilter = () => {
-    currentGridApi.setFilterModel(filterValue);
+    currentGridApi.setFilterModel(createFilterModel());
     setIsEditing(false);
     setOpen(false);
     handleClose();
   };
+
   const saveFilter = async () => {
-    // const oldSavedFilters = savedFilters ? savedFilters : [];
     const newFilter = {
       title: filterName,
       resource: resource,
-      filterValue: filterValue
+      filterValue: { colsWithFilterValue }
     };
     await axiosInstance()
       .post(`/user-resource-filter/`, newFilter)
       .then((res) => {
         fetchGridFilters();
-        // setSelectedFilter(res.data);
         toastConfig.setToastConfig({
           severity: 'success',
           message: 'Filter saved successfully'
@@ -190,11 +240,6 @@ const GridFilters = ({ currentGridApi, columnApi, columns, tableSource, open, se
       .catch((err) => {
         toastConfig.setToastConfig(err);
       });
-    // console.log([...oldSavedFilters, newFilter]);
-    // setSelectedFilter(newFilter);
-    // setSavedFilters([...oldSavedFilters, newFilter]);
-    // saveToLocalStorage([...oldSavedFilters, newFilter]);
-    // setFilterName('');
   };
   const updateFilter = () => {
     if (!selectedFilter?._id) {
@@ -203,13 +248,6 @@ const GridFilters = ({ currentGridApi, columnApi, columns, tableSource, open, se
         message: 'Please select a filter to update'
       });
     }
-    // fetchGridFilters();
-    // const oldSavedFilters = savedFilters ? savedFilters : [];
-    // const newFilterValue = createFilterModel();
-    // const updatedFilterValue = {
-    //   ...selectedFilter,
-    //   filterValue: createFilterModel()
-    // };
     axiosInstance()
       .put(`/user-resource-filter`, selectedFilter)
       .then((res) => {
@@ -224,42 +262,7 @@ const GridFilters = ({ currentGridApi, columnApi, columns, tableSource, open, se
       .catch((err) => {
         toastConfig.setToastConfig(err);
       });
-    // const newFilters = oldSavedFilters.map((item) => {
-    //   if (item._id === selectedFilter._id) {
-    //     const obj = { ...item };
-    //     obj.filterValue = newFilterValue;
-    //     return obj;
-    //   } else return item;
-    // });
-    // setSavedFilters(updatedFilterValue);
-
-    // saveToLocalStorage(updatedFilterValue);
-    // applyFilter();
-    // setIsEditing(false);
-    // setOpen(false);
   };
-  // const onFliterSelect = (newValue) => {
-  //   if (!newValue) return;
-
-  //   let filterCols = [];
-  //   for (const key in newValue?.filterValue) {
-  //     let obj = {
-  //       name: key,
-  //       value: newValue?.filterValue[key].filter
-  //     };
-  //     filterCols.push(obj);
-  //   }
-  //   const emptyCols = createEmptyColWithFilterValue();
-  //   const key = 'name';
-  //   let updatedColumns = emptyCols.map((el) => {
-  //     const found = filterCols.find((s) => s[key] === el[key]);
-  //     if (found) {
-  //       el = Object.assign(el, found);
-  //     }
-  //     return el;
-  //   });
-  //   // setColsWithFilterValue(updatedColumns);
-  // };
 
   // <<<<<<<<<<<<<<<<<<<<<<<<< FUNCTIONS FOR DELETE FILTER >>>>>>>>>>>>>>>>>>>>>>>>>
   const deleteFilter = (id) => {
@@ -278,14 +281,8 @@ const GridFilters = ({ currentGridApi, columnApi, columns, tableSource, open, se
       .catch((err) => {
         toastConfig.setToastConfig(err);
       });
-    // const oldSavedFilters = savedFilters ? savedFilters : [];
-    // let newFilters = oldSavedFilters.filter((filter) => filter._id !== id);
-    // setSavedFilters(newFilters);
-    // setSelectedFilter(null);
-    // saveToLocalStorage(newFilters);
-    // setIsFieldsDisabled(false);
-    // setIsConfrimDialogOpen(false);
   };
+
   const openDeleteConFirmation = (id, title) => {
     setConfrimDialogParam({
       head: 'Confrim',
@@ -295,23 +292,17 @@ const GridFilters = ({ currentGridApi, columnApi, columns, tableSource, open, se
     setIsConfrimDialogOpen(true);
   };
 
-  // <<<<<<<<<<<<<<<<<<<<<<<<< LOCAL STORAGE FUNCTIONS >>>>>>>>>>>>>>>>>>>>>>>>>
-  // const saveToLocalStorage = (data) => {
-  //   const dataToSave = JSON.stringify(data);
-  //   localStorage.setItem(`${tableSource}TableFilters`, dataToSave);
-  // };
-  // const getFiltersFromLocalStorage = () => {
-  //   let data = localStorage.getItem(`${tableSource}TableFilters`);
-  //   data = JSON.parse(data);
-  //   return data;
-  // };
-
   // <<<<<<<<<<<<<<<<<<<<<<<<< FUNCTIONS TO REMOVE FILTER >>>>>>>>>>>>>>>>>>>>>>>>>
   const handleClearSelectedFilter = () => {
     setSelectedFilter(null);
   };
   const handleClearSingleFilter = (colName) => {
-    updateColWithFilterValue(colName, '');
+    const oldFilters = [...colsWithFilterValue];
+    const colIndex = oldFilters.findIndex((col) => Object.keys(col)[0] === colName);
+    if (colIndex !== -1) {
+      oldFilters.splice(colIndex, 1);
+    }
+    setColsWithFilterValue(oldFilters);
   };
 
   return (
@@ -359,42 +350,15 @@ const GridFilters = ({ currentGridApi, columnApi, columns, tableSource, open, se
                 renderInput={(params) => <TextField fullWidth {...params} label="Select a Filter Set" variant="outlined" />}
               />
             </Grid>
-            {colsWithFilterValue ? (
-              colsWithFilterValue?.map((field, index) => (
-                <Grid item xs={12} sm={6} md={4}>
-                  {/* {console.log(field)} */}
-                  <FormTypes
-                    key={index}
-                    disabled={isFieldsDisabled}
-                    values={filterValue[field.fieldData?.fieldName] ?? ''}
-                    errors={{}}
-                    touched={{}}
-                    label={field.fieldData?.fieldLabel || ''}
-                    name={field.fieldData?.fieldName || ''}
-                    type={field.fieldData?.type}
-                    options={field.fieldData?.option}
-                    setFieldValue={(name, value) => {
-                      setFilterValue({
-                        ...filterValue,
-                        [field.fieldData?.fieldName]: value
-                      });
-                      console.log(filterValue);
-                    }}
-                    required={false}
-                    fullWidth
-                    size="small"
-                  />
-                  {/* <TextField
-                    disabled={isFieldsDisabled}
-                    fullWidth
-                    label={field.title}
-                    size="small"
-                    value={field.value}
-                    onChange={(e) => updateColumnValue(index, e.target.value)}
-                    variant="outlined"
-                  /> */}
-                </Grid>
-              ))
+            {allColumns ? (
+              allColumns?.map((field, index) => {
+                const value = getFieldValue(field.fieldData?.fieldName);
+                return (
+                  <Grid item xs={12} sm={6} md={4}>
+                    <FormTypesSimple key={index} index={index} field={field.fieldData} value={value} onChange={formOnChange} />
+                  </Grid>
+                );
+              })
             ) : (
               <Box p={2} height={500} bgcolor="white">
                 <CommonSkeleton lenArray={[...Array(10).keys()]} />
@@ -416,7 +380,7 @@ const GridFilters = ({ currentGridApi, columnApi, columns, tableSource, open, se
             onClick={() => {
               applyFilter();
             }}
-            className={'btn-primary-v1 light'}
+            size="small"
             color="primary"
             variant="contained"
           >
@@ -449,7 +413,7 @@ const GridFilters = ({ currentGridApi, columnApi, columns, tableSource, open, se
             }}
             color="primary"
             variant="contained"
-            className={'btn-primary-v1'}
+            size="small"
           >
             Confirm
           </Button>
@@ -532,7 +496,30 @@ const DisplayFilters = ({ gridApi, selectedFilter, colsWithFilterValue, handleCl
   };
 
   const filterOutFilters = () => {
-    const data = colsWithFilterValue?.filter((item) => item.value !== '');
+    const data = colsWithFilterValue?.map((item) => {
+      const key = Object.keys(item)[0];
+      if (item.type === inputTypes.singleLine) {
+        return { title: item.name, value: item[key], name: key };
+      }
+      if (item.type === inputTypes.dropDown) {
+        return { title: item.name, value: item[key]?.optionLabel, name: key };
+      }
+      if (item.type === inputTypes.currency) {
+        return { title: item.name, value: item[key]?.currencyCode, name: key };
+      }
+      if (item.type === inputTypes.dateTime) {
+        if (key.toLocaleLowerCase().includes('start')) {
+          return { title: `${item.name}`, value: `After ${moment(item[key]).format(dateTimeFormat)}`, name: key };
+        } else if (key.toLocaleLowerCase().includes('end')) {
+          return { title: `${item.name}`, value: `Before ${moment(item[key]).format(dateTimeFormat)}`, name: key };
+        } else {
+          return { title: `${item.name}`, value: moment(item[key]).format(dateTimeFormat), name: key };
+        }
+      }
+      if (item.type === inputTypes.multiSelect) {
+        return { title: item.name, value: item[key]?.map((item) => item.optionLabel).join(), name: key };
+      }
+    });
     return data ? data : [];
   };
 
@@ -581,7 +568,7 @@ const DisplayFilters = ({ gridApi, selectedFilter, colsWithFilterValue, handleCl
                   onClick={handleOpen}
                   className={'filter-chip'}
                   deleteIcon={<CloseIcon />}
-                  label={`${filter.title}=${filter.value}`}
+                  label={`${filter?.title}=${filter?.value}`}
                   onDelete={() => clearSingleFilter(filter.name)}
                 />
               ))}
