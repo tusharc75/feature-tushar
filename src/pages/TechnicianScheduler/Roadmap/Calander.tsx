100c263b2dd2e20b226ccc3df3171f062940f824
unknown <romit@vebholic.com>
2023-02-16 14:25:40 +0530

    fix: bug fix

--- src/pages/TechnicianScheduler/Roadmap/Calander.tsx
+++ src/pages/TechnicianScheduler/Roadmap/Calander.tsx
@@ -43,7 +43,7 @@ const DaysBetweenDates = function (calendarType, startDate, endDate) {
 };
 
 function Calendar({ calendarType, dayPixel, startDate, endDate }) {
-  
+
   const [totalDay, setTotalDay] = React.useState(0);
   const [dates, setDates] = React.useState(null);
   const [calType, setCalType] = React.useState(calendarType);
@@ -52,7 +52,7 @@ function Calendar({ calendarType, dayPixel, startDate, endDate }) {
     setCalType(calendarType);
     setTotalDay(endDate.diff(startDate, 'days') + 1);
     setDates(DaysBetweenDates(calendarType, startDate, endDate));
-  }, [calendarType]);
+  }, [calendarType, startDate, endDate]);
 
   return (
     dates &&
@@ -60,69 +60,69 @@ function Calendar({ calendarType, dayPixel, startDate, endDate }) {
       <Box height={60} display="flex" width={totalDay * dayPixel} bgcolor="grey.200" style={{ position: 'sticky', top: 0, zIndex: 2 }}>
         {calendarType === 'week'
           ? Array.from(dates, (date, index) => {
-              return (
-                <Box key={index} borderColor="grey.300" minWidth={moment(date).daysInMonth() * dayPixel}>
-                  <Typography variant="caption" color="textSecondary" display="block">
-                    {moment(date).format('MMM YYYY').toUpperCase()}
-                  </Typography>
-                  {Array.from(Array(moment(date).daysInMonth()), (data, index) => {
-                    return (
-                      <div
-                        key={index}
-                        style={{
-                          minWidth: dayPixel,
-                          maxWidth: dayPixel,
-                          textAlign: 'center',
-                          float: 'left',
-                          borderLeft: '1px solid #dfdfdf'
-                        }}
-                      >
-                        <Typography variant="body2" color={'textSecondary'} display="block">
-                          {moment(moment(date).format('YYYY-MM') + '-' + (index + 1)).format('ddd')[0]}
-                        </Typography>
-                        <Typography variant="caption" color={'textSecondary'} display="block">
-                          {moment(moment(date).format('YYYY-MM') + '-' + (index + 1)).format('D')}
-                        </Typography>
-                      </div>
-                    );
-                  })}
-                </Box>
-              );
-            })
+            return (
+              <Box key={index} borderColor="grey.300" minWidth={moment(date).daysInMonth() * dayPixel}>
+                <Typography variant="caption" color="textSecondary" display="block">
+                  {moment(date).format('MMM YYYY').toUpperCase()}
+                </Typography>
+                {Array.from(Array(moment(date).daysInMonth()), (data, index) => {
+                  return (
+                    <div
+                      key={index}
+                      style={{
+                        minWidth: dayPixel,
+                        maxWidth: dayPixel,
+                        textAlign: 'center',
+                        float: 'left',
+                        borderLeft: '1px solid #dfdfdf'
+                      }}
+                    >
+                      <Typography variant="body2" color={'textSecondary'} display="block">
+                        {moment(moment(date).format('YYYY-MM') + '-' + (index + 1)).format('ddd')[0]}
+                      </Typography>
+                      <Typography variant="caption" color={'textSecondary'} display="block">
+                        {moment(moment(date).format('YYYY-MM') + '-' + (index + 1)).format('D')}
+                      </Typography>
+                    </div>
+                  );
+                })}
+              </Box>
+            );
+          })
           : null}
         {calendarType === 'month'
           ? Array.from(dates, (date, index) => {
-              return (
-                <Box
-                  key={index}
-                  p={2}
-                  display="inline"
-                  minWidth={moment(date).daysInMonth() * dayPixel}
-                  border={1}
-                  borderColor="grey.300"
-                  textAlign="center"
-                >
-                  <Typography variant="body2" color="textSecondary" display="block">
-                    {moment(date).format('MMM YYYY').toUpperCase()}
-                  </Typography>
-                </Box>
-              );
-            })
+            return (
+              <Box
+                key={index}
+                p={2}
+                display="inline"
+                minWidth={moment(date).daysInMonth() * dayPixel}
+                border={1}
+                borderColor="grey.300"
+                textAlign="center"
+              >
+                <Typography variant="body2" color="textSecondary" display="block">
+                  {moment(date).format('MMM YYYY').toUpperCase()}
+                </Typography>
+              </Box>
+            );
+          })
           : null}
         {calendarType === 'quater'
           ? dates.map((date, index) => (
-              <Box key={index} p={2} display="inline" minWidth={dayPixel * parseInt(date.days)} border={1} borderColor="grey.300" textAlign="center">
-                <Typography variant="body2" display="block">
-                  {(
-                    moment(date.q_s_date).format('MMM') +
-                    ' - ' +
-                    moment(date.q_e_date).format('MMM') +
-                    ' ' +
-                    moment(date.q_s_date).format('YYYY')
-                  ).toUpperCase()}
-                </Typography>
-              </Box>
-            ))
+            <Box key={index} p={2} display="inline" minWidth={dayPixel * parseInt(date.days)} border={1} borderColor="grey.300" textAlign="center">
+              <Typography variant="body2" display="block">
+                {(
+                  moment(date.q_s_date).format('MMM') +
+                  ' - ' +
+                  moment(date.q_e_date).format('MMM') +
+                  ' ' +
+                  moment(date.q_s_date).format('YYYY')
+                ).toUpperCase()}
+              </Typography>
+            </Box>
+          ))
           : null}
       </Box>
     )
