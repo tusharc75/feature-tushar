e40bb0225780ade5e9468c4a368bfb20e51284d6
manu patel <manupatel18112000@gmail.com>
2023-02-24 18:28:41 +0530

    add filter in calendar view in rental planning calendar

--- src/pages/RentalPlanningCalendar/CalendarView.tsx
+++ src/pages/RentalPlanningCalendar/CalendarView.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useCallback, useMemo, useState } from 'react'
+import { useHistory } from 'react-router-dom';
 import { Calendar, View, momentLocalizer } from 'react-big-calendar'
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss'
 import moment from 'moment';
-import { Grid, makeStyles, Checkbox, TextField } from '@material-ui/core';
+import { Grid, makeStyles, Checkbox, TextField, ButtonGroup, Button, Popper, Paper, ClickAwayListener, MenuList, MenuItem, Grow } from '@material-ui/core';
 import axiosInstance from 'src/axios/axiosInstance';
 import { Autocomplete } from '@material-ui/lab';
+import routes from 'src/components/Helpers/Routes';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 const DragAndDropCalendar = withDragAndDrop(Calendar as any)
 const localizer = momentLocalizer(moment);
@@ -29,10 +32,16 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-export default function CalendarView() {
 
+const options = ['All', 'Rental', 'Schedule'];
+
+export default function CalendarView() {
+    const history = useHistory();
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const [selectedOption, setSelectedOption] = useState(options[0])
     const [events, setEvents] = useState([])
+    const [totalEvents, setTotalEvents] = useState([])
     const [view, setView] = useState<View>('month');
     const [staticEvents, setStaticEvents] = useState([])
     const [filterToKeep, setFilterToKeep] = useState([]);
@@ -49,6 +58,10 @@ export default function CalendarView() {
         estimateStartDate: moment().startOf('month').format('MM/DD/YYYY'),
         estimateEndDate: moment().endOf('month').format('MM/DD/YYYY')
     })
+
+    const [updateCount, setUpdateCount] = useState(0)
+
+    const anchorRef = React.useRef<HTMLDivElement>(null);
 
     const defaultDate = useMemo(() => moment().toDate(), [])
 
@@ -87,37 +100,36 @@ export default function CalendarView() {
         return query;
     }
 
+    const createDataForCalendar = (data: [], type: string) => {
+        const createdData = data?.map((d: any) => {
+            const title = type === 'rental' ? d.rentalJobName : type === 'schedule' ? d.scheduleNumber : '- - -'
+            const start = type === 'rental' ? new Date(d.estimateStartDate) : type === 'schedule' ? new Date(d.startDate) : '- - -'
+            const end = type === 'rental' ? new Date(d.estimateEndDate) : type === 'schedule' ? new Date(d.endDate) : '- - -'
+            return (
+                {
+                    id: d._id,
+                    title: title,
+                    start: start,
+                    end: end,
+                    allDay: true,
+                    type: type
+                }
+            )
+        });
+        return createdData;
+    }
+
     const fetchData = () => {
         const queryString = getQueryString();
         axiosInstance()
             .get(queryString)
             .then(({ data: { data } }) => {
-                const rentalData = data?.rental.map((d: any) => {
-                    return (
-                        {
-                            id: d._id,
-                            title: d.rentalJobName,
-                            start: new Date(d.estimateStartDate),
-                            end: new Date(d.estimateEndDate),
-                            allDay: true,
-                            type: 'rental'
-                        }
-                    )
-                });
-                const scheduleData = data?.schedule.map((d: any) => {
-                    return (
-                        {
-                            id: d._id,
-                            title: d.scheduleNumber,
-                            start: new Date(d.startDate),
-                            end: new Date(d.endDate),
-                            allDay: true,
-                            type: 'schedule'
-                        }
-                    )
-                });
-                setEvents([...rentalData, ...scheduleData]);
+                const rentalData = createDataForCalendar(data.rental, 'rental')
+                const scheduleData = createDataForCalendar(data.schedule, 'schedule')
+                setTotalEvents([...rentalData, ...scheduleData])
                 setStaticEvents([...rentalData, ...scheduleData])
+                setEvents([...rentalData, ...scheduleData]);
+                setUpdateCount(updateCount + 1)
             })
             .catch((err) => { });
     }
@@ -166,32 +178,124 @@ export default function CalendarView() {
         [setView]
     );
 
+    const handleClose = (event: React.MouseEvent<Document, MouseEvent>) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target as HTMLElement)) {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+
+    const filterEvent = () => {
+        const option = selectedOption === options[1] ? 'rental' : selectedOption === options[2] ? 'schedule' : null
+        if (option) {
+            const filteredEvents = totalEvents.filter((item) => item.type === option)
+            setEvents(filteredEvents)
+        } else {
+            setEvents(totalEvents)
+        }
+    }
+
+    useEffect(() => {
+        if (updateCount > 1) {
+            filterEvent()
+        }
+    }, [updateCount])
+
+    useEffect(() => {
+        filterEvent()
+    }, [selectedOption])
+
     return (
         <>
             <div className={`bgLight ${classes.whiteBg}`}>
                 <Grid container spacing={2} className={`greyBox ${classes.topbar}`}>
-                    <Grid item xs={12} sm={6} md={4} lg={4}>
-                        <Autocomplete
-                            fullWidth
-                            multiple
-                            options={Object.keys(RENTAL_PLANNING_CALENDAR_FILTER)?.map((key) => key) || []}
-                            disableCloseOnSelect
-                            getOptionLabel={(option) => RENTAL_PLANNING_CALENDAR_FILTER[option]}
-                            renderOption={(option: any) => (
-                                <React.Fragment>
-                                    <Checkbox checked={filterToKeep?.includes(option)} />
-                                    {RENTAL_PLANNING_CALENDAR_FILTER[option]}
-                                </React.Fragment>
-                            )}
-                            size="small"
-                            renderInput={(params) => <TextField {...params} label="Filters" placeholder="filter" variant="outlined" />}
-                            value={filterToKeep}
-                            onChange={(event: any, newValue: any) => {
-                                setFilterToKeep(newValue);
-                            }}
-                        />
+                    <Grid item xs={12} sm={6} md={6} lg={6}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={12} md={3} lg={3}>
+                                <ButtonGroup
+                                    id="approveDisapprove"
+                                    size="small"
+                                    className={'accountActions'}
+                                    variant="outlined"
+                                    color="primary"
+                                    ref={anchorRef}
+                                    aria-label="small outlined button group"
+                                >
+                                    <Button style={{ minWidth: '85px' }}>{selectedOption}</Button>
+                                    <Button
+                                        color="primary"
+                                        size="small"
+                                        aria-controls={open ? 'split-button-menu' : undefined}
+                                        aria-expanded={open ? 'true' : undefined}
+                                        aria-label="select merge strategy"
+                                        aria-haspopup="menu"
+                                        onClick={handleToggle}
+                                        className="all-button"
+                                    >
+                                        <ArrowDropDownIcon className="all-button-sub-icon" />
+                                    </Button>
+                                </ButtonGroup>
+                                <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal style={{ zIndex: 1111111 }}>
+                                    {({ TransitionProps, placement }) => (
+                                        <Grow
+                                            {...TransitionProps}
+                                            style={{
+                                                transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom'
+                                            }}
+                                        >
+                                            <Paper>
+                                                <ClickAwayListener onClickAway={handleClose}>
+                                                    <MenuList id="menu" style={{ backgroundColor: 'transparent', fontSize: '10px' }}>
+                                                        {options.map((option, index) => (
+                                                            <MenuItem
+                                                                key={option}
+                                                                selected={option === selectedOption}
+                                                                onClick={(event) => {
+                                                                    setSelectedOption(options[index])
+                                                                    setOpen(false);
+                                                                }}
+                                                                style={{ color: 'black' }}
+                                                            >
+                                                                {option}
+                                                            </MenuItem>
+                                                        ))}
+                                                    </MenuList>
+                                                </ClickAwayListener>
+                                            </Paper>
+                                        </Grow>
+                                    )}
+                                </Popper>
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={9} lg={9}>
+                                <Autocomplete
+                                    fullWidth
+                                    multiple
+                                    options={Object.keys(RENTAL_PLANNING_CALENDAR_FILTER)?.map((key) => key) || []}
+                                    disableCloseOnSelect
+                                    getOptionLabel={(option) => RENTAL_PLANNING_CALENDAR_FILTER[option]}
+                                    renderOption={(option: any) => (
+                                        <React.Fragment>
+                                            <Checkbox checked={filterToKeep?.includes(option)} />
+                                            {RENTAL_PLANNING_CALENDAR_FILTER[option]}
+                                        </React.Fragment>
+                                    )}
+                                    size="small"
+                                    renderInput={(params) => <TextField {...params} label="Filters" placeholder="filter" variant="outlined" />}
+                                    value={filterToKeep}
+                                    onChange={(event: any, newValue: any) => {
+                                        setFilterToKeep(newValue);
+                                    }}
+                                />
+                            </Grid>
+                        </Grid>
+
                     </Grid>
-                    <Grid item xs={12} sm={6} md={8} lg={8}>
+                    <Grid item xs={12} sm={6} md={6} lg={6}>
                         <Grid container spacing={1}>
                             {filterToKeep?.includes('warehouse') &&
                                 < Grid item xs={12} sm={6} md={4} lg={4}>
@@ -276,6 +380,13 @@ export default function CalendarView() {
                                 estimateStartDate: moment(date).startOf('month').format('MM/DD/YYYY'),
                                 estimateEndDate: moment(date).endOf('month').format('MM/DD/YYYY')
                             });
+                        }
+                    }}
+                    onSelectEvent={(event: any) => {
+                        if (event.type === 'rental') {
+                            history.push(`${routes.rentalManagementDetail.path}/${event.id}`);
+                        } else if (event.type === 'schedule') {
+                            history.push(`${routes.scheduleDetail.path}/${event.id}`);
                         }
                     }}
                 />
