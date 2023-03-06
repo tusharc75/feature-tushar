fbd2580a5eed04bf32220c9fe981f234e188ed0a
manu patel <manupatel18112000@gmail.com>
2023-03-06 16:52:38 +0530

    fix changes in cakendar view

--- src/pages/RentalPlanningCalendar/CalendarView.tsx
+++ src/pages/RentalPlanningCalendar/CalendarView.tsx
@@ -369,7 +369,6 @@ function CalendarView() {
             }
         </Box>
         <DragAndDropCalendar
-            style={{ height: "calc(100vh - 50px)" }}
             defaultDate={defaultDate}
             defaultView={'day'}
             events={events}
