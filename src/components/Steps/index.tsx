603d0bee056fadfe1d6089a3f614de0da72482f8
dhrubobarman <dhrubobarman847@gmail.com>
2023-05-04 10:42:59 +0530

    fix: pPIR-359

--- src/components/Steps/index.tsx
+++ src/components/Steps/index.tsx
@@ -79,7 +79,7 @@ const Steps = ({
     <div>
       {isMobile ? (
         <MobileSteps
-          stepName={`${activeStep + 1}/${steps.length} ${steps[currentStep].title ? steps[currentStep].title : ''}`}
+          stepName={`${activeStep + 1}/${steps.length} ${steps[currentStep]?.title ? steps[currentStep].title : ''}`}
           nextButton={
             <Button
               size="small"
