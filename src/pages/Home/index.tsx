46072a9901f7435032997dfc9c7ff92ce270da9d
dhrubobarman <dhrubobarman847@gmail.com>
2023-05-09 15:58:15 +0530

    feat: new Dashboard modal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -167,7 +167,7 @@ const DisplayCardGrid = ({ sections, handleRoutes }) => {
               role="button"
               className={styles.singlecard}
               style={style}
-              onClick={() => section.items.length > 0 && setModalContent({ items: section.items, title: section.head })}
+              onClick={() => section.items.length > 0 && setModalContent({ items: section.items, title: section.head, icon: section.icon })}
             >
               <div className={styles.cardContent}>
                 <div className={styles.cardTop}>
@@ -208,32 +208,50 @@ const RenderDialog = ({ modalContent, handleClose, handleRoutes }) => {
           backdropFilter: 'blur(2px)'
         }
       }}
+      PaperProps={{
+        style: {
+          borderRadius: 16,
+          margin: 15
+        }
+      }}
       open={Boolean(modalContent)}
       className={styles.dialogContainer}
     >
-      <MuiDialogTitle disableTypography className={styles.modalHead}>
-        <Typography variant="h6" className={styles.modalTitle}>
-          {modalContent?.title}
-        </Typography>
-        <IconButton aria-label="close" onClick={() => handleClose()}>
-          <CloseIcon />
-        </IconButton>
-      </MuiDialogTitle>
-      <DialogContent className={styles.dialogContent}>
-        <ul className={styles.linkList}>
-          {modalContent?.items
-            ?.filter((item) => !item?.isHidden)
-            .map((item) => (
-              <li key={item.name}>
-                <Typography component="span">
-                  <Link to={handleRoutes(item)} className={styles.dialogLinks}>
-                    {item.resourceLabel || item.name}
-                  </Link>
-                </Typography>
-              </li>
-            ))}
-        </ul>
-      </DialogContent>
+      <Box className={styles.dialogContentContainer}>
+        <MuiDialogTitle disableTypography className={styles.modalHead}>
+          <Box className={styles.modalIconAndName}>
+            <Box className={styles.modalIcon}>{modalContent?.icon}</Box>
+            <Typography variant="h6" className={styles.modalTitle}>
+              {modalContent?.title}
+            </Typography>
+          </Box>
+          <IconButton aria-label="close" onClick={() => handleClose()}>
+            <CloseIcon />
+          </IconButton>
+        </MuiDialogTitle>
+        <DialogContent className={styles.dialogContent}>
+          <ul className={styles.linkList}>
+            {modalContent?.items
+              ?.filter((item) => !item?.isHidden)
+              .map((item) => (
+                <li key={item.name}>
+                  <Typography component="span">
+                    <Link to={handleRoutes(item)} className={styles.dialogLinks}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 13 13" fill="none">
+                        <path
+                          d="M6.50049 0H13.0005V6.5H12.188V1.39014L0.59082 12.981L0.0195312 12.4097L11.6104 0.8125H6.50049V0Z"
+                          fill="currentcolor"
+                          stroke="currentcolor"
+                        ></path>
+                      </svg>
+                      {item.resourceLabel || item.name}
+                    </Link>
+                  </Typography>
+                </li>
+              ))}
+          </ul>
+        </DialogContent>
+      </Box>
     </Dialog>
   );
 };
@@ -267,7 +285,12 @@ const DisplaySideCard = ({ objBySectionName, handleRoutes, mode = 'Collaboration
   return (
     <>
       {colabData ? (
-        <div role="button" style={style} className={`${styles.rightInner} `} onClick={() => setModalContent({ items: colabData, title: mode })}>
+        <div
+          role="button"
+          style={style}
+          className={`${styles.rightInner} `}
+          onClick={() => setModalContent({ items: colabData, title: mode, icon: <img src={SVGImages(mode)} alt={`${mode} Logo`} /> })}
+        >
           <img src={SVGImages(mode)} alt={`${mode} Logo`} className={styles.colabLogo} />
           <Typography component={'h2'}>{mode}</Typography>
           <Typography component={'p'}>{description}</Typography>
