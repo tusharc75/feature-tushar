e191c3474cc7a3704efbf6c4e1a0085606974add
Romit Sadaria <romit.sadaria@gmail.com>
2023-05-12 07:49:31 -0500

    fix: bug fix

--- src/pages/Report/AverageCostHistory/index.tsx
+++ src/pages/Report/AverageCostHistory/index.tsx
@@ -85,7 +85,7 @@ const AverageCostHistory = ({ handleClose, product, productName, showPricefilter
             }
         },
         { field: "warehouse", headerName: routes.warehouse.title, show: true, cellRenderer: "commonRenderer", },
-        { field: 'purchaseOrderRejectedDate', headerName: 'Purchase Order Rejected Date', filter: false, sortable: false, cellRenderer: 'dateTimeRenderer' },
+        //{ field: 'purchaseOrderRejectedDate', headerName: 'Purchase Order Rejected Date', filter: false, sortable: false, cellRenderer: 'dateTimeRenderer' },
         { field: 'transactionDate', headerName: 'Actual Transaction Date', show: false, filter: false, sortable: false, cellRenderer: 'dateTimeRenderer' }
     ];
 
