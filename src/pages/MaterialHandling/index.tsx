08fb40badd9b8c52b30cd2e66383158ca874be89
Romit Sadaria <romit.sadaria@gmail.com>
2023-05-17 14:26:20 +0530

    fix: bug fix

--- src/pages/MaterialHandling/index.tsx
+++ src/pages/MaterialHandling/index.tsx
@@ -9,7 +9,6 @@ import CommonSkeleton from 'src/components/Helpers/CommonSkeleton';
 import Request from './Request';
 import RefreshIcon from '@material-ui/icons/Refresh';
 import { Autocomplete } from '@material-ui/lab';
-import { Link } from 'react-router-dom';
 import TabPanel from 'src/components/TabPanel';
 import Consumables from '../WorkOrder/Consumables';
 
@@ -22,7 +21,7 @@ const MaterialHandling = () => {
 
   const [workOrder, setWorkOrder] = useState(null);
   const [selectedWorkOrder, setSelectedWorkOrder] = useState(null);
-  const [plantOptions, setPlantOptions] = useState([]);
+  const [warehouseOptions, setWarehouseOptions] = useState([]);
   const [selectedPlant, setSelectedPlant] = useState(null);
   const [tabValue, setTabValue] = useState<any>(0);
 
@@ -30,21 +29,39 @@ const MaterialHandling = () => {
     axiosInstance()
       .get('/sa-formbuilder/lookup?lookupResource=Warehouse')
       .then(({ data: { data } }) => {
-        setPlantOptions(data.Warehouse);
+        const warehouses: any = [];
+        if (data['Warehouse'] && data['Warehouse']?.length) {
+          data['Warehouse']?.forEach((ele) => {
+            if (ele?.manager && ele?.manager?.includes(user?.user?._id)) {
+              warehouses.push(ele)
+            }
+          })
+        }
+        setWarehouseOptions(warehouses);
       });
   }, []);
 
   useEffect(() => {
-    fetchData();
-  }, [selectedPlant]);
+    if (warehouseOptions && warehouseOptions?.length) {
+      fetchData();
+    }
+    else {
+      setWorkOrder([])
+    }
+  }, [selectedPlant, warehouseOptions]);
 
   const fetchData = () => {
     setWorkOrder(null);
     setSelectedWorkOrder(null);
     let api = `/material-handling`;
+    const filterById: any = []
     if (selectedPlant) {
-      api = `${api}?filterById=${JSON.stringify([{ field: 'warehouse', term: selectedPlant.optionValue }])}&filterType=and`;
+      filterById.push({ field: 'warehouse', term: selectedPlant.optionValue })
     }
+    else {
+      filterById.push({ field: 'warehouse', term: { "$in": warehouseOptions?.map((e) => e?.optionValue) } })
+    }
+    api = `${api}?filterById=${JSON.stringify(filterById)}&filterType=and`;
     axiosInstance()
       .get(api)
       .then(({ data: { data } }) => {
@@ -81,13 +98,13 @@ const MaterialHandling = () => {
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6} md={3}>
               <Autocomplete
-                options={plantOptions}
+                options={warehouseOptions}
                 fullWidth
                 getOptionLabel={(option: any) => option.optionLabel}
                 getOptionSelected={(option: any, value: any) => option.optionValue === value.optionValue}
                 value={
-                  plantOptions.filter((data) => data.optionValue === selectedPlant?.optionValue).length
-                    ? plantOptions.filter((data) => data.optionValue === selectedPlant?.optionValue)[0]
+                  warehouseOptions.filter((data) => data.optionValue === selectedPlant?.optionValue).length
+                    ? warehouseOptions.filter((data) => data.optionValue === selectedPlant?.optionValue)[0]
                     : ''
                 }
                 onChange={(e, val) => {
@@ -114,10 +131,11 @@ const MaterialHandling = () => {
                     key={index}
                     onClick={() => {
                       setSelectedWorkOrder(data);
+                      setTabValue(0)
                     }}
                     style={{
                       cursor: 'pointer',
-                      backgroundColor: selectedWorkOrder === data ? '#298b88' : 'white',
+                      backgroundColor: selectedWorkOrder === data ? '#0f9fa9' : 'white',
                       color: selectedWorkOrder === data ? 'white' : 'black',
                       border: '1px solid #ebebeb'
                     }}
@@ -134,12 +152,7 @@ const MaterialHandling = () => {
             </Grid>
             <Grid item xs={12} md={9} sm={12}>
               {selectedWorkOrder && (
-                <Box
-                  style={{
-                    border: '1px solid #D3D3D3',
-                    borderTop: 'none'
-                  }}
-                >
+                <Box>
                   <Tabs
                     className="new-tab-container-v1"
                     value={tabValue}
@@ -155,8 +168,7 @@ const MaterialHandling = () => {
                       className={'tabLayout'}
                       label={
                         <div className="d-flex align-items-center tab-font">
-                          {/* <FaWpforms className="mr-1" fontSize="inherit" /> */}
-                          Field Ticket
+                          Requests
                         </div>
                       }
                       {...a11yProps(0)}
@@ -165,7 +177,6 @@ const MaterialHandling = () => {
                       className={'tabLayout'}
                       label={
                         <div className="d-flex align-items-center tab-font">
-                          {/* <BiFoodMenu className="mr-1" fontSize="inherit" /> */}
                           Consumables
                         </div>
                       }
@@ -180,7 +191,7 @@ const MaterialHandling = () => {
                   <TabPanel value={tabValue} index={1}>
                     <Box>
                       <Consumables
-                        allowedToEdit={true}
+                        allowedToEdit={false}
                         isCreate={false}
                         workOrderId={selectedWorkOrder?._id}
                         warehouse={selectedWorkOrder?.warehouse?.optionValue}
