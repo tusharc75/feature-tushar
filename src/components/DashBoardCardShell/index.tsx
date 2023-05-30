bc505eca97a0eca0e62b59bb0a7909f31a243ff9
dhrubobarman <dhrubobarman847@gmail.com>
2023-05-30 14:11:17 +0530

    feat: added dark theme support in  shell card component

--- src/components/DashBoardCardShell/index.tsx
+++ src/components/DashBoardCardShell/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './dashBoardCardShell.module.scss';
+import { useAppTheme } from 'src/constants/AppConfig';
 
 interface CardInterface extends React.HTMLAttributes<HTMLDivElement> {
   background?: string;
   gradientColors?: string[];
   minHeight?: boolean;
+  darkThemeBackgroundColor?: string;
 }
 
 const DashBoardCardShell: React.FC<CardInterface> = ({
@@ -14,10 +16,12 @@ const DashBoardCardShell: React.FC<CardInterface> = ({
   gradientColors = ['#577BFC', '#1608BD'],
   style,
   minHeight = true,
+  darkThemeBackgroundColor = 'var(--dark-primary)',
   ...props
 }) => {
+  const [theme] = useAppTheme();
   const styleConfig = {
-    '--bg_color': background,
+    '--bg_color': theme === 'dark' ? darkThemeBackgroundColor : background,
     textAlign: 'left',
     '--bg-gradient-colors': `to bottom, ${gradientColors.join(', ')}`,
     '--min-height': minHeight ? '219px' : 'auto'
