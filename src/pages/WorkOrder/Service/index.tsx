736d56f52dbff9695b72e188634fc3b1035905bb
Romit Sadaria <romit.sadaria@gmail.com>
2023-05-30 12:18:00 +0530

    fix: bug fix

--- src/pages/WorkOrder/Service/index.tsx
+++ src/pages/WorkOrder/Service/index.tsx
@@ -55,7 +55,7 @@ const getTotalTime = (stepTimes: any) => {
       totalTimes += new Date().getTime() - new Date(item?.pauseDate || item?.startDate).getTime();
     }
   });
-  stepTimes.forEach((item) => {});
+  stepTimes.forEach((item) => { });
   return { shouldTimerRun, totalTimes };
 };
 
@@ -229,7 +229,7 @@ const Service = ({ workOrderId, allowedToEdit, workOrderData, completed, fetchWo
       setServiceSteps(services);
       if (
         services.filter((e) => e.type === 'service' && e.status === WORKORDER_SERVICE_STATUS.completed)?.length ===
-          services.filter((e) => e.type === 'service')?.length &&
+        services.filter((e) => e.type === 'service')?.length &&
         workOrderData?.status !== WORK_ORDER_STATUS.completed
       ) {
         fetchWorkOrderData();
@@ -655,24 +655,24 @@ const Service = ({ workOrderId, allowedToEdit, workOrderData, completed, fetchWo
                                                   data?.status === WORKORDER_SERVICE_STEP_STATUS.completed
                                                     ? '#E1FCE3'
                                                     : data?.status === WORKORDER_SERVICE_STEP_STATUS.failed
-                                                    ? '#fabebe'
-                                                    : '#FFF5DD',
+                                                      ? '#fabebe'
+                                                      : '#FFF5DD',
                                                 color:
                                                   data?.status === WORKORDER_SERVICE_STEP_STATUS.completed
                                                     ? data?.serviceStatus === WORKORDER_SERVICE_STEP_STATUS.passed
                                                       ? '#059825'
                                                       : '#EE0E06'
                                                     : data?.status === WORKORDER_SERVICE_STEP_STATUS.failed
-                                                    ? '#fa0202'
-                                                    : '#FF8C21',
+                                                      ? '#fa0202'
+                                                      : '#FF8C21',
                                                 background:
                                                   data?.status === WORKORDER_SERVICE_STEP_STATUS.completed
                                                     ? data?.serviceStatus === WORKORDER_SERVICE_STEP_STATUS.passed
                                                       ? '#E1FCE3'
                                                       : '#FFECEB'
                                                     : data?.status === WORKORDER_SERVICE_STEP_STATUS.failed
-                                                    ? '#fabebe'
-                                                    : '#FFF5DD',
+                                                      ? '#fabebe'
+                                                      : '#FFF5DD',
                                                 fontWeight: 700
                                               }}
                                             />
@@ -761,7 +761,7 @@ const Service = ({ workOrderId, allowedToEdit, workOrderData, completed, fetchWo
                 <>
                   {selectedService?.type === 'service' ? (
                     allowedToEdit ||
-                    (selectedService?.assignedUsers?.length > 0 && selectedService?.assignedUsers?.map((u) => u?.optionValue).includes(user?._id)) ? (
+                      (selectedService?.assignedUsers?.length > 0 && selectedService?.assignedUsers?.map((u) => u?.optionValue).includes(user?._id)) ? (
                       <Steps
                         workOrderId={workOrderId}
                         warehouse={workOrderData?.warehouse}
@@ -769,6 +769,7 @@ const Service = ({ workOrderId, allowedToEdit, workOrderData, completed, fetchWo
                         allowedToEdit={isAllowedToServiceEdit && selectedService?.clickable}
                         setDisableCompleteFail={setDisableCompleteFail}
                         fetchService={fetchServiceData}
+                        referencType='workOrder'
                       />
                     ) : (
                       <Box textAlign="center">
@@ -924,20 +925,20 @@ const Service = ({ workOrderId, allowedToEdit, workOrderData, completed, fetchWo
                                                         data?.status === WORKORDER_SERVICE_STEP_STATUS.completed
                                                           ? '#E1FCE3'
                                                           : data?.status === WORKORDER_SERVICE_STEP_STATUS.failed
-                                                          ? '#fabebe'
-                                                          : '#FFF5DD',
+                                                            ? '#fabebe'
+                                                            : '#FFF5DD',
                                                       color:
                                                         data?.status === WORKORDER_SERVICE_STEP_STATUS.completed
                                                           ? '#048E0A'
                                                           : data?.status === WORKORDER_SERVICE_STEP_STATUS.failed
-                                                          ? '#fa0202'
-                                                          : '#FF8C21',
+                                                            ? '#fa0202'
+                                                            : '#FF8C21',
                                                       background:
                                                         data?.status === WORKORDER_SERVICE_STEP_STATUS.completed
                                                           ? '#E1FCE3'
                                                           : data?.status === WORKORDER_SERVICE_STEP_STATUS.failed
-                                                          ? '#fabebe'
-                                                          : '#FFF5DD',
+                                                            ? '#fabebe'
+                                                            : '#FFF5DD',
                                                       fontWeight: 700
                                                     }}
                                                   />
@@ -1253,7 +1254,7 @@ const Service = ({ workOrderId, allowedToEdit, workOrderData, completed, fetchWo
           handleClose={() => {
             setAttchmentsDialog({ open: false, uniqueServiceId: null, stepId: null, serviceName: null, stepName: null });
           }}
-          handleSuccess={() => {}}
+          handleSuccess={() => { }}
         />
       )}
       {showManagePurchaseOrder && (
