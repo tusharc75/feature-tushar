13ea067b64db5bfeda1b7dc972d2cabf8f818fe8
sunanda35 <sunandasamanta60@gmail.com>
2023-06-05 14:25:59 +0530

    fix

--- src/components/FormBuilder/Properties/FieldDependent.tsx
+++ src/components/FormBuilder/Properties/FieldDependent.tsx
@@ -13,8 +13,9 @@ function FieldDependent({ fields, values, fieldSet }) {
 
   const getFields = async () => {
     setResourceFieldsLoading(true);
+    const resource = fields.filter((data) => data.fieldName === values['lookupDependentOn'])[0];
     try {
-      const res = await axiosInstance().get('/field?resource=Quotation');
+      const res = await axiosInstance().get(`/field?resource=${resource.lookupResource}`);
       setResourceFields(res.data.data);
       setResourceFieldsLoading(false);
     } catch (e) {
