43c353230552c9feb32beb3a527526895f08a12f
dhrubobarman <dhrubobarman847@gmail.com>
2023-06-06 13:58:15 +0530

    feat: new Material handling

--- src/pages/MaterialHandling/Request/index.tsx
+++ src/pages/MaterialHandling/Request/index.tsx
@@ -18,18 +18,19 @@ import HistoryIcon from '@material-ui/icons/History';
 import ProcessLogs from 'src/pages/WorkOrder/Consumables/ProcessLogs';
 
 import CustomTable, { ColumnsInterface } from 'src/components/CustomTable';
+import CustomTableWithCard, { CardInterface } from 'src/components/CustomTableWithCard';
 
 const Request = ({ workOrder }) => {
   const toastConfig = useContext(CustomToastContext);
 
   const [loading, setLoading] = useState(false);
 
-  const [columns, setColumns] = useState(null);
   const [rowsData, setRowsData] = useState(null);
   const [selectedRecords, setSelectedRecords] = useState([]);
   const [anchorEl, setAnchorEl] = useState(null);
   const [qtyDialog, setQtyDialog] = useState({ open: false, status: null, data: null });
   const [openProcessLogs, setOpenProcessLogs] = useState({ open: false, logs: [], productName: '', product: '', data: null });
+  const [accessor, setAccessor] = useState<CardInterface | null>(null);
 
   const {
     state: { user }
@@ -85,213 +86,174 @@ const Request = ({ workOrder }) => {
   };
 
   const fetchColumn = async () => {
-    setColumns(null);
-    // const {
-    //   data: { data }
-    // } = await axiosInstance().put(`/field/find-field-labels`, {
-    //   fields: [
-    //     {
-    //       resource: 'Product',
-    //       fieldNames: ['productName', 'productNumber', 'productDescription']
-    //     }
-    //   ]
-    // });
-    // const productFields = data?.find((e) => e.resource === 'Product')?.fieldNames || [];
-
-    const column: ColumnsInterface[] = [
-      {
-        header: 'Product',
-        width: 100,
-        render: ({ row }) => {
-          return (
-            <>
-              <Typography className="text-truncate new-table-font">
-                <a className="link" href={`${routes.productDetail.path}/${row?.product?.optionValue}`} target="_blank">
-                  {row['productName']}
-                </a>
-              </Typography>
-              <Typography className="text-truncate new-table-font">
-                <span>{row['productNumber']}</span>
-              </Typography>
-              <Typography className="text-truncate new-table-font">
-                <span>{row['productDescription']}</span>
-              </Typography>
-            </>
-          );
+    setAccessor(null);
+    const accessor: CardInterface = {
+      name: (row) => (
+        <>
+          Product Type :{' '}
+          <a className="link" href={`${routes.productDetail.path}/${row?.product?.optionValue}`} target="_blank">
+            {row['productName']}
+          </a>
+        </>
+      ),
+      headerColumns: [
+        {
+          style: { marginRight: 'auto' },
+          render: (row) => row['status'],
+          component: (row) => (row['status'] === 'Processed' ? 'completedChip' : 'pendingChip')
+        },
+        {
+          render: (row) => {
+            return (
+              <>
+                <Box display="flex">
+                  <Box display="flex" flexGrow={1}>
+                    {row['status'] === MATERIAL_REQUEST_STATUS.requested && (
+                      <Fragment>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          size="small"
+                          disabled={loading}
+                          onClick={() => {
+                            setQtyDialog({ open: true, status: MATERIAL_REQUEST_STATUS.processed, data: row });
+                          }}
+                        >
+                          Process
+                        </Button>
+                        <Box pl={1} />
+                        <Button
+                          variant="outlined"
+                          color="secondary"
+                          size="small"
+                          disabled={loading}
+                          onClick={() => {
+                            setQtyDialog({ open: true, status: MATERIAL_REQUEST_STATUS.closed, data: row });
+                          }}
+                        >
+                          Close
+                        </Button>
+                        <Box pl={1} />
+                      </Fragment>
+                    )}
+                  </Box>
+                  <Box>
+                    {row['processesLogs'] && row['processesLogs']?.length > 0 && (
+                      <HtmlTooltip title="View Process Logs">
+                        <IconButton
+                          size="small"
+                          aria-label="Delete"
+                          onClick={() => {
+                            setOpenProcessLogs({
+                              open: true,
+                              logs: row['processesLogs'],
+                              productName: row?.productName,
+                              product: row?.product?.optionValue,
+                              data: row
+                            });
+                          }}
+                        >
+                          <HistoryIcon />
+                        </IconButton>
+                      </HtmlTooltip>
+                    )}
+                  </Box>
+                </Box>
+              </>
+            );
+          }
         }
-      },
-      {
-        header: 'Requested Qty',
-        width: 100,
-        render: ({ row }) => {
-          return (
-            <>
-              <Typography className="text-truncate new-table-font">
-                <Typography style={{ fontWeight: 500 }} component="span">
-                  {row['qty'] ? row['qty'] : <NoDataCell />}
-                </Typography>
-              </Typography>
-            </>
-          );
-        }
-      },
-      {
-        header: 'Processed Qty',
-        width: 100,
-        render: ({ row }) => {
-          return (
-            <>
-              <Typography className="text-truncate new-table-font">
-                <Typography style={{ fontWeight: 500 }} component="span">
-                  {row['processedQty'] ? row['processedQty'] : <NoDataCell />}
-                </Typography>
-              </Typography>
-            </>
-          );
-        }
-      },
-      {
-        header: 'Requested By',
-        width: 100,
-        render: ({ row }) => {
-          return (
-            <>
-              <Typography className="text-truncate new-table-font" style={{ marginBottom: '5px' }}>
-                {row['requestBy'] ? (
-                  <a className="link text-truncate new-table-font " href={`${routes.userDetail.path}/${row?.['requestById']}`} target="_blank">
-                    {row?.['requestBy']}
-                  </a>
-                ) : (
-                  <NoDataCell />
-                )}
-              </Typography>
-              <Typography className="text-truncate new-table-font-small ">
-                {row['requestDate'] ? <span>{moment(row['requestDate']).format(dateTimeFormat)}</span> : <NoDataCell />}
-              </Typography>
-            </>
-          );
-        }
-      },
-      {
-        header: 'Processed By',
-        width: 100,
-        render: ({ row }) => {
-          return (
-            <>
-              {row['processBy'] ? (
-                <>
-                  <Typography className="text-truncate new-table-font" style={{ marginBottom: '5px' }}>
-                    <a className="link text-truncate new-table-font " href={`${routes.userDetail.path}/${row?.['processById']}`} target="_blank">
-                      {row['processBy']}
+      ],
+      bodyColumns: [
+        {
+          render: (row) => {
+            return (
+              <>
+                <Typography>Requested By:</Typography>
+                <Typography>
+                  {row['requestBy'] ? (
+                    <a className="link" href={`${routes.userDetail.path}/${row?.['requestById']}`} target="_blank">
+                      {row?.['requestBy']}
                     </a>
-                  </Typography>
-                  <Typography className="text-truncate new-table-font-small ">
-                    <span>{moment(row['processDate']).format(dateTimeFormat)}</span>
-                  </Typography>
-                </>
-              ) : (
-                <NoDataCell />
-              )}
-            </>
-          );
+                  ) : (
+                    '---'
+                  )}
+                </Typography>
+                <Typography>{row['requestDate'] ? <span>{moment(row['requestDate']).format(dateTimeFormat)}</span> : <NoDataCell />}</Typography>
+              </>
+            );
+          }
+        },
+        {
+          render: (row) => {
+            return (
+              <>
+                <Typography>Processed By:</Typography>
+                <Typography>
+                  {row['processBy'] ? (
+                    <a className="link" href={`${routes.userDetail.path}/${row?.['processById']}`} target="_blank">
+                      {row?.['processBy']}
+                    </a>
+                  ) : (
+                    '---'
+                  )}
+                </Typography>
+                <Typography>{row['processDate'] ? <span>{moment(row['processDate']).format(dateTimeFormat)}</span> : <NoDataCell />}</Typography>
+              </>
+            );
+          }
+        },
+        {
+          render: (row) => {
+            return (
+              <>
+                <Typography>Requested Qty:</Typography>
+                <Typography>{row['qty'] ? row['qty'] : '---'}</Typography>
+              </>
+            );
+          }
+        },
+        {
+          render: (row) => {
+            return (
+              <>
+                <Typography>Processed Qty:</Typography>
+                <Typography>{row['processedQty'] ? row['processedQty'] : '---'}</Typography>
+              </>
+            );
+          }
+        },
+        {
+          render: (row) => {
+            return (
+              <>
+                <Typography>Comment:</Typography>
+                <Typography>{row['comment'] ? row['comment'] : '---'}</Typography>
+              </>
+            );
+          }
         }
-      }
-    ];
+      ]
+    };
     if (user?.user?.brandPolicy?.storageLocation) {
-      column.push({
-        header: 'Storage Location',
-        width: 200,
-        render: ({ row }) => {
-          return row?.['storageLocation'] ? (
-            <Typography className="text-truncate new-table-font">
-              <a className="link" href={`${routes.storageLocationDetail.path}/${row?.['storageLocationId']}`} target="_blank">
-                {row?.['storageLocation']}
-              </a>
+      accessor.bodyColumns.push({
+        render: (row) => (
+          <>
+            <Typography>Storage Location:</Typography>
+            <Typography>
+              {row?.['storageLocation'] ? (
+                <a className="link" href={`${routes.storageLocationDetail.path}/${row?.['storageLocationId']}`} target="_blank">
+                  {row?.['storageLocation']}
+                </a>
+              ) : (
+                '---'
+              )}
             </Typography>
-          ) : (
-            <NoDataCell />
-          );
-        }
+          </>
+        )
       });
     }
-    column.push({
-      header: 'Comment',
-      width: 150,
-      render: ({ row }) => {
-        return row['comment'] ? <Typography className="text-truncate new-table-font">{row['comment']}</Typography> : <NoDataCell />;
-      }
-    });
-    column.push({
-      header: 'Status',
-      width: 150,
-      render: ({ row }) => {
-        return row['status'] ? <Typography className="text-truncate new-table-font">{row['status']}</Typography> : <NoDataCell />;
-      }
-    });
-    column.push({
-      header: 'Action',
-      sticky: 'right',
-      render: ({ row }) => {
-        return (
-          <>
-            <Box display="flex">
-              <Box display="flex" flexGrow={1}>
-                {row['status'] === MATERIAL_REQUEST_STATUS.requested && (
-                  <Fragment>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      size="small"
-                      disabled={loading}
-                      onClick={() => {
-                        setQtyDialog({ open: true, status: MATERIAL_REQUEST_STATUS.processed, data: row });
-                      }}
-                    >
-                      Process
-                    </Button>
-                    <Box pl={1} />
-                    <Button
-                      variant="outlined"
-                      color="secondary"
-                      size="small"
-                      disabled={loading}
-                      onClick={() => {
-                        setQtyDialog({ open: true, status: MATERIAL_REQUEST_STATUS.closed, data: row });
-                      }}
-                    >
-                      Close
-                    </Button>
-                    <Box pl={1} />
-                  </Fragment>
-                )}
-              </Box>
-              <Box>
-                {row['processesLogs'] && row['processesLogs']?.length > 0 && (
-                  <HtmlTooltip title="View Process Logs">
-                    <IconButton
-                      size="small"
-                      aria-label="Delete"
-                      onClick={() => {
-                        setOpenProcessLogs({
-                          open: true,
-                          logs: row['processesLogs'],
-                          productName: row?.productName,
-                          product: row?.product?.optionValue,
-                          data: row
-                        });
-                      }}
-                    >
-                      <HistoryIcon />
-                    </IconButton>
-                  </HtmlTooltip>
-                )}
-              </Box>
-            </Box>
-          </>
-        );
-      }
-    });
-    setColumns([...column]);
+    setAccessor(accessor);
   };
   const openActions = (event) => {
     setAnchorEl(event.currentTarget);
@@ -310,9 +272,7 @@ const Request = ({ workOrder }) => {
             style={{
               color: 'var(--card-color-primary)',
               fontSize: 15,
-              background: 'var(--dark-secondary, #F1F5FF)',
-              padding: '8px 40px 8px 10px',
-              borderRadius: '8px 4px'
+              fontWeight: 700
             }}
           >
             Consumables Requests
@@ -320,13 +280,8 @@ const Request = ({ workOrder }) => {
         </Box>
         <Box>
           <Button
-            disabled={
-              selectedRecords?.length > 0 &&
-              selectedRecords?.filter((e) => e.status === MATERIAL_REQUEST_STATUS.requested)?.length === selectedRecords?.length
-                ? false
-                : true
-            }
             variant={'outlined'}
+            className="new-dropdown-v1"
             color="default"
             size="small"
             onClick={openActions}
@@ -348,6 +303,12 @@ const Request = ({ workOrder }) => {
             onClose={closeActions}
           >
             <MenuItem
+              disabled={
+                selectedRecords?.length > 0 &&
+                selectedRecords?.filter((e) => e.status === MATERIAL_REQUEST_STATUS.requested)?.length === selectedRecords?.length
+                  ? false
+                  : true
+              }
               onClick={() => {
                 setQtyDialog({ open: true, status: MATERIAL_REQUEST_STATUS.processed, data: null });
                 closeActions();
@@ -356,6 +317,12 @@ const Request = ({ workOrder }) => {
               Process
             </MenuItem>
             <MenuItem
+              disabled={
+                selectedRecords?.length > 0 &&
+                selectedRecords?.filter((e) => e.status === MATERIAL_REQUEST_STATUS.requested)?.length === selectedRecords?.length
+                  ? false
+                  : true
+              }
               onClick={() => {
                 setQtyDialog({ open: true, status: MATERIAL_REQUEST_STATUS.closed, data: null });
                 closeActions();
@@ -367,11 +334,11 @@ const Request = ({ workOrder }) => {
         </Box>
       </Box>
       <Box pt={2}>
-        {rowsData && columns ? (
+        {rowsData && accessor ? (
           <Box zIndex={5} width={'100%'} height={'calc(100vh - 290px)'}>
-            <CustomTable
+            <CustomTableWithCard
               data={rowsData}
-              columns={columns}
+              accessor={accessor}
               uniqueKey={(data) => data._id}
               onSelect={setSelectedRecords}
               checkBox={true}
