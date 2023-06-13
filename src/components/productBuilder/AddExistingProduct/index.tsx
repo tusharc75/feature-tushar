ceff96d5e45bd9d2e97f8152b82cb75f88e1b301
Avisekh999 <avisekhgurung8578@gmail.com>
2023-06-13 14:58:27 +0530

    fix: fix
    fix

--- src/components/productBuilder/AddExistingProduct/index.tsx
+++ src/components/productBuilder/AddExistingProduct/index.tsx
@@ -105,7 +105,7 @@ const AddExistingProduct = (props) => {
                 }
               ];
             } else {
-              let currentColumn = getColumnData(routes.product.title, o?.fieldData, routes.product.path);
+              let currentColumn = getColumnData(routes.product.title, o?.fieldData, routes.product.path, true);
               if (currentColumn !== null) {
                 columns = [...columns, currentColumn?.columnData];
                 if (currentColumn?.rendererName && rendererNames.indexOf(currentColumn?.rendererName) < 0) {
