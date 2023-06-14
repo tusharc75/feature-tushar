92d2e594d6b9b5d62f3835d350d0245314202cff
Romit Sadaria <romit.sadaria@gmail.com>
2023-06-14 10:51:31 +0530

    fix: bug fix

--- src/components/AgGridComponents/CustomAgGrid.tsx
+++ src/components/AgGridComponents/CustomAgGrid.tsx
@@ -181,6 +181,7 @@ export default function CustomAgGrid({
   showFilters = false,
   resource = null
 }) {
+  console.log(cols)
   const [columns, setColumns] = useState([]);
   const [columnApi, setColumnApi] = useState(null);
 
