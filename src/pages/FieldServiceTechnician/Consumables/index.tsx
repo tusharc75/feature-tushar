b1dc8d6dc1b5ec1a361f4ca1952e33a0bf7b4679
dhrubobarman <dhrubobarman847@gmail.com>
2023-06-19 20:51:52 +0530

    feat: added Select all checkbox

--- src/pages/FieldServiceTechnician/Consumables/index.tsx
+++ src/pages/FieldServiceTechnician/Consumables/index.tsx
@@ -103,6 +103,7 @@ const Consumables = ({ selectedFieldService, recall }) => {
                 onSelect={setSelectedRecords}
                 checkBox={true}
                 height={'calc(100vh - 290px)'}
+                showSelectAll={true}
               />
               {/* <CustomReactTable
                 height={'calc(100vh - 345px)'}
