407dfdf492e4ee28888fa638dcb5cc54b6c69e6a
manu patel <manupatel18112000@gmail.com>
2023-06-20 16:16:35 +0530

    implement vite

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -38,7 +38,7 @@ export function register(config?: Config) {
         }
 
         window.addEventListener('load', () => {
-            const swUrl = `${PUBLIC_URL}/service-worker.js`;
+            const swUrl = `${PUBLIC_URL}service-worker.js`;
             if (isLocalhost) {
                 // This is running on localhost. Let's check if a service worker still exists or not.
                 checkValidServiceWorker(swUrl, config);
