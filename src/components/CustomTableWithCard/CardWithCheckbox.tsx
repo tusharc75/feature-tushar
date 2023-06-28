c315de6282f0052b0fb45fda602e6278e8aab70b
dhrubobarman <dhrubobarman847@gmail.com>
2023-06-28 13:04:39 +0530

    refactor: collapsible hook in its own separate file

--- src/components/CustomTableWithCard/CardWithCheckbox.tsx
+++ src/components/CustomTableWithCard/CardWithCheckbox.tsx
@@ -5,6 +5,7 @@ import type { CardInterface } from './';
 import { Checkbox, IconButton, Grid } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import { useCollapse } from 'src/hooks';
 
 interface CardWithCheckboxProps extends CardInterface {
   row: any;
@@ -110,67 +111,3 @@ const CardWithCheckbox: FC<CardWithCheckboxProps> = ({
 };
 
 export default CardWithCheckbox;
-interface CollapseProps {
-  minHeight?: number | 'auto';
-  collapsed?: boolean;
-  childClassName: string;
-}
-
-type CollpasebleElement = HTMLDivElement;
-const useCollapse = ({ minHeight = 'auto', collapsed = true, childClassName }: CollapseProps) => {
-  const containerRef = React.useRef<CollpasebleElement>(null);
-  const [firstElementHeight, setFirstElementHeight] = React.useState<number>(61);
-
-  function collapseSection(element: CollpasebleElement) {
-    const sectionHeight = element.scrollHeight;
-    const elementTransition = element.style.transition;
-    element.style.transition = '';
-    element.style.overflow = 'hidden';
-
-    requestAnimationFrame(function () {
-      element.style.height = sectionHeight + 'px';
-      element.style.transition = elementTransition;
-      requestAnimationFrame(function () {
-        element.style.height = minHeight === 'auto' ? firstElementHeight + 'px' : minHeight + 'px';
-      });
-    });
-    element.setAttribute('data-collapsed', 'true');
-  }
-
-  function expandSection(element: CollpasebleElement) {
-    const sectionHeight = element.scrollHeight;
-
-    const transitionComplete = () => {
-      element.style.height = null;
-      element.style.overflow = null;
-      element.removeEventListener('transitionend', transitionComplete);
-    };
-    element.style.height = sectionHeight + 'px';
-    element.addEventListener('transitionend', transitionComplete, false);
-    element.setAttribute('data-collapsed', 'false');
-
-    // useEffect cleanup
-    return () => element.removeEventListener('transitionend', transitionComplete);
-  }
-
-  const getFirstElementHeight = (element: CollpasebleElement) => {
-    if (!childClassName || childClassName === '') return;
-    const heightOffset = 20;
-    const firstElement = element.querySelector('.' + childClassName + ':first-child');
-    if (firstElement) {
-      setFirstElementHeight(firstElement.clientHeight - heightOffset);
-    }
-  };
-
-  React.useLayoutEffect(() => {
-    if (containerRef.current) getFirstElementHeight(containerRef.current);
-    if (collapsed && containerRef.current) {
-      collapseSection(containerRef.current);
-    }
-    if (!collapsed && containerRef.current) {
-      expandSection(containerRef.current);
-    }
-  }, [collapsed]);
-
-  return containerRef;
-};
