4ec32b1994f276d7535a2c3340e5b73dc85d687b
manu patel <manupatel18112000@gmail.com>
2023-07-13 14:16:09 +0530

    changes in field ticket

---
+++ src/pages/FieldTicket/Invoice/index.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@material-ui/core";
+import React from "react";
+
+const Invoice = () => {
+
+    return (
+        <>
+            <Box display="flex" justifyContent="space-between" m={1}>
+                Invoice
+            </Box>
+
+        </>
+    );
+};
+
+export default Invoice;
