9e23940e1fd3af406032996bc235f4835c64ba97
Romit Sadaria <romit.sadaria@gmail.com>
2023-07-20 17:27:59 +0530

    fix: bug fix

--- src/pages/RepairOrder/WorkOrder/index.tsx
+++ src/pages/RepairOrder/WorkOrder/index.tsx
@@ -265,8 +265,8 @@ const WorkOrder = ({
                     row?.original?.type === 'package' && row?.original?.subRows?.length === 0
                       ? false
                       : row?.original?.status === WORKORDER_SERVICE_STATUS.pending && allowedToDelete
-                      ? false
-                      : true
+                        ? false
+                        : true
                   }
                   size="small"
                   aria-label="Details"
@@ -281,8 +281,8 @@ const WorkOrder = ({
                       row?.original?.type === 'package' && row?.original?.subRows?.length === 0
                         ? 'error'
                         : row?.original?.status === WORKORDER_SERVICE_STATUS.pending && allowedToDelete
-                        ? 'error'
-                        : 'disabled'
+                          ? 'error'
+                          : 'disabled'
                     }
                   />
                 </IconButton>
@@ -433,37 +433,35 @@ const WorkOrder = ({
     createWorkorderService(rows);
     rows.forEach((parent, i) => {
       parent.index = i + 1;
-      parent.detail = `${
-        parent.type === 'service'
+      parent.detail = `${parent.type === 'service'
           ? parent?.serviceDetail?.serviceName
           : parent.type === 'product'
-          ? parent?.productDetail?.productName
-          : parent.type === 'serializedAsset'
-          ? parent?.serializedAsset?.assetNumber
-          : parent?.packageDetail?.packageName
-      }`;
+            ? parent?.productDetail?.productName
+            : parent.type === 'serializedAsset'
+              ? parent?.serializedAsset?.assetNumber
+              : parent?.packageDetail?.packageName
+        }`;
       parent.description =
         parent.type === 'service'
           ? parent?.serviceDetail?.serviceDescription || ''
           : parent.type === 'product'
-          ? parent?.productDetail?.productDescription || ''
-          : parent.type === 'package'
-          ? parent?.packageDetail?.packageDescription || ''
-          : parent.type === 'serializedAsset'
-          ? parent?.serializedAssetDetail?.product?.productDescription || ''
-          : '';
+            ? parent?.productDetail?.productDescription || ''
+            : parent.type === 'package'
+              ? parent?.packageDetail?.packageDescription || ''
+              : parent.type === 'serializedAsset'
+                ? parent?.serializedAssetDetail?.product?.productDescription || ''
+                : '';
       parent.productName = parent?.serializedAssetDetail?.product?.optionLabel || '';
       parent.productId = parent?.serializedAssetDetail?.product?.optionValue || '';
       parent.qty = parent.qty;
-      parent.status = `${
-        parent.type === 'service'
+      parent.status = `${parent.type === 'service'
           ? parent.serviceDetail?.status
           : parent.type === 'product'
-          ? parent.productDetail?.status
-          : parent.type === 'serializedAsset'
-          ? parent.serializedAssetDetail.status
-          : parent.packageDetail?.status
-      }`;
+            ? parent.productDetail?.status
+            : parent.type === 'serializedAsset'
+              ? parent.serializedAssetDetail.status
+              : parent.packageDetail?.status
+        }`;
       parent.workOrderNumber = parent?.workOrder?.workOrderNumber;
       parent.hideSelection = false;
       if (parent?.workOrder?.status === WORK_ORDER_STATUS.completed) {
@@ -510,18 +508,18 @@ const WorkOrder = ({
         _subRow.type === 'service'
           ? _subRow?.serviceDetail?.serviceName
           : _subRow.type === 'product'
-          ? _subRow?.productDetail?.productName
-          : _subRow.type === 'serializedAsset'
-          ? _subRow?.serializedAsset?.assetNumber
-          : _subRow?.packageDetail?.packageName;
+            ? _subRow?.productDetail?.productName
+            : _subRow.type === 'serializedAsset'
+              ? _subRow?.serializedAsset?.assetNumber
+              : _subRow?.packageDetail?.packageName;
       _subRow.description =
         _subRow.type === 'service'
           ? _subRow?.serviceDetail?.serviceDescription || ''
           : _subRow.type === 'product'
-          ? _subRow?.productDetail?.productDescription || ''
-          : _subRow.type === 'package'
-          ? _subRow?.packageDetail?.packageDescription || ''
-          : '';
+            ? _subRow?.productDetail?.productDescription || ''
+            : _subRow.type === 'package'
+              ? _subRow?.packageDetail?.packageDescription || ''
+              : '';
       _subRow.productName = _subRow?.serializedAssetDetail?.product?.optionLabel || '';
       _subRow.productId = _subRow?.serializedAssetDetail?.product?.optionValue || '';
       _subRow.qtyDisplay = `${parent.qtyDisplay * _subRow.qty}`;
@@ -706,7 +704,7 @@ const WorkOrder = ({
                   setCompleteConfirmBox(true);
                   closeActions();
                 }}
-                disabled={selectedProducts.some((e) => !e?.canAutoCompleteWorkOrder)}
+                disabled={selectedProducts.some((e) => e?.canAutoCompleteWorkOrder) ? false : true}
               >
                 Auto Complete Work Order
               </MenuItem>
@@ -719,19 +717,19 @@ const WorkOrder = ({
                 disabled={
                   selectedProducts?.filter((e) => e.type === 'service').length
                     ? selectedServices?.filter(
-                        (d) =>
-                          d.type === 'service' &&
-                          d.workOrder?._id === selectedServices[0]?.workOrder?._id &&
-                          d.status === WORKORDER_SERVICE_STATUS.pending
-                      )?.length === selectedServices?.length
+                      (d) =>
+                        d.type === 'service' &&
+                        d.workOrder?._id === selectedServices[0]?.workOrder?._id &&
+                        d.status === WORKORDER_SERVICE_STATUS.pending
+                    )?.length === selectedServices?.length
                       ? false
                       : true
                     : selectedAssets?.length
-                    ? selectedAssets?.filter((d) => rowsData?.filter((c) => c?._id === d?._id)?.some((d) => !d?.subRows?.length))?.length ===
-                      selectedAssets?.length
-                      ? false
+                      ? selectedAssets?.filter((d) => rowsData?.filter((c) => c?._id === d?._id)?.some((d) => !d?.subRows?.length))?.length ===
+                        selectedAssets?.length
+                        ? false
+                        : true
                       : true
-                    : true
                 }
               >
                 Delete
