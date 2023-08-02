13545516dfe7aaa8e19990c7d1d3edf42b0cdde2
Ubuntu <ubuntu@ip-172-31-73-167.us-east-2.compute.internal>
2023-08-02 12:48:26 +0000

    sso fix

--- src/pages/Auth/AzureSSOLogin.tsx
+++ src/pages/Auth/AzureSSOLogin.tsx
@@ -3,10 +3,7 @@ import { useHistory, Link } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import { useData } from 'src/StateProvider/Provider';
 import { SET_GRID_METADATA, SET_SELECTED_ENTITY, SET_USER } from 'src/StateProvider/actionTypes';
-import axiosInstance from 'src/axios/axiosInstance';
-import routes from 'src/components/Helpers/Routes';
 import { backendApi } from 'src/config';
-import { camelCase } from 'lodash';
 
 const AzureSSOLogin = () => {
   const { dispatch }: any = useData();
@@ -16,13 +13,18 @@ const AzureSSOLogin = () => {
     const token = searchParams.get('token');
     (async () => {
       if (token) {
-        const res = await axios.get(backendApi+"/user/me",{
-          headers:{
-            Authorization:`Bearer ${token}`
+        const res = await axios.get(backendApi + "/user/me", {
+          headers: {
+            Authorization: `Bearer ${token}`
           }
         })
+        const { data: { data } } = res;
 
-        const { data } = res;
+        if (data?.user?.gridMetaData) {
+          let tempMetaData = JSON.stringify(data?.user?.gridMetaData);
+          localStorage.setItem('gridMetaData', tempMetaData);
+          dispatch({ type: SET_GRID_METADATA, payload: data?.user?.gridMetaData });
+        }
 
         let mappedEntities = [];
         if (data.entity && data.entity.length) {
@@ -30,10 +32,19 @@ const AzureSSOLogin = () => {
             mappedEntities = [...mappedEntities, { optionLabel: o?.entityName, optionValue: o?._id }];
           });
         }
-
         localStorage.setItem('mappedEntities', JSON.stringify(mappedEntities));
 
+        dispatch({ type: SET_USER, payload: data });
+
+        if (data?.role?.selectedEntity?._id) {
+          dispatch({
+            type: SET_SELECTED_ENTITY,
+            payload: data.role.selectedEntity._id
+          });
+        }
+
         localStorage.setItem('token', token);
+
         window.location.href = '/';
       } else {
         window.location.href = '/sso-login-error';
