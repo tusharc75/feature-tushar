550456d8e35cb961dbafdb9301e41c8d3ad6bd4b
sunanda35 <sunandasamanta60@gmail.com>
2023-08-07 16:07:38 +0530

    remove comment

--- src/components/CustomReactTable/CustomReactTable.tsx
+++ src/components/CustomReactTable/CustomReactTable.tsx
@@ -346,15 +346,6 @@ export default function CustomReactTable({
         setColumnOrder(newColumns.map((m) => m?.id || m?.accessor));
         setHiddenColumns(gridMetaData[renderedFrom]?.hide || [])
       }
-      // const storedColumns = localStorage.getItem(renderedFrom);
-      // if (storedColumns) {
-      //   setColumnOrder(JSON.parse(storedColumns).map((m) => m.id));
-      //   setHiddenColumns(
-      //     JSON.parse(storedColumns)
-      //       .filter((f) => f.isVisible === false && !['expander', 'selection', 'action']?.includes(f.id))
-      //       .map((m) => m.id)
-      //   );
-      // }
     } catch (ex) {
       console.error(`Error while getting stored data from local storage - ${renderedFrom}`);
     }
