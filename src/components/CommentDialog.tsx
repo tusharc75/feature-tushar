a28eecc1dfc7b532581ba55a6f89cb0602837824
manu patel <manupatel18112000@gmail.com>
2023-08-09 18:24:38 +0530

    fix bug

---
+++ src/components/CommentDialog.tsx
@@ -0,0 +1,71 @@
+import { Box, Button, Dialog, TextField, makeStyles } from "@material-ui/core";
+import CustomDialogHeader from "./CustomDialog/CustomDialogHeader";
+import CustomDialogContent from "./CustomDialog/CustomDialogContent";
+import CustomDialogFooter from "./CustomDialog/CustomDialogFooter";
+import { useState } from "react";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper
+    },
+    paper: {
+        width: '80%',
+        maxHeight: 435
+    }
+}));
+
+export default function CommentDialog({ required = false, label, handleSubmit, handleClose }) {
+    const classes = useStyles();
+
+    const [comment, setComment] = useState(null)
+    const [error, setError] = useState(null)
+
+    return <Dialog
+        open={true}
+        classes={{
+            paper: classes.paper
+        }}
+        onClose={handleClose}
+    >
+        <CustomDialogHeader
+            title="Are you sure ?"
+            showRequiredLabel={false}
+            onClose={handleClose}
+        />
+        <CustomDialogContent>
+            <Box className="my-2">
+                <TextField
+                    id="outlined-multiline-static"
+                    label={`Please enter the reason for ${label}`}
+                    multiline
+                    required={required}
+                    fullWidth
+                    rows={4}
+                    value={comment}
+                    variant="outlined"
+                    error={Boolean(error)}
+                    helperText={Boolean(error) && error}
+                    onChange={(e) => {
+                        setComment(e.target.value)
+                    }}
+                />
+            </Box>
+        </CustomDialogContent>
+        <CustomDialogFooter>
+            <Button size="small" variant="outlined" color="primary" onClick={handleClose}>
+                Cancel
+            </Button>
+            <Button size="small" onClick={() => {
+                if (comment) {
+                    handleSubmit(comment)
+                } else {
+                    setError('Comment is Required !!')
+                }
+            }} variant="contained" color="primary">
+                Submit
+            </Button>
+        </CustomDialogFooter>
+    </Dialog>
+}
