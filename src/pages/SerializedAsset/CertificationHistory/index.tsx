0a43bfe12737e2249b0437e47d87ea137477703e
Romit Sadaria <romit.sadaria@gmail.com>
2023-08-10 14:24:01 +0530

    fix: bug fix

--- src/pages/SerializedAsset/CertificationHistory/index.tsx
+++ src/pages/SerializedAsset/CertificationHistory/index.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect, useContext, useReducer } from 'react';
-import { Box, Button, Grid } from '@material-ui/core';
+import { Box, Button, Dialog, Grid, IconButton } from '@material-ui/core';
 import axiosInstance from '../../../axios/axiosInstance';
 import routes from '../../../components/Helpers/Routes';
 import CommonSkeleton from '../../../components/Helpers/CommonSkeleton';
 import { CustomToastContext } from '../../../StateProvider/CustomToastContext/CustomToastContext';
-import { prepareDataForGrid, serializedAsset } from '../../../constants/helpers';
+import { CustomDialogTransition, prepareDataForGrid, serializedAsset } from '../../../constants/helpers';
 import CustomAgGrid, { intialState, reducer } from '../../../components/AgGridComponents/CustomAgGrid';
 import { CommonRenderer, DateRenderer } from '../../../components/AgGridComponents/CustomAgGridCellRenderers';
 import { camelCase } from 'lodash';
@@ -14,14 +14,20 @@ import { Link } from 'react-router-dom';
 import { isMobile, isTablet } from 'react-device-detect';
 import styles from '../../Leads/Header.module.scss';
 import IssueCertificateDialog from '../IssueCertificate';
+import HtmlTooltip from 'src/components/CustomTooltipTitle';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import ManageAttachment from 'src/components/Activity/Attachments/ManageAttachment';
 
-const CertificationHistory = ({ id, canIssueCertificate }) => {
+const CertificationHistory = ({ id, canIssueCertificate, supplierAccount }) => {
 
   const toastConfig = useContext(CustomToastContext);
   const [gridApi, setGridApi] = useState(null);
   const [state, dispatch] = useReducer(reducer, intialState);
   const { dataRows, rowCount, loading, page, limit, pageSizes } = state;
+
   const [openDialog, setOpenDialog] = useState({ open: false, id: null });
+  const [openAttachment, setOpenAttachment] = useState({ open: false, attachmentId: null });
+  const [fullScreen, setFullScreen] = useState(isMobile || isTablet);
 
   useEffect(() => {
     if (id) {
@@ -34,11 +40,15 @@ const CertificationHistory = ({ id, canIssueCertificate }) => {
     if (gridApi) {
       gridApi.setRowData([]);
     }
-    axiosInstance()
-      .get(`${serializedAsset.api}/${id}/certificate`)
+    var api = `${serializedAsset.api}/${id}/certificate`
+    if (supplierAccount) {
+      api = api + `?supplierAccount=${supplierAccount}`
+    }
+    axiosInstance().get(api)
       .then(({ data: { data } }) => {
         let rows = data?.map((u) => {
-          let finalObject = prepareDataForGrid(u);
+          let finalObject: any = prepareDataForGrid(u);
+          finalObject.attachmentId = u?.attachmentId
           return {
             ...finalObject
           };
@@ -59,6 +69,24 @@ const CertificationHistory = ({ id, canIssueCertificate }) => {
     { field: 'createdBy', headerName: 'Created By', show: true, filter: false, sortable: false, cellRenderer: 'createdByRenderer' }
   ];
 
+  const ActionsRenderer = (params) => (
+    <>
+      {params.data.attachmentId &&
+        <HtmlTooltip title="View Attachment">
+          <IconButton
+            size="small"
+            aria-label="Issue"
+            onClick={() => {
+              setOpenAttachment({ open: true, attachmentId: params.data.attachmentId })
+            }}
+          >
+            <AttachFileIcon color='primary' />
+          </IconButton>
+        </HtmlTooltip>
+      }
+    </>
+  );
+
   const SupplierAccountRenderer = (params: { value: any; data: any }) => (
     <>
       {params.value ? (
@@ -75,6 +103,7 @@ const CertificationHistory = ({ id, canIssueCertificate }) => {
     supplierAccountRenderer: SupplierAccountRenderer,
     commonRenderer: CommonRenderer,
     dateRenderer: DateRenderer,
+    actionsRenderer: ActionsRenderer,
     ...staticFrameworkRender
   };
 
@@ -107,7 +136,8 @@ const CertificationHistory = ({ id, canIssueCertificate }) => {
             limit={limit}
             pageSizes={pageSizes}
             page={page}
-            allowAction={false}
+            allowAction={true}
+            actionWidth={100}
             allowSelection={false}
             isClientSideGrid={true}
             loading={loading}
@@ -130,6 +160,38 @@ const CertificationHistory = ({ id, canIssueCertificate }) => {
           assetId={openDialog?.id}
         />
       )}
+      {openAttachment.open &&
+        <Dialog
+          open={true}
+          aria-labelledby="customized-dialog-title"
+          maxWidth="md"
+          onClose={(e, reason) => {
+            if (reason !== 'backdropClick') {
+              setFullScreen(false);
+              setOpenAttachment({ open: false, attachmentId: null })
+            }
+          }}
+          fullWidth
+          fullScreen={fullScreen || isMobile || isTablet}
+          TransitionComponent={CustomDialogTransition}
+        >
+          <ManageAttachment
+            attachmentId={openAttachment.attachmentId?._id}
+            handleClose={() => {
+              setFullScreen(false);
+              setOpenAttachment({ open: false, attachmentId: null })
+            }}
+            relatedTo={openAttachment.attachmentId?.relatedTo}
+            isMinimized={!fullScreen}
+            onMinimizeMaximize={() => {
+              setFullScreen((prevState) => !prevState);
+            }}
+            showManimizeMaximize={true}
+            parentFolder={openAttachment.attachmentId?.parentFolder}
+            type={openAttachment.attachmentId?.type}
+          />
+        </Dialog>
+      }
     </>
   );
 };
