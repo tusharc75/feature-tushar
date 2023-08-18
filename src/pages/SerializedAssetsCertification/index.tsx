920759954f2bea28ff2cf76e0a87fd8c43d1698a
dhrubobarman <dhrubobarman847@gmail.com>
2023-08-18 10:52:00 +0530

    fix: serialized assets Certificate Fixes

--- src/pages/SerializedAssetsCertification/index.tsx
+++ src/pages/SerializedAssetsCertification/index.tsx
@@ -275,118 +275,114 @@ const SerializedAssetsCertification = () => {
       </Grid>
       <div className="main-container">
         <div className="header-panel">
-          <Grid container justifyContent="space-between" spacing={2}>
-            <Grid item md={10}>
-              <Grid container spacing={1}>
-                <Grid item md={3}>
-                  <Autocomplete
-                    onChange={(event, value) => {
-                      setSelectedAsset(value);
-                    }}
-                    fullWidth
-                    options={assetOptions}
-                    getOptionSelected={(option, val) => (option ? option.optionLabel === val.optionLabel : false)}
-                    getOptionLabel={(option) => option.optionLabel}
-                    size="small"
-                    renderInput={(params) => <TextField {...params} label={'Asset'} variant="outlined" size="small" />}
-                  />
-                </Grid>
-                <Grid item md={9}>
-                  <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                    <Grid container spacing={1}>
-                      <Grid item md={3}>
-                        <KeyboardDatePicker
-                          inputVariant="outlined"
-                          variant="inline"
-                          fullWidth
-                          size="small"
-                          format={dateFormatForInputControl}
-                          maxDate={issueDuration.to}
-                          label="From (Issue Date)"
-                          autoOk
-                          InputLabelProps={{
-                            shrink: true
-                          }}
-                          views={['year', 'month', 'date']}
-                          value={issueDuration.from}
-                          onChange={(date) => {
-                            setIssueDuration({ to: issueDuration.to, from: date });
-                          }}
-                        />
-                      </Grid>
-                      <Grid item md={3}>
-                        <KeyboardDatePicker
-                          inputVariant="outlined"
-                          variant="inline"
-                          fullWidth
-                          size="small"
-                          format={dateFormatForInputControl}
-                          label="To (Issue Date)"
-                          autoOk
-                          InputLabelProps={{
-                            shrink: true
-                          }}
-                          views={['year', 'month', 'date']}
-                          value={issueDuration.to}
-                          onChange={(date) => {
-                            setIssueDuration({ from: issueDuration.from, to: date });
-                          }}
-                        />
-                      </Grid>
-                      <Grid item md={3}>
-                        <KeyboardDatePicker
-                          inputVariant="outlined"
-                          variant="inline"
-                          fullWidth
-                          size="small"
-                          format={dateFormatForInputControl}
-                          maxDate={expireDuration.to}
-                          label="From (Expiry Date)"
-                          autoOk
-                          InputLabelProps={{
-                            shrink: true
-                          }}
-                          views={['year', 'month', 'date']}
-                          value={expireDuration.from}
-                          onChange={(date) => {
-                            setExpireDuration({ to: expireDuration.to, from: date });
-                          }}
-                        />
-                      </Grid>
-                      <Grid item md={3}>
-                        <KeyboardDatePicker
-                          inputVariant="outlined"
-                          variant="inline"
-                          fullWidth
-                          size="small"
-                          format={dateFormatForInputControl}
-                          label="To (Expiry Date)"
-                          autoOk
-                          InputLabelProps={{
-                            shrink: true
-                          }}
-                          views={['year', 'month', 'date']}
-                          value={expireDuration.to}
-                          onChange={(date) => {
-                            setExpireDuration({ from: expireDuration.from, to: date });
-                          }}
-                        />
-                      </Grid>
-                    </Grid>
-                  </MuiPickersUtilsProvider>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item md={2} style={{ display: 'flex', justifyContent: 'flex-end' }}>
-              <SearchBox
-                onChange={handleSearch}
-                className={styles.search_box_input}
-                width={isMobile ? '200px' : '210px'}
+          <div className="grid gap-x-2 gap-y-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+            <Autocomplete
+              onChange={(event, value) => {
+                setSelectedAsset(value);
+              }}
+              fullWidth
+              options={assetOptions}
+              getOptionSelected={(option, val) => (option ? option.optionLabel === val.optionLabel : false)}
+              getOptionLabel={(option) => option.optionLabel}
+              size="small"
+              renderInput={(params) => <TextField {...params} label={'Asset'} variant="outlined" size="small" />}
+            />
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker
+                inputVariant="outlined"
+                variant="inline"
+                fullWidth
                 size="small"
-                value={search}
+                format={dateFormatForInputControl}
+                maxDate={issueDuration.to}
+                label="From (Issue Date)"
+                autoOk
+                InputLabelProps={{
+                  shrink: true
+                }}
+                views={['year', 'month', 'date']}
+                value={issueDuration.from}
+                onChange={(date) => {
+                  setIssueDuration({ to: issueDuration.to, from: date });
+                }}
+                InputProps={{
+                  style: { minHeight: '38px' }
+                }}
               />
-            </Grid>
-          </Grid>
+              <KeyboardDatePicker
+                inputVariant="outlined"
+                variant="inline"
+                fullWidth
+                size="small"
+                format={dateFormatForInputControl}
+                label="To (Issue Date)"
+                autoOk
+                InputLabelProps={{
+                  shrink: true
+                }}
+                views={['year', 'month', 'date']}
+                value={issueDuration.to}
+                onChange={(date) => {
+                  setIssueDuration({ from: issueDuration.from, to: date });
+                }}
+                InputProps={{
+                  style: { minHeight: '38px' }
+                }}
+              />
+              <KeyboardDatePicker
+                inputVariant="outlined"
+                variant="inline"
+                fullWidth
+                size="small"
+                format={dateFormatForInputControl}
+                maxDate={expireDuration.to}
+                label="From (Expiry Date)"
+                autoOk
+                InputLabelProps={{
+                  shrink: true
+                }}
+                views={['year', 'month', 'date']}
+                value={expireDuration.from}
+                onChange={(date) => {
+                  setExpireDuration({ to: expireDuration.to, from: date });
+                }}
+                InputProps={{
+                  style: { minHeight: '38px' }
+                }}
+              />
+              <KeyboardDatePicker
+                inputVariant="outlined"
+                variant="inline"
+                fullWidth
+                size="small"
+                format={dateFormatForInputControl}
+                label="To (Expiry Date)"
+                autoOk
+                InputLabelProps={{
+                  shrink: true
+                }}
+                views={['year', 'month', 'date']}
+                value={expireDuration.to}
+                onChange={(date) => {
+                  setExpireDuration({ from: expireDuration.from, to: date });
+                }}
+                InputProps={{
+                  style: { minHeight: '38px' }
+                }}
+              />
+            </MuiPickersUtilsProvider>
+
+            <SearchBox
+              InputProps={{
+                style: { minHeight: '38px' }
+              }}
+              onChange={handleSearch}
+              className={styles.search_box_input}
+              width={'150px'}
+              size="small"
+              value={search}
+            />
+          </div>
         </div>
         {columns ? (
           Object.keys(frameWorkComponent).length > 0 && columns ? (
