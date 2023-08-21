0341208f6a7afe68765b1520afdc0940680052e1
dhrubobarman <dhrubobarman847@gmail.com>
2023-08-21 16:10:33 +0530

    fix: iot Data Points fixes

--- src/pages/IotChart/Chart/index.tsx
+++ src/pages/IotChart/Chart/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useData } from 'src/StateProvider/Provider';
 import axiosInstance from 'src/axios/axiosInstance';
 import routes from 'src/components/Helpers/Routes';
@@ -12,7 +12,6 @@ import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import { Accordion, AccordionSummary, AccordionDetails } from 'src/components/CustomAccordion';
 
 function Index({ serializedAssets = [], type }) {
-
   const {
     state: { userLoading }
   }: any = useData();
@@ -23,7 +22,7 @@ function Index({ serializedAssets = [], type }) {
   const [expandedAccordition, setExpandedAccordition] = React.useState<string | false>('');
 
   function getRandomDateInMonth(monthYearStr) {
-    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
     const [monthStr, yearStr] = monthYearStr.split('-');
     const month = monthNames.indexOf(monthStr);
@@ -38,17 +37,25 @@ function Index({ serializedAssets = [], type }) {
     return new Date(year, month, randomDay);
   }
 
-
   const fetchIotDataPointsData = async () => {
     setLoading(true);
-    const data: any = await axiosInstance().get(`/report/iot/data-points${serializedAssets?.length > 0 ? `? filterById=${JSON.stringify([{
-      field: 'asset', term: {
-        "$in": serializedAssets
-      }
-    }])}
-}`: ``}`)
+    const data: any = await axiosInstance().get(
+      `/report/iot/data-points${
+        serializedAssets?.length > 0
+          ? `? filterById=${JSON.stringify([
+              {
+                field: 'asset',
+                term: {
+                  $in: serializedAssets
+                }
+              }
+            ])}
+}`
+          : ``
+      }`
+    );
     const reportData = data?.data?.data?.data;
-    console.log('report', reportData)
+    console.log('report', reportData);
     axiosInstance()
       .get(`${routes?.iotDataPoints?.path}`)
       .then(({ data: { data } }) => {
@@ -66,7 +73,7 @@ function Index({ serializedAssets = [], type }) {
 
           if (type === 'dataPoint') {
             if (i === 0) {
-              setExpandedAccordition(fieldLabel)
+              setExpandedAccordition(fieldLabel);
             }
             if (!result[fieldLabel]) {
               result[fieldLabel] = {};
@@ -82,14 +89,14 @@ function Index({ serializedAssets = [], type }) {
                     date: getRandomDateInMonth(d.date)
                   });
                 }
-              })
-              console.log('dataArr', dataArr)
+              });
+              console.log('dataArr', dataArr);
 
               const newObj = {
                 fieldLabel: fieldLabel,
                 dataPoints: dataArr,
                 hide: false,
-                fill: false,
+                fill: false
               };
 
               result[fieldLabel][fieldName] = newObj;
@@ -107,15 +114,15 @@ function Index({ serializedAssets = [], type }) {
                     date: getRandomDateInMonth(d.date)
                   });
                 }
-              })
-              console.log('dataArr', dataArr)
+              });
+              console.log('dataArr', dataArr);
               const newObj = {
                 fieldLabel: fieldLabel,
                 dataPoints: dataArr,
                 hide: false,
                 tension: 0.4,
                 fill: false,
-                borderWidth: 1,
+                borderWidth: 1
               };
 
               result[category][fieldName] = newObj;
@@ -145,6 +152,10 @@ function Index({ serializedAssets = [], type }) {
     }
   };
 
+  const handleChange = useCallback((name: string) => {
+    setExpandedAccordition((prev) => (!prev ? name : prev === name ? false : name));
+  }, []);
+
   return (
     <div>
       {!userLoading ? (
@@ -168,29 +179,33 @@ function Index({ serializedAssets = [], type }) {
                 </Typography>
               </Box>
             ) : (
-              <div className="flex flex-col overflow-auto gap-2">
-                {
-                  type === 'dataPoint' ? (
-                    Object.keys(dataVal).length !== 0 &&
+              <div className="flex flex-col gap-2">
+                {type === 'dataPoint'
+                  ? Object.keys(dataVal).length !== 0 &&
                     Object.keys(dataVal).map((particularCategory) => (
-                      <Accordion expanded={expandedAccordition === particularCategory} onChange={() => {
-                        setExpandedAccordition(particularCategory)
-                      }}>
+                      <Accordion
+                        expanded={expandedAccordition === particularCategory}
+                        onChange={() => {
+                          handleChange(particularCategory);
+                        }}
+                        className="w-full"
+                      >
                         <AccordionSummary aria-controls="panel1d-content" id="particularCategory">
-                          <Typography style={{ fontSize: '14.2056px', fontWeight: 600 }}>{particularCategory}
-                            <IconButton size="small">{expandedAccordition === particularCategory ? <ExpandLessIcon /> : <ExpandMoreIcon />}</IconButton></Typography>
+                          <Typography style={{ fontSize: '14.2056px', fontWeight: 600 }}>
+                            {particularCategory}
+                            <IconButton size="small">
+                              {expandedAccordition === particularCategory ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                            </IconButton>
+                          </Typography>
                         </AccordionSummary>
                         <AccordionDetails>
                           <ChartRenderer key={particularCategory} dataVal={dataVal} particularCategory={particularCategory} chart={chart} />
                         </AccordionDetails>
-
                       </Accordion>
                     ))
-                  ) : (
-                    Object.keys(dataVal).map((particularCategory) => (
+                  : Object.keys(dataVal).map((particularCategory) => (
                       <ChartRenderer key={particularCategory} dataVal={dataVal} particularCategory={particularCategory} chart={chart} />
-                    )))
-                }
+                    ))}
                 {/* {Object.keys(dataVal).length !== 0 &&
                   Object.keys(dataVal).map((particularCategory) => (
                     <ChartRenderer key={particularCategory} dataVal={dataVal} particularCategory={particularCategory} chart={chart} />
--- src/pages/IotChart/index.tsx
+++ src/pages/IotChart/index.tsx
@@ -1,9 +1,9 @@
 import { AppBar, Box, FormControl, Grid, InputLabel, MenuItem, Select, TextField, makeStyles } from '@material-ui/core';
-import React, { Fragment, useCallback, useEffect, useState } from 'react'
+import React, { Fragment, useCallback, useEffect, useState } from 'react';
 import CustomBreadCrumbs from 'src/components/CustomBreadCrumbs';
 import CustomContainer from 'src/components/CustomContainer';
 import routes from 'src/components/Helpers/Routes';
-import Chart from './Chart'
+import Chart from './Chart';
 import FormTypes from 'src/components/Helpers/FormTypes';
 import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { RESOURCE_LABEL, dateFormatForInputControl, serializedAsset, sidebarResource } from '../../constants/helpers';
@@ -14,82 +14,83 @@ import { debounce, set } from 'lodash';
 import axiosInstance from 'src/axios/axiosInstance';
 
 function IotChart() {
-    const [serializedAssets, setSerializedAssets] = useState([])
-    const [typeAlignment, setTypeAlignment] = useState('byCategory')
-    const [loading, setLoading] = useState(false)
-    const [serializedAssetOptions, setSerializedAssetOptions] = useState([])
+  const [serializedAssets, setSerializedAssets] = useState([]);
+  const [typeAlignment, setTypeAlignment] = useState('byCategory');
+  const [loading, setLoading] = useState(false);
+  const [serializedAssetOptions, setSerializedAssetOptions] = useState([]);
 
-    const fetchOptions = useCallback(
-        debounce(async (searchKey: string = '') => {
-            try {
-                const lookupResourceName = sidebarResource.serializedAsset;
-                let query = `sa-field/options?resource=${lookupResourceName}&limit=10&search=${searchKey}`;
-                const response = await axiosInstance().get(query);
-                const options = [...response.data.data];
-                setSerializedAssetOptions(options);
-                setLoading(false);
-            } catch (error) {
-                console.error(error);
-            }
-        }, 1000),
-        []
-    );
+  const fetchOptions = useCallback(
+    debounce(async (searchKey: string = '') => {
+      try {
+        const lookupResourceName = sidebarResource.serializedAsset;
+        let query = `sa-field/options?resource=${lookupResourceName}&limit=10&search=${searchKey}`;
+        const response = await axiosInstance().get(query);
+        const options = [...response.data.data];
+        setSerializedAssetOptions(options);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    }, 1000),
+    []
+  );
 
-    const types = [{
-        optionValue: 'byCategory',
-        optionLabel: 'By Category'
-    }, {
-        optionValue: 'dataPoint',
-        optionLabel: 'Data Points'
-    }]
+  const types = [
+    {
+      optionValue: 'byCategory',
+      optionLabel: 'By Category'
+    },
+    {
+      optionValue: 'dataPoint',
+      optionLabel: 'Data Points'
+    }
+  ];
 
-    return (
-        <div className="main-container-v1">
-            <Grid container className="headerbox-v1">
-                <Grid item md={4} sm={11} xs={10}>
-                    <CustomBreadCrumbs routes={[routes.iotChart]} />
-                </Grid>
-            </Grid>
-            <CustomContainer>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                        <Autocomplete
-                            multiple
-                            onOpen={() => {
-                                setSerializedAssetOptions([]);
-                                setLoading(true);
-                                fetchOptions('');
-                            }}
-                            onInputChange={(event, value) => fetchOptions(value)}
-                            options={serializedAssetOptions}
-                            fullWidth
-                            loading={loading}
-                            getOptionLabel={(option: any) => option.optionLabel ?? ''}
-                            getOptionSelected={(option: any, value: any) => option?.optionValue === value?.optionValue}
-                            value={serializedAssets || []}
-                            onChange={(e, val) => {
-                                setSerializedAssets(val);
-                            }}
-                            size="small"
-                            renderInput={(params) => <TextField {...params} label={'Serialized Assets'} variant="outlined" name={'serializedAssets'} />}
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Autocomplete
-                            options={types}
-                            fullWidth
-                            loading={loading}
-                            getOptionLabel={(option: any) => option.optionLabel ?? ''}
-                            getOptionSelected={(option: any, value: any) => option?.optionValue === value?.optionValue}
-                            value={types?.find(t => t.optionValue === typeAlignment) || {}
-                            }
-                            onChange={(e, val) => {
-                                setTypeAlignment(val?.optionValue)
-                            }}
-                            size="small"
-                            renderInput={(params) => <TextField {...params} label={'Type'} variant="outlined" name={'type'} />}
-                        />
-                        {/* <ToggleButtonGroup
+  return (
+    <div className="main-container-v1">
+      <Grid container className="headerbox-v1">
+        <Grid item md={4} sm={11} xs={10}>
+          <CustomBreadCrumbs routes={[routes.iotChart]} />
+        </Grid>
+      </Grid>
+      <CustomContainer>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 pb-3">
+          <Autocomplete
+            options={types}
+            fullWidth
+            loading={loading}
+            getOptionLabel={(option: any) => option.optionLabel ?? ''}
+            getOptionSelected={(option: any, value: any) => option?.optionValue === value?.optionValue}
+            value={types?.find((t) => t.optionValue === typeAlignment) || {}}
+            onChange={(e, val) => {
+              setTypeAlignment(val?.optionValue);
+            }}
+            size="small"
+            renderInput={(params) => <TextField {...params} margin="none" size={'small'} label={'Type'} variant="outlined" name={'type'} />}
+          />
+          <Autocomplete
+            multiple
+            onOpen={() => {
+              setSerializedAssetOptions([]);
+              setLoading(true);
+              fetchOptions('');
+            }}
+            onInputChange={(event, value) => fetchOptions(value)}
+            options={serializedAssetOptions}
+            fullWidth
+            loading={loading}
+            getOptionLabel={(option: any) => option.optionLabel ?? ''}
+            getOptionSelected={(option: any, value: any) => option?.optionValue === value?.optionValue}
+            value={serializedAssets || []}
+            onChange={(e, val) => {
+              setSerializedAssets(val);
+            }}
+            size="small"
+            renderInput={(params) => (
+              <TextField {...params} margin="none" size={'small'} label={'Serialized Assets'} variant="outlined" name={'serializedAssets'} />
+            )}
+          />
+          {/* <ToggleButtonGroup
                             color="primary"
                             value={typeAlignment}
                             size='small'
@@ -100,12 +101,13 @@ function IotChart() {
                             <ToggleButton color='primary' value="byCategory">By Category</ToggleButton>
                             <ToggleButton value="dataPoint">Data Point</ToggleButton>
                         </ToggleButtonGroup> */}
-                    </Grid>
-                </Grid>
-                <Chart serializedAssets={serializedAssets?.map(s => s?.optionValue)} type={typeAlignment} />
-            </CustomContainer>
         </div>
-    )
+        <div className=" max-h-[calc(100vh-200px)] overflow-y-auto -mx-5 px-5 pb-5">
+          <Chart serializedAssets={serializedAssets?.map((s) => s?.optionValue)} type={typeAlignment} />
+        </div>
+      </CustomContainer>
+    </div>
+  );
 }
 
 export default IotChart;
