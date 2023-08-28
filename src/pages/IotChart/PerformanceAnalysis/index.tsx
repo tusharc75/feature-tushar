198b7ff5af6d656c88dc437898e4186353500dfe
dhrubobarman <dhrubobarman847@gmail.com>
2023-08-28 14:14:02 +0530

    fix: small fix

--- src/pages/IotChart/PerformanceAnalysis/index.tsx
+++ src/pages/IotChart/PerformanceAnalysis/index.tsx
@@ -9,7 +9,6 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 const PerformanceAnalysis = ({ assetId, dataPoints = [] }) => {
-
   const [dateFilters, setDateFilters] = useState({
     from: new Date(moment().subtract(15, 'days').format('MM-DD-YYYY')),
     to: new Date(),
@@ -35,17 +34,14 @@ const PerformanceAnalysis = ({ assetId, dataPoints = [] }) => {
             </p>
             <div className="sm:h-[calc(574px-48px)] h-[250px] px-4 overflow-auto py-1">
               <FormGroup>
-                {Array.isArray(dataPoints) && (
+                {Array.isArray(dataPoints) &&
                   _.uniqBy(dataPoints, 'category')?.map((d: any, i) => {
                     return (
-                      <TreeView
-                        aria-label="disabled items"
-                        defaultCollapseIcon={<ExpandMoreIcon />}
-                        defaultExpandIcon={<ChevronRightIcon />}
-                      >
+                      <TreeView aria-label="disabled items" defaultCollapseIcon={<ExpandMoreIcon />} defaultExpandIcon={<ChevronRightIcon />}>
                         <TreeItem nodeId={d?.category?.toString()} label={d?.category}>
-                          {
-                            dataPoints?.filter(data => data?.category === d?.category)?.map(dataPoint => {
+                          {dataPoints
+                            ?.filter((data) => data?.category === d?.category)
+                            ?.map((dataPoint) => {
                               return (
                                 <FormControlLabel
                                   control={
@@ -61,14 +57,12 @@ const PerformanceAnalysis = ({ assetId, dataPoints = [] }) => {
                                   }
                                   label={dataPoint?.fieldLabel}
                                 />
-                              )
-                            })
-                          }
+                              );
+                            })}
                         </TreeItem>
                       </TreeView>
-                    )
-                  })
-                )}
+                    );
+                  })}
               </FormGroup>
             </div>
           </div>
@@ -77,11 +71,13 @@ const PerformanceAnalysis = ({ assetId, dataPoints = [] }) => {
               <Chart
                 dateFilters={dateFilters}
                 assetId={assetId}
-                dataPoints={Object.keys(selectedDataPoint).filter((_k) => selectedDataPoint[_k])?.map((k) => dataPoints?.find((d) => d.fieldName === k))}
+                dataPoints={Object.keys(selectedDataPoint)
+                  .filter((_k) => selectedDataPoint[_k])
+                  ?.map((k) => dataPoints?.find((d) => d.fieldName === k))}
               />
             ) : (
               <div className="text-center grid place-items-center text-xl font-semibold text-gray-400 dark:text-gray-300 min-h-[574px]">
-                <p className="border-dashed border-r-0 border-l-0 py-4">Select Some Datapoints</p>
+                <p className="border-dashed border-r-0 border-l-0 py-4 select-none">Select Some Datapoints</p>
               </div>
             )}
           </div>
