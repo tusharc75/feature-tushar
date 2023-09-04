ac929440713e7909f49de71cd9ef83a3a69bfd5e
Romit Sadaria <romit.sadaria@gmail.com>
2023-09-04 10:42:40 +0530

    fix: bug fix

--- src/pages/RepairOrder/WorkOrder/index.tsx
+++ src/pages/RepairOrder/WorkOrder/index.tsx
@@ -656,8 +656,6 @@ const WorkOrder = ({
   const handleSaveData = async (rows: any) => {
     setUpdating(true);
     const workOrderIds = uniq(rows?.map((e) => e?.workOrder?._id))
-    console.log(rows)
-    console.log(workOrderIds)
     try {
       await asyncForEach(workOrderIds, async (id: any) => {
         const data: any = JSON.parse(JSON.stringify(rows?.filter((e) => e?.workOrder?._id === id)))
