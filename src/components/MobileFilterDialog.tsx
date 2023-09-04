b22664b3f0398e3d3ccbf3003e254903a776f8f7
dhrubobarman <dhrubobarman847@gmail.com>
2023-09-04 13:38:53 +0530

    feat: mobile filter and mobile display filter Component

--- src/components/MobileFilterDialog.tsx
+++ src/components/MobileFilterDialog.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Dialog, IconButton } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
-import { Add, HighlightOff } from '@material-ui/icons';
+import { Add, HighlightOff, Close } from '@material-ui/icons';
 import { Autocomplete } from '@material-ui/lab';
 import * as React from 'react';
 import CustomDialogContent from 'src/components/CustomDialog/CustomDialogContent';
@@ -10,22 +10,24 @@ import SaveFilterDialog from 'src/components/GridFilter/SaveFilterDialog';
 import { v4 as uuidv4 } from 'uuid';
 import { CustomToastContext } from '../StateProvider/CustomToastContext/CustomToastContext';
 import { isObjectEmpty } from '../constants/helpers';
+import lodash from 'lodash';
 
 import axiosInstance from 'src/axios/axiosInstance';
 import './MobileFilterDialog.scss';
+import { useStore, MOBILE_FILTER_FORM_DATA, MOBILE_USER_FILTER, MOBILE_FILTER_MODEL, MOBILE_FILTER_CLEARED } from 'src/StateProvider/fastContext';
+import type { IMoileFilterModel } from 'src/StateProvider/fastContext';
 
-// const Transition = React.forwardRef(function Transition(
-//   props: TransitionProps & {
-//     children: React.ReactElement<any, any>;
-//   },
-//   ref: React.Ref<unknown>
-// ) {
-//   return <Slide direction="up" ref={ref} {...props} />;
-// });
+type TInputField = { id: string; fieldValue: string; fieldName: string };
 
 export default function MobileFilterDialog({ isOpen, handleClose, contentPart, columns, dispatch, filters, title, resource = '' }) {
+  const [storeUserFilter, setStoreUserFilter] = useStore((store) => store[MOBILE_USER_FILTER]);
+  const [storeFormData, setStoreFormData] = useStore((store) => store[MOBILE_FILTER_FORM_DATA]);
+  const [storeFilterCleared, setStoreFilterCleared] = useStore((store) => store[MOBILE_FILTER_CLEARED]);
+  const [storeMobileFilterModel, setStoreMobileFilterModel] = useStore((store) => store[MOBILE_FILTER_MODEL]);
+  const initialRender = React.useRef(true);
+
   const toastConfig = React.useContext(CustomToastContext);
-  const [inputFields, setInputFields] = React.useState(null);
+  const [inputFields, setInputFields] = React.useState<TInputField[] | null>(null);
   const [userFilters, setUserFilters] = React.useState(null);
   const [isSaveFilter, setIsSaveFilter] = React.useState({ open: false, data: null });
   const [formValues, setFormValues] = React.useState({});
@@ -66,12 +68,15 @@ export default function MobileFilterDialog({ isOpen, handleClose, contentPart, c
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    var result = {};
+    var result: IMoileFilterModel['data'] = {};
     inputFields?.forEach((v) => {
       if (v.fieldValue && v.fieldValue !== '') {
         result[v.fieldName] = { filter: v.fieldValue };
       }
     });
+    saveDataInStore();
+    setStoreMobileFilterModel({ [MOBILE_FILTER_MODEL]: { data: result, changedFrom: 'applyFilter' } });
+    console.log(result);
     dispatch({ type: 'filter', filters: result });
     toastConfig.setToastConfig({
       open: true,
@@ -131,7 +136,7 @@ export default function MobileFilterDialog({ isOpen, handleClose, contentPart, c
 
   const openSaveFilterModal = (e) => {
     e.preventDefault();
-    var result = {};
+    const result = {};
     inputFields?.forEach((v) => {
       if (v.fieldValue && v.fieldValue !== '') {
         result[v.fieldName] = v.fieldValue;
@@ -140,6 +145,44 @@ export default function MobileFilterDialog({ isOpen, handleClose, contentPart, c
     setFormValues(result);
     setIsSaveFilter({ open: true, data: selectedUserFilter });
   };
+
+  const saveDataInStore = React.useCallback(() => {
+    const result = {};
+    inputFields?.forEach((v) => {
+      if (v.fieldValue && v.fieldValue !== '') {
+        result[v.fieldName] = v.fieldValue;
+      }
+    });
+    setStoreFormData({ [MOBILE_FILTER_FORM_DATA]: result });
+    setStoreUserFilter({ [MOBILE_USER_FILTER]: selectedUserFilter });
+  }, [setStoreFormData, setStoreUserFilter, inputFields, selectedUserFilter]);
+
+  const resetStore = React.useCallback(() => {
+    setStoreFormData({ [MOBILE_FILTER_FORM_DATA]: null });
+    setStoreUserFilter({ [MOBILE_USER_FILTER]: null });
+  }, [setStoreFormData, setStoreUserFilter]);
+
+  // To reset filter for other pages
+  React.useEffect(() => {
+    if (initialRender.current) {
+      resetStore();
+      initialRender.current = false;
+    }
+  }, [resetStore]);
+
+  //  // To change filter from display filter component
+  React.useEffect(() => {
+    if (storeMobileFilterModel?.changedFrom === 'showFilter' && !storeFilterCleared) {
+      if (!storeUserFilter) setSelectedUserFilter(null);
+      dispatch({ type: 'filter', filters: storeMobileFilterModel.data });
+      toastConfig.setToastConfig({
+        open: true,
+        type: 'success',
+        message: 'Filtered Successfully'
+      });
+      setStoreFilterCleared({ [MOBILE_FILTER_CLEARED]: true });
+    }
+  }, [storeMobileFilterModel, storeFilterCleared, setStoreFilterCleared, dispatch, toastConfig, storeUserFilter]);
 
   return (
     <>
@@ -278,3 +321,83 @@ export default function MobileFilterDialog({ isOpen, handleClose, contentPart, c
     </>
   );
 }
+
+type TData = { id: string; name: string; value: string | number | null; from: 'userFilter' | 'formData' };
+
+export const DisplayFiltersForMobile = () => {
+  const [storeUserFilter, setStoreUserFilter] = useStore((store) => store[MOBILE_USER_FILTER]);
+  const [storeFormData, setStoreFormData] = useStore((store) => store[MOBILE_FILTER_FORM_DATA]);
+  const [_, setStoreMobileFilterModel] = useStore((store) => store[MOBILE_FILTER_MODEL]);
+  const [storeFilterCleared, setStoreFilterCleared] = useStore((store) => store[MOBILE_FILTER_CLEARED]);
+  const [data, setData] = React.useState<TData[]>([]);
+
+  function humanize(str: string): string {
+    return lodash.capitalize(lodash.trim(lodash.snakeCase(str).replace(/_id$/, '').replace(/_/g, ' ')));
+  }
+
+  React.useEffect(() => {
+    if (storeUserFilter) {
+      const newData: TData = { id: uuidv4(), name: '', value: '', from: 'userFilter' };
+      newData.name = storeUserFilter?.title;
+      setData([newData]);
+    } else {
+      const newData: TData[] = [];
+      if (storeFormData) {
+        for (const [key, value] of Object.entries(storeFormData)) {
+          newData.push({ id: uuidv4(), name: key, value: value, from: 'formData' });
+        }
+      }
+      setData(newData);
+    }
+  }, [storeUserFilter, storeFormData]);
+
+  const clearFilter = React.useCallback(
+    (data: TData) => {
+      const filterModel: IMoileFilterModel = { changedFrom: 'showFilter', data: {} };
+      if (data.from === 'userFilter') {
+        setStoreUserFilter({ [MOBILE_USER_FILTER]: null });
+        setStoreFormData({ [MOBILE_FILTER_FORM_DATA]: null });
+        setData([]);
+      } else {
+        const newStoreData: { [key: string]: string } = {};
+        const newData: TData[] = [];
+        for (const [key, value] of Object.entries(storeFormData)) {
+          if (key !== data.name) {
+            newStoreData[key] = value;
+            newData.push({ id: uuidv4(), name: key, value: value, from: 'formData' });
+            if (value && value !== '') {
+              filterModel.data[key] = { filter: value };
+            }
+          }
+        }
+
+        setData(newData);
+        setStoreFormData({ [MOBILE_FILTER_FORM_DATA]: newStoreData });
+      }
+      setStoreMobileFilterModel({ [MOBILE_FILTER_MODEL]: filterModel });
+      setStoreFilterCleared({ [MOBILE_FILTER_CLEARED]: false });
+    },
+    [setStoreUserFilter, setStoreFormData, storeFormData, setStoreFilterCleared, setStoreMobileFilterModel]
+  );
+
+  return (
+    <div className="flex flex-wrap gap-2">
+      {data.map((item) => (
+        <div
+          key={item.id}
+          className="bg-[var(--new\_theme\_secondary\_color)] text-[var(--primary)] font-medium rounded-lg pl-2 pr-1 text-[14px] flex gap-1 items-center"
+        >
+          <span>
+            {item.from === 'formData' ? humanize(item.name) : item.name}
+            {item.value !== '' ? `=${item.value}` : ''}
+          </span>
+          <span>
+            <IconButton aria-label="delete" size="small" style={{ width: 20, height: 20 }} onClick={() => clearFilter(item)}>
+              <Close style={{ width: 16, height: 16 }} />
+            </IconButton>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
