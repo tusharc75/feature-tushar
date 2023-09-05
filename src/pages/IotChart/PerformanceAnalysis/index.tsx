172acae369d9e419e5dc2579ebb19c60efe3873e
manu patel <manupatel18112000@gmail.com>
2023-09-05 18:28:55 +0530

    fix bug

--- src/pages/IotChart/PerformanceAnalysis/index.tsx
+++ src/pages/IotChart/PerformanceAnalysis/index.tsx
@@ -1,10 +1,12 @@
 import { useCallback, useEffect, useState } from 'react';
-import { Box, Checkbox, FormControlLabel, FormGroup, Collapse, IconButton } from '@material-ui/core';
+import { Box, Checkbox, FormControlLabel, FormGroup, Collapse, IconButton, TextField } from '@material-ui/core';
 import moment from 'moment';
 import FilterModel from '../Helper/FilterModel';
 import Chart from '../Helper/Chart1';
 import { uniqBy } from 'lodash';
 import { ExpandLess, ExpandMore } from '@material-ui/icons';
+import { Autocomplete } from '@material-ui/lab';
+import axiosInstance from 'src/axios/axiosInstance';
 
 const PerformanceAnalysis = ({ assetId, dataPoints = [] }) => {
   const [dateFilters, setDateFilters] = useState({
@@ -17,6 +19,9 @@ const PerformanceAnalysis = ({ assetId, dataPoints = [] }) => {
 
   const [open, setOpen] = useState<string | false>(false);
   const [openChild, setOpenChild] = useState<string | false>(false);
+  const [showAlert, setShowAlert] = useState(false)
+  const [errorOptions, setErrorOptions] = useState([])
+  const [selectedErrorOptions, setSelectedErrorOptions] = useState(null)
 
   const handleChange = useCallback((name: string) => {
     setOpen((prev) => (!prev ? name : prev === name ? false : name));
@@ -108,6 +113,28 @@ const PerformanceAnalysis = ({ assetId, dataPoints = [] }) => {
     )
   }
 
+  const fetchErrorData = async () => {
+    const { data: { data } } = await axiosInstance().get(`/report/iot/asset-error-message?asset=${assetId}`)
+    const error: any = []
+    uniqBy(data, '_id')?.forEach((e: any) => {
+      if (e?.errorMessage) {
+        error.push({
+          optionLabel: e?.errorMessage,
+          optionValue: e?._id
+        })
+      }
+    });
+    setErrorOptions(error)
+  };
+
+  useEffect(() => {
+    if (showAlert) {
+      fetchErrorData()
+    } else {
+      setSelectedErrorOptions(null)
+    }
+  }, [assetId, showAlert])
+
   return (
     <>
       <FilterModel dateFilters={dateFilters} setDateFilters={setDateFilters} />
@@ -150,13 +177,54 @@ const PerformanceAnalysis = ({ assetId, dataPoints = [] }) => {
           </div>
           <div className="container-with-border sm:h-[calc(574px-48px)] h-[250px] px-4 overflow-auto py-1">
             {Object.keys(selectedDataPoint).filter((item) => selectedDataPoint[item]).length ? (
-              <Chart
-                dateFilters={dateFilters}
-                assetId={assetId}
-                dataPoints={Object.keys(selectedDataPoint)
-                  .filter((_k) => selectedDataPoint[_k])
-                  ?.map((k) => dataPoints?.find((d) => d.fieldName === k))}
-              />
+              <>
+                <Box display='flex' alignItems='center'>
+                  <FormControlLabel
+                    style={{ margin: 0 }}
+                    control={
+                      <Checkbox
+                        checked={showAlert}
+                        onChange={(e) => {
+                          setShowAlert(e.target.checked);
+                        }}
+                        name="showAlert"
+                        color="primary"
+                      />
+                    }
+                    label="Show Alert"
+                  />
+                  {
+                    showAlert && (
+                      <Box ml={2}>
+                        <Autocomplete
+                          options={errorOptions}
+                          multiple
+                          fullWidth
+                          style={{ minWidth: '260px' }}
+                          getOptionLabel={(option: any) => option?.optionLabel ?? ''}
+                          value={selectedErrorOptions ? errorOptions?.filter((data: any) => selectedErrorOptions?.includes(data.optionValue)) : []}
+                          getOptionSelected={(option: any, val: any) => option.optionValue === val.optionValue}
+                          onChange={(e, newVal) => {
+                            setSelectedErrorOptions(newVal?.map((val) => val.optionValue));
+                          }}
+                          size="small"
+                          renderInput={(params) => <TextField {...params} label="Select Error" variant="outlined" />}
+                        />
+                      </Box>
+                    )
+                  }
+                </Box>
+                <Box mt={1}>
+                  <Chart
+                    dateFilters={dateFilters}
+                    assetId={assetId}
+                    dataPoints={Object.keys(selectedDataPoint)
+                      .filter((_k) => selectedDataPoint[_k])
+                      ?.map((k) => dataPoints?.find((d) => d.fieldName === k))}
+                    errorDescriptions={selectedErrorOptions}
+                  />
+                </Box>
+              </>
             ) : (
               <div className="text-center grid place-items-center text-xl font-semibold text-gray-400 dark:text-gray-300 min-h-[574px]">
                 <p className="border-dashed border-r-0 border-l-0 py-4 select-none">Select Some Datapoints</p>
