b10b5367ef9574e35c58f913b40c6f831db0c369
Romit Sadaria <romit.sadaria@gmail.com>
2023-09-21 13:53:25 +0530

    fix: bug fix

--- src/pages/RentalManagement/ProgressiveBilling/CreateBillingDialog.tsx
+++ src/pages/RentalManagement/ProgressiveBilling/CreateBillingDialog.tsx
@@ -577,6 +577,9 @@ const CreateBillingDialog = ({ rentalManagementData, currencySymbol, invoiceData
 
         let priceFieldName = Object.keys(element).find((d) => d.includes('price_'));
 
+        const priceField = allFields?.find((e) => e.fieldName === 'price');
+        console.log(priceField)
+
         let calValues: any;
         let values = JSON.parse(JSON.stringify(tempValues));
 
@@ -594,7 +597,7 @@ const CreateBillingDialog = ({ rentalManagementData, currencySymbol, invoiceData
           if (proRata) {
             values['pricingMethod'] = 'Per Day';
             if (priceFieldName) {
-              values[priceFieldName] = orginalMaterial.find((d) => d._id === element._id)[priceFieldName] / 7;
+              values[priceFieldName] = parseFloat((orginalMaterial.find((d) => d._id === element._id)[priceFieldName] / 7)?.toFixed(priceField?.decimalPlaces || 2));
             }
           }
           calValues = autoCalculateSpecificFields(values, { ...element, ...values }, allFields);
@@ -603,7 +606,7 @@ const CreateBillingDialog = ({ rentalManagementData, currencySymbol, invoiceData
           if (proRata) {
             values['pricingMethod'] = 'Per Day';
             if (priceFieldName) {
-              values[priceFieldName] = orginalMaterial.find((d) => d._id === element._id)[priceFieldName] / 30;
+              values[priceFieldName] = parseFloat((orginalMaterial.find((d) => d._id === element._id)[priceFieldName] / 30)?.toFixed(priceField?.decimalPlaces || 2));
             }
           }
           calValues = autoCalculateSpecificFields(values, { ...element, ...values }, allFields);
