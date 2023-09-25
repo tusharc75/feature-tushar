033c9f84ca2f8e46e1ca4e386a660920d8ad2f74
Harshit <harshit@vebholic.com>
2023-09-25 13:19:01 +0530

    fix: bug fi

--- src/pages/DeviceTemplates/CustomDataPoints/ManageCustomDataPoints.tsx
+++ src/pages/DeviceTemplates/CustomDataPoints/ManageCustomDataPoints.tsx
@@ -78,8 +78,6 @@ export default function ManageRules({ deviceTemplate, open, isClone = false, id 
     }
   };
 
-  console.log(initialValue?.dataPoints)
-
   const handleSubmit = (values) => {
     setLoading(true);
     if (id && !isClone) {
@@ -135,8 +133,6 @@ export default function ManageRules({ deviceTemplate, open, isClone = false, id 
     }
     return errors;
   }
-
-  console.log(iotDataPoints);
 
   return (
     <>
