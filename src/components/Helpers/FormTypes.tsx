14ec88489dfd5885b701148c435d68a2e9b4ab2e
FanFeet <fanfeettutorial@gmail.com>
2023-09-25 16:11:42 +0530

    fix bug

--- src/components/Helpers/FormTypes.tsx
+++ src/components/Helpers/FormTypes.tsx
@@ -825,10 +825,17 @@ const FormTypes = (props) => {
             onChange
               ? onChange
               : (e) => {
-                handleChange(name, e.target.value ?
-                  parseFloat((parseFloat(e.target.value)?.toFixed(fieldData?.decimalPlaces === undefined ? 2 : fieldData?.decimalPlaces))) : 0);
+                handleChange(name, e.target.value === '' ? 0 : parseFloat(parseFloat(e.target.value)?.toFixed(fieldData?.decimalPlaces === undefined ? 2 : fieldData?.decimalPlaces)));
               }
           }
+          // onChange={
+          //   onChange
+          //     ? onChange
+          //     : (e) => {
+          //       handleChange(name, e.target.value ?
+          //         parseFloat((parseFloat(e.target.value)?.toFixed(fieldData?.decimalPlaces === undefined ? 2 : fieldData?.decimalPlaces))) : 0);
+          //     }
+          // }
         />
       </InfoLabel>
     ) : type === 'email' ? (
