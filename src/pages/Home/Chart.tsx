09d206dbe3f390c8104821cfc9084b80d669ea8c
dhrubobarman <dhrubobarman847@gmail.com>
2023-10-04 10:58:59 +0530

    fix: added shadow

--- src/pages/Home/Chart.tsx
+++ src/pages/Home/Chart.tsx
@@ -83,7 +83,7 @@ const Chart = () => {
   };
 
   return charts && !userLoading ? (
-    <Box p={2} mt={3} className=" rounded-[10px] bg-[var(--card-bg)]">
+    <Box p={2} mt={3} className=" rounded-[10px] bg-[var(--card-bg)] shadow-[0px_3.90676px_39.0676px_rgba(0,0,0,0.08)]">
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <React.Fragment>
           <GlobalFilter globalFilters={globalFilters} setGlobalFilters={setGlobalFilters} disabled={false} dashboardList={[]} />
