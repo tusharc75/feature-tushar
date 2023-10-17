08e50cea33ecc4b1d06b30435c2d93656f8ae511
Romit Sadaria <romit.sadaria@gmail.com>
2023-10-17 18:16:57 +0530

    fix: bug fix

--- src/pages/Quotation/Productpackage/index.tsx
+++ src/pages/Quotation/Productpackage/index.tsx
@@ -9,7 +9,7 @@ import HtmlTooltip from '../../../components/CustomTooltipTitle';
 import CustomReactTable from '../../../components/CustomReactTable/CustomReactTable';
 import NoDataCell from '../../../components/Helpers/NoDataCell';
 import Add from '@material-ui/icons/Add';
-import { quotation, pricingCondition, supplierContact, QUOTATION_TYPE, MATERIAL_TYPE, ASSET_STATUS } from '../../../constants/helpers';
+import { quotation, pricingCondition, supplierContact, QUOTATION_TYPE, MATERIAL_TYPE, ASSET_STATUS, SERVICE_TYPE } from '../../../constants/helpers';
 import ConfirmationDialog from '../../../components/Helpers/ConfirmationDialog';
 import QuotationQtyDialog from './QuotationQtyDialog';
 import { autoCalculateSpecificFields } from '../../../constants/formulaUtility';
@@ -581,14 +581,16 @@ const Productpackage = ({ quotationData, setNextStep, renderedFrom, stepFullScre
             >
               Add Existing Products
             </MenuItem>
-            <MenuItem
-              onClick={() => {
-                closeAddActions();
-                setAddDialog({ open: true, type: 'package', parentId: null });
-              }}
-            >
-              Add Existing Packages
-            </MenuItem>
+            {quotationData?.type !== QUOTATION_TYPE.fieldJob &&
+              <MenuItem
+                onClick={() => {
+                  closeAddActions();
+                  setAddDialog({ open: true, type: 'package', parentId: null });
+                }}
+              >
+                Add Existing Packages
+              </MenuItem>
+            }
             <MenuItem
               onClick={() => {
                 closeAddActions();
@@ -766,7 +768,6 @@ const Productpackage = ({ quotationData, setNextStep, renderedFrom, stepFullScre
       {addDialog.open && addDialog.type === 'product' && (
         <AssignProductDialog
           reference={'quotation'}
-          serialized={null}
           productsDialogOpen={addDialog.open}
           productId={null}
           handleCloseDialog={() => setAddDialog({ open: false, type: '', parentId: null })}
@@ -774,6 +775,7 @@ const Productpackage = ({ quotationData, setNextStep, renderedFrom, stepFullScre
           onSuccess={(d) => {
             handleAdd(d);
           }}
+          serialized={quotationData?.type === QUOTATION_TYPE.fieldJob ? false : null}
         />
       )}
       {addDialog.open && addDialog.type === MATERIAL_TYPE.serializedAsset && (
@@ -817,6 +819,8 @@ const Productpackage = ({ quotationData, setNextStep, renderedFrom, stepFullScre
           onSuccess={(rows) => {
             handleAdd(rows);
           }}
+          extraStaticFilter={quotationData?.type === QUOTATION_TYPE.fieldJob ? [{ field: 'serviceType', term: SERVICE_TYPE.fieldService }]
+            : []}
         />
       )}
       {addDialog.open && addDialog.type === 'package' && (
@@ -900,14 +904,16 @@ const Productpackage = ({ quotationData, setNextStep, renderedFrom, stepFullScre
             >
               Add Existing Products
             </MenuItem>
-            <MenuItem
-              onClick={() => {
-                setAddDialog({ open: true, type: 'package', parentId: addchildDialog.parentId });
-                setAddchildDialog({ open: false, parentId: null, parentType: null, serializedProduct: false, top: null, bottom: null });
-              }}
-            >
-              Add Existing Packages
-            </MenuItem>
+            {quotationData?.type !== QUOTATION_TYPE.fieldJob &&
+              <MenuItem
+                onClick={() => {
+                  setAddDialog({ open: true, type: 'package', parentId: addchildDialog.parentId });
+                  setAddchildDialog({ open: false, parentId: null, parentType: null, serializedProduct: false, top: null, bottom: null });
+                }}
+              >
+                Add Existing Packages
+              </MenuItem>
+            }
             <MenuItem
               onClick={() => {
                 setAddDialog({ open: true, type: 'service', parentId: addchildDialog.parentId });
