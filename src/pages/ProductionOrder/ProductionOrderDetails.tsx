6ff55d6cde1acd8c3991cc38deb28878e79e921a
sunanda35 <sunandasamanta60@gmail.com>
2023-10-17 16:36:28 +0530

    loading ticket in po

--- src/pages/ProductionOrder/ProductionOrderDetails.tsx
+++ src/pages/ProductionOrder/ProductionOrderDetails.tsx
@@ -25,6 +25,7 @@ import { ACTIVITY_RESOURCE, PRODUCTION_ORDER_STATUS, productionOrder, production
 import ManageProductionOrder from './ManageProductionOrder';
 import Material from './Material';
 import WorkOrder from './WorkOrder';
+import LoadingTicket from './LoadingTicket';
 
 function a11yProps(index: any) {
   return {
@@ -126,6 +127,9 @@ const ProductionOrderDetails = () => {
         var isAllowedToEdit = [...(data.collaborator ?? []), data.owner].some((d) => d?.optionValue === user?.user?._id);
         if (user?.role?.selectedEntity?.superAdminAccess) {
           isAllowedToEdit = true;
+        }
+        if (!data?.customerAccount) {
+          setProductionOrderProcessSteps(productionOrderSteps.filter((o) => o.name !== 'Loading Ticket'));
         }
         setAllowedToEdit(isAllowedToEdit);
         setAllowedToDelete(data?.owner?.optionValue === user?.user?._id);
@@ -344,6 +348,17 @@ const ProductionOrderDetails = () => {
                 setCurrentStep={setCurrentStep}
               />
             )}
+            {productionOrderProcessStepsNames[currentStep] === 'Loading Ticket' && productionOrderData && (
+              <LoadingTicket
+                productionOrderData={productionOrderData}
+                setNextStep={setNextStep}
+                renderedFrom={`${renderedFrom}_grid-2`}
+                stepFullScreen={stepFullScreen}
+                allowedToEdit={allowedToEdit && permissions?.productionOrder?.isUpdate ? true : false}
+                setCurrentStep={setCurrentStep}
+              />
+            )}
+
           </ContentFullScreen>
         </TabPanel>
       </Box>
