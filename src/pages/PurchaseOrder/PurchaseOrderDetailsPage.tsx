8e2ea70c9e8cde1d1c422bcc4b6ce120973c944a
Harshit <harshit@vebholic.com>
2023-10-23 17:29:20 +0530

    fix: bug fi

--- src/pages/PurchaseOrder/PurchaseOrderDetailsPage.tsx
+++ src/pages/PurchaseOrder/PurchaseOrderDetailsPage.tsx
@@ -322,10 +322,12 @@ const PurchaseOrderDetailsPage = () => {
               <BiFoodMenu className="mr-1" fontSize="inherit" /> Invoice
             </CustomTab>
           )}
-          {purchaseOrderData?.deleted && (isMobile && !isTablet) ? null : (
-            <CustomTab className={'tabLayout'} index={2} {...a11yProps(2)}>
-              <RiFlowChart className="mr-1" fontSize="inherit" /> Views
-            </CustomTab>
+          {purchaseOrderData?.deleted ? null : (
+            !(isMobile && !isTablet) && (
+              <CustomTab className={'tabLayout'} index={2} {...a11yProps(2)}>
+                <RiFlowChart className="mr-1" fontSize="inherit" /> Views
+              </CustomTab>
+            )
           )}
         </CustomTabs>
         <TabPanel value={tabValue} index={0}>
