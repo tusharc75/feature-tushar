8a7e008c78e66b76c6d62a6cc944eba16cb4c796
dhrubobarman <dhrubobarman847@gmail.com>
2023-10-26 15:54:26 +0530

    fix: alert modal

--- src/components/MessageDialog/index.tsx
+++ src/components/MessageDialog/index.tsx
@@ -15,6 +15,8 @@ import CustomDialogHeader from '../CustomDialog/CustomDialogHeader';
 import { useState } from 'react';
 import { isMobile, isTablet } from 'react-device-detect';
 import { CustomDialogTransition } from 'src/constants/helpers';
+import DashboardModal, { ModalHead } from '../DashboardModal';
+import { Error } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   table: {
@@ -57,54 +59,48 @@ export default function CustomMessageDialog({
   };
 
   return (
-    <Dialog
-      disableEscapeKeyDown
-      fullScreen={fullScreen || isMobile || isTablet}
-      maxWidth="sm"
-      TransitionComponent={CustomDialogTransition}
+    <DashboardModal
+      dialogProps={{
+        disableEscapeKeyDown: true,
+        fullScreen: fullScreen || isMobile || isTablet,
+        maxWidth: 'sm',
+        TransitionComponent: CustomDialogTransition,
+        onClose: (e, reason) => {
+          if (reason !== 'backdropClick') {
+          }
+        },
+        keepMounted: true,
+        fullWidth: true
+      }}
+      modalHead={{
+        icon: <Error color={'error'} />,
+        title: 'Message',
+        fullScreenOption: true
+      }}
+      handleClose={() => onClose()}
       aria-labelledby="confirmation-dialog-title"
       open={open}
-      onClose={(e, reason) => {
-        if (reason !== 'backdropClick') {
-        }
-      }}
       id="confirmation-dialog"
-      keepMounted
-      fullWidth
     >
-      <CustomDialogHeader
-        title={'Message'}
-        onClose={onClose}
-        showRequiredLabel={false}
-        isMinimized={!fullScreen}
-        onMinimizeMaximize={() => {
-          setFullScreen((prevState) => !prevState);
-        }}
-        showManimizeMaximize={true}
-      />
-      <DialogContent>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Index</TableCell>
-              <TableCell align="right">Message</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {getMessageList(errorMessages)?.map((d) => (
-              <TableRow>
-                <TableCell>{d?.indexes?.map((index) => index).toString()}</TableCell>
-                <TableCell align="right">{d?.message}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </DialogContent>
-      <DialogActions>
-        <Button size="small" onClick={onClose} color="primary">
-          {!forwardText ? 'Close' : forwardText}
-        </Button>
-      </DialogActions>
-    </Dialog>
+      <div className="grid gap-2">
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            title={d?.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            {console.log(d)}
+            <div>
+              <h6 className="text-[13px] mb-[6px]">Index:</h6>
+              <p className="text-[14px]">{d?.indexes?.map((index) => `${index}`.padStart(2, '0'))}</p>
+            </div>
+            <div>
+              <h6 className="text-[13px] mb-[6px]">Error Message:</h6>
+              <p className="text-[14px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </DashboardModal>
   );
 }
