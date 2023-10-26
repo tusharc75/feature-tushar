37aa2ffa0fcfc3c290ce9e69555f3e9e7b2ab903
dhrubobarman <dhrubobarman847@gmail.com>
2023-10-26 15:57:20 +0530

    perf: cleanup

--- src/components/MessageDialog/index.tsx
+++ src/components/MessageDialog/index.tsx
@@ -1,28 +1,9 @@
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  makeStyles
-} from '@material-ui/core';
-import CustomDialogHeader from '../CustomDialog/CustomDialogHeader';
+import { makeStyles } from '@material-ui/core';
+import { Error } from '@material-ui/icons';
 import { useState } from 'react';
 import { isMobile, isTablet } from 'react-device-detect';
 import { CustomDialogTransition } from 'src/constants/helpers';
-import DashboardModal, { ModalHead } from '../DashboardModal';
-import { Error } from '@material-ui/icons';
-
-const useStyles = makeStyles((theme) => ({
-  table: {
-    minWidth: 100
-  }
-}));
+import DashboardModal from '../DashboardModal';
 
 interface ErrorMessages {
   index?: number;
@@ -40,10 +21,6 @@ export default function CustomMessageDialog({
   onClose: () => void;
   forwardText?: string;
 }) {
-  const classes = useStyles();
-
-  const [fullScreen, setFullScreen] = useState(false);
-
   const getMessageList = (message) => {
     const errorMessages: any = [];
     message?.forEach((m) => {
@@ -62,7 +39,7 @@ export default function CustomMessageDialog({
     <DashboardModal
       dialogProps={{
         disableEscapeKeyDown: true,
-        fullScreen: fullScreen || isMobile || isTablet,
+        fullScreen: isMobile || isTablet,
         maxWidth: 'sm',
         TransitionComponent: CustomDialogTransition,
         onClose: (e, reason) => {
@@ -85,11 +62,11 @@ export default function CustomMessageDialog({
       <div className="grid gap-2">
         {getMessageList(errorMessages)?.map((d) => (
           <div
+            key={d.message}
             title={d?.message}
             className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
             style={{ border: '1px solid var(--common-border-color)' }}
           >
-            {console.log(d)}
             <div>
               <h6 className="text-[13px] mb-[6px]">Index:</h6>
               <p className="text-[14px]">{d?.indexes?.map((index) => `${index}`.padStart(2, '0'))}</p>
