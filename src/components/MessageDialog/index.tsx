6dd4d9ddabd6021959a14e61b3c80f5aee8fa0b4
dhrubobarman <dhrubobarman847@gmail.com>
2023-10-27 14:02:34 +0530

    fix: jira EP-863

--- src/components/MessageDialog/index.tsx
+++ src/components/MessageDialog/index.tsx
@@ -74,6 +74,246 @@ export default function CustomMessageDialog({
             </div>
           </div>
         ))}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}{' '}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}{' '}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}{' '}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}{' '}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}{' '}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}{' '}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}{' '}
+        {getMessageList(errorMessages)?.map((d) => (
+          <div
+            key={d.message}
+            className="grid grid-cols-[1fr_5fr] gap-[20px]  shadow-[0px_5.44444px_27.22222px_0px_rgba(0,_0,_0,_0.06)] px-[15px] py-[12px] md:px-[20px] rounded-lg"
+            style={{ border: '1px solid var(--common-border-color)' }}
+          >
+            <div title={d?.indexes?.toString()} className="cursor-help">
+              <p className="text-[13px] mb-[8px]">Index</p>
+              <h6 className="text-[16px] line-clamp-1">{d?.indexes?.toString()}</h6>
+            </div>
+            <div title={d?.message} className=" cursor-help">
+              <p className="text-[13px] mb-[8px]">Message</p>
+              <p className="text-[16px] line-clamp-1">{d?.message}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </DashboardModal>
   );
