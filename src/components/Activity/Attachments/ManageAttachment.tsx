35b65dabe9461e9faa1d66f6e8cf5c505f2c36fe
Harshil Singhal <59230670+harshil1509@users.noreply.github.com>
2023-11-01 16:57:21 +0530

    file category attachments

--- src/components/Activity/Attachments/ManageAttachment.tsx
+++ src/components/Activity/Attachments/ManageAttachment.tsx
@@ -21,9 +21,12 @@ import CommonSkeleton from 'src/components/Helpers/CommonSkeleton';
 import AttachmentThumbnail from 'src/components/AttachmentThumbnail';
 import { isEqual } from 'lodash';
 import DocumentScanner from '../Helpers/DocumentScanner';
+import {attachmentFileCategory} from 'src/constants/helpers';
+import { Autocomplete } from '@material-ui/lab';
 
 const AttachmentSchema = object().shape({
   name: string().required('Attachment Name is required'),
+  category : object().nullable(),
   fileUrl: string().required('please upload attachment')
 });
 
@@ -111,7 +114,8 @@ export default function ManageAttachment({
       request = {
         name: values.name,
         file: otherAttachments,
-        relatedTo: relatedTo
+        relatedTo: relatedTo,
+        category : values.category ? values.category : {value : '', label : ''}
       };
     } else {
       request = {
@@ -226,8 +230,8 @@ export default function ManageAttachment({
             <CustomDialogContent>
               <Form autoComplete="off" autoCorrect="off" noValidate>
                 <Box padding={1}>
-                  <Grid container spacing={3}>
-                    <Grid item xs={12}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={type == "file" ? 6 : 12}>
                       <TextField
                         variant="outlined"
                         type="text"
@@ -245,6 +249,30 @@ export default function ManageAttachment({
                         }}
                       />
                     </Grid>
+                    {type === 'file' && (
+                      <Grid item xs={6}>
+                        <Autocomplete
+                          id="file-category"
+                          size="small"
+                          options={attachmentFileCategory}
+                          renderInput={(params) => (
+                            <TextField
+                              {...params}
+                              size="small"
+                              variant="outlined"
+                              label="Category"
+                              margin="dense"
+                            />
+                          )}
+                          getOptionLabel={(option) => option.label}
+                          getOptionSelected={(option: any, value: any) => option.label === value.label}
+                          onChange={(e, val) => {
+                            setFieldValue('category', val)
+                          }}
+                          value={values['category']}
+                        />
+                      </Grid>
+                    )}
                     {type === 'file' && (
                       <Grid container item xs={12}>
                         <Grid item xs={12}>
