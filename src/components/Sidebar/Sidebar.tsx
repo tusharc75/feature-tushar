00021753a942795d1fe5632c9e66196d0c2962e2
dhrubobarman <dhrubobarman847@gmail.com>
2023-11-01 10:48:22 +0530

    feat: sidebar collapsing feature improved

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,25 +1,33 @@
-import { Collapse, CssBaseline, Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar, Tooltip } from '@material-ui/core';
-import { ExpandLess, ExpandMore } from '@material-ui/icons';
+import { Collapse, CssBaseline, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, Toolbar, Tooltip } from '@material-ui/core';
+import { Close, ExpandLess, ExpandMore } from '@material-ui/icons';
 import clsx from 'clsx';
 import { kebabCase, lowerCase } from 'lodash';
 import React, { useContext, useEffect, useState } from 'react';
 import { Link, useHistory, withRouter } from 'react-router-dom';
+import { SVG } from 'src/assets';
 import { setDataBySectionName } from 'src/pages/Home/helpers';
 import { GlobalChatContext } from '../../StateProvider/GlobalChatContext';
+import { CustomOfflineContext } from '../../StateProvider/OfflineContext/OfflineContext';
 import { useData } from '../../StateProvider/Provider';
 import Header from '../Header/Header';
-import { BsChatLeftTextFill } from 'react-icons/bs';
-import { CustomOfflineContext } from '../../StateProvider/OfflineContext/OfflineContext';
-import { SVG } from 'src/assets';
 import routes from '../Helpers/Routes';
-import useStyles from './style';
 import styles from './sidebar.module.scss';
+import useStyles from './style';
 import { TResource, TSidebarItem, TSidebarSection } from './type';
 import { isSectionActive, isSectionVisible, staticSidebarData } from './utils';
+import { useStore, SIDEBAR_OPEN, SIDEBAR_OPENED_BY_BUTTON } from 'src/StateProvider/fastContext';
 
-function SideBar({ toggleDrawer, setToggleDrawer, location }) {
+function SideBar({ location }) {
   const [itemToAddActiveClass, setItemToAddActiveClass] = useState(NaN);
   const [subItemToAddActiveClass, setSubItemToAddActiveClass] = useState(NaN);
+
+  const [isSidebarOpen, setIsSidebarOpen] = useStore((store) => store[SIDEBAR_OPEN]);
+  const [sidebarOpenedByButton, setSidebarOpenedByButton] = useStore((store) => store[SIDEBAR_OPENED_BY_BUTTON]);
+
+  const handleSidebarClose = () => {
+    setIsSidebarOpen({ [SIDEBAR_OPEN]: false });
+    setSidebarOpenedByButton({ [SIDEBAR_OPENED_BY_BUTTON]: false });
+  };
 
   const {
     state: { permissions, user, selectedEntity, tour }
@@ -38,13 +46,6 @@ function SideBar({ toggleDrawer, setToggleDrawer, location }) {
     return sideBarIcon;
   };
 
-  const handleToggleDrawer = () => {
-    setToggleDrawer(!toggleDrawer);
-    if (toggleDrawer) {
-      setOpen({});
-    }
-  };
-
   const handleRoutes = (item) => {
     switch (item.name) {
       case 'Pos':
@@ -55,10 +56,10 @@ function SideBar({ toggleDrawer, setToggleDrawer, location }) {
   };
 
   useEffect(() => {
-    if (!toggleDrawer) {
+    if (!isSidebarOpen) {
       setOpen({});
     }
-  }, [toggleDrawer]);
+  }, [isSidebarOpen]);
 
   const getListItem = () => {
     if (user) {
@@ -104,59 +105,72 @@ function SideBar({ toggleDrawer, setToggleDrawer, location }) {
   return (
     <div>
       <CssBaseline />
-      <Header toggleDrawer={handleToggleDrawer} isDrawerOpen={toggleDrawer} />
+      <Header />
       <Drawer
         variant="permanent"
         className={clsx(styles.drawer, 'sidebar-drawer', {
-          [classes.drawerOpen]: toggleDrawer,
-          [classes.drawerClose]: !toggleDrawer,
-          'sidebar-overflow-hide': !toggleDrawer && tour.stepIndex !== 1,
-          'sidebar-overflow-auto': toggleDrawer && tour.stepIndex !== 1
+          [classes.drawerOpen]: isSidebarOpen,
+          [classes.drawerClose]: !isSidebarOpen,
+          'sidebar-overflow-hide': !isSidebarOpen && tour.stepIndex !== 1,
+          'sidebar-overflow-auto': isSidebarOpen && tour.stepIndex !== 1
         })}
         classes={{
           paper: clsx(styles.drawer, {
-            [classes.drawerOpen]: toggleDrawer,
-            [classes.drawerClose]: !toggleDrawer,
-            'sidebar-overflow-hide': !toggleDrawer && tour.stepIndex !== 1,
-            'sidebar-overflow-auto': toggleDrawer && tour.stepIndex !== 1,
+            [classes.drawerOpen]: isSidebarOpen,
+            [classes.drawerClose]: !isSidebarOpen,
+            'sidebar-overflow-hide': !isSidebarOpen && tour.stepIndex !== 1,
+            'sidebar-overflow-auto': isSidebarOpen && tour.stepIndex !== 1,
             'sidebar-drawer': true
           })
         }}
+        onClick={() => {
+          if (sidebarOpenedByButton) return;
+          toggleTimeout = setTimeout(() => setIsSidebarOpen({ [SIDEBAR_OPEN]: true }), 300);
+        }}
         onMouseEnter={() => {
-          toggleTimeout = setTimeout(() => setToggleDrawer(true), 300);
+          if (sidebarOpenedByButton) return;
+          toggleTimeout = setTimeout(() => setIsSidebarOpen({ [SIDEBAR_OPEN]: true }), 300);
         }}
         onMouseLeave={() => {
+          if (sidebarOpenedByButton) return;
           if (toggleTimeout) {
             clearTimeout(toggleTimeout);
           }
-          if (toggleDrawer)
+          if (isSidebarOpen)
             setTimeout(() => {
-              setToggleDrawer(false);
+              setIsSidebarOpen({ [SIDEBAR_OPEN]: false });
             }, 500);
         }}
         keepMounted
       >
         <Toolbar />
         <div id="sidebarOrDrawer" style={{ borderTop: '1px solid #485B64' }}>
-          <div className="max-[959px]:min-h-[56px] min-[771px]:min-h-[unset] max-[770px]:min-h-[56px] ">
-            <img
-              className={`${styles.logo} ${toggleDrawer ? 'block' : 'hidden'} ml-[22px]`}
-              src={user.brandLogo || SVG('LogoNew')}
-              onClick={() => history.push('/')}
-              alt="equip logo"
-              title="eQuipt Logo"
-            />
-            <img
-              className={`${styles.logo} ${toggleDrawer ? 'hidden' : 'block'} mx-auto `}
-              src={SVG('LogoNewShort')}
-              onClick={() => history.push('/')}
-              alt="equip logo"
-              title="eQuipt Logo"
-            />
+          <div className="max-[959px]:min-h-[56px] min-[769px]:min-h-[unset] max-[768px]:min-h-[56px] ">
+            <div className={`max-[768px]:pr-[50px] ${styles.logo}`}>
+              <img
+                className={` ${isSidebarOpen ? 'block' : 'hidden'} ml-[22px] max-h-[33px]`}
+                src={user.brandLogo || SVG('LogoNew')}
+                onClick={() => history.push('/')}
+                alt="equip logo"
+                title="eQuipt Logo"
+              />
+              <img
+                className={`${isSidebarOpen ? 'hidden' : 'block'} mx-auto max-h-[33px]`}
+                src={SVG('LogoNewShort')}
+                onClick={() => history.push('/')}
+                alt="equip logo"
+                title="eQuipt Logo"
+              />
+            </div>
+            <div className={`${sidebarOpenedByButton && 'max-[768px]:block'} hidden absolute right-0 top-[5px]`}>
+              <IconButton onClick={handleSidebarClose}>
+                <Close />
+              </IconButton>
+            </div>
           </div>
           <List
             className={`${styles.listContainer} sidebar-list max-h-[calc(100vh-80px)] ${
-              toggleDrawer ? 'overflow-y-auto' : 'overflow-y-hidden'
+              isSidebarOpen ? 'overflow-y-auto' : 'overflow-y-hidden'
             } overflow-x-hidden`}
           >
             {getListItem()?.map((listItem, i) => {
@@ -164,13 +178,13 @@ function SideBar({ toggleDrawer, setToggleDrawer, location }) {
 
               return (
                 <React.Fragment key={listItem.name}>
-                  <Tooltip title={!toggleDrawer ? listItem.name : ''}>
+                  <Tooltip title={!isSidebarOpen ? listItem.name : ''}>
                     <ListItem
                       className={`${styles.listItem} dropdown-items ${isSectionActive(pathName, location.pathname, listItem) && styles.activeList}`}
                       button
                       key={listItem.name + '' + i}
                       onClick={() => {
-                        if (toggleDrawer) {
+                        if (isSidebarOpen) {
                           handleCollapse(listItem.name);
                         }
                         if (!listItem.items?.length) {
