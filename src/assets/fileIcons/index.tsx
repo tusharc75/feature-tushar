4f8425f02929c347b3c5aade703671b9951fcd55
dhrubobarman <dhrubobarman847@gmail.com>
2023-11-06 18:30:15 +0530

    fix: uniform file icons

--- src/assets/fileIcons/index.tsx
+++ src/assets/fileIcons/index.tsx
@@ -81,23 +81,3 @@ export const fileIcons = [
     icon: VideoFile
   }
 ];
-
-export const getFileIcon = (fileName: string) => {
-  let extension = fileName.substring(fileName.lastIndexOf('.')).toLowerCase();
-  let data = fileIcons.find((o) => o.extensions.indexOf(extension) >= 0);
-  if (data && data?.icon) {
-    return data.icon;
-  } else {
-    return FileIcon; // default icon if no icon is found for the file extension.
-  }
-};
-
-export const getFileNameWithExtension = (file: { url: string; name: string }) => {
-  const fileNameExtension = file.name.lastIndexOf('.');
-  if (fileNameExtension > -1) {
-    return file.name;
-  } else {
-    const extension = file.url.substring(file.url.lastIndexOf('.')).toLowerCase();
-    return `${file.name}${extension}`;
-  }
-};
