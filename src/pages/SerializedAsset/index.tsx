24eccca9a751a2dfb7fd02359fae107324c09142
dhrubobarman <dhrubobarman847@gmail.com>
2023-11-07 11:13:13 +0530

    fix: jira ep-845

--- src/pages/SerializedAsset/index.tsx
+++ src/pages/SerializedAsset/index.tsx
@@ -36,7 +36,7 @@ import HtmlTooltip from '../../components/CustomTooltipTitle';
 import ImportExportLinks from '../../components/Helpers/ImportExportLinks';
 import useColumns, { getStaticFields, getFrameworkComponents, gridFilterParser } from '../../constants/useColumns';
 import { prepareDataForGrid } from '../../constants/helpers';
-import { AiFillCrown, MdAdd } from 'react-icons/all';
+import { AiFillCrown, BsBox, HiOutlineDocumentText, MdAdd } from 'react-icons/all';
 import CustomSwipableList from '../../components/SwipableListComponents/CustomSwipableList';
 import { isMobile, isTablet } from 'react-device-detect';
 import { Autocomplete } from '@material-ui/lab';
@@ -825,6 +825,7 @@ const SerializedAsset = () => {
             </div>
           </div>
         </div>
+        {console.log(columns)}
         {columns ? (
           isMobile && !isTablet ? (
             <CustomSwipableList
@@ -841,7 +842,16 @@ const SerializedAsset = () => {
               // onEdit={(d) => {
               //   history.push(`${routes.serializedAssetDetail.path}/${d._id}`);
               // }}
-              additionalDetails={[]}
+              additionalDetails={[
+                {
+                  icon: <BsBox />,
+                  field: 'product'
+                },
+                {
+                  icon: <HiOutlineDocumentText />,
+                  field: 'productDescription'
+                }
+              ]}
               extraParamsToCheckDelete={false}
               // onDelete={(d) => {
               //   setDeleteRecord(d);
