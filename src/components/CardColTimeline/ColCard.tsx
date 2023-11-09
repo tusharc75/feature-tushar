82c5084a970c660f2f46eb8b121dce7a41ff2276
dhrubobarman <dhrubobarman847@gmail.com>
2023-11-09 14:02:30 +0530

    fix: small fix

--- src/components/CardColTimeline/ColCard.tsx
+++ src/components/CardColTimeline/ColCard.tsx
@@ -88,7 +88,6 @@ const ColCard: React.FC<colDataInterface> = ({ data, cardOnClick, cardDataRows, 
         if (item.type === 'timer') {
           const stepTimes = getFieldsWithOtherDetails(data[item.accessor] || []);
           if (!stepTimes.length) return <div className="min-h-[20px]" />;
-          console.log(stepTimes);
           if (stepTimes.length > 0)
             return (
               <Typography key={index} className={styles.cardDetails}>
