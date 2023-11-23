a0abf8b9bc4d7f8286d27e8e0962e6c1d17786c6
Romit Sadaria <romit.sadaria@gmail.com>
2023-11-23 11:33:02 +0530

    fix: bug fix

--- src/pages/RentalManagement/Productpackage/index.tsx
+++ src/pages/RentalManagement/Productpackage/index.tsx
@@ -219,7 +219,8 @@ const Productpackage = ({ rentalManagementData, setNextStep, setNextStepToolTip,
             </HtmlTooltip>
             {allowedToEdit ? (
               row.original.hideSelection ? (
-                <HtmlTooltip title={'Asset is already assigned'}>
+                <HtmlTooltip title={row.original.assetQty ? 'Asset is already assigned' :
+                  row.original?.status ? rentalManagementMessage.loadingAlreadyCreated : ''}>
                   <span>
                     <IconButton size="small" aria-label="Details" disabled={true}>
                       <DeleteIcon fontSize="small" color={'disabled'} />
