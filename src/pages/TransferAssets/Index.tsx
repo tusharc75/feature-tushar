4637be90aebfb6d18c49e4e0636a2510e3a78d7d
Romit Sadaria <romit.sadaria@gmail.com>
2023-11-24 17:30:51 +0530

    fix: bug fix

--- src/pages/TransferAssets/Index.tsx
+++ src/pages/TransferAssets/Index.tsx
@@ -1,4 +1,4 @@
-import { Box, Chip, Tooltip } from '@material-ui/core';
+import { Box, Chip } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/Add';
@@ -7,9 +7,7 @@ import FileCopyIcon from '@material-ui/icons/FileCopy';
 import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
 import { camelCase } from 'lodash';
 import queryString from 'query-string';
-import React, { useContext, useEffect, useReducer, useState } from 'react';
-import { isMobile, isTablet } from 'react-device-detect';
-import { MdOutlineFilterAlt, TbArrowsSort } from 'react-icons/all';
+import { useContext, useEffect, useReducer, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { CustomToastContext } from 'src/StateProvider/CustomToastContext/CustomToastContext';
 import { useData } from 'src/StateProvider/Provider';
@@ -22,16 +20,15 @@ import ConfirmationDialog from 'src/components/Helpers/ConfirmationDialog';
 import ImportExportLinks from 'src/components/Helpers/ImportExportLinks';
 import routes from 'src/components/Helpers/Routes';
 import SearchBox from 'src/components/Helpers/SearchBox';
-import HideWhenOffline from 'src/components/HideWhenOffline';
-import MobileFilterDialog, { DisplayFiltersForMobile } from 'src/components/MobileFilterDialog';
-import MobileSortDialog from 'src/components/MobileSortDialog';
 import { gridLoadingTimeout, prepareDataForGrid, sidebarResource, transferAsset } from 'src/constants/helpers';
 import styles from '../Leads/Header.module.scss';
 import ManageTransferAsset from './ManageTransferAsset';
 import CustomReactTable, { getStaticFields, gridFilterParser, useColumns } from 'src/components/CustomReactTableNew';
+import { cloneDisable, deleteDisable } from 'src/constants/messageHelpers';
 
 const TransferAsset = () => {
-  const TransferAssetType = [
+
+  const types = [
     {
       key: `My ${routes.transferAsset.title}`,
       value: 1
@@ -41,7 +38,9 @@ const TransferAsset = () => {
       value: 2
     }
   ];
+
   let renderedFrom = camelCase(routes?.transferAsset.title);
+
   const toastConfig = useContext(CustomToastContext);
   const [showManageTransferAssetDialog, setShowManageTransferAssetDialog] = useState({ open: false, isClone: false, idToClone: null });
   const [showDeleteConfirmBox, setShowDeleteConfirmBox] = useState(false);
@@ -49,14 +48,10 @@ const TransferAsset = () => {
   const [deleteRecord, setDeleteRecord] = useState(null);
   const [columns, setColumns] = useState(null);
   const [state, dispatch] = useReducer(reducer, intialState);
-  const { dataRows, appendRows, rowCount, page, limit, search, filters, sorting, selectedRecords, showFilteredRecordsOnly } =
-    state;
-  const [open, setOpen] = React.useState(false);
-  const [isOpenDialog, setisOpenDialog] = useState(false);
+  const { rowCount, page, limit, search, filters, sorting, selectedRecords, showFilteredRecordsOnly } = state;
   const history = useHistory();
   let { type, referenceId, referenceType }: any = queryString.parse(history.location.search);
   const [selectedType, setSelectedType] = useState(type ? parseInt(type) : 1);
-  const [filter, setFilter] = useState(`All ${routes.transferAsset.title}`);
 
   const {
     state: { user, permissions, selectedEntity }
@@ -68,7 +63,7 @@ const TransferAsset = () => {
   }, []);
 
   useEffect(() => {
-    fetchTransferAsset();
+    fetchData();
   }, [page, limit, filters, sorting, search, selectedEntity, selectedType, showFilteredRecordsOnly]);
 
   const fetchGridColumns = () => {
@@ -98,76 +93,52 @@ const TransferAsset = () => {
     canDrag: false,
     Cell: ({ row }) => (
       <>
-        {permissions?.transferAsset?.isCreate && (
-          <HtmlTooltip title="Clone">
+        <HtmlTooltip title={permissions?.transferAsset?.isCreate ? "Clone" : cloneDisable}  >
+          <span>
             <IconButton
               size="small"
               aria-label="Clone"
+              disabled={permissions?.transferAsset?.isCreate ? false : true}
               onClick={() => {
                 setShowManageTransferAssetDialog({ open: true, isClone: true, idToClone: row.original._id });
               }}
             >
-              <FileCopyIcon color="primary" />
+              <FileCopyIcon fontSize="small" color={permissions?.transferAsset?.isCreate ? 'primary' : 'disabled'} />
             </IconButton>
-          </HtmlTooltip>
-        )}
-        {permissions?.transferAsset?.isDelete && row.original.status === 'New' ? (
-          <Tooltip title="Delete">
+          </span>
+        </HtmlTooltip>
+        <HtmlTooltip title={row?.original?.canDelete ? "Delete" : deleteDisable}>
+          <span>
             <IconButton
               size="small"
               aria-label="Delete"
+              disabled={row?.original?.canDelete ? false : true}
               onClick={() => {
                 setDeleteRecord(row.original);
                 setShowDeleteConfirmBox(true);
               }}
             >
-              <DeleteIcon color="error" />
+              <DeleteIcon fontSize="small" color={row?.original?.canDelete ? 'error' : 'disabled'} />
             </IconButton>
-          </Tooltip>
-        ) : (
-          <Tooltip title="Don't have the permissions to Delete">
-            <IconButton size="small" aria-label="Delete" className="cursor-stop">
-              <DeleteIcon color="disabled" />
-            </IconButton>
-          </Tooltip>
-        )}
+          </span>
+        </HtmlTooltip>
       </>
     )
   };
 
-  const fetchTransferAsset = () => {
+  const fetchData = () => {
     dispatch({ type: 'loading', loading: true });
 
     const queryString = getQueryString();
-    axiosInstance()
-      .get(`${transferAsset.api}${queryString}`)
-      .then(({ data }) => {
-        let rows = data.data?.map((u) => {
-          let finalObject = prepareDataForGrid(u, user);
-          finalObject['canDelete'] = false;
-          finalObject['isChecked'] = selectedRecords.some((s) => s._id === u._id);
-          finalObject['allowedToEdit'] = permissions?.transferAsset?.isUpdate;
+    axiosInstance().get(`${transferAsset.api}${queryString}`)
+      .then(({ data: { data, count } }) => {
+        let rows = data?.map((u) => {
+          let finalObject: any = prepareDataForGrid(u, user);
+          finalObject['isChecked'] = false;
+          finalObject['canDelete'] = permissions?.transferAsset?.isDelete && finalObject?.ownerId === user?.user?._id && u?.canDelete;
           return finalObject;
         });
-
-        data.data = data.data?.map((u, i) => ({
-          ...prepareDataForGrid(u, user)
-        }));
-        if (appendRows) {
-          dispatch({
-            type: 'initialize',
-            data: [...dataRows, ...rows],
-            count: data.count
-          });
-        } else {
-          dispatch({
-            type: 'initialize',
-            data: rows,
-            count: data.count
-          });
-        }
-
-        // dispatch({ type: 'initialize', data: rows, count: data.count });
+        dispatch({ type: 'initialize', data: rows, count: count });
         setTimeout(() => {
           dispatch({ type: 'loading', loading: false });
         }, gridLoadingTimeout);
@@ -221,17 +192,6 @@ const TransferAsset = () => {
     return deepFilter;
   };
 
-  const columnState = JSON.parse(localStorage.getItem(routes.transferAsset?.title));
-  if (columnState) {
-    columns.map((item) => {
-      columnState.map((d) => {
-        if (d.colId == item.field) {
-          item.show = !d.hide;
-        }
-      });
-    });
-  }
-
   const handleDelete = () => {
     let ids = [];
     if (deleteRecord) {
@@ -243,7 +203,8 @@ const TransferAsset = () => {
     axiosInstance()
       .put(`${transferAsset.api}/remove`, { ids: ids })
       .then(() => {
-        fetchTransferAsset();
+        dispatch({ type: 'selection', selectedRecords: [] });
+        fetchData();
         setShowDeleteConfirmBox(false);
         setDeleteRecord(null);
         setDeleting(false);
@@ -253,42 +214,9 @@ const TransferAsset = () => {
         toastConfig.setToastConfig(error);
       });
   };
-  const handleTransferAssetTypeSel = (filterValues) => {
-    dispatch({ type: 'setPage', page: 0 });
-    setSelectedType(filterValues);
-    if (referenceId && referenceType) {
-      history.push(`?type=${filterValues}&referenceType=${referenceType}&referenceId=${referenceId}`);
-    } else {
-      history.push(`?type=${filterValues}`);
-    }
-  };
-
-  const handleFilter = (event, newFilter) => {
-    if (newFilter != null) {
-      dispatch({ type: 'setPage', page: 0 });
-      setFilter(newFilter);
-      handleTransferAssetTypeSel(TransferAssetType.find((d) => d.key === newFilter).value);
-    }
-  };
 
   const handleSearch = (e) => {
     dispatch({ type: 'search', search: e.target.value });
-  };
-
-  const handleOpen = () => {
-    setisOpenDialog(true);
-  };
-
-  const handleClose = () => {
-    setisOpenDialog(false);
-  };
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClickClose = () => {
-    setOpen(false);
   };
 
   const updateQueryParams = () => {
@@ -300,20 +228,19 @@ const TransferAsset = () => {
     history.replace({
       search: queryParams.toString()
     });
-    fetchTransferAsset();
+    fetchData();
   };
 
-  const toggleInner = TransferAssetType && (
-    <ToggleButtonGroup size="small" value={TransferAssetType[selectedType - 1].key} exclusive onChange={handleFilter}>
-      {TransferAssetType.map((k, index) => {
-        return (
-          <ToggleButton value={k.key} key={index}>
-            {k.key}
-          </ToggleButton>
-        );
-      })}
-    </ToggleButtonGroup>
-  );
+  const onTypeChange = (event, type) => {
+    dispatch({ type: 'pageChange', page: 0 });
+    const value = types.find((d) => d.key === type).value;
+    setSelectedType(value);
+    if (referenceId && referenceType) {
+      history.push(`?type=${value}&referenceType=${referenceType}&referenceId=${referenceId}`);
+    } else {
+      history.push(`?type=${value}`);
+    }
+  };
 
   return (
     <section className="main-container-v1">
@@ -321,17 +248,17 @@ const TransferAsset = () => {
         <CustomBreadCrumbs routes={[routes.transferAsset]} />
         <ImportExportLinks
           permissions={permissions?.transferAsset}
-          module="purchase order"
+          module={routes.transferAsset.title}
           api={transferAsset.api}
           afterImportCompleted={() => {
-            fetchTransferAsset();
+            fetchData();
           }}
           isExportAllOrSomeFeature={true}
           total={rowCount}
           recordsToExport={selectedRecords?.length}
           ids={selectedRecords?.map((obj) => obj._id)}
           onExportToExcelSuccess={() => {
-            fetchTransferAsset();
+            fetchData();
           }}
           additionalParams={getQueryString(true)}
         />
@@ -340,60 +267,24 @@ const TransferAsset = () => {
         <div className="header-panel">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
             <div className={'d-flex align-items-center gap-1 w-full'}>
-              {isMobile && !isTablet ? (
-                <div className="d-flex flex-wrap items-center justify-between w-full gap-2">
-                  <div>
-                    <HideWhenOffline>{toggleInner}</HideWhenOffline>
-                  </div>
-                  <div className="flex flex-wrap items-center gap-1 justify-end">
-                    <IconButton
-                      onClick={handleClickOpen}
-                      id="demo-customized-button"
-                      aria-controls="demo-customized-menu"
-                      aria-haspopup="true"
-                      // aria-expanded={open ? 'true' : undefined}
-                      className={'mobileIconButton secondary'}
-                      size="small"
-                    >
-                      <TbArrowsSort className="rotate-90" size={16} />
-                    </IconButton>
-                    <MobileSortDialog
-                      isOpen={open}
-                      handleClose={handleClickClose}
-                      contentPart={null}
-                      secHeading={['Sort Transfer Assests']}
-                      columns={columns}
-                      dispatch={dispatch}
-                    />
-                    <IconButton
-                      id="demo-customized-button"
-                      aria-controls="demo-customized-menu"
-                      aria-haspopup="true"
-                      // aria-expanded={open ? 'true' : undefined}
-                      className={'mobileIconButton secondary'}
-                      size="small"
-                      onClick={handleOpen}
-                    >
-                      <MdOutlineFilterAlt size={16} />
-                    </IconButton>
-                    <MobileFilterDialog
-                      isOpen={isOpenDialog}
-                      handleClose={handleClose}
-                      contentPart={null}
-                      columns={columns}
-                      dispatch={dispatch}
-                      title={routes?.transferAsset?.title}
-                      filters={filters}
-                      resource={sidebarResource.transferAsset}
-                    />
-                  </div>
-                </div>
-              ) : null}
-              {!isMobile && <HideWhenOffline>{toggleInner}</HideWhenOffline>}
+
+              <ToggleButtonGroup size="small" value={types[selectedType - 1].key} exclusive onChange={onTypeChange}>
+                {types.map((k, index) => {
+                  return (
+                    <ToggleButton value={k.key} key={index}>
+                      {k.key}
+                    </ToggleButton>
+                  );
+                })}
+              </ToggleButtonGroup>
               {referenceType && <Chip className="ml-3" color="primary" label={`Rental Job : ${referenceType}`} onDelete={updateQueryParams} />}
             </div>
             <div className="flex flex-wrap gap-[8px]  justify-end">
-              <SearchBox onChange={handleSearch} className={styles.search_box_input} size="small" value={search} />
+              <SearchBox
+                onChange={handleSearch}
+                className={styles.search_box_input}
+                size="small"
+                value={search} />
               <div className="flex gap-[8px] flex-wrap items-center">
                 {permissions?.transferAsset?.isCreate && (
                   <Button
@@ -411,7 +302,6 @@ const TransferAsset = () => {
                 )}
               </div>
             </div>
-            <DisplayFiltersForMobile resource={sidebarResource.transferAsset} />
           </div>
         </div>
         {columns ? (
@@ -423,10 +313,10 @@ const TransferAsset = () => {
             dispatch={dispatch}
             renderedFrom={renderedFrom}
             isClientSideGrid={false}
-            refreshGrid={fetchTransferAsset}
+            refreshGrid={fetchData}
             showOnlyShowFilteredRecordSwitch={true}
             showFilters={true}
-            resource={sidebarResource.productionOrder}
+            resource={sidebarResource.transferAsset}
           />
         ) : (
           <Box p={2} height={500}>
@@ -441,7 +331,7 @@ const TransferAsset = () => {
           onClose={() => setShowManageTransferAssetDialog({ open: false, isClone: false, idToClone: null })}
           onSuccess={(data) => {
             setShowManageTransferAssetDialog({ open: false, isClone: false, idToClone: null });
-            fetchTransferAsset();
+            fetchData();
             history.push(`${routes.transferAssetDetail.path}/${data._id}`);
           }}
         />
