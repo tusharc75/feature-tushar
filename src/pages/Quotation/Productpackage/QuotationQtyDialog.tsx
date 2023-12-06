aed2d4abf556784766faa05c80a8a35dcc630a38
Romit Sadaria <romit.sadaria@gmail.com>
2023-12-06 19:31:42 +0530

    fix: bug fix

--- src/pages/Quotation/Productpackage/QuotationQtyDialog.tsx
+++ src/pages/Quotation/Productpackage/QuotationQtyDialog.tsx
@@ -145,6 +145,8 @@ const QuotationQtyDialog: FC<EditDialogProps> = ({
         pricingMethodOptions = arrayToDropwdownOption(rowData?.[`${rowData.type}Detail`]?.pricingMethod);
       }
       setPriceMethodList(pricingMethodOptions);
+      console.log(pricingMethodOptions)
+
       let pricingConditionOptions = await getPricing(rowData, pricingMethodOptions);
       data.forEach((element) => {
         if (rowData?.type === 'serializedAsset') {
@@ -176,10 +178,10 @@ const QuotationQtyDialog: FC<EditDialogProps> = ({
           if (element.fieldName === 'pricingMethod') {
             element.option = pricingMethodOptions;
           }
-          if (element.fieldName === 'pricingCondition') {
-            if (Array.isArray(pricingConditionOptions)) {
-              element.option = pricingConditionOptions;
-            }
+        }
+        if (element.fieldName === 'pricingCondition') {
+          if (Array.isArray(pricingConditionOptions)) {
+            element.option = pricingConditionOptions;
           }
         }
       });
