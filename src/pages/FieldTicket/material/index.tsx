931f1b39bced251e2e4dfd9e56001956319ae3f6
Romit Sadaria <romit.sadaria@gmail.com>
2023-12-08 17:00:58 +0530

    fix: bug fix

--- src/pages/FieldTicket/material/index.tsx
+++ src/pages/FieldTicket/material/index.tsx
@@ -17,7 +17,7 @@ import MaterialQtyDialog from './MaterialQtyDialog';
 import { fetch_field_ticket_material_fields } from '../helper';
 import { CustomToastContext } from 'src/StateProvider/CustomToastContext/CustomToastContext';
 import AssignServiceDialog from 'src/components/AssignRolesDialog/AssignServiceDialog';
-import { calculateRowsField } from 'src/components/RentalManagment/helper';
+import { calculatePrice, calculateRowsField } from 'src/components/RentalManagment/helper';
 import Consumables from './Consumables';
 import { FIELD_TICKET_STATUS, MATERIAL_TYPE, SERVICE_TYPE, fieldTicket } from 'src/constants/helpers';
 import EditIcon from '@material-ui/icons/Edit';
@@ -255,14 +255,19 @@ const Material = ({ fieldTicketData, renderedFrom, allowedToEdit, setNextStep, h
       if (calValues && calValues['estimateJobDuration']) {
         element.estimateJobDuration = calValues['estimateJobDuration'];
       }
-      if(!isEmpty(tax)){
+      if (!isEmpty(tax)) {
         element.taxCode = tax?.taxCode;
         element.taxPercentage = tax?.taxPercentage;
       }
       material.push(element);
     });
-    //const priceData: any = await calculatePrice(fieldTicketData, material);
-    AddMaterial(material, null);
+    if (fieldTicketData?.pricingCondition?.optionValue) {
+      const priceData: any = await calculatePrice(fieldTicketData, material);
+      AddMaterial(material, priceData?.filter((e) => e.conditionId === fieldTicketData?.pricingCondition?.optionValue));
+    }
+    else {
+      AddMaterial(material, null);
+    }
   };
 
   const AddMaterial = async (material, priceData) => {
