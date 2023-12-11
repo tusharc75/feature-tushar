9e28f1f2f1519ebba82241a3c0699a9539018cb9
dhrubobarman <dhrubobarman847@gmail.com>
2023-12-11 15:42:40 +0530

    fix: small fix

--- src/components/CustomReactTableNew/TableComponents/TableHelperComponents.tsx
+++ src/components/CustomReactTableNew/TableComponents/TableHelperComponents.tsx
@@ -378,7 +378,9 @@ export const DraggableHeader: React.FC<DraggableHeaderProps> = ({ header, table,
         <div className={`d-flex gap-2 align-items-center ${column.id === 'selection' ? 'justify-center' : 'justify-between'}`}>
           <div className="line-clamp-1">
             <span
-              className={`overflow-hidden overflow-ellipsis whitespace-normal ${header.column.getCanSort() ? 'cursor-pointer' : ''}`}
+              className={`overflow-hidden overflow-ellipsis whitespace-normal ${
+                header.column.getCanSort() && columnDef.disableSortBy !== true ? 'cursor-pointer' : ''
+              }`}
               onClick={header.column.getToggleSortingHandler()}
             >
               {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
