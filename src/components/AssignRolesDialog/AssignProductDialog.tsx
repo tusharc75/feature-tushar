f58858ef0c817c3f7052a5e323e1d93549199874
dhruv <dhruv21.dk@gmail.com>
2023-12-12 01:52:58 +0530

    fix: bug fix

--- src/components/AssignRolesDialog/AssignProductDialog.tsx
+++ src/components/AssignRolesDialog/AssignProductDialog.tsx
@@ -211,6 +211,14 @@ const AssignProductDialog = ({
       if (editRow) {
         dispatch({ type: 'selection', selectedRecords: [...selectedRecords, editRow] });
       }
+    }else{
+      const updatedSelectedRecords = selectedRecords.map((e) => {
+        if (e?._id === row?._id) {
+          return { ...e, qty: parseInt(data.qty), isChecked: true };
+        }
+        return e;
+      });
+      dispatch({ type: 'selection', selectedRecords: updatedSelectedRecords });
     }
     dispatch({ type: 'update', data: rows });
   };
