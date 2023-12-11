6e1370392f5387633e426dad8bf13c79fce35482
dhrubobarman <dhrubobarman847@gmail.com>
2023-12-11 11:02:06 +0530

    fix: row selection issue

--- src/components/CustomReactTableNew/CustomReactTableV8.tsx
+++ src/components/CustomReactTableNew/CustomReactTableV8.tsx
@@ -107,46 +107,6 @@ const CustomReactTable = ({
     }
   }, [newColumns]);
 
-  // For row selection
-  useEffect(() => {
-    let flatSelectedData = [];
-    Object.keys(rowSelection).forEach((key) => {
-      const splittedArray = key.split('.');
-      if (splittedArray.length === 0) {
-        const { subRows, ...rest } = data[key];
-        flatSelectedData.push({ ...rest });
-      } else if (rowSelection[key]) {
-        let dataToStore = null;
-        splittedArray.forEach((f, index) => {
-          if (index === 0) {
-            dataToStore = { ...data[f] };
-          } else {
-            dataToStore = { ...dataToStore['subRows'][f] };
-          }
-        });
-        const { subRows, ...rest } = dataToStore;
-        flatSelectedData.push({ ...rest });
-      }
-    });
-
-    const includedRow: any = [];
-    const notIncludedRow: any = [];
-
-    selectedRecords.forEach((row) => {
-      if (data.find((d) => d.id === row.id)) {
-        includedRow.push(row);
-      } else {
-        notIncludedRow.push(row);
-      }
-    });
-
-    if (onSelect) onSelect([...notIncludedRow, ...flatSelectedData]);
-    dispatch({
-      type: 'selection',
-      selectedRecords: [...notIncludedRow, ...flatSelectedData]
-    });
-  }, [rowSelection, renderedFrom]);
-
   const resetField = () => {
     dispatch({
       type: 'currentEditingCellPosition',
@@ -393,6 +353,30 @@ const CustomReactTable = ({
   }, [isClientSideGrid, limit, page, table, data]);
 
   const { rows } = table.getRowModel();
+
+  // For row selection
+  useEffect(() => {
+    const selectedRows = table.getSelectedRowModel().flatRows.map((d) => {
+      const { subRows, ...rest } = d.original;
+      return { ...rest };
+    });
+    dispatch({
+      type: 'selection',
+      selectedRecords: selectedRows
+    });
+  }, [rowSelection, renderedFrom, table]);
+
+  // Row selection effect for parent component;
+  useEffect(() => {
+    const selectedRows = table.getSelectedRowModel().flatRows;
+    const selectedRecordIds = selectedRecords.map((d) => d._id);
+    if (selectedRecords.length !== selectedRows.length) {
+      for (const row of rows) {
+        if (!selectedRecordIds.includes(row.original._id)) continue;
+        row.toggleSelected(true);
+      }
+    }
+  }, [selectedRecords]);
 
   return (
     <DndProvider backend={isMobile || isTablet ? TouchBackend : HTML5Backend}>
