1c4770ab22a8db867c70314202cfec8d4b44686c
Rajat Gurung <35159243+Rajatgurung@users.noreply.github.com>
2023-12-12 13:03:44 +0530

    feat: iot alert

--- src/pages/IotChart/Alarms/index.tsx
+++ src/pages/IotChart/Alarms/index.tsx
@@ -1,34 +1,74 @@
 import { Box, TextField } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
-import { useEffect, useState } from 'react';
+import moment from 'moment';
+import { useContext, useEffect, useState } from 'react';
+import { CustomToastContext } from 'src/StateProvider/CustomToastContext/CustomToastContext';
 import axiosInstance from 'src/axios/axiosInstance';
 import CustomReactTable, { useTableReducer } from 'src/components/CustomReactTableNew';
 import CommonSkeleton from 'src/components/Helpers/CommonSkeleton';
 import routes from 'src/components/Helpers/Routes';
+import { dateTimeFormat, gridLoadingTimeout } from 'src/constants/helpers';
 
-const Alarms = ({ deviceTemplate }) => {
+const Alarms = ({ deviceTemplate,assetId }) => {
   const { state, dispatch } = useTableReducer();
+  const {  page, limit } = state;
+  const toastConfig = useContext(CustomToastContext);
 
-  const [alarmOptions, setAlarmOptions] = useState([]);
-  const [selectedAlarm, setSelectedAlarm] = useState(null);
+  const [alarmOptions, setAlarmOptions] = useState();
+
+  const [selectedAlarm, setSelectedAlarm] = useState<{
+    optionLabel:string,
+    optionValue:string
+  }>(null);
+
+  
+  const fetchData = async () => {
+    dispatch({ type: 'loading', loading: true });
+
+    try{
+      const res = await axiosInstance().get<{data:any[],count:number}>(`/report/iot/alerts`,
+      {
+        params:{
+          asset:assetId,
+          dataPoint:selectedAlarm.optionValue,
+          page,
+          limit
+        }
+      });
+      const {data:rows,count} = res.data
+      dispatch({ type: 'initialize', data: rows, count:count || 0 });
+    }catch(e){
+      toastConfig.setToastConfig(e);
+    }finally{
+      setTimeout(() => {
+        dispatch({ type: 'loading', loading: false });
+      }, gridLoadingTimeout);
+    }
+  };
+
 
   const columns = [
     {
-      accessor: 'dateTime',
+      accessor: 'time',
       Header: 'Date Time',
-      Cell: ({ row }) => <>dateTime</>
+      Cell: ({ row:{original} }) => <>{moment(original.time).format(dateTimeFormat)}</>
     },
     {
-      accessor: 'alertNumber ',
+      accessor: 'alertNumber',
       Header: 'Alert Number ',
-      Cell: ({ row }) => <>alertNumber</>
+      Cell: ({ row:{original} }) => <>{original.fieldValue}</>
     },
     {
       accessor: 'message',
       Header: 'Message',
-      Cell: ({ row }) => <>message</>
+      Cell: ({ row:{original} }) => <>{original.message}</>
     }
   ];
+
+  useEffect(() => {
+    if(!selectedAlarm) return
+    fetchData()
+  },[selectedAlarm?.optionValue,page])
 
   useEffect(() => {
     if (deviceTemplate) {
@@ -58,7 +98,7 @@ const Alarms = ({ deviceTemplate }) => {
           getOptionLabel={(option) => (option && option?.optionLabel) || ''}
           style={{ width: '350px' }}
             value={selectedAlarm}
-          onChange={(event, newValue) => {
+          onChange={(event, newValue:any) => {
             setSelectedAlarm(newValue);
           }}
           size="small"
@@ -72,11 +112,11 @@ const Alarms = ({ deviceTemplate }) => {
           state={state}
           dispatch={dispatch}
           renderedFrom={'iotChart_Alarms'}
-          refreshGrid={() => {}}
+          refreshGrid={fetchData}
           hideSelection={true}
           hideAction={true}
         />
-      ) : (
+      ) : ( 
         <Box p={2} height={500}>
           <CommonSkeleton lenArray={[...Array(10).keys()]} />
         </Box>
