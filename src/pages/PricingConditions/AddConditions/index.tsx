219f60a607fb18a1e54dc24696c21f010d34a71c
ABHIMANYU KUMAR <manupatel18112000@gmail.com>
2023-12-13 14:30:33 +0530

    fix bugs

--- src/pages/PricingConditions/AddConditions/index.tsx
+++ src/pages/PricingConditions/AddConditions/index.tsx
@@ -9,7 +9,7 @@ import { CustomToastContext } from '../../../StateProvider/CustomToastContext/Cu
 import { pricingCondition, gridLoadingTimeout, PRICING_TYPE, sidebarResource, MATERIAL_TYPE } from '../../../constants/helpers';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
-import CustomReactTable, { useTableReducer } from 'src/components/CustomReactTableNew';
+import CustomReactTable, { gridFilterParser, useTableReducer } from 'src/components/CustomReactTableNew';
 import HtmlTooltip from '../../../components/CustomTooltipTitle';
 import ConditionDialog from './ConditionDialog';
 import { camelCase, startCase } from 'lodash';
@@ -31,7 +31,7 @@ const AddConditions = ({ pricingConditionId, detailData }) => {
   }: any = useData();
 
   const { state, dispatch } = useTableReducer();
-  const { dataRows, rowCount, selectedRecords } = state;
+  const { dataRows, rowCount, page, limit, sorting, selectedRecords } = state;
   const [addMaterialDialog, setAddMaterialDialog] = useState({ open: false, materialType: '' });
   const [condition, setCondition] = useState(null);
   const [showDialog, setShowDialog] = useState({ open: false, isBulkedit: false });
@@ -44,16 +44,18 @@ const AddConditions = ({ pricingConditionId, detailData }) => {
 
   useEffect(() => {
     fetchCondition();
-  }, [pricingConditionId]);
+  }, [page, limit, pricingConditionId]);
 
   const fetchCondition = () => {
     dispatch({ type: 'loading', loading: true });
     dispatch({ type: 'selection', selectedRecords: [] });
 
+    const queryString = getQueryString();
+
     setCondition(null);
     axiosInstance()
-      .get(`${pricingCondition.api}/condition/${pricingConditionId}`)
-      .then(({ data: { data } }) => {
+      .get(`${pricingCondition.api}/condition/${pricingConditionId}${queryString}`)
+      .then(({ data: { data, count } }) => {
         setCondition(JSON.parse(JSON.stringify(data)));
         data.forEach((element) => {
           element.detail = `${
@@ -70,7 +72,7 @@ const AddConditions = ({ pricingConditionId, detailData }) => {
           element.unit = element.unit?.toString();
           element.pricingMethod = element.pricingMethod?.toString();
         });
-        dispatch({ type: 'initialize', data: data, count: data.length });
+        dispatch({ type: 'initialize', data: data, count: count });
         setTimeout(() => {
           dispatch({ type: 'loading', loading: false });
         }, gridLoadingTimeout);
@@ -78,6 +80,15 @@ const AddConditions = ({ pricingConditionId, detailData }) => {
       .catch((error) => {
         toastConfig.setToastConfig(error);
       });
+  };
+
+  const getQueryString = () => {
+    let deepFilter = `?page=${page}&limit=${limit}`;
+
+    if (sorting.length > 0) {
+      deepFilter = `${deepFilter}&sortBy=${sorting[0].colId}&orderBy=${sorting[0].sort}`;
+    }
+    return deepFilter;
   };
 
   const handleAdd = (rows) => {
@@ -421,7 +432,6 @@ const AddConditions = ({ pricingConditionId, detailData }) => {
             dispatch={dispatch}
             renderedFrom={renderFrom}
             refreshGrid={fetchCondition}
-            isClientSideGrid={true}
           />
         ) : (
           <Box p={2} height={500}>
