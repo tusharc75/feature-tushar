8565e0c5b18b59d63b1bfd90a5ca66aae0450017
Romit Sadaria <romit.sadaria@gmail.com>
2023-12-13 16:48:01 +0530

    fix: bug fix

--- src/pages/Quotation/QuoteBuilder/index.tsx
+++ src/pages/Quotation/QuoteBuilder/index.tsx
@@ -133,8 +133,8 @@ const QuoteBuilder = ({
             Header: 'Lead Time (Days)',
             Cell: ({ row }) => (row.original?.leadTime && row.original?.leadTime?.length ? <p>{row.original['leadTime']}</p> : <p>0</p>),
             Footer: (info) => {
-              const total = info.rows
-                .filter((f) => f.values.hasOwnProperty('leadTime') && !isNaN(f.values['leadTime']))
+              let rows = info.table.getExpandedRowModel().rows;
+              const total = rows?.filter((f) => f.values.hasOwnProperty('leadTime') && !isNaN(f.values['leadTime']))
                 .reduce((sum, row) => parseInt(row.values['leadTime']) + sum, 0);
               return <>{total}</>;
             }
