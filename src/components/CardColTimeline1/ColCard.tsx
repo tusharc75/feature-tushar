ae30b71a4a4ab90d258607028114c2c830ad6bb8
dhrubobarman <dhrubobarman847@gmail.com>
2023-12-27 16:26:48 +0530

    fix: small fix

--- src/components/CardColTimeline1/ColCard.tsx
+++ src/components/CardColTimeline1/ColCard.tsx
@@ -129,14 +129,14 @@ const RenderStatusIcon = ({ stepStatus }: { stepStatus: string }) => {
   return (
     <>
       {stepStatus === WORKORDER_SERVICE_STEP_STATUS.passed && (
-        <HtmlTooltip title={stepStatus} placement="top" arrow enterDelay={0}>
+        <HtmlTooltip title={stepStatus} placement="top" arrow enterTouchDelay={0}>
           <Box style={{ color: '#4BAE4F', fontSize: '25px', width: 25 }}>
             <AiFillCheckCircle style={{ display: 'block' }} />
           </Box>
         </HtmlTooltip>
       )}
       {stepStatus === WORKORDER_SERVICE_STEP_STATUS.failed && (
-        <HtmlTooltip title={stepStatus} placement="top" arrow enterDelay={0}>
+        <HtmlTooltip title={stepStatus} placement="top" arrow enterTouchDelay={0}>
           <Box style={{ color: '#F25F54', fontSize: '25px', width: 25 }}>
             <AiFillExclamationCircle style={{ display: 'block' }} />
           </Box>
