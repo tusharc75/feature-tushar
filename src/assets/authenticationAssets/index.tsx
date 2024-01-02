6f93c3a9eb3c54ea4db9b949def9ac829fa7d1a1
dhrubobarman <dhrubobarman847@gmail.com>
2024-01-02 12:24:27 +0530

    fix: small fix

--- src/assets/authenticationAssets/index.tsx
+++ src/assets/authenticationAssets/index.tsx
@@ -381,7 +381,7 @@ export const ForgetPasswordImage: React.FC<svgInterface> = ({ ...others }) => {
           stroke="white"
           stroke-width="8.7548"
           strokeLinecap="round"
-          stroke-linejoin="round"
+          strokeLinejoin="round"
         />
         <path
           d="M35.1187 138.658C36.9177 136.928 39.7246 136.928 41.388 138.658C43.0515 140.387 42.9416 143.192 41.1426 144.922C39.3436 146.652 36.5368 146.652 34.8733 144.922C33.2099 143.192 33.3197 140.388 35.1187 138.658Z"
@@ -510,7 +510,7 @@ export const CreatePasswordImage: React.FC<svgInterface> = ({ ...others }) => {
           stroke="#0F9FA9"
           stroke-width="7.47759"
           strokeLinecap="round"
-          stroke-linejoin="round"
+          strokeLinejoin="round"
         />
         <path
           fill-rule="evenodd"
@@ -520,21 +520,21 @@ export const CreatePasswordImage: React.FC<svgInterface> = ({ ...others }) => {
           stroke="#0F9FA9"
           stroke-width="5.56939"
           strokeLinecap="round"
-          stroke-linejoin="round"
+          strokeLinejoin="round"
         />
         <path
           d="M94.9733 94.485V79.5331M94.9566 70.6266C92.529 70.6266 90.5587 72.597 90.5763 75.0246C90.5763 77.4522 92.5466 79.4221 94.9738 79.4221C97.4014 79.4221 99.3717 77.4522 99.3717 75.0246C99.3717 72.597 97.4014 70.6266 94.9566 70.6266Z"
           stroke="white"
           stroke-width="11.6207"
           strokeLinecap="round"
-          stroke-linejoin="round"
+          strokeLinejoin="round"
         />
         <path
           d="M132.657 78.2682L148.055 116.902M119.035 83.6964L132.657 78.2673M125.276 99.3535L138.897 93.9244M161.164 117.078C152.473 113.34 142.398 117.356 138.661 126.046C134.924 134.737 138.939 144.811 147.63 148.549C156.32 152.286 166.395 148.27 170.132 139.58C173.869 130.89 169.854 120.815 161.164 117.078Z"
           stroke="#31446C"
           stroke-width="6.00324"
           strokeLinecap="round"
-          stroke-linejoin="round"
+          strokeLinejoin="round"
         />
         <path
           fill-rule="evenodd"
--- src/pages/IotChart/Helper/Chart.tsx
+++ src/pages/IotChart/Helper/Chart.tsx
@@ -13,7 +13,7 @@ import { dateTimeFormat24Hours } from 'src/constants/helpers';
 import routes from 'src/components/Helpers/Routes';
 
 const downloadIconHTML = `<div>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ico-download">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" strokeLinejoin="round" class="ico-download">
 <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
 <polyline points="7 10 12 15 17 10"></polyline>
 <line x1="12" y1="15" x2="12" y2="3"></line>
