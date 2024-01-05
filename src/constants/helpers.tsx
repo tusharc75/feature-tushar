95582e20aca6641e657bafaad40e23823b7c6015
Harshit <harshit@vebholic.com>
2024-01-05 17:31:16 +0530

    fix: fix

--- src/constants/helpers.tsx
+++ src/constants/helpers.tsx
@@ -1273,6 +1273,9 @@ export const getPermissions = (user, selectedEntity = undefined): IPermission | 
       const hasApproveAccountPermission = user.user.permissions.approveAccount;
       const accounts = [sidebarResource.customerAccount, sidebarResource.supplierAccount];
 
+      console.log("inside helpers");
+      console.log(data);
+
       data.forEach((d) => {
         const indexOfPermission = sidebarFieldsValues.indexOf(d.name);
         if (indexOfPermission > -1) {
