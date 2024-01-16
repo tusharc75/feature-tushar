573dbd4c6c7e6ec49e037194b7efe98abb7f4e60
sunanda35 <sunandasamanta60@gmail.com>
2024-01-16 14:55:06 +0530

    liveness fix

--- src/components/FacialLogin/FaceLiveNess.tsx
+++ src/components/FacialLogin/FaceLiveNess.tsx
@@ -1,54 +1,17 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { CustomDialogTransition } from 'src/constants/helpers';
 import Dialog from '@material-ui/core/Dialog';
 import axiosInstance from 'src/axios/axiosInstance';
-import { FaceLivenessDetector } from '@aws-amplify/ui-react-liveness';
-import { Loader, ThemeProvider, View } from '@aws-amplify/ui-react';
-import "@aws-amplify/ui-react/styles.css";
-import { Amplify } from 'aws-amplify';
-import awsexports from '../../amplifyconfiguration.json';
-import "./faceLiveness.scss"
 import CustomDialogHeader from '../CustomDialog/CustomDialogHeader';
 import CustomDialogContent from '../CustomDialog/CustomDialogContent';
 import CommonSkeleton from '../Helpers/CommonSkeleton';
 import { Box } from '@material-ui/core';
+import FaceLiveNess from '../FaceLiveness/AWS';
 
-Amplify.configure(awsexports);
-
-const FaceLiveNess = ({ onClose, onComplete }) => {
-  const [cameraPermission, setCameraPermission] = useState('prompt');
+const FaceLogin = ({ onClose, onComplete }) => {
 
   const [loading, setLoading] = useState<boolean>(true);
   const [sessionId, setSessionId] = useState<any>(null);
-  const videoStream = useRef(null);
-
-  useEffect(() => {
-    if (cameraPermission === 'granted' || cameraPermission === 'prompt') {
-      navigator.mediaDevices
-        .getUserMedia({
-          video: {
-            width: { ideal: 1920 },
-            height: { ideal: 1080 },
-          },
-        })
-        .then((stream) => {
-          setCameraPermission('granted');
-          videoStream.current = stream;
-          stream.getTracks().forEach((track) => track.stop());
-        })
-        .catch((err) => {
-          console.error('Camera access denied:', err);
-          setCameraPermission('denied');
-        });
-    }
-
-    return () => {
-      if (videoStream.current) {
-        videoStream.current.getTracks().forEach((track) => track.stop());
-      }
-    };
-  }, [cameraPermission]);
-
   useEffect(() => {
     fetchCreateLiveness();
   }, []);
@@ -59,13 +22,6 @@ const FaceLiveNess = ({ onClose, onComplete }) => {
     const data = res.data.data;
     setSessionId(data.SessionId);
     setLoading(false);
-  };
-
-  const handleCancel = () => {
-    if (videoStream.current) {
-      videoStream.current.getTracks().forEach((track) => track.stop());
-    }
-    onClose();
   };
 
   return (<Dialog
@@ -89,24 +45,7 @@ const FaceLiveNess = ({ onClose, onComplete }) => {
           <CommonSkeleton lenArray={[...Array(10).keys()]} />
         </Box>
       ) : (
-        <ThemeProvider>
-          <View as="div" width={'calc(100vh - 100px)'} margin={'auto'} >
-            <FaceLivenessDetector
-              sessionId={sessionId}
-              region={"us-east-1"}
-              onAnalysisComplete={() => onComplete(sessionId)}
-              onUserCancel={handleCancel}
-              onError={(error) => {
-                console.error('err', error);
-              }}
-              components={{
-                PhotosensitiveWarning: (): JSX.Element => {
-                  return null;
-                }
-              }}
-            />
-          </View>
-        </ThemeProvider>
+        <FaceLiveNess sessionId={sessionId} onUserCancel={onClose} onComplete={onComplete} onError={() => { }} />
       )}
     </CustomDialogContent>
   </Dialog>
@@ -114,4 +53,4 @@ const FaceLiveNess = ({ onClose, onComplete }) => {
   );
 };
 
-export default FaceLiveNess;
+export default FaceLogin;
