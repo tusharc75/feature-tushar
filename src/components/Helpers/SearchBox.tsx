54288d38b32e0b70093dbc95b298535eed4fd4c6
dhrubobarman <dhrubobarman847@gmail.com>
2024-02-16 17:57:13 +0530

    fix: search box

--- src/components/Helpers/SearchBox.tsx
+++ src/components/Helpers/SearchBox.tsx
@@ -1,17 +1,17 @@
 import React, { memo, useState } from 'react';
-import { TextField, InputAdornment, TextFieldProps } from '@material-ui/core';
 // import { Search } from '@material-ui/icons';
 // import PropTypes from 'prop-types';
 // import { isMobile, isTablet } from 'react-device-detect';
-import { FiSearch } from 'react-icons/fi';
 import { debounce } from 'lodash';
+import { FiSearch } from 'react-icons/fi';
 
-type SerachBoxProps = Omit<TextFieldProps, 'variant' | 'type'> & {
+type SerachBoxProps = React.InputHTMLAttributes<HTMLInputElement> & {
   width?: string;
+  value: string;
 };
 
-function SearchBox({ onChange, value, size, width, placeholder, style, className, InputProps, ...otherProps }: SerachBoxProps) {
-  const [inputvalue, setInputValue] = useState(value);
+function SearchBox({ onChange, value, size, width, placeholder, className, ...otherProps }: SerachBoxProps) {
+  const [inputvalue, setInputValue] = useState<string>(value ?? '');
 
   const debouncedInputDispatch = debounce((e) => {
     onChange(e);
@@ -23,25 +23,20 @@ function SearchBox({ onChange, value, size, width, placeholder, style, className
   };
 
   return (
-    <TextField
-      {...otherProps}
-      style={{ ...style, display: 'flex' }}
-      variant="outlined"
-      placeholder={placeholder || 'Search'}
-      type="search"
-      size={size || 'small'}
-      value={inputvalue}
-      className={`${className} sm:max-w-[300px] sm:min-w-[200px] flex-grow small-searchbar`}
-      onChange={onChangeWrapper}
-      InputProps={{
-        ...InputProps,
-        startAdornment: (
-          <InputAdornment position="start">
-            <FiSearch style={{ color: '#737373' }} />
-          </InputAdornment>
-        )
-      }}
-    />
+    <>
+      <div className="relative sm:max-w-[300px] sm:min-w-[200px] flex-grow">
+        <FiSearch style={{ color: '#737373' }} className="absolute top-1/2 [transform:translateY(-50%)] left-[10px]" />
+        <input
+          title={'search'}
+          value={inputvalue}
+          onChange={onChangeWrapper}
+          placeholder={placeholder || 'Search..'}
+          type={'search'}
+          className={`${className} sm:max-w-[300px] sm:min-w-[200px] w-full flex-grow small-searchbar bg-transparent dark:bg-[var(--dark-secondary)] shadow-none [border:1px_solid_var(--common-border-color)] focus:[outline:1px_solid_var(--new-theme-color)] focus-within:[outline:1px_solid_var(--new-theme-color)] dark:text-white outline-transparent rounded-[4px] placeholder:text-[#737373] h-[32px] p-[10px_5px_10px_32px] `}
+          {...otherProps}
+        />
+      </div>
+    </>
   );
 }
 
