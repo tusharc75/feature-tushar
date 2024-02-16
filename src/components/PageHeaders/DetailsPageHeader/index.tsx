28ee93b5fcc7d7f389190cccd9e5b094985e1364
dhrubobarman <dhrubobarman847@gmail.com>
2024-02-16 10:35:05 +0530

    fix: field ticket add step

--- src/components/PageHeaders/DetailsPageHeader/index.tsx
+++ src/components/PageHeaders/DetailsPageHeader/index.tsx
@@ -48,7 +48,7 @@ const DetailsPageHeader = ({
   };
 
   return (
-    <div className={`flex flex-wrap justify-between items-center gap-2 py-2 ${hasXpadding ? 'px-2' : ''}`}>
+    <div className={`flex details-page-header flex-wrap justify-between items-center gap-2 py-2 ${hasXpadding ? 'px-2' : ''}`}>
       <div className="flex flex-wrap gap-2 items-center">
         {isAddButtonVisible ? (
           <>
