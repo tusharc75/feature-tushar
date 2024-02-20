4938253c1da1fd18ed7159e8c2344a40dc1cd6ca
dhrubobarman <dhrubobarman847@gmail.com>
2024-02-20 14:49:00 +0530

    fix: serialized asset asset history

--- src/components/DurationFilter/index.tsx
+++ src/components/DurationFilter/index.tsx
@@ -6,117 +6,110 @@ import moment from 'moment';
 import DateFnsUtils from '@date-io/date-fns';
 
 const DurationFilter = ({ label, duration, setDuration, defaultTimeFrame }) => {
+  const [timeFrame, setTimeFrame] = React.useState<any>(defaultTimeFrame);
 
-    const [timeFrame, setTimeFrame] = React.useState<any>(defaultTimeFrame);
+  React.useEffect(() => {
+    switch (timeFrame) {
+      case '1-month':
+        setDuration({
+          from: new Date(moment().subtract('1', 'month').calendar()),
+          to: new Date()
+        });
+        break;
 
-    React.useEffect(() => {
-        switch (timeFrame) {
-            case '1-month':
-                setDuration({
-                    from: new Date(moment().subtract('1', 'month').calendar()),
-                    to: new Date()
-                });
-                break;
+      case '3-months':
+        setDuration({
+          from: new Date(moment().subtract('3', 'months').calendar()),
+          to: new Date()
+        });
+        break;
 
-            case '3-months':
-                setDuration({
-                    from: new Date(moment().subtract('3', 'months').calendar()),
-                    to: new Date()
-                });
-                break;
+      case '6-months':
+        setDuration({
+          from: new Date(moment().subtract('6', 'months').calendar()),
+          to: new Date()
+        });
+        break;
 
-            case '6-months':
-                setDuration({
-                    from: new Date(moment().subtract('6', 'months').calendar()),
-                    to: new Date()
-                });
-                break;
+      case '1-year':
+        setDuration({
+          from: new Date(moment().subtract('1', 'year').calendar()),
+          to: new Date()
+        });
+        break;
+      case 'current-year':
+        setDuration({
+          from: new Date(moment().startOf('year').calendar()),
+          to: new Date(moment().endOf('year').calendar())
+        });
+        break;
 
-            case '1-year':
-                setDuration({
-                    from: new Date(moment().subtract('1', 'year').calendar()),
-                    to: new Date()
-                });
-                break;
-            case 'current-year':
-                setDuration({
-                    from: new Date(moment().startOf('year').calendar()),
-                    to: new Date(moment().endOf('year').calendar()),
-                });
-                break;
+      default:
+        break;
+    }
+  }, [timeFrame]);
 
-            default:
-                break;
-        }
-    }, [timeFrame]);
-
-    return (
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Grid container spacing={2}>
-                <Grid item xs={12} sm={4}>
-                    <FormControl fullWidth size="small" variant="outlined">
-                        <InputLabel id="duration">Select Duration</InputLabel>
-                        <Select
-                            labelId="duration"
-                            id="time-duration"
-                            value={timeFrame}
-                            onChange={(e) => setTimeFrame(e.target.value)}
-                            label="Select Duration"
-                        >
-                            <MenuItem value={'1-year'}>Last 1 Year</MenuItem>
-                            <MenuItem value={'6-months'}>Last 6 Months</MenuItem>
-                            <MenuItem value={'3-months'}>Last 3 Months</MenuItem>
-                            <MenuItem value={'1-month'}>Last 1 Month</MenuItem>
-                            <MenuItem value={'current-year'}>Current Year</MenuItem>
-                            <MenuItem value={'custom'}>Custom</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={6} sm={4}>
-                    <KeyboardDatePicker
-                        disabled={timeFrame !== 'custom'}
-                        inputVariant="outlined"
-                        variant="inline"
-                        fullWidth
-                        size="small"
-                        format={dateFormatForInputControl}
-                        maxDate={duration.to}
-                        label={`From ${label}`}
-                        autoOk
-                        InputLabelProps={{
-                            shrink: true
-                        }}
-                        views={['year', 'month', 'date']}
-                        value={duration.from}
-                        onChange={(date) => {
-                            setDuration({ ...duration, from: date });
-                        }}
-                    />
-                </Grid>
-                <Grid item xs={6} sm={4}>
-                    <KeyboardDatePicker
-                        disabled={timeFrame !== 'custom'}
-                        inputVariant="outlined"
-                        variant="inline"
-                        fullWidth
-                        size="small"
-                        autoOk
-                        InputLabelProps={{
-                            shrink: true
-                        }}
-                        minDate={duration.from}
-                        format={dateFormatForInputControl}
-                        label={`To ${label}`}
-                        views={['year', 'month', 'date']}
-                        value={duration.to}
-                        onChange={(date) => {
-                            setDuration({ ...duration, to: date });
-                        }}
-                    />
-                </Grid>
-            </Grid>
-        </MuiPickersUtilsProvider>
-    );
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={12} md={4}>
+          <FormControl fullWidth size="small" variant="outlined">
+            <InputLabel id="duration">Select Duration</InputLabel>
+            <Select labelId="duration" id="time-duration" value={timeFrame} onChange={(e) => setTimeFrame(e.target.value)} label="Select Duration">
+              <MenuItem value={'1-year'}>Last 1 Year</MenuItem>
+              <MenuItem value={'6-months'}>Last 6 Months</MenuItem>
+              <MenuItem value={'3-months'}>Last 3 Months</MenuItem>
+              <MenuItem value={'1-month'}>Last 1 Month</MenuItem>
+              <MenuItem value={'current-year'}>Current Year</MenuItem>
+              <MenuItem value={'custom'}>Custom</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <KeyboardDatePicker
+            disabled={timeFrame !== 'custom'}
+            inputVariant="outlined"
+            variant="inline"
+            fullWidth
+            size="small"
+            format={dateFormatForInputControl}
+            maxDate={duration.to}
+            label={`From ${label}`}
+            autoOk
+            InputLabelProps={{
+              shrink: true
+            }}
+            views={['year', 'month', 'date']}
+            value={duration.from}
+            onChange={(date) => {
+              setDuration({ ...duration, from: date });
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <KeyboardDatePicker
+            disabled={timeFrame !== 'custom'}
+            inputVariant="outlined"
+            variant="inline"
+            fullWidth
+            size="small"
+            autoOk
+            InputLabelProps={{
+              shrink: true
+            }}
+            minDate={duration.from}
+            format={dateFormatForInputControl}
+            label={`To ${label}`}
+            views={['year', 'month', 'date']}
+            value={duration.to}
+            onChange={(date) => {
+              setDuration({ ...duration, to: date });
+            }}
+          />
+        </Grid>
+      </Grid>
+    </MuiPickersUtilsProvider>
+  );
 };
 
 export default DurationFilter;
--- src/pages/SerializedAsset/AssetHistory/index.tsx
+++ src/pages/SerializedAsset/AssetHistory/index.tsx
@@ -14,7 +14,6 @@ import CustomReactTable, { useTableReducer } from 'src/components/CustomReactTab
 import { camelCase } from 'lodash';
 
 const AssetHistory = ({ id }) => {
-
   const renderedFrom = `${camelCase(routes?.serializedAsset.title)}_assetHistory`;
 
   const toastConfig = useContext(CustomToastContext);
@@ -40,9 +39,9 @@ const AssetHistory = ({ id }) => {
         <div>
           {row.original.reference ? (
             row.original.type === 'Loading Ticket' ||
-              row.original.type === 'Receiving Ticket' ||
-              row.original.type === 'Return Ticket' ||
-              row.original.type === 'Delivery Ticket' ? (
+            row.original.type === 'Receiving Ticket' ||
+            row.original.type === 'Return Ticket' ||
+            row.original.type === 'Delivery Ticket' ? (
               <Link
                 className="link"
                 title={row.original.reference}
@@ -184,22 +183,14 @@ const AssetHistory = ({ id }) => {
     {
       accessor: 'type',
       Header: 'Type',
-      Cell: ({ row }) => (
-        row.original?.type ? (
-          <div>
-            {row.original?.type}
-          </div>
-        ) : (
-          <NoDataCell />
-        )
-      )
+      Cell: ({ row }) => (row.original?.type ? <div>{row.original?.type}</div> : <NoDataCell />)
     },
     {
       accessor: 'date',
       Header: 'Date & Time',
       disableFilters: true,
       disableSortBy: false,
-      Cell: ({ row }) => (
+      Cell: ({ row }) =>
         row.original?.date ? (
           <div className="createBy" title={`${moment(row.original?.date)?.format(dateTimeFormat)}`}>
             {moment(row.original?.date)?.format(dateTimeFormat)}
@@ -207,48 +198,23 @@ const AssetHistory = ({ id }) => {
         ) : (
           <NoDataCell />
         )
-      )
     },
     {
       accessor: 'days',
       Header: 'Days',
       disableFilters: true,
       disableSortBy: false,
-      Cell: ({ row }) => (
-        <div>
-          {row.original?.days ? (
-            <span>{row.original?.days}</span>
-          ) : (
-            <span>Less than a day</span>
-          )}
-        </div>
-      )
+      Cell: ({ row }) => <div>{row.original?.days ? <span>{row.original?.days}</span> : <span>Less than a day</span>}</div>
     },
     {
       accessor: 'status',
       Header: 'Status',
-      Cell: ({ row }) => (
-        row.original?.status ? (
-          <div>
-            {row.original?.status}
-          </div>
-        ) : (
-          <NoDataCell />
-        )
-      )
+      Cell: ({ row }) => (row.original?.status ? <div>{row.original?.status}</div> : <NoDataCell />)
     },
     {
       accessor: 'comments',
       Header: 'Comment',
-      Cell: ({ row }) => (
-        row.original?.comments ? (
-          <div>
-            {row.original?.comments}
-          </div>
-        ) : (
-          <NoDataCell />
-        )
-      )
+      Cell: ({ row }) => (row.original?.comments ? <div>{row.original?.comments}</div> : <NoDataCell />)
     },
     {
       accessor: 'warehouse',
@@ -256,7 +222,7 @@ const AssetHistory = ({ id }) => {
       Cell: ({ row }) => (
         <div>
           {row.original?.warehouse ? (
-            permissions?.warehouse?.isRead ?
+            permissions?.warehouse?.isRead ? (
               <Link
                 className="link"
                 title={row.original?.warehouse}
@@ -265,8 +231,10 @@ const AssetHistory = ({ id }) => {
                 rel="noopener noreferrer"
               >
                 {row.original?.warehouse}
-              </Link> :
+              </Link>
+            ) : (
               <span>{row.original?.warehouse}</span>
+            )
           ) : (
             <NoDataCell />
           )}
@@ -276,41 +244,17 @@ const AssetHistory = ({ id }) => {
     {
       accessor: 'location',
       Header: 'Location',
-      Cell: ({ row }) => (
-        row.original?.location ? (
-          <div>
-            {row.original?.location}
-          </div>
-        ) : (
-          <NoDataCell />
-        )
-      )
+      Cell: ({ row }) => (row.original?.location ? <div>{row.original?.location}</div> : <NoDataCell />)
     },
     {
       accessor: 'ownerType',
       Header: 'Owner Type',
-      Cell: ({ row }) => (
-        row.original?.ownerType ? (
-          <div>
-            {row.original?.ownerType}
-          </div>
-        ) : (
-          <NoDataCell />
-        )
-      )
+      Cell: ({ row }) => (row.original?.ownerType ? <div>{row.original?.ownerType}</div> : <NoDataCell />)
     },
     {
       accessor: 'owner',
       Header: 'Owner',
-      Cell: ({ row }) => (
-        row.original?.owner ? (
-          <div>
-            {row.original?.owner}
-          </div>
-        ) : (
-          <NoDataCell />
-        )
-      )
+      Cell: ({ row }) => (row.original?.owner ? <div>{row.original?.owner}</div> : <NoDataCell />)
     },
     {
       accessor: 'transactionDate',
@@ -328,7 +272,7 @@ const AssetHistory = ({ id }) => {
           )}
         </>
       )
-    },
+    }
   ];
 
   useEffect(() => {
@@ -393,7 +337,7 @@ const AssetHistory = ({ id }) => {
 
   return (
     <Box>
-      <Box width="60%">
+      <Box className="max-w-[800px]">
         <DurationFilter label={''} defaultTimeFrame="1-year" duration={duration} setDuration={setDuration} />
       </Box>
       {columns ? (
