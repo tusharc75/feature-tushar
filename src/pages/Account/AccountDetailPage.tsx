8b28b0781b582e53b7f9319a8cba3ad6e644d3e9
dhrubobarman <dhrubobarman847@gmail.com>
2024-02-20 15:59:10 +0530

    fix: small fix

--- src/pages/Account/AccountDetailPage.tsx
+++ src/pages/Account/AccountDetailPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Box, Button, Grid, Typography, IconButton, Card, CardContent, List, ListItemIcon, Tooltip } from '@material-ui/core';
-import { isMobile } from 'react-device-detect';
+import { Box, Button, Grid, Typography, IconButton, Card, CardContent, List, ListItemIcon, Tooltip, useMediaQuery } from '@material-ui/core';
 import { useHistory, useParams } from 'react-router-dom';
 import { reverse as _reverse } from 'lodash';
 import { Skeleton } from '@material-ui/lab';
@@ -83,6 +82,8 @@ function DisplayData({ label, value, icon, highlightsHead = false }) {
 }
 
 export default function AccountDetailPage(props) {
+  const isMobile = useMediaQuery('(max-width:600px)');
+
   const toastConfig = useContext(CustomToastContext);
   const history = useHistory();
   const parsed = queryString.parse(history.location.search);
@@ -322,9 +323,9 @@ export default function AccountDetailPage(props) {
           current: true,
           parentAccount: data.parentAccount
             ? {
-              _id: data.parentAccount.optionValue,
-              accountName: data.parentAccount.optionLabel
-            }
+                _id: data.parentAccount.optionValue,
+                accountName: data.parentAccount.optionLabel
+              }
             : null,
           canEdit: [...(data?.collaborator ?? []), data?.owner].some((obj) => obj.optionValue === user.user._id)
         }
@@ -730,8 +731,8 @@ export default function AccountDetailPage(props) {
                         ? accountClass.mobile_button_layout_secondary
                         : ''
                       : isMobile
-                        ? accountClass.mobile_button_layout
-                        : ''
+                      ? accountClass.mobile_button_layout
+                      : ''
                   }
                   onClick={() => {
                     setShowApproveDisapproveConfirmBox(true);
@@ -759,18 +760,14 @@ export default function AccountDetailPage(props) {
               </>
             )}
             {permissions &&
-              permissions[accountResource] &&
-              permissions[accountResource].isDelete &&
-              accountData?.owner?.optionValue &&
-              user?.user?._id &&
-              accountData.owner.optionValue === user.user._id ? (
+            permissions[accountResource] &&
+            permissions[accountResource].isDelete &&
+            accountData?.owner?.optionValue &&
+            user?.user?._id &&
+            accountData.owner.optionValue === user.user._id ? (
               <DeleteButton text="Delete" onClick={() => setShowConfirmBox(true)} />
             ) : null}
-            <ActivityButton
-              referenceId={accountData?._id}
-              resource={accountResource}
-              resourceLabel={accountData?.accountName}
-            />
+            <ActivityButton referenceId={accountData?._id} resource={accountResource} resourceLabel={accountData?.accountName} />
           </Box>
         </Box>
       </Box>
@@ -807,9 +804,9 @@ export default function AccountDetailPage(props) {
               <Tab label={<div className="tab-font">Details</div>} aria-controls="a11y-tabpanel-0" id="a11y-tab-0" className="tabLayout" />
               <Tab label={<div className="tab-font">Account Hierarchy</div>} id="a11y-tab-1" className="tabLayout" />
               <Tab label={<div className="tab-font">OM-Neurons</div>} aria-controls="a11y-tabpanel-2" id="a11y-tab-2" className="tabLayout" />
-              {(accountResource === 'supplierAccount' && user?.user?.brandPolicy?.serializedAssetCertification) &&
-                <Tab label={<div className="tab-font">Supplier View</div>}
-                  aria-controls="a11y-tabpanel-3" id="a11y-tab-3" className="tabLayout" />}
+              {accountResource === 'supplierAccount' && user?.user?.brandPolicy?.serializedAssetCertification && (
+                <Tab label={<div className="tab-font">Supplier View</div>} aria-controls="a11y-tabpanel-3" id="a11y-tab-3" className="tabLayout" />
+              )}
               {accountResource === 'customerAccount' && permissions?.productInventory && (
                 <Tab
                   label={<div className="tab-font">{routes.warehouse.title}</div>}
@@ -1025,7 +1022,8 @@ export default function AccountDetailPage(props) {
                   api={accountApi}
                   id={id}
                   allowedToEdit={permissions[accountResource].isUpdate}
-                  permission={permissions[accountResource]} />
+                  permission={permissions[accountResource]}
+                />
               </TabPanel>
             )}
             {accountResource === 'customerAccount' && permissions?.productInventory && tabValue === 3 && (
@@ -1039,8 +1037,9 @@ export default function AccountDetailPage(props) {
       {showConfirmBox ? (
         <ConfirmationDialog
           open={showConfirmBox}
-          message={`Are you sure you want to delete this Account ${deleteAccount?.accountName ? deleteAccount?.accountName : accountData.accountName || ''
-            }`}
+          message={`Are you sure you want to delete this Account ${
+            deleteAccount?.accountName ? deleteAccount?.accountName : accountData.accountName || ''
+          }`}
           onClose={() => {
             setShowConfirmBox(false);
             setDeleteAccountId({});
@@ -1097,7 +1096,7 @@ export default function AccountDetailPage(props) {
           handleSubmit={onUpdateAccount}
           accountId={editAccountData._id ? editAccountData._id : accountData?._id}
           formValues={formValues}
-          handleAddressDataSource={() => { }}
+          handleAddressDataSource={() => {}}
         />
       ) : null}
 
