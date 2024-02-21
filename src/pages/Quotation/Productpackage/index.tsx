0862360d8c6ecdd2e9ea1140745b439684024f02
dhruv@dk <dhruv21.dk@gmail.com>
2024-02-21 18:09:04 +0530

    fix: fix

--- src/pages/Quotation/Productpackage/index.tsx
+++ src/pages/Quotation/Productpackage/index.tsx
@@ -110,7 +110,8 @@ const Productpackage = ({ quotationData, fetchQuotationData, setNextStep, render
         Header: 'Type',
         sticky: isMobile || isTablet ? 'none' : 'left',
         width: 100,
-        Cell: ({ row }) => <p className="text-truncate">
+        Cell: ({ row }) => 
+        <p className="text-truncate">
         {row.original.type === 'serializedAsset' ? 'Asset' : `${capitalize(row.original.type)} `}
         {row.original['type'] === 'product'
           ? row.original?.productDetail?.serializedProduct
