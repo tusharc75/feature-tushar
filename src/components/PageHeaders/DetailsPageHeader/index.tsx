837f7d8116dae396749932899cad9ce3d63896c9
dhrubobarman <dhrubobarman847@gmail.com>
2024-02-21 12:02:12 +0530

    fix: small fix

--- src/components/PageHeaders/DetailsPageHeader/index.tsx
+++ src/components/PageHeaders/DetailsPageHeader/index.tsx
@@ -122,7 +122,7 @@ const DetailsPageHeader = ({
                   size="small"
                   onClick={ActionClick}
                   aria-controls="action-menu"
-                  className="new-dropdown-v1 min-h-[30px] max-[600px]:[border:0px_!important] max-[600px]:[max-width:36px_!important]"
+                  className="new-dropdown-v1 min-h-[30px] max-[600px]:[border:0px_!important] max-[600px]:[max-width:36px_!important] max-[600px]:min-h-[32px]"
                   {...restOfActionButtonProps}
                 >
                   <FaCircleChevronDown size={20} className="max-[600px]:not-sr-only sr-only" />
