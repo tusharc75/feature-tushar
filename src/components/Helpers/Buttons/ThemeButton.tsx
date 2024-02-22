24242cd6253d4f3026d5000959626c0e0c6c678f
dhrubobarman <dhrubobarman847@gmail.com>
2024-02-22 13:36:39 +0530

    fix: button fix

--- src/components/Helpers/Buttons/ThemeButton.tsx
+++ src/components/Helpers/Buttons/ThemeButton.tsx
@@ -1,25 +1,24 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useMemo } from 'react';
 import { Button, ButtonProps, useMediaQuery } from '@material-ui/core';
 import { FaMobileButton } from 'react-icons/fa6';
 import HtmlTooltip from 'src/components/CustomTooltipTitle';
 
 type SimpleButton = {
   borderColor?: 'none';
-  iconForMobile: ReactNode;
-  variant: 'contained';
+  iconForMobile: ReactNode | boolean;
   tooltip?: string;
 } & Omit<ButtonProps, 'variant'>;
 
 type OutlinedButtonProps = {
   borderColor?: 'default';
-  iconForMobile: ReactNode;
+  iconForMobile: ReactNode | boolean;
   hasMobileBorder?: boolean;
   tooltip?: string;
 } & ButtonProps;
 
 type RedOutlineProps = {
   borderColor?: 'red';
-  iconForMobile: ReactNode;
+  iconForMobile: ReactNode | boolean;
   hasMobileBorder?: boolean;
   mode?: 'dark' | 'light';
   tooltip?: string;
@@ -27,37 +26,43 @@ type RedOutlineProps = {
 
 type ButtonType = OutlinedButtonProps | RedOutlineProps | SimpleButton;
 
-const ThemeButton = ({
-  borderColor = 'default',
-  iconForMobile = <FaMobileButton size={18} />,
-  variant,
-  children,
-  tooltip = '',
-  ...rest
-}: ButtonType) => {
+const ThemeButton = ({ borderColor = 'default', iconForMobile = <FaMobileButton size={18} />, children, tooltip = '', ...rest }: ButtonType) => {
   const isMobile = useMediaQuery('(max-width:600px)');
 
-  let className = `max-[600px]:[max-width:36px_!important] max-[600px]:[height:32px_!important] `;
+  const getButtonProps = useMemo(() => {
+    const buttonProps: Pick<ButtonProps, 'className' | 'variant'> = {
+      className: `${iconForMobile ? 'max-[600px]:[max-width:36px_!important] max-[600px]:[height:32px_!important]' : ''} no-shadow`,
+      variant: 'contained'
+    };
 
-  switch (borderColor) {
-    case 'default': {
-      const { hasMobileBorder = true } = rest as RedOutlineProps;
-      className += ` btn-outline-v1 ${hasMobileBorder ? 'with-border' : ''}`;
-      break;
+    switch (borderColor) {
+      case 'default': {
+        const { hasMobileBorder = true } = rest as OutlinedButtonProps;
+        buttonProps.variant = isMobile && iconForMobile ? 'text' : 'outlined';
+        buttonProps.className += ` btn-outline-v1 ${hasMobileBorder ? 'with-border' : ''}`;
+        break;
+      }
+      case 'red': {
+        const { mode, hasMobileBorder = true } = rest as RedOutlineProps;
+        buttonProps.variant = isMobile && iconForMobile ? 'text' : 'outlined';
+        buttonProps.className += ` btn-outline-red-v1 ${mode} ${hasMobileBorder ? 'with-border' : ''}`;
+        break;
+      }
+      case 'none': {
+        buttonProps.className += ` `;
+        buttonProps.variant = 'contained';
+        break;
+      }
+      default:
+        break;
     }
-    case 'red': {
-      const { mode, hasMobileBorder = true } = rest as RedOutlineProps;
-      className += ` btn-outline-red-v1 ${mode} ${hasMobileBorder ? 'with-border' : ''}`;
-      break;
-    }
-    default:
-      break;
-  }
+    return buttonProps;
+  }, [borderColor, isMobile, rest]);
 
   return (
     <HtmlTooltip title={tooltip} placement="top" arrow enterTouchDelay={0}>
-      <Button className={`${className}`} variant={isMobile ? 'text' : 'outlined'} size="small" {...rest}>
-        {isMobile ? iconForMobile : children}
+      <Button size="small" {...rest} {...getButtonProps}>
+        {isMobile ? (iconForMobile ? iconForMobile : children) : children}
       </Button>
     </HtmlTooltip>
   );
