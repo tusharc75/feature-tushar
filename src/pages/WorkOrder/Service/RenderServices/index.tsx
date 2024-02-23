6ae3431f1e3632d778d4fa8be5486280e871a4a3
dhrubobarman <dhrubobarman847@gmail.com>
2024-02-23 20:11:40 +0530

    fix: pass fail status position

--- src/pages/WorkOrder/Service/RenderServices/index.tsx
+++ src/pages/WorkOrder/Service/RenderServices/index.tsx
@@ -317,26 +317,13 @@ const RenderSingleService = ({
                     </IconButton>
                   </HtmlTooltip>
                 )}
-                {/* PassFail */}
-                {data?.type === 'service' && data?.serviceStatus && (
-                  <RenderStatusIcon
-                    className={`${isMobile ? 'max-w-[16px] h-[16px]' : 'max-w-[24px] h-[24px]'} flex-shrink-0 mt-[3px]`}
-                    stepStatus={data?.serviceStatus}
-                  />
-                )}
-                {data?.type === 'quotation' && quotationData && (
-                  <RenderStatusIcon
-                    className={`${isMobile ? 'max-w-[16px] h-[16px]' : 'max-w-[24px] h-[24px]'} flex-shrink-0 mt-[3px]`}
-                    stepStatus={quotationData?.status}
-                  />
-                )}
               </div>
             )}
           </>
         )}
       </div>
       {/* Chips */}
-      <div className={`flex items-center flex-wrap gap-2 basis-full w-full ml-[20px] ${isColapsed ? 'hidden' : ''}`}>
+      <div className={`flex items-center flex-wrap gap-2 basis-full w-full pl-[20px] ${isColapsed ? 'hidden' : ''}`}>
         {data?.type === 'service' && (
           <div className="ml-1">
             <Chip
@@ -356,6 +343,21 @@ const RenderSingleService = ({
         )}
 
         {user?.brandPolicy?.workOrderTimer && <RenderTotalTime stepTimes={stepTimes} />}
+        {/* PassFail */}
+        <div className="ml-auto max-w-fit">
+          {data?.type === 'service' && data?.serviceStatus && (
+            <RenderStatusIcon
+              className={`${isMobile ? 'max-w-[20px] h-[20px]' : 'max-w-[24px] h-[24px]'} flex-shrink-0 mt-[3px]`}
+              stepStatus={data?.serviceStatus}
+            />
+          )}
+          {data?.type === 'quotation' && quotationData && (
+            <RenderStatusIcon
+              className={`${isMobile ? 'max-w-[20px] h-[20px]' : 'max-w-[24px] h-[24px]'} flex-shrink-0 mt-[3px]`}
+              stepStatus={quotationData?.status}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
