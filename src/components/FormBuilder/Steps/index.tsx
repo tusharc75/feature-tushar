fc31d601d59fa42de140fadf74f2b764e8e7b0d4
ABHIMANYU KUMAR <manupatel18112000@gmail.com>
2024-03-07 19:20:52 +0530

    updated form builder

--- src/components/FormBuilder/Steps/index.tsx
+++ src/components/FormBuilder/Steps/index.tsx
@@ -15,6 +15,7 @@ import HtmlTooltip from 'src/components/CustomTooltipTitle';
 import ConfirmationDialog from '../../../components/Helpers/ConfirmationDialog';
 import ConfigureField from './ConfigureField';
 import ManageSteps from './ManageSteps';
+import _ from 'lodash';
 
 const DND_NAME = 'Box';
 
@@ -22,6 +23,7 @@ const Steps = ({ resource }) => {
   const toastConfig = useContext(CustomToastContext);
 
   const [steps, setSteps] = useState(null);
+  const [initialSteps, setInitialSteps] = useState(null);
   const [open, setOpen] = useState({ open: false, data: null });
   const [openField, setOpenField] = useState({ open: false, step: null });
   const [resourceId, setResourceId] = useState(null);
@@ -33,7 +35,8 @@ const Steps = ({ resource }) => {
       .get(`/sa-formbuilder/steps/${resource}`)
       .then(({ data: { data } }) => {
         setResourceId(data?._id);
-        setSteps(data?.steps);
+        setSteps(_.sortBy(data?.steps, 'order'));
+        setInitialSteps(_.sortBy(data?.steps, 'order'));
       })
       .catch((error) => {
         toastConfig.setToastConfig(error);
@@ -60,6 +63,34 @@ const Steps = ({ resource }) => {
       });
   };
 
+  const handleUpdateOrder = () => {
+    if (!isEqualOrder(initialSteps, steps)) {
+      axiosInstance()
+        .put(
+          `/sa-formbuilder/steps/order/${resourceId}`,
+          steps?.map((step) => ({ stepId: step?._id, order: step?.order }))
+        )
+        .then(() => {
+          fetchData();
+        })
+        .catch((error) => {
+          toastConfig.setToastConfig(error);
+        });
+    }
+  };
+
+  const isEqualOrder = (initialSteps, newSteps) => {
+    let isEqual = true;
+
+    initialSteps?.forEach((step) => {
+      if (newSteps?.find((s) => s?._id === step?._id)?.order !== step?.order) {
+        isEqual = false;
+      }
+    });
+
+    return isEqual;
+  };
+
   return (
     <Box>
       <Box>
@@ -76,7 +107,7 @@ const Steps = ({ resource }) => {
       </Box>
       <Box pt={2}>
         <DndProvider backend={isMobile || isTablet ? TouchBackend : HTML5Backend}>
-          <RenderStepItems {...{ steps, setSteps, setOpen, setOpenField, setDeleteData }} />
+          <RenderStepItems {...{ steps, setSteps, setOpen, setOpenField, setDeleteData, handleUpdateOrder }} />
         </DndProvider>
       </Box>
 
@@ -124,7 +155,7 @@ const Steps = ({ resource }) => {
 
 export default Steps;
 
-const RenderStepItems = ({ steps, setSteps, setOpen, setOpenField, setDeleteData }) => {
+const RenderStepItems = ({ steps, setSteps, setOpen, setOpenField, setDeleteData, handleUpdateOrder }) => {
   const findStep = useCallback(
     (id: string) => {
       const card = steps.filter((c) => `${c._id}` === id)[0] as {
@@ -161,13 +192,15 @@ const RenderStepItems = ({ steps, setSteps, setOpen, setOpenField, setDeleteData
     <div className="grid grid-cols-1 gap-2" ref={drop}>
       {steps &&
         steps?.map((step, i) => {
-          return <SingleStep key={step._id} {...{ step, i, setSteps, setOpen, setOpenField, setDeleteData, moveStep, findStep }} />;
+          return (
+            <SingleStep key={step._id} {...{ step, i, setSteps, setOpen, setOpenField, setDeleteData, moveStep, findStep, handleUpdateOrder }} />
+          );
         })}
     </div>
   );
 };
 
-const SingleStep = ({ step, i, setOpen, setOpenField, setDeleteData, moveStep, findStep }) => {
+const SingleStep = ({ step, i, setOpen, setOpenField, setDeleteData, moveStep, findStep, handleUpdateOrder }) => {
   const [{ opacity }, drag, preview] = useDrag(() => ({
     type: DND_NAME,
     item: { id: step._id, originalIndex: i },
@@ -191,6 +224,11 @@ const SingleStep = ({ step, i, setOpen, setOpenField, setDeleteData, moveStep, f
           const { index: overIndex } = findStep(step._id);
           moveStep(draggedId, overIndex);
         }
+      },
+      drop(item, monitor) {
+        if (!monitor.didDrop()) {
+          handleUpdateOrder();
+        }
       }
     }),
     [findStep, moveStep]
@@ -199,46 +237,48 @@ const SingleStep = ({ step, i, setOpen, setOpenField, setDeleteData, moveStep, f
   return (
     <div ref={preview} style={{ opacity }}>
       <div ref={(node) => drop(node)} className="[border:1px_solid_var(--common-border-color)] p-3 rounded-[5px]">
-        <div className="flex gap-2 justify-between">
-          <h3 className="line-clamp-1 font-semibold">{step?.stepName}</h3>
-          <IconButton size={'small'} className={`[cursor:move_!important]`} ref={drag}>
-            <MdDragIndicator size={20} className="text-[var(--primary-text)]" />
-          </IconButton>
-        </div>
-        <Box display={'flex'} justifyContent={'end'}>
-          <HtmlTooltip title={'Edit'}>
-            <IconButton
-              size="small"
-              aria-label="Edit"
-              onClick={() => {
-                setOpen({ open: true, data: step });
-              }}
-            >
-              <EditIcon fontSize="small" color={'primary'} />
+        <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+          <Box display={'flex'} alignItems={'center'}>
+            <IconButton size={'small'} className={`[cursor:move_!important]`} ref={drag}>
+              <MdDragIndicator size={20} className="text-[var(--primary-text)]" />
             </IconButton>
-          </HtmlTooltip>
-          <HtmlTooltip title={'Add Fields'}>
-            <IconButton
-              size="small"
-              aria-label="Edit"
-              onClick={() => {
-                setOpenField({ open: true, step: step });
-              }}
-            >
-              <BuildIcon fontSize="small" color={'primary'} />
-            </IconButton>
-          </HtmlTooltip>
-          <HtmlTooltip title={'Delete'}>
-            <IconButton
-              size="small"
-              aria-label="Delete"
-              onClick={() => {
-                setDeleteData(step);
-              }}
-            >
-              <DeleteIcon fontSize="small" color={'error'} />
-            </IconButton>
-          </HtmlTooltip>
+            <h3 className="line-clamp-1 font-semibold">{step?.stepName}</h3>
+          </Box>
+          <Box>
+            <HtmlTooltip title={'Edit'}>
+              <IconButton
+                size="small"
+                aria-label="Edit"
+                onClick={() => {
+                  setOpen({ open: true, data: step });
+                }}
+              >
+                <EditIcon fontSize="small" color={'primary'} />
+              </IconButton>
+            </HtmlTooltip>
+            <HtmlTooltip title={'Add Fields'}>
+              <IconButton
+                size="small"
+                aria-label="Edit"
+                onClick={() => {
+                  setOpenField({ open: true, step: step });
+                }}
+              >
+                <BuildIcon fontSize="small" color={'primary'} />
+              </IconButton>
+            </HtmlTooltip>
+            <HtmlTooltip title={'Delete'}>
+              <IconButton
+                size="small"
+                aria-label="Delete"
+                onClick={() => {
+                  setDeleteData(step);
+                }}
+              >
+                <DeleteIcon fontSize="small" color={'error'} />
+              </IconButton>
+            </HtmlTooltip>
+          </Box>
         </Box>
       </div>
     </div>
