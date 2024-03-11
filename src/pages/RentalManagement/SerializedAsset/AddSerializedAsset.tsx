edd07909089b673fa46d8b6e81fee97d4fac9446
vivekSharma3008 <viveksh.701700@gmail.com>
2024-03-11 14:29:02 +0530

    fix: fix

--- src/pages/RentalManagement/SerializedAsset/AddSerializedAsset.tsx
+++ src/pages/RentalManagement/SerializedAsset/AddSerializedAsset.tsx
@@ -247,7 +247,7 @@ const AddSerializedAsset = ({
         const dateFilter = { from: referenceData?.fromDate, to: referenceData?.toDate };
         deepFilter = `${deepFilter}&rental=true&rentalJobId=${referenceData?._id}&date=${JSON.stringify(dateFilter)}`;
       } else {
-        deepFilter = `${deepFilter}&allAsset=true`;
+        deepFilter = `${deepFilter}&availableAsset=true`;
       }
     }
 
