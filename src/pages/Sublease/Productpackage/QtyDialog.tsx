014963b82453e6b6bf045eae426ab9fa2845e04f
Romit Sadaria <romit.sadaria@gmail.com>
2024-03-15 15:42:19 +0530

    fix: bug fix

--- src/pages/Sublease/Productpackage/QtyDialog.tsx
+++ src/pages/Sublease/Productpackage/QtyDialog.tsx
@@ -94,6 +94,9 @@ const QtyDialog: FC<EditDialogProps> = ({
         if (element.fieldName === 'pricingMethod') {
           element.option = pricingMethodOptions;
         }
+        if (element.fieldName === 'pricingCondition') {
+          element.option = [];
+        }
         element.required = false;
         element.isFormula = false;
         element.isMulitFormula = false;
