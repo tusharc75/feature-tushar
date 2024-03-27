6807fc8750642c75e3377b0f37888503293c2fe1
dhrubobarman <dhrubobarman847@gmail.com>
2024-03-27 11:17:22 +0530

    fix: dynamic form

--- src/pages/DynamicForm/Step/index.tsx
+++ src/pages/DynamicForm/Step/index.tsx
@@ -9,6 +9,7 @@ import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import View from './View';
 import { CURReplaceByCurrencySingle } from 'src/constants/formulaUtility';
 import { STEPS_STYLE } from 'src/constants/helpers';
+import { KeyboardArrowLeft } from '@material-ui/icons';
 
 const Step = ({ resourceData, resourceId, resource, data, allowedToEdit }) => {
   const [steps, setSteps] = useState(null);
@@ -17,6 +18,7 @@ const Step = ({ resourceData, resourceId, resource, data, allowedToEdit }) => {
   const [expended, setExpended] = useState({});
   const [nextStep, setNextStep] = useState(false);
   const [index, setIndex] = useState({});
+  const [isExpanded, setIsExpanded] = useState(true);
 
   useEffect(() => {
     resourceData?.steps?.forEach((step) => {
@@ -24,10 +26,10 @@ const Step = ({ resourceData, resourceId, resource, data, allowedToEdit }) => {
     });
     setSteps(_.sortBy(resourceData?.steps, 'order'));
   }, [resourceData]);
-  
+
   useEffect(() => {
-    if(steps?.length && resourceData?.stepsStyle === STEPS_STYLE.sideBar)setIndex(steps[0]);
-  },[steps]);
+    if (steps?.length && resourceData?.stepsStyle === STEPS_STYLE.sideBar) setIndex(steps[0]);
+  }, [steps]);
 
   const handleClick = (step) => {
     setIndex(step);
@@ -35,7 +37,8 @@ const Step = ({ resourceData, resourceId, resource, data, allowedToEdit }) => {
 
   return (
     <>
-      {steps && steps?.length &&
+      {steps &&
+        steps?.length &&
         (resourceData?.stepsStyle === STEPS_STYLE.step ? (
           <>
             <Steps
@@ -59,23 +62,40 @@ const Step = ({ resourceData, resourceId, resource, data, allowedToEdit }) => {
               />
             </ContentFullScreen>
           </>
-        ) : (resourceData?.stepsStyle === STEPS_STYLE.sideBar ? (
-        <Grid>
-          <Grid style={{ display: 'flex'}}>
-            <Grid style={{ marginRight: '20px',border: '1px solid black', borderRadius: '10px', padding:'12px'}}
-              className={`overflow-x-hidden overflow-y-auto max-h-[calc(100vh-300px)]` }>      
-              {steps?.map((step, i) => {
-                return (
-                  <Box mt={2} key={i} onClick={() => handleClick(step)}
-                    style={{border: step === index ? '1px solid blue' : '1px solid black',borderRadius: '2px',cursor: 'pointer'}}>
-                    <Box padding="5px">
-                      <Typography variant="subtitle2">{step?.stepName}</Typography>
-                    </Box>
-                  </Box>
-                );
-              })}
-            </Grid>
-            <Grid className={`overflow-x-hidden overflow-y-auto max-h-[calc(100vh-300px)]`}>
+        ) : resourceData?.stepsStyle === STEPS_STYLE.sideBar ? (
+          <>
+            <div className={`grid ${isExpanded ? 'grid-cols-[300px_1fr]' : 'grid-cols-[100px_1fr]'} gap-3 transition-[all] duration-200`}>
+              <div className={`overflow-x-hidden overflow-y-auto max-h-[calc(100vh-300px)] container-with-border p-[20px]`}>
+                <div className={`${isExpanded ? 'ml-auto' : 'mx-auto'} mb-2 max-w-fit`}>
+                  <IconButton size="small" onClick={() => setIsExpanded((prev) => !prev)}>
+                    <KeyboardArrowLeft
+                      className="transition-all duration-200 data-[expanded=false]:[transform:rotate(180deg)]"
+                      data-expanded={isExpanded}
+                    />
+                  </IconButton>
+                </div>
+                {steps?.map((step, i) => {
+                  return (
+                    <div
+                      key={i}
+                      title={step?.stepName}
+                      onClick={() => handleClick(step)}
+                      data-active={index === step}
+                      className={`p-[18px] [border:1px_solid_var(--common-border-color)] first:rounded-t-md last:rounded-b-md cursor-pointer data-[active=true]:[border:1px_solid_var(--dark-active-border-color,#298B88)]`}
+                    >
+                      <div className="flex gap-2">
+                        <span className="bg-[var(--dark-secondary,var(--primary))] text-white w-[20px] h-[20px] text-center rounded-full text-[10px] leading-[20px] flex-shrink-0">
+                          {i + 1}
+                        </span>
+                        <Typography variant="subtitle2" className={`${isExpanded ? '' : 'sr-only'} transition-all duration-200 line-clamp-1`}>
+                          {step?.stepName}
+                        </Typography>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className={`overflow-x-hidden overflow-y-auto container-with-border max-h-[calc(100vh-300px)] p-[20px]`}>
                 <View
                   step={index}
                   allowedToEdit={allowedToEdit}
@@ -85,10 +105,10 @@ const Step = ({ resourceData, resourceId, resource, data, allowedToEdit }) => {
                   setNextStep={setNextStep}
                   stepFullScreen={stepFullScreen}
                 />
-            </Grid>
-          </Grid>
-        </Grid>)
-        :
+              </div>
+            </div>
+          </>
+        ) : (
           <>
             {steps?.map((step, i) => {
               return (
