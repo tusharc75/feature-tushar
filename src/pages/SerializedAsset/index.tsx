9fdc593ada8c83af5f283a058b04a6b516fb5162
dhrubobarman <dhrubobarman847@gmail.com>
2024-04-04 16:47:30 +0530

    fix: remove console

--- src/pages/SerializedAsset/index.tsx
+++ src/pages/SerializedAsset/index.tsx
@@ -320,7 +320,6 @@ const SerializedAsset = () => {
     let deepFilter = !isExport ? `?page=${page}&limit=${limit}` : '?';
 
     const { filterByIds, deepFilters } = gridFilterParser(filters);
-    console.log({ filterByIds, deepFilters });
 
     if (warehouse?.optionValue) {
       filterByIds.push({ field: 'warehouse', term: warehouse?.optionValue });
