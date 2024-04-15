6d7fd8e609b7c72e822ea09971e30131af93bb57
dhrubobarman <dhrubobarman847@gmail.com>
2024-04-15 11:42:36 +0530

    fix: accesibility and keyboard navigation

--- src/components/CustomReactTable/SwipableListForMobile/RenderSubCard.tsx
+++ src/components/CustomReactTable/SwipableListForMobile/RenderSubCard.tsx
@@ -26,7 +26,8 @@ const RenderSubCard = ({
   submitInput,
   cellValue,
   setCellValue,
-  state
+  state,
+  onRowClick
 }: any) => {
   if (row.depth !== depth) return null;
   let expanderCell = null;
@@ -37,11 +38,31 @@ const RenderSubCard = ({
     <div
       className={`shadow-[0px_3px_26px_0px_rgba(0,0,0,0.06)] rounded-md my-2 px-3 py-2 [--left-gutter:20px] dark:bg-[var(--dark-secondary)] ${
         backgroundColorClass && backgroundColorClass(row.original) + ' td-color'
-      }`}
+      } ${typeof onRowClick === 'function' ? 'focus:[box-shadow:inset_0px_0px_0px_1px_var(--primary-text)] focus:outline-0' : ''}`}
       key={row.original._id}
       style={{
         border: '1px solid var(--common-border-color)',
         cursor: otherFieldsLength > DEFAULT_DATA_ROWS_VISIBLE ? 'pointer' : 'auto'
+      }}
+      onClick={() => (typeof onRowClick === 'function' ? onRowClick(row.original) : null)}
+      tabIndex={typeof onRowClick === 'function' ? 0 : -1}
+      role={typeof onRowClick === 'function' ? 'button' : 'none'}
+      onKeyDown={(e) => {
+        if (typeof onRowClick !== 'function') return;
+        const target = e.target as HTMLDivElement;
+
+        if (e.key === 'Enter' || e.key === ' ') {
+          e.preventDefault();
+          onRowClick(row.original);
+        }
+        if (e.key === 'ArrowDown') {
+          const next = target?.nextSibling as HTMLDivElement;
+          next?.focus();
+        }
+        if (e.key === 'ArrowUp') {
+          const previous = target?.previousSibling as HTMLDivElement;
+          previous?.focus();
+        }
       }}
     >
       <div className={`flex gap-2 items-center`}>
