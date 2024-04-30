7eb5e2fb5479301bef4f9943b3199bcff7ff06f8
dhruv@dk <dhruv21.dk@gmail.com>
2024-04-30 11:30:33 +0530

    fix: bug fix

--- src/pages/FieldTicket/material/AddRentalDataDialog.tsx
+++ src/pages/FieldTicket/material/AddRentalDataDialog.tsx
@@ -127,7 +127,7 @@ const AddRentalDataDialog = ({
     }
     else {
       let inventoryData = data?.inventory;
-      inventoryData = inventoryData.filter((e)=>!dataRows?.some((ele)=> ele.materialId===e?.inventoryDetail?._id))
+      inventoryData = inventoryData.filter((e)=>!dataRows?.some((ele)=> ele.type===MATERIAL_TYPE.serializedAsset && ele.materialId===e?.inventoryDetail?._id))
       inventoryData.forEach((parent, i) => {
         const product = data?.material?.find((e) => e._id === parent._id);
         const obj: any = { ...product }
