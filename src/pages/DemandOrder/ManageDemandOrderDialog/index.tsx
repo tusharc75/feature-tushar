fc4df30de86aa2637eb2c80e17d03e722a506d27
dhruv@dk <dhruv21.dk@gmail.com>
2024-05-20 14:20:25 +0530

    fix: bug fix

--- src/pages/DemandOrder/ManageDemandOrderDialog/index.tsx
+++ src/pages/DemandOrder/ManageDemandOrderDialog/index.tsx
@@ -67,7 +67,7 @@ const ManageDemandOrderDialog = ({ isClone, demandOrderId, demandOrderData = nul
             setCloneHeading(demandOrderNumber);
             setSalesData({
               fields: fieldsDataForCreate,
-              initialValues: { ...getObjKeysWithValues(rest, fieldsDataForCreate, true, user), estimateEndDate: null }
+              initialValues: { ...getObjKeysWithValues(rest, fieldsDataForCreate, true, user) }
             });
             setLoading(false);
           } else {
