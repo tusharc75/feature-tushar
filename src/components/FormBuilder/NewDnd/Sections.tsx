2cc652124ef2be15652c40030b7905b91078fa46
dhrubobarman <dhrubobarman847@gmail.com>
2024-05-30 14:38:51 +0530

    fix: form builder bux fix

--- src/components/FormBuilder/NewDnd/Sections.tsx
+++ src/components/FormBuilder/NewDnd/Sections.tsx
@@ -2,6 +2,7 @@ import { SortableContext } from '@dnd-kit/sortable';
 import { useMemo } from 'react';
 import SingleSection from './SingleSection';
 import { ThemeButton } from 'src/components/Helpers/Buttons';
+import { useDroppable } from '@dnd-kit/core';
 
 type SectionPorps = {
   sections: any[];
@@ -28,8 +29,17 @@ export default function Sections({
 }: SectionPorps) {
   const sectionIds = useMemo(() => sections.map((s) => `${s.sectionId}`), [sections]);
 
+  const { setNodeRef, isOver, over, active } = useDroppable({
+    id: 'droppable',
+    data: {
+      type: 'droppable'
+    }
+  });
+
+  const isNewSectionPreviewActive = active?.data.current.type === 'NewSection' && isOver;
+
   return (
-    <div className=" container-with-border p-5 space-y-2 max-h-[calc(100vh-200px)] overflow-auto max-[960px]:h-[calc(100vh-200px)] overflow-x-hidden !overflow-y-auto">
+    <div className=" container-with-border max-h-[calc(100vh-200px)] space-y-2 overflow-auto !overflow-y-auto overflow-x-hidden p-5 max-[960px]:h-[calc(100vh-200px)]">
       <SortableContext items={sectionIds} strategy={() => null}>
         {sections.length > 0 ? (
           <>
@@ -51,12 +61,17 @@ export default function Sections({
             ))}
           </>
         ) : (
-          <div className="p-8">
-            <p className="text-center">Drag and drop your sections here</p>
+          <div
+            className={`${isNewSectionPreviewActive ? 'flex min-h-[160px] items-center justify-center bg-blue-200 p-10' : 'p-8'} transition-all`}
+            ref={setNodeRef}
+          >
+            <p className={`${isNewSectionPreviewActive ? 'text-center text-2xl font-bold text-gray-400 dark:text-gray-600' : ''} text-center`}>
+              {isNewSectionPreviewActive ? 'Drop New Section' : 'Drag and drop your sections here'}
+            </p>
           </div>
         )}
       </SortableContext>
-      <div className=" text-center my-4">
+      <div className=" my-4 text-center">
         <ThemeButton iconForMobile={false} onClick={() => addSection(sections.length)}>
           Add Section
         </ThemeButton>
