bc8ab614be06a194bc3b7814c8e88e2cee03b630
harshit <harshit@vebholic.com>
2024-06-06 16:04:25 +0530

    fix: fux

--- src/pages/Dashboard/ChartTypes.tsx
+++ src/pages/Dashboard/ChartTypes.tsx
@@ -227,10 +227,6 @@ const ChartTypes = ({
       });
   };
 
-  console.log(chart);
-
-  console.log(chartData);
-
   return (
     <Grid item xs={12} md={fullScreen ? 12 : chart.column}>
       {chart.graphType === 'Custom' ? (
