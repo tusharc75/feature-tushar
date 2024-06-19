f2b1b410f2a419085805b055b8a49cabdf9d2cec
ABHIMANYU KUMAR <manupatel18112000@gmail.com>
2024-06-19 17:50:06 +0530

    fix bugs

--- src/pages/RentalManagement/ReceivingTicket/TransferToAnotherPackageDialog.tsx
+++ src/pages/RentalManagement/ReceivingTicket/TransferToAnotherPackageDialog.tsx
@@ -7,14 +7,16 @@ import CustomDialogContent from "src/components/CustomDialog/CustomDialogContent
 import CustomDialogHeader from "src/components/CustomDialog/CustomDialogHeader";
 import CustomReactTable, { useTableReducer } from "src/components/CustomReactTable";
 import NoDataCell from "src/components/Helpers/NoDataCell";
-import { CustomDialogTransition, rentalManagement } from "src/constants/helpers";
+import { ASSET_STATUS, CustomDialogTransition, rentalManagement } from "src/constants/helpers";
+import AssetDetailsChangeDialog from "src/pages/RentalManagement/ReceivingTicket/AssetDetailsChangeDialog";
 
-const TransferToAnotherPackageDialog = ({ onClose, onSuccess, rentalManagementData, selectedAssets }) => {
+const TransferToAnotherPackageDialog = ({ onClose, onSuccess, rentalManagementData, selectedAssets, assetPolicyData }) => {
 	const toastConfig = useContext(CustomToastContext);
 
 	const { state, dispatch } = useTableReducer();
 	const { dataRows } = state;
 	const [isAdding, setIsAdding] = useState(false)
+	const [openAssetDataDialog, setOpenAssetDataDialog] = useState({ open: false, statusPolicy: null });
 
 	const column = [
 		{
@@ -56,8 +58,11 @@ const TransferToAnotherPackageDialog = ({ onClose, onSuccess, rentalManagementDa
 				data?.forEach(d => {
 					rows.push(...d?.products?.map(p => ({
 						productName: p?.productName,
+						productId: p?._id,
 						packageName: d?.package?.packageName,
+						packageId: d?.package?._id,
 						packageUniqueId: d?.package?.uniqueId,
+						wellNumber: d?.package?.wellNumber,
 						uniqueId: p?.uniqueId,
 						qty: p?.qty
 					})))
@@ -69,7 +74,7 @@ const TransferToAnotherPackageDialog = ({ onClose, onSuccess, rentalManagementDa
 			})
 	}
 
-	const handleAdd = () => {
+	const handleAdd = (assetsData) => {
 		setIsAdding(true)
 		axiosInstance()
 			.post(
@@ -78,7 +83,13 @@ const TransferToAnotherPackageDialog = ({ onClose, onSuccess, rentalManagementDa
 					const assets: any = [];
 					for (let i = 0; i < _d.qty; i++) {
 						if (selectedAssets[i]) {
-							assets.push(selectedAssets[i]?._id)
+							const obj: any = { _id: selectedAssets[i]?._id }
+							const matchedAsset = assetsData?.find((asset) => asset._id === selectedAssets[i]?._id);
+							if (matchedAsset) {
+								const { _id, ...assetData } = matchedAsset;
+								obj.assetData = assetData;
+							}
+							assets.push(obj)
 						}
 					}
 					return ({
@@ -98,61 +109,84 @@ const TransferToAnotherPackageDialog = ({ onClose, onSuccess, rentalManagementDa
 	}
 
 	return (
-		<Dialog
-			TransitionComponent={CustomDialogTransition}
-			open={true}
-			fullScreen={true}
-			aria-labelledby="customized-dialog-title"
-			fullWidth
-			maxWidth={'md'}
-			onClose={(e, reason) => {
-				if (reason !== 'backdropClick') {
-				}
-			}}
-		>
-			<Box>
-				<CustomDialogHeader
-					onClose={onClose}
-					title={"Transfer Assets To Another Package"}
-					showManimizeMaximize={false}
-					showRequiredLabel={false}
+		<>
+			<Dialog
+				TransitionComponent={CustomDialogTransition}
+				open={true}
+				fullScreen={true}
+				aria-labelledby="customized-dialog-title"
+				fullWidth
+				maxWidth={'md'}
+				onClose={(e, reason) => {
+					if (reason !== 'backdropClick') {
+					}
+				}}
+			>
+				<Box>
+					<CustomDialogHeader
+						onClose={onClose}
+						title={"Transfer Assets To Another Package"}
+						showManimizeMaximize={false}
+						showRequiredLabel={false}
 
-				/>
-				<CustomDialogContent>
-					<Box>
-						<Box display={'flex'} justifyContent={'end'} alignItems={'center'}>
-							<Button
-								variant="contained"
-								color="primary"
-								size="small"
-								disabled={
-									state?.loading ||
-									isAdding ||
-									selectedAssets?.reduce((sum, cur) => sum + cur?.qty, 0) != dataRows?.reduce((sum, cur) => sum + cur?.qty, 0)
-								}
-								onClick={() => {
-									handleAdd()
-								}}
-							>
-								Add
-							</Button>
-						</Box>
+					/>
+					<CustomDialogContent>
 						<Box>
-							<CustomReactTable
-								height={'calc(100vh - 210px)'}
-								columns={column}
-								state={state}
-								dispatch={dispatch}
-								refreshGrid={fetchData}
-								hideAction={true}
-								renderedFrom={'ReceivingTicket_TransferAnotherPackage'}
-								isClientSideGrid={true}
-							/>
+							<Box display={'flex'} justifyContent={'end'} alignItems={'center'}>
+								<Button
+									variant="contained"
+									color="primary"
+									size="small"
+									disabled={
+										state?.loading ||
+										isAdding ||
+										selectedAssets?.reduce((sum, cur) => sum + cur?.qty, 0) != dataRows?.reduce((sum, cur) => sum + cur?.qty, 0)
+									}
+									onClick={() => {
+										const statusPolicy = assetPolicyData?.policy?.statusChangeFields?.find((ele) => ele.status === ASSET_STATUS.reserved);
+										if (statusPolicy) {
+											setOpenAssetDataDialog({ open: true, statusPolicy: statusPolicy });
+										}
+									}}
+								>
+									Add
+								</Button>
+							</Box>
+							<Box>
+								<CustomReactTable
+									height={'calc(100vh - 210px)'}
+									columns={column}
+									state={state}
+									dispatch={dispatch}
+									refreshGrid={fetchData}
+									hideAction={true}
+									renderedFrom={'ReceivingTicket_TransferAnotherPackage'}
+									isClientSideGrid={true}
+								/>
+							</Box>
 						</Box>
-					</Box>
-				</CustomDialogContent>
-			</Box>
-		</Dialog>
+					</CustomDialogContent>
+				</Box>
+			</Dialog>
+			{openAssetDataDialog.open && (
+				<AssetDetailsChangeDialog
+					ids={selectedAssets?.map((e) => e._id)}
+					statusPolicy={openAssetDataDialog.statusPolicy}
+					setAssetsData={() => { }}
+					onClose={() => setOpenAssetDataDialog({ open: false, statusPolicy: null })}
+					onSuccess={(data) => {
+						handleAdd(data)
+						setOpenAssetDataDialog({ open: false, statusPolicy: null });
+					}}
+					staticLookUpFilters={{
+						wellNumber: rentalManagementData?.wellNumber
+							? rentalManagementData?.wellNumber?.optionValue || rentalManagementData?.wellNumber?.map((e) => e?.optionValue)
+							: null,
+					}}
+					productsDefaultData={dataRows?.map(d => ({ materialId: d?.productId, assetDefaultData: [{ wellNumber: d?.wellNumber, qty: d?.qty, package: d?.packageId }] }))}
+				/>
+			)}
+		</>
 	)
 }
 
