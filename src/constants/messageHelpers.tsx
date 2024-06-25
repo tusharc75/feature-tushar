0c0cf6130469bda46241d8373fd3d1a2ebd7d35b
ABHIMANYU KUMAR <manupatel18112000@gmail.com>
2024-06-25 13:13:09 +0530

    fix bugs

--- src/constants/messageHelpers.tsx
+++ src/constants/messageHelpers.tsx
@@ -142,4 +142,5 @@ export const subcontractAssemblyMessage = {
     loadingAlreadyCreated: 'Loading Ticket is already created',
     loadingNotCreated: 'Loading Ticket is not created',
     loadingAlreadyDelivered: 'Loading Ticket is already delivered',
+    loadingAlreadyReceived: 'Loading Ticket is already received',
 }
