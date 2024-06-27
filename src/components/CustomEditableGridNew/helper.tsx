bc2640b616a8431dd8ba7714fa27e8d7f9d6634d
ABHIMANYU KUMAR <manupatel18112000@gmail.com>
2024-06-27 10:22:14 +0530

    fix bugs

---
+++ src/components/CustomEditableGridNew/helper.tsx
@@ -0,0 +1,12 @@
+
+export const yupSchemaForBulkEdit = (fields: any[], values: any[]) => {
+	const schema = {};
+	values.forEach((element) => {
+		fields.forEach((input) => {
+			if (input.required && !Boolean(element[`${input.fieldName}`])) {
+				schema[`${element._id}_${input.fieldName}`] = `${input.fieldLabel} is required`;
+			}
+		});
+	});
+	return schema;
+};
