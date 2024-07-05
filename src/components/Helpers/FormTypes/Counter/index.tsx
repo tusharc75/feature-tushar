e792eeed6692582e1f3be55d9814f3a6f6a0087b
dhrubobarman <dhrubobarman847@gmail.com>
2024-07-05 15:57:56 +0530

    fix: counter field

--- src/components/Helpers/FormTypes/Counter/index.tsx
+++ src/components/Helpers/FormTypes/Counter/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Box, Button, Grid, Typography } from '@material-ui/core';
+import { Box, Button, Grid, IconButton, TextField, Typography } from '@material-ui/core';
 import { getObjKeys } from 'src/constants/helpers';
 import FormTypes from '../../FormTypes';
 
@@ -50,78 +50,88 @@ const Counter = ({ label, values, name, setFieldValue, fieldData, touched, error
 
   return (
     <Box>
-      <Box border={1} borderColor="var(--common-border-color)" p={0.5}>
-        <Typography
-          style={{ marginBottom: '12px', fontWeight: '500', textAlign: 'center' }}
-          color={touched[name] && Boolean(errors[name]) ? 'error' : 'primary'}
-        >
-          {label}
-        </Typography>
-        <Box border={1} borderColor="var(--common-border-color)" p={0.5} display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
-          <Button
-            variant="outlined"
-            size="small"
-            onClick={() => {
-              handleAddRemove('remove');
-            }}
+      <Box border={1} borderColor="var(--common-border-color)" className="rounded-md  p-2">
+        <div className="flex items-center justify-between gap-2">
+          <Typography
+            className=" line-clamp-2 flex-grow "
+            title={label}
+            variant="body2"
+            style={{ fontWeight: '500' }}
+            color={touched[name] && Boolean(errors[name]) ? 'error' : 'primary'}
           >
-            -
-          </Button>
-          {values[name]?.length}
-          <Button
-            variant="outlined"
-            size="small"
-            onClick={() => {
-              handleAddRemove();
-            }}
-          >
-            +
-          </Button>
-        </Box>
-        {values[name]?.map((value, index) => {
-          return (
-            <Box border={1} borderColor="var(--common-border-color)" p={0.5} mt={1}>
-              <Grid container spacing={2}>
-                {fieldData?.subFields?.map((field) => {
-                  return (
-                    <Grid item xs={12} sm={12} md={6} lg={6}>
-                      <FormTypes
-                        {...field}
-                        fieldData={field}
-                        values={value}
-                        errors={error[`${index}`] || {}}
-                        touched={touch[`${index}`] || {}}
-                        label={field.fieldLabel}
-                        name={field.fieldName}
-                        type={field.type}
-                        options={field.option}
-                        setFieldValue={(n, v) => {
-                          setFieldValue(
-                            name,
-                            values[name]?.map((_v, i) => {
-                              if (index === i) {
-                                return {
-                                  ..._v,
-                                  [n]: v
-                                };
-                              }
-                              return _v;
-                            })
-                          );
-                        }}
-                        required={field.required}
-                        fullWidth
-                        isTooltip={field?.isTooltip || false}
-                        tooltipMessage={field?.tooltipMessage}
-                        size="small"
-                      />
-                    </Grid>
-                  );
-                })}
-              </Grid>
-            </Box>
-          );
-        })}
+            {label}
+          </Typography>
+          <div className="flex w-[130px] flex-shrink-0 items-center gap-2 overflow-hidden rounded-md [border:1px_solid_var(--common-border-color)] md:w-[156px]">
+            <IconButton
+              color="secondary"
+              className=" !rounded-r-none !bg-[var(--new-theme-color)] hover:!opacity-80"
+              style={{ maxHeight: 40, color: 'white' }}
+              onClick={() => {
+                handleAddRemove('remove');
+              }}
+            >
+              -
+            </IconButton>
+            <TextField disabled value={values[name]?.length} size="small" />
+            <IconButton
+              className=" !rounded-l-none !bg-[var(--new-theme-color)] hover:!opacity-80"
+              style={{ maxHeight: 40, color: 'white' }}
+              onClick={() => {
+                handleAddRemove();
+              }}
+            >
+              +
+            </IconButton>
+          </div>
+        </div>
+        {values[name].length > 0 && (
+          <div className="mt-3 space-y-3">
+            {values[name]?.map((value, index) => {
+              return (
+                <div className={` ${values[name].length - 1 === index ? '' : 'pb-3 [border-bottom:2px_dashed_var(--common-border-color)]'} `}>
+                  <Grid container spacing={1}>
+                    {fieldData?.subFields?.map((field) => {
+                      return (
+                        <Grid item xs={12} md={6}>
+                          <FormTypes
+                            {...field}
+                            fieldData={field}
+                            values={value}
+                            errors={error[`${index}`] || {}}
+                            touched={touch[`${index}`] || {}}
+                            label={field.fieldLabel}
+                            name={field.fieldName}
+                            type={field.type}
+                            options={field.option}
+                            setFieldValue={(n, v) => {
+                              setFieldValue(
+                                name,
+                                values[name]?.map((_v, i) => {
+                                  if (index === i) {
+                                    return {
+                                      ..._v,
+                                      [n]: v
+                                    };
+                                  }
+                                  return _v;
+                                })
+                              );
+                            }}
+                            required={field.required}
+                            fullWidth
+                            isTooltip={field?.isTooltip || false}
+                            tooltipMessage={field?.tooltipMessage}
+                            size="small"
+                          />
+                        </Grid>
+                      );
+                    })}
+                  </Grid>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </Box>
       <Box pl={1} pt={0.5}>
         {touched[name] && Boolean(errors[name]) && (
