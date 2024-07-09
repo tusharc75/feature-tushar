b1990ff943d2aaa461960895770ffb749f0eb3ca
dhrubobarman <dhrubobarman847@gmail.com>
2024-07-09 12:04:30 +0530

    fix: structured files

--- src/components/CustomIntro/index.tsx
+++ src/components/CustomIntro/index.tsx
@@ -1,18 +1,18 @@
-import { Dialog, DialogContent, IconButton, Popper, TextField } from '@material-ui/core';
+import { Dialog, IconButton, Popper, TextField } from '@material-ui/core';
 import { Close } from '@material-ui/icons';
 import { KeyboardEvent, ReactNode, useRef, useState } from 'react';
-import { FaCaretUp } from 'react-icons/fa';
 import { cn, CustomDialogTransition } from 'src/constants/helpers';
 
 import React, { useEffect } from 'react';
 import { FaArrowLeft, FaArrowRight, FaQuestion } from 'react-icons/fa';
 import { GiFinishLine } from 'react-icons/gi';
+import { useLocation } from 'react-router-dom';
 import { HandleSteps } from 'src/components/CustomIntro/HandleStep';
+import { getCurrentUrl } from 'src/components/CustomIntro/IntorCreator/helper';
 import HtmlTooltip from 'src/components/CustomTooltipTitle';
 import { ThemeButton } from 'src/components/Helpers/Buttons';
 import { useStore, WALK_ME_STEPS } from 'src/StateProvider/fastContext';
-import { useLocation } from 'react-router-dom';
-import { getCurrentUrl } from 'src/components/CustomIntro/IntorCreator/helper';
+export * from 'src/components/CustomIntro/CustomIntroWrapper';
 export * from 'src/components/CustomIntro/useSetWalkmeSteps';
 
 export type WalkmeData = {
@@ -89,8 +89,6 @@ const CustomIntro = () => {
     currentStepData?.element.click();
     handleSteps.current?.next();
   };
-
-  // console.log(handleSteps.current);
 
   if (handleSteps?.current?.error || !handleSteps || isHiddenStep) return null;
 
@@ -277,75 +275,5 @@ const SelectIntro = ({ handleStart }: { handleStart: (intro: WalkmeData) => void
         </div>
       </Dialog>
     </>
-  );
-};
-
-type CustomIntroWrapperProps = {
-  title: ReactNode;
-  content: ReactNode;
-  disabled?: boolean;
-  children: ReactNode;
-};
-
-export const CustomIntroWrapper = ({ title, content, disabled, children, ...rest }: CustomIntroWrapperProps) => {
-  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
-  const arrowRef = useRef(null);
-
-  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handlePopoverClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
-  return (
-    <span className="relative">
-      {children}
-      {!disabled && (
-        <button
-          type="button"
-          onClick={handlePopoverOpen}
-          className={cn(
-            'absolute right-0 top-0 z-[2] !-mr-[5px] flex size-3 cursor-help items-center justify-center rounded-full border-0 bg-[var(--new-theme-color)] focus-within:border-0 focus-within:outline-none',
-            open && 'opacity-0'
-          )}
-        >
-          <span className="h-3 w-3 animate-ping rounded-full bg-[var(--new-theme-color)] opacity-75"></span>
-          <span className=" sr-only">more info</span>
-        </button>
-      )}
-
-      <Popper
-        open={open}
-        anchorEl={anchorEl}
-        modifiers={[
-          {
-            name: 'arrow',
-            enabled: true,
-            options: {
-              element: arrowRef
-            }
-          }
-        ]}
-      >
-        <div className="relative mt-3 min-w-[200px] max-w-[300px] rounded-md bg-[var(--dark-secondary,white)] p-2 shadow-md">
-          <div className="mb-2 flex items-center justify-between gap-2 pb-1 [border-bottom:1px_solid_var(--common-border-color)]">
-            <p className=" truncate text-[16px] font-semibold ">{title}</p>
-            <IconButton size="small" onClick={handlePopoverClose}>
-              <Close />
-            </IconButton>
-          </div>
-          <div className="p-1 text-gray-600 dark:text-gray-300">{content}</div>
-          <span
-            ref={arrowRef}
-            className="absolute left-1/2 top-[-20px] text-[var(--dark-secondary,white)]  drop-shadow-md [transform:translateX(-50%)]"
-          >
-            <FaCaretUp size={30} />
-          </span>
-        </div>
-      </Popper>
-    </span>
   );
 };
