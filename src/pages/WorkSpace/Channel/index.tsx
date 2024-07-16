bc1ca6d0745fdf781f354693a13497fe16892386
harshit <harshit@vebholic.com>
2024-07-16 16:53:25 +0530

    feat: adding/Removing Members in WorkSpace Channels

--- src/pages/WorkSpace/Channel/index.tsx
+++ src/pages/WorkSpace/Channel/index.tsx
@@ -1,31 +1,54 @@
-import { useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { Box, IconButton, Typography } from '@material-ui/core';
-import { AiOutlinePlus } from 'react-icons/ai';
-import AddMembersDialog from './AddMembersDialog';
+import axiosInstance from 'src/axios/axiosInstance';
+import { CustomToastContext } from 'src/StateProvider/CustomToastContext/CustomToastContext';
+import CommonSkeleton from 'src/components/Helpers/CommonSkeleton';
+import { Visibility } from '@material-ui/icons';
+import HtmlTooltip from 'src/components/CustomTooltipTitle';
+import Members from './Members';
 
-const Channel = ({ channelData, fetchChannels }) => {
-  const [addMemberDialog, setAddMemberDialog] = useState(false);
+const Channel = ({ channelId }) => {
+  const [memberDialog, setMemberDialog] = useState(false);
+  const [channelData, setChannelData] = useState(null);
+  const toastConfig = useContext(CustomToastContext);
+
+  const fetchChannelData = async () => {
+    try {
+      const { data } = await axiosInstance().get(`work-space/channel/${channelId}`);
+      setChannelData(data?.data);
+    } catch (error) {
+      toastConfig.setToastConfig(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchChannelData();
+  }, [channelId]);
 
   return (
     <Box>
-      <Box display="flex" alignItems="center" justifyContent="space-between" padding={2}>
-        <Typography variant="h6">{channelData.title}</Typography>
-        <IconButton color="primary" onClick={() => setAddMemberDialog(true)}>
-          <AiOutlinePlus />
-        </IconButton>
-      </Box>
-      <Typography>
-        {channelData.description}
-      </Typography>
-      {addMemberDialog && (
-        <AddMembersDialog
-          onClose={() => setAddMemberDialog(false)}
-          channelId={channelData._id}
-          onSuccess={() => {
-            fetchChannels();
-            setAddMemberDialog(false);
-          }}
-          ignoreIds={channelData?.members}
+      {channelData ? <>
+        <Box display="flex" alignItems="center" justifyContent="space-between" padding={2}>
+          <Typography variant="h6">{channelData.title}</Typography>
+          <HtmlTooltip title={'View Members'}>
+            <span>
+              <IconButton color="primary" onClick={() => setMemberDialog(true)}>
+                <Visibility fontSize="small" color="primary" />
+              </IconButton>
+            </span>
+          </HtmlTooltip>
+        </Box>
+        <Typography>
+          {channelData.description}
+        </Typography>
+      </> : <>
+        <CommonSkeleton lenArray={[...Array(2).keys()]} />
+      </>}
+      {memberDialog && (
+        <Members
+          onClose={() => setMemberDialog(false)}
+          channelData={channelData}
+          fetchChannelData={fetchChannelData}
         />
       )}
     </Box>
