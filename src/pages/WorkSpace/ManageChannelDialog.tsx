5a0b297023bc35d7bd5ca97556e1c281cb05a3e2
harshit <harshit@vebholic.com>
2024-07-16 11:53:43 +0530

    fix: simple formik for creating channel

--- src/pages/WorkSpace/ManageChannelDialog.tsx
+++ src/pages/WorkSpace/ManageChannelDialog.tsx
@@ -1,5 +1,5 @@
 import { useState, useContext, Fragment } from 'react';
-import { Box, Dialog, Button, Grid } from '@material-ui/core';
+import { Box, Dialog, Button, Grid, TextField, RadioGroup, FormControlLabel, Radio, FormLabel } from '@material-ui/core';
 import { Formik, Form } from 'formik';
 import CustomDialogHeader from '../../components/CustomDialog/CustomDialogHeader';
 import CustomDialogContent from '../../components/CustomDialog/CustomDialogContent';
@@ -7,66 +7,28 @@ import CustomDialogFooter from '../../components/CustomDialog/CustomDialogFooter
 import axiosInstance from '../../axios/axiosInstance';
 import { CustomToastContext } from '../../StateProvider/CustomToastContext/CustomToastContext';
 import { isMobile, isTablet } from 'react-device-detect';
-import {
-  CustomDialogTransition,
-  setFieldsInAscendingOrder,
-} from '../../constants/helpers';
-import { yupSchema } from '../../constants/helpers';
+import { CustomDialogTransition } from '../../constants/helpers';
 import ConfirmCancelDialog from '../../components/ConfirmCancelDialog';
-import FormTypes from '../../components/Helpers/FormTypes';
-import { FaDiceOne } from 'react-icons/fa';
-import CommonSkeleton from '../../components/Helpers/CommonSkeleton';
 import { isEqual } from 'lodash';
 import CustomButton from '../../components/Helpers/CustomButton';
+import { object, string } from 'yup';
 
 const ManageChannel = ({ onClose, onSuccess }) => {
   const toastConfig = useContext(CustomToastContext);
 
-  const fields = [
-    {
-      fieldLabel: "Title",
-      fieldName: "title",
-      isColumnEditable: true,
-      type: 'singleLine',
-      required: true,
-      option: [],
-      sectionName: 'Channel Information',
-      order: 1,
-    },
-    {
-      fieldLabel: "Description",
-      fieldName: "description",
-      isColumnEditable: true,
-      type: 'singleLine',
-      required: false,
-      option: [],
-      sectionName: 'Channel Information',
-      order: 2,
-    },
-    {
-      fieldLabel: "Access",
-      fieldName: "access",
-      isColumnEditable: true,
-      type: 'dropDown',
-      isDropdown: true,
-      required: true,
-      sectionName: 'Channel Information',
-      option: [
-        {
-          optionLabel: 'Public',
-          optionValue: 'public'
-        },
-        {
-          optionLabel: 'Private',
-          optionValue: 'private'
-        }
-      ],
-      order: 3,
-    }
-  ];
+  const validationSchema = object().shape({
+    title: string().required('Please enter Channel name'),
+    description: string(),
+    access: string().required('Please select access type'),
+  });
+
+  const initialValues = {
+    title: '',
+    description: '',
+    access: 'public',
+  };
 
   const [loading, setLoading] = useState(false);
-  const initialData = { fields: fields, values: {} };
   const [showConfirmDialog, setShowConfirmDialog] = useState(false);
   const [isSubmitting, setSubmitting] = useState(false);
   const [fullScreen, setFullScreen] = useState(isMobile || isTablet);
@@ -108,8 +70,6 @@ const ManageChannel = ({ onClose, onSuccess }) => {
     }
   };
 
-  const formsData = setFieldsInAscendingOrder(initialData.fields);
-
   return (
     <Dialog
       maxWidth="md"
@@ -124,129 +84,131 @@ const ManageChannel = ({ onClose, onSuccess }) => {
         }
       }}
     >
-      {initialData?.fields?.length ? (
-        <Formik
-          initialValues={initialData.values}
-          validationSchema={yupSchema(initialData.fields)}
-          onSubmit={handleSubmit}
-        >
-          {({ values, errors, setFieldValue, touched, submitForm }) => (
-            <Fragment>
-              <CustomDialogHeader
-                onClose={() => {
-                  if (!isEqual(values, initialData.values)) {
+      <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+        {({ values, errors, setFieldValue, touched, submitForm }) => (
+          <Fragment>
+            <CustomDialogHeader
+              onClose={() => {
+                if (!isEqual(values, initialValues)) {
+                  setShowConfirmDialog(true);
+                } else {
+                  onClose();
+                }
+              }}
+              title={`Create Channel`}
+              isMinimized={!fullScreen}
+              onMinimizeMaximize={() => {
+                setFullScreen((prevState) => !prevState);
+              }}
+              showManimizeMaximize={true}
+            />
+            <CustomDialogContent>
+              <Form autoComplete="off" autoCorrect="off" noValidate>
+                <Box padding={1}>
+                  <Grid container spacing={3}>
+                    <Grid item xs={12} md={6} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        type="text"
+                        label="Channel Name"
+                        required={true}
+                        name="title"
+                        fullWidth
+                        margin="dense"
+                        value={values['title']}
+                        error={touched['title'] && Boolean(errors['title'])}
+                        helperText={touched['title'] && errors['title']}
+                        onChange={(e) => {
+                          setFieldValue('title', e.target.value.trimStart());
+                        }}
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={6} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        type="text"
+                        label="Channel Description"
+                        name="description"
+                        fullWidth
+                        margin="dense"
+                        value={values['description']}
+                        error={touched['description'] && Boolean(errors['description'])}
+                        helperText={touched['description'] && errors['description']}
+                        onChange={(e) => {
+                          setFieldValue('description', e.target.value.trimStart());
+                        }}
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={6} sm={6}>
+                      <FormLabel component="legend" required={true}>Access</FormLabel>
+                      <RadioGroup
+                        row
+                        name="access"
+                        value={values.access}
+                        onChange={(e) => setFieldValue('access', e.target.value)}
+                      >
+                        <FormControlLabel value="public" control={<Radio />} label="Public" />
+                        <FormControlLabel value="private" control={<Radio />} label="Private" />
+                      </RadioGroup>
+                      {touched.access && errors.access && (
+                        <div style={{ color: 'red', marginTop: 8 }}>{errors.access}</div>
+                      )}
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Form>
+            </CustomDialogContent>
+            <CustomDialogFooter>
+              <Button
+                variant="outlined"
+                color="primary"
+                size="small"
+                disabled={isSubmitting || loading}
+                onClick={() => {
+                  if (!isEqual(values, initialValues)) {
                     setShowConfirmDialog(true);
                   } else {
                     onClose();
                   }
                 }}
-                title={`Create Channel`}
-                isMinimized={!fullScreen}
-                onMinimizeMaximize={() => {
-                  setFullScreen((prevState) => !prevState);
+              >
+                Cancel
+              </Button>
+              <CustomButton
+                disabled={isSubmitting || loading}
+                loading={loading}
+                variant="contained"
+                color="primary"
+                type="submit"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleScroll(errors);
+                  submitForm();
                 }}
-                showManimizeMaximize={true}
+              >
+                {' '}
+                Save
+              </CustomButton>
+            </CustomDialogFooter>
+            {showConfirmDialog ? (
+              <ConfirmCancelDialog
+                close={() => setShowConfirmDialog(false)}
+                open={showConfirmDialog}
+                onSave={() => {
+                  setShowConfirmDialog(false);
+                  handleScroll(errors);
+                  submitForm();
+                }}
+                onClose={() => {
+                  setShowConfirmDialog(false);
+                  onClose();
+                }}
               />
-              <CustomDialogContent>
-                <Form autoComplete="off" autoCorrect="off" noValidate>
-                  {formsData &&
-                    formsData?.map((form, i) => {
-                      return form.name && (
-                        <div key={i}>
-                          <div className="detail-box-content">
-                            <FaDiceOne size={16} color={'var(--white)'} style={{ marginRight: '5px' }} />
-                            <h2 className="form-label-style form-label-quotes">{form.name}</h2>
-                          </div>
-                          <Box marginY={2}>
-                            <Grid spacing={3} container>
-                              {form.sectionFields.map((field, index2) => (
-                                <Grid key={index2} item xs={12} sm={6} md={6}>
-                                  <FormTypes
-                                    {...field}
-                                    fieldData={field}
-                                    fields={initialData.fields}
-                                    values={values}
-                                    errors={errors}
-                                    touched={touched}
-                                    label={field.fieldLabel}
-                                    name={field.fieldName}
-                                    type={field.type}
-                                    options={field.option}
-                                    setFieldValue={(name, value) => {
-                                      setFieldValue(name, value);
-                                    }}
-                                    required={field.required}
-                                    fullWidth
-                                    isTooltip={field?.isTooltip || false}
-                                    tooltipMessage={field?.tooltipMessage}
-                                    size="small"
-                                    imageOrFileUploadCompletePercentage={null}
-                                  />
-                                </Grid>
-                              ))}
-                            </Grid>
-                          </Box>
-                        </div>
-                      )
-                    })}
-                </Form>
-              </CustomDialogContent>
-              <CustomDialogFooter>
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  size="small"
-                  disabled={isSubmitting || loading}
-                  onClick={() => {
-                    if (!isEqual(values, initialData.values)) {
-                      setShowConfirmDialog(true);
-                    } else {
-                      onClose();
-                    }
-                  }}
-                >
-                  Cancel
-                </Button>
-                <CustomButton
-                  disabled={isSubmitting || loading}
-                  loading={loading}
-                  variant="contained"
-                  color="primary"
-                  type="submit"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleScroll(errors);
-                    submitForm();
-                  }}
-                >
-                  {' '}
-                  Save
-                </CustomButton>
-              </CustomDialogFooter>
-              {showConfirmDialog ? (
-                <ConfirmCancelDialog
-                  close={() => setShowConfirmDialog(false)}
-                  open={showConfirmDialog}
-                  onSave={() => {
-                    setShowConfirmDialog(false);
-                    handleScroll(errors);
-                    submitForm();
-                  }}
-                  onClose={() => {
-                    setShowConfirmDialog(false);
-                    onClose();
-                  }}
-                />
-              ) : null}
-            </Fragment>
-          )}
-        </Formik>
-      ) : (
-        <Box p={2} height={500}>
-          <CommonSkeleton lenArray={[...Array(10).keys()]} />
-        </Box>
-      )}
-    </Dialog>
+            ) : null}
+          </Fragment>
+        )}
+      </Formik>
+    </Dialog >
   );
 };
 
