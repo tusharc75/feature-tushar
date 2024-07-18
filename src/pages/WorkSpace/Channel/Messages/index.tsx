7fe28bd3f6b4e313d0502199a6296e1b0b1fc061
harshit <harshit@vebholic.com>
2024-07-18 10:53:31 +0530

    fix: socket in workspace channel

--- src/pages/WorkSpace/Channel/Messages/index.tsx
+++ src/pages/WorkSpace/Channel/Messages/index.tsx
@@ -3,11 +3,31 @@ import axiosInstance from 'src/axios/axiosInstance';
 import { List, ListItem, ListItemText, Typography, Box, TextField, Button } from '@material-ui/core';
 import { CustomToastContext } from 'src/StateProvider/CustomToastContext/CustomToastContext';
 import CommonSkeleton from 'src/components/Helpers/CommonSkeleton';
+import io, { Socket } from 'socket.io-client';
+import { backendApi } from 'src/config';
+import { useData } from 'src/StateProvider/Provider';
 
 const Messages = ({ channelId }) => {
   const [messages, setMessages] = useState(null);
   const [message, setMessage] = useState('');
   const toastConfig = useContext(CustomToastContext);
+  const [socket, setSocket] = useState<Socket>(null);
+
+  const { state: { user } }: any = useData();
+
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    if (!token) return;
+    const s = io(`${backendApi?.replace('/api', '')}/workspace/channel`, {
+      path: backendApi?.includes('/api') ? '/api/socket.io/' : '/socket.io/',
+      auth: { token },
+      reconnectionAttempts: 5,
+      reconnectionDelay: 5000,
+      transports: ['websocket', 'pooling']
+    });
+    setSocket(s);
+  }, [token]);
 
   const fetchMessages = async () => {
     try {
@@ -18,15 +38,33 @@ const Messages = ({ channelId }) => {
     }
   };
 
-  const postMessage = async () => {
+  const postMessage = () => {
     try {
-      await axiosInstance().post('/work-space/channel/message', { channelId, message });
-      setMessage('');
-      fetchMessages();
+      if (socket && message.trim()) {
+        socket.emit('sendMessage', { channelId, message, user: { _id: user?.user?._id, name: user?.user?.firstName + ' ' + user?.user?.lastName, brand: user?.user?.brand } });
+        setMessage('');
+      }
     } catch (error) {
       toastConfig.setToastConfig(error);
     }
   };
+
+  useEffect(() => {
+    if (socket) {
+      socket.emit('joinChannel', channelId);
+
+      socket.on('receiveMessage', (newMessage) => {
+        console.log("hello");
+        console.log(newMessage);
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+      });
+
+      return () => {
+        socket.emit('leaveChannel', channelId);
+        socket.off('receiveMessage');
+      };
+    }
+  }, [socket])
 
 
   useEffect(() => {
