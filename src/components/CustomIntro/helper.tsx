13a97023c191f37d81506543b182721c382f25f4
harshit <harshit@vebholic.com>
2024-07-23 12:49:52 +0530

    feat: walkme in FSP

--- src/components/CustomIntro/helper.tsx
+++ src/components/CustomIntro/helper.tsx
@@ -73,10 +73,10 @@ export const injectFormFields = (data, fields) => {
 };
 
 export function generateFormFieldSteps(fields: any[], ignoreField?: string[]) {
-  let fieldsStpes: StepDefination[] = [];
+  let fieldsSteps: StepDefination[] = [];
   fields?.forEach((e) => {
     if (e?.fieldData?.required && !ignoreField?.includes(e?.fieldData?.fieldName) && !e?.fieldData?.isDefaultValue && !e?.fieldData?.isUneditable) {
-      fieldsStpes.push({
+      fieldsSteps.push({
         title: `Select ${e?.fieldData?.fieldLabel}`,
         target: `#field-${e?.fieldData?.fieldLabel?.toLowerCase()?.split(' ').join('-')}`,
         content: '',
@@ -85,14 +85,14 @@ export function generateFormFieldSteps(fields: any[], ignoreField?: string[]) {
       });
     }
   });
-  return fieldsStpes;
+  return fieldsSteps;
 }
 
 export function generateStepsFormfieldData(fields: any[], ignoreField?: string[]) {
-  let fieldsStpes: StepDefination[] = [];
+  let fieldsSteps: StepDefination[] = [];
   fields?.forEach((e) => {
     if (e?.required && !ignoreField?.includes(e?.fieldName) && !e?.isDefaultValue && !e?.isUneditable) {
-      fieldsStpes.push({
+      fieldsSteps.push({
         title: `Select ${e?.fieldLabel}`,
         target: `#field-${e?.fieldLabel?.toLowerCase()?.split(' ').join('-')}`,
         content: '',
@@ -101,7 +101,7 @@ export function generateStepsFormfieldData(fields: any[], ignoreField?: string[]
       });
     }
   });
-  return fieldsStpes;
+  return fieldsSteps;
 }
 
 export function createAddItemStepdata(route: { title: string; path: string }, fields: any[]) {
