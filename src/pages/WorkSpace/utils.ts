20a037575778bccdda0fe9bdebb6d47a40b7c378
dhrubobarman <dhrubobarman847@gmail.com>
2024-07-31 12:39:44 +0530

    fix: work space thread

--- src/pages/WorkSpace/utils.ts
+++ src/pages/WorkSpace/utils.ts
@@ -1,6 +1,6 @@
 import moment from 'moment';
 
-export function formatDateWithTodayYestarday(date: Date | string, options?: { onlyMonths: boolean; dateFormat: string }) {
+export function formatDateWithTodayYestarday(date: Date | string, options?: { onlyMonths?: boolean; dateFormat?: string }) {
   const { onlyMonths, dateFormat } = options || { onlyMonths: false };
   const now = moment();
   const inputDate = moment(date, dateFormat);
@@ -16,9 +16,9 @@ export function formatDateWithTodayYestarday(date: Date | string, options?: { on
   }
 
   if (inputDate.isSame(now, 'day')) {
-    return `today at ${inputDate.format('h:mm A')}`;
+    return `Today at ${inputDate.format('h:mm A')}`;
   } else if (inputDate.isSame(now.clone().subtract(1, 'day'), 'day')) {
-    return `yesterday at ${inputDate.format('h:mm A')}`;
+    return `Yesterday at ${inputDate.format('h:mm A')}`;
   } else {
     return inputDate.format('MMM Do [at] h:mm A');
   }
