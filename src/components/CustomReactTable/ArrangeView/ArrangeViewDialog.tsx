a7491f3a653a64dbc152c613ba86c63dd67751bf
dhrubobarman <dhrubobarman847@gmail.com>
2024-08-02 10:56:13 +0530

    feat: userGridview

--- src/components/CustomReactTable/ArrangeView/ArrangeViewDialog.tsx
+++ src/components/CustomReactTable/ArrangeView/ArrangeViewDialog.tsx
@@ -3,6 +3,7 @@ import {
   Box,
   Button,
   Dialog,
+  IconButton,
   List,
   ListItem,
   ListItemIcon,
@@ -17,20 +18,26 @@ import {
   makeStyles,
   useMediaQuery
 } from '@material-ui/core';
-import { DragHandle } from '@material-ui/icons';
+import { Delete, DragHandle, Edit, Save } from '@material-ui/icons';
 import update from 'immutability-helper';
 import { startCase } from 'lodash';
-import React, { Dispatch, useEffect, useRef, useState } from 'react';
+import React, { Dispatch, useContext, useEffect, useRef, useState } from 'react';
 import { isMobile, isTablet } from 'react-device-detect';
 import CustomDialogContent from '../../CustomDialog/CustomDialogContent';
 import CustomDialogFooter from '../../CustomDialog/CustomDialogFooter';
 import CustomDialogHeader from '../../CustomDialog/CustomDialogHeader';
 import { TActios, TInitialState } from '../hooks/useTableReducer';
+import ConfirmationDialog from '../../Helpers/ConfirmationDialog';
 
 import { DndContext, DragEndEvent, DragOverlay, DragStartEvent } from '@dnd-kit/core';
 import { restrictToVerticalAxis } from '@dnd-kit/modifiers';
 import { SortableContext, useSortable } from '@dnd-kit/sortable';
 import { useDndSensors } from 'src/hooks';
+import { ThemeButton } from 'src/components/Helpers/Buttons';
+import SaveEditArrangeView, { FormSchema } from 'src/components/CustomReactTable/ArrangeView/SaveEditArrangeView';
+import axiosInstance from 'src/axios/axiosInstance';
+import { CustomToastContext } from 'src/StateProvider/CustomToastContext/CustomToastContext';
+import { Autocomplete } from '@material-ui/lab';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -53,7 +60,25 @@ interface ArrangeColumnsProps {
   stickycolumns: { left: any[]; right: any[]; stickyColumns: any[] };
 }
 
+export type SavedData = {
+  _id: string;
+  name: string;
+  access: string;
+  key: string;
+  hide: string[];
+  order: string[];
+  brand: string;
+  user: string;
+  createdBy: CreatedBy;
+};
+
+export type CreatedBy = {
+  user: string;
+  date: Date;
+};
+
 const ArrangeViewDialog = (props: ArrangeColumnsProps) => {
+  const toastConfig = useContext(CustomToastContext);
   const isMobileView = useMediaQuery('(max-width:768px)');
   const { onClose, columns = [], updateGridHiddenColumns, renderedFrom, dispatch, state, stickycolumns } = props;
   const { visibleColumns, columnOrder } = state;
@@ -65,6 +90,11 @@ const ArrangeViewDialog = (props: ArrangeColumnsProps) => {
   const [searchVal, setSearchVal] = React.useState('');
   const [stateVisibleColumns, setStateVisibleColumns] = useState(visibleColumns);
   const [activeItem, setActiveItem] = useState(null);
+
+  const [isSaveDialogOpen, setIsSaveDialogOpen] = useState<{ open: boolean; defaultValue: (FormSchema & { _id: string }) | null }>(null);
+  const [savedData, setSavedData] = useState<SavedData[]>(null);
+  const [selectedSavedData, setSelectedSavedData] = useState<SavedData>(null);
+  const [isSaveViewDeleteConfirm, setIsSaveViewDeleteConfirm] = useState<{ ids: string[]; open: boolean }>({ open: false, ids: null });
 
   const [allChecked, setAllChecked] = React.useState(true);
   const [isMinimized, setMinimized] = React.useState(true);
@@ -120,26 +150,41 @@ const ArrangeViewDialog = (props: ArrangeColumnsProps) => {
     onClose();
   };
 
-  const handleSaveChange = () => {
-    const columnOrderToStore = [];
-    for (const col of sortedColumns) {
-      if (col.accessor === 'qtyDisplay') {
-        columnOrderToStore.push('qtyDisplay');
-        columnOrderToStore.push('qty');
-        continue;
-      }
-      columnOrderToStore.push(col.id);
+  const handleApplyChange = (order?: string[], hidden?: string[]) => {
+    let columnOrderToStore, hidedColumns;
+    if (order && hidden) {
+      columnOrderToStore = order;
+      hidedColumns = hidden;
+    } else {
+      const data = getOrderAndHiddenColumns();
+      columnOrderToStore = data.order;
+      hidedColumns = data.hidden;
     }
     const columnOrder = [...stickycolumns.left, ...columnOrderToStore, ...stickycolumns.right];
     dispatch({ type: 'setVisibleColumns', visibleColumns: stateVisibleColumns });
     dispatch({ type: 'setColumnOrder', columnOrder: columnOrder });
 
     if (renderedFrom && renderedFrom !== '') {
-      const hidedColumns = Object.keys(stateVisibleColumns).filter((c) => !stateVisibleColumns[c]);
       updateGridHiddenColumns(hidedColumns, columnOrderToStore);
     }
 
     onClose();
+  };
+
+  const getOrderAndHiddenColumns = () => {
+    const order = [];
+    const hidden = Object.keys(stateVisibleColumns).filter((c) => !stateVisibleColumns[c]);
+
+    for (const col of sortedColumns) {
+      if (col.accessor === 'qtyDisplay') {
+        order.push('qtyDisplay');
+        order.push('qty');
+        continue;
+      }
+      order.push(col.id);
+    }
+
+    return { order, hidden };
   };
 
   const moveItem = (event: DragEndEvent) => {
@@ -180,6 +225,64 @@ const ArrangeViewDialog = (props: ArrangeColumnsProps) => {
 
   const sensors = useDndSensors();
 
+  const applySavedColumns = (data: SavedData) => {
+    if (!data) return;
+    setStateVisibleColumns((prev) => {
+      const temp = { ...prev };
+      Object.keys(temp).forEach((key) => {
+        temp[key] = !data.hide.includes(key);
+      });
+      setAllChecked(sortedColumns.every((col) => temp[col.id]));
+      return temp;
+    });
+    const tempSortedColumns = [...columns]
+      .filter((c) => !stickycolumns?.stickyColumns?.includes(c.id))
+      ?.sort((a: any, b: any) => {
+        return data.order?.indexOf(a.id) - data.order?.indexOf(b.id);
+      });
+
+    if (tempSortedColumns)
+      setSortedColumns(
+        tempSortedColumns.filter((column) => !['left', 'right']?.includes(column?.sticky) || !['selection', 'expander'].includes(column.accessor))
+      );
+  };
+
+  const getAllSavedViews = async () => {
+    try {
+      const {
+        data: { data }
+      } = await axiosInstance().get('/user/grid-view');
+      setSavedData(data.filter((d) => d.key === renderedFrom));
+    } catch (error) {
+      toastConfig.setToastConfig(error);
+    }
+  };
+
+  const deleteSavedView = async () => {
+    if (!isSaveViewDeleteConfirm?.ids) return;
+    try {
+      const {
+        data: { data }
+      } = await axiosInstance().put('/user/grid-view/remove', { ids: isSaveViewDeleteConfirm?.ids });
+      if (data) {
+        toastConfig.setToastConfig({
+          open: true,
+          type: 'success',
+          message: 'Deleted successfully'
+        });
+        setIsSaveViewDeleteConfirm({ open: false, ids: null });
+        setSelectedSavedData(null);
+        getAllSavedViews();
+      }
+    } catch (error) {
+      toastConfig.setToastConfig(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllSavedViews();
+  }, []);
+
   return (
     <Dialog open onClose={onClose} maxWidth="sm" fullWidth fullScreen={!isMinimized || (isMobile && !isTablet) || isMobileView}>
       <CustomDialogHeader
@@ -192,6 +295,38 @@ const ArrangeViewDialog = (props: ArrangeColumnsProps) => {
       />
 
       <CustomDialogContent>
+        <div className="my-2">
+          <Autocomplete
+            disabled={savedData?.length === 0 || !savedData}
+            value={selectedSavedData}
+            onChange={(event: any, newValue: SavedData) => {
+              setSelectedSavedData(newValue);
+              applySavedColumns(newValue);
+            }}
+            id="controllable-states-demo"
+            options={savedData || []}
+            fullWidth
+            size="small"
+            getOptionLabel={(option) => option?.name}
+            renderOption={(option) => (
+              <div className="flex w-full justify-between gap-2">
+                <Typography noWrap>{option.name}</Typography>
+                <div>
+                  <IconButton
+                    size="small"
+                    onClick={(e) => {
+                      e.currentTarget.disabled = true;
+                      setIsSaveViewDeleteConfirm({ open: true, ids: [option._id] });
+                    }}
+                  >
+                    <Delete fontSize="small" color="error" />
+                  </IconButton>
+                </div>
+              </div>
+            )}
+            renderInput={(params) => <TextField size="small" margin="none" {...params} label="Select saved view" variant="outlined" />}
+          />
+        </div>
         <List
           disablePadding
           subheader={
@@ -291,16 +426,67 @@ const ArrangeViewDialog = (props: ArrangeColumnsProps) => {
         </List>
       </CustomDialogContent>
       <CustomDialogFooter>
-        <Button variant="outlined" color="primary" onClick={onClose}>
-          Close
-        </Button>
-        <Button variant="outlined" color="primary" onClick={handleReset}>
-          Reset
-        </Button>
-        <Button variant="contained" color="primary" disableElevation disabled={false} onClick={handleSaveChange}>
-          Save changes
-        </Button>
+        <div className="flex w-full items-center justify-between gap-2">
+          <ThemeButton
+            borderColor="none"
+            color="primary"
+            iconForMobile={false}
+            onClick={() => {
+              setSavedData(null);
+              if (selectedSavedData) {
+                setSelectedSavedData(null);
+                setIsSaveDialogOpen({
+                  open: true,
+                  defaultValue: { access: selectedSavedData.access, name: selectedSavedData.name, setAsDefault: false, _id: selectedSavedData._id }
+                });
+              } else {
+                setIsSaveDialogOpen((prev) => ({ ...prev, open: true }));
+              }
+            }}
+          >
+            {selectedSavedData ? 'Update' : 'Save'}
+          </ThemeButton>
+          <div className="flex items-center gap-2 ">
+            <ThemeButton iconForMobile={false} onClick={onClose}>
+              Close
+            </ThemeButton>
+            <ThemeButton iconForMobile={false} onClick={handleReset}>
+              Reset
+            </ThemeButton>
+
+            <ThemeButton
+              iconForMobile={false}
+              borderColor="none"
+              color="primary"
+              disableElevation
+              disabled={false}
+              onClick={() => handleApplyChange()}
+            >
+              Apply
+            </ThemeButton>
+          </div>
+        </div>
       </CustomDialogFooter>
+      {isSaveDialogOpen?.open && (
+        <SaveEditArrangeView
+          onClose={() => setIsSaveDialogOpen(null)}
+          getAllSavedViews={getAllSavedViews}
+          renderedFrom={renderedFrom}
+          defaultValue={isSaveDialogOpen.defaultValue}
+          {...getOrderAndHiddenColumns()}
+        />
+      )}
+      {isSaveViewDeleteConfirm.open && (
+        <ConfirmationDialog
+          open={true}
+          message={`Are you sure you want to delete ?`}
+          onClose={() => setIsSaveViewDeleteConfirm({ open: false, ids: null })}
+          onOk={() => {
+            setSavedData(null);
+            deleteSavedView();
+          }}
+        />
+      )}
     </Dialog>
   );
 };
