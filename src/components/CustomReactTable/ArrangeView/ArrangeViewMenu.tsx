5cd8cb453c02dbebc1f13d686fd744bab56ff2e5
dhrubobarman <dhrubobarman847@gmail.com>
2024-08-05 16:23:29 +0530

    fix: arrange view layout issues

--- src/components/CustomReactTable/ArrangeView/ArrangeViewMenu.tsx
+++ src/components/CustomReactTable/ArrangeView/ArrangeViewMenu.tsx
@@ -191,7 +191,7 @@ const ArrangeViewMenu = ({ renderedFrom, dispatch, state, columns, hideSelection
                     <div className="head p-2 text-center [border-bottom:1px_solid_var(--common-border-color)]">
                       <h5 className="bold text-lg">Views</h5>
                     </div>
-                    <div className="content p-3 ">
+                    <div className="content max-h-[350px] overflow-auto p-3">
                       {!savedData ? (
                         <div className="relative flex min-h-[60px]">
                           <ImSpinner2 className="absolute inset-0 m-auto animate-spin" size={50} />
@@ -213,22 +213,29 @@ const ArrangeViewMenu = ({ renderedFrom, dispatch, state, columns, hideSelection
                                         applyView(d);
                                       }}
                                     >
-                                      <div className="flex w-full  justify-between gap-2">
-                                        <span className="flex items-center gap-2">
-                                          {d.name}
+                                      <div className="flex  w-full justify-between gap-2">
+                                        <span className=" flex items-center gap-1">
+                                          <span className="line-clamp-1 ">{d.name}</span>
                                           {defaultView._id === d._id && (
-                                            <HtmlTooltip title="This view is set as the default view">
-                                              <FaStar size={10} className="text-[var(--new-theme-color)]" />
-                                            </HtmlTooltip>
+                                            <>
+                                              <HtmlTooltip title="Default view">
+                                                <FaStar size={10} className="text-[var(--new-theme-color)]" />
+                                              </HtmlTooltip>
+                                              {/* <span className="block flex-shrink-0 text-[10px] text-gray-400">(default view)</span> */}
+                                            </>
                                           )}
                                         </span>
-                                        <div className="span">
-                                          <IconButton size={'small'} onClick={(e) => openEditModal(e, d)}>
-                                            <Edit fontSize="small" />
-                                          </IconButton>
-                                          <IconButton size={'small'} onClick={(e) => openDeleteConfirmationModal(e, d)}>
-                                            <Delete fontSize="small" color="error" />
-                                          </IconButton>
+                                        <div className="flex gap-2">
+                                          <HtmlTooltip title={'Edit'}>
+                                            <IconButton size={'small'} onClick={(e) => openEditModal(e, d)}>
+                                              <Edit fontSize="small" />
+                                            </IconButton>
+                                          </HtmlTooltip>
+                                          <HtmlTooltip title={'Delete'}>
+                                            <IconButton size={'small'} onClick={(e) => openDeleteConfirmationModal(e, d)}>
+                                              <Delete fontSize="small" color="error" />
+                                            </IconButton>
+                                          </HtmlTooltip>
                                         </div>
                                       </div>
                                     </ListItem>
@@ -237,8 +244,8 @@ const ArrangeViewMenu = ({ renderedFrom, dispatch, state, columns, hideSelection
                               })
                             ) : (
                               <>
-                                <p className="select-none text-center text-3xl font-semibold text-gray-300 dark:text-gray-700">No views found.</p>
-                                <p className="text-md select-none text-center text-gray-300 dark:text-gray-700">Please create a view first.</p>
+                                <p className="text-md select-none text-center text-gray-400 dark:text-gray-700">No views found.</p>
+                                <p className="select-none text-center text-sm text-gray-400 dark:text-gray-700">Please create a view first.</p>
                               </>
                             )}
                           </List>
@@ -246,6 +253,9 @@ const ArrangeViewMenu = ({ renderedFrom, dispatch, state, columns, hideSelection
                       )}
                     </div>
                     <div className="footer mt-2 flex justify-end gap-2 p-2 [border-top:1px_solid_var(--common-border-color)]">
+                      <ThemeButton iconForMobile={false} onClick={() => applyViewInTable([], [])}>
+                        Reset
+                      </ThemeButton>
                       <ThemeButton borderColor="none" color="primary" iconForMobile={false} onClick={openCreateEditModal}>
                         Create view
                       </ThemeButton>
