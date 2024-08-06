2bf93f001e5d48a1f87dd2c004a296078bdea7ba
dhrubobarman <dhrubobarman847@gmail.com>
2024-08-06 10:34:05 +0530

    fix: quotes arrange view

--- src/components/CustomReactTable/index.tsx
+++ src/components/CustomReactTable/index.tsx
@@ -2,6 +2,7 @@ import { useTableReducer } from './hooks/useTableReducer';
 import type { TInitialState, TActios } from './hooks/useTableReducer';
 import CustomReactTable from './CustomReactTable';
 import Pagination from './TableComponents/Pagination';
+import { useGridMetaData } from './ArrangeView/utils';
 import useColumns, {
   headerName,
   detailPagePath,
@@ -11,6 +12,17 @@ import useColumns, {
   staticColumns
 } from './hooks/useColumnsReactTable';
 export default CustomReactTable;
-export { useTableReducer, Pagination, useColumns, headerName, detailPagePath, getStaticFields, getCompletedByField, getSortedColumns, staticColumns };
+export {
+  useTableReducer,
+  Pagination,
+  useColumns,
+  headerName,
+  detailPagePath,
+  getStaticFields,
+  getCompletedByField,
+  getSortedColumns,
+  staticColumns,
+  useGridMetaData
+};
 export type { TInitialState, TActios };
 export * from './utils';
