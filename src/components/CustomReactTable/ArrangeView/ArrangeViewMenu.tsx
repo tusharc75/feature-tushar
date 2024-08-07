4d792cb5f2cb4314e160b55880dee1cb22e4b505
dhrubobarman <dhrubobarman847@gmail.com>
2024-08-07 17:19:38 +0530

    fix: fix

--- src/components/CustomReactTable/ArrangeView/ArrangeViewMenu.tsx
+++ src/components/CustomReactTable/ArrangeView/ArrangeViewMenu.tsx
@@ -77,7 +77,6 @@ const ArrangeViewMenu = ({ renderedFrom, dispatch, state, columns, hideSelection
       const columnsWithoutSticky = columns.filter((d) => !stickycolumns.stickyColumns.includes(d.id)).map((c) => c.id);
       columnOrder = [...stickycolumns.left, ...columnsWithoutSticky, ...stickycolumns.right];
     }
-    console.log(columnOrder);
     dispatch({ type: 'setVisibleColumns', visibleColumns: columnHiddenStateData });
     dispatch({ type: 'setColumnOrder', columnOrder: columnOrder });
 
