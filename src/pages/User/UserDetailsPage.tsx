480d9040ec8af1435073fef241c45fe5d7654e41
Vivek Sharma <viveksh.701700@gmail.com>
2024-08-13 16:18:20 +0530

    fix: fix

--- src/pages/User/UserDetailsPage.tsx
+++ src/pages/User/UserDetailsPage.tsx
@@ -68,6 +68,7 @@ import LeadAccordionInUserDetailPage from './LeadAccordionInUserDetailPage';
 import ManageUserDialog from './ManageUserDialog';
 import OpportunityAccordionInUserDetail from './OpportunityAccordionInUserDetail';
 import UserSession from './UserSession';
+import { isMobile, isTablet } from "react-device-detect";
 
 const useStyles = makeStyles((theme) => ({
   dataValue: {
@@ -730,6 +731,7 @@ const UserDetailsPage = () => {
       {showAssignEntityDialog && (
         <Dialog
           fullWidth
+          fullScreen={isMobile || isTablet}
           maxWidth="xs"
           open={showAssignEntityDialog}
           onClose={entityDialogClose}
