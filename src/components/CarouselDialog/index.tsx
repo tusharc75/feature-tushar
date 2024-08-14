8e77fbe16920e3e6667e4b9c23209da5d4eed2bf
dhrubobarman <dhrubobarman847@gmail.com>
2024-08-14 16:33:01 +0530

    fix: image failed to load and loading handler

--- src/components/CarouselDialog/index.tsx
+++ src/components/CarouselDialog/index.tsx
@@ -3,6 +3,9 @@ import Carousel from 'react-material-ui-carousel';
 
 import CustomDialogContent from '../CustomDialog/CustomDialogContent';
 import CustomDialogHeader from '../CustomDialog/CustomDialogHeader';
+import { cn } from 'src/constants/helpers';
+import { SyntheticEvent } from 'react';
+import imageLoadingFailed from 'src/assets/imageLoadingFailed.png';
 
 const useStyles = makeStyles(() => ({
   imageContainer: {
@@ -25,6 +28,16 @@ type CarouselDialogProps = {
 const CarouselDialog = ({ images, index, close, title = 'Images' }: CarouselDialogProps) => {
   const classes = useStyles();
 
+  const handleOnload = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    const target = e.target as HTMLImageElement;
+    target.classList.remove('bg-gray-300', 'min-h-[400px]', 'dark:bg-gray-800', 'rounded-md', 'max-h-[200px]', 'max-w-[200px]');
+  };
+  const handleOnError = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    const target = e.target as HTMLImageElement;
+    target.src = imageLoadingFailed;
+    target.classList.add('max-h-[200px]', 'max-w-[200px]', 'rounded-md');
+  };
+
   return (
     <Dialog maxWidth="md" fullWidth open={true} onClose={close}>
       <CustomDialogHeader title={title} onClose={close} showRequiredLabel={false} />
@@ -44,7 +57,14 @@ const CarouselDialog = ({ images, index, close, title = 'Images' }: CarouselDial
         >
           {images.map((item: any, i) => (
             <div key={i} className={classes.imageContainer}>
-              <img className={classes.img} src={item} alt={''} />
+              <img
+                onError={handleOnError}
+                onLoad={handleOnload}
+                className={cn(classes.img, 'min-h-[300px] rounded-md bg-gray-300 dark:bg-gray-800')}
+                src={item}
+                alt={''}
+                loading="lazy"
+              />
             </div>
           ))}
         </Carousel>
