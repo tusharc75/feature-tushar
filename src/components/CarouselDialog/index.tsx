da590944128dad6289824e131acc4eff82c1e8c1
dhrubobarman <dhrubobarman847@gmail.com>
2024-08-14 17:34:16 +0530

    fix: jira EP-3087

--- src/components/CarouselDialog/index.tsx
+++ src/components/CarouselDialog/index.tsx
@@ -27,15 +27,16 @@ type CarouselDialogProps = {
 
 const CarouselDialog = ({ images, index, close, title = 'Images' }: CarouselDialogProps) => {
   const classes = useStyles();
-
+  const classList = ['min-h-[300px]', 'bg-gray-300', 'dark:bg-gray-800'];
   const handleOnload = (e: SyntheticEvent<HTMLImageElement, Event>) => {
     const target = e.target as HTMLImageElement;
-    target.classList.remove('bg-gray-300', 'min-h-[300px]', 'dark:bg-gray-800', 'rounded-md', '[filter:blur(6px)]', 'max-h-[200px]', 'max-w-[200px]');
+    target.classList.remove(...classList);
   };
+
   const handleOnError = (e: SyntheticEvent<HTMLImageElement, Event>) => {
     const target = e.target as HTMLImageElement;
     target.src = imageLoadingFailed;
-    target.classList.remove('bg-gray-300', 'min-h-[300px]', 'dark:bg-gray-800', 'rounded-md', '[filter:blur(6px)]');
+    target.classList.remove(...classList);
     target.classList.add('max-h-[200px]', 'max-w-[200px]', 'rounded-md');
     target.width = 300;
     target.height = 300;
@@ -60,14 +61,7 @@ const CarouselDialog = ({ images, index, close, title = 'Images' }: CarouselDial
         >
           {images.map((item: any, i) => (
             <div key={i} className={classes.imageContainer}>
-              <img
-                onError={handleOnError}
-                onLoad={handleOnload}
-                className={cn(classes.img, 'min-h-[300px] bg-gray-300 dark:bg-gray-800')}
-                src={item}
-                alt={''}
-                loading="lazy"
-              />
+              <img onError={handleOnError} onLoad={handleOnload} className={cn(classes.img, ...classList)} src={item} alt={''} loading="lazy" />
             </div>
           ))}
         </Carousel>
