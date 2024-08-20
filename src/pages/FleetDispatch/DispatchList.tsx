8524ca9fa5fd07daff38a08ada859e59ea6a5590
dhrubobarman <dhrubobarman847@gmail.com>
2024-08-20 20:06:18 +0530

    fix: fleet dispatch

--- src/pages/FleetDispatch/DispatchList.tsx
+++ src/pages/FleetDispatch/DispatchList.tsx
@@ -1,13 +1,13 @@
 import { SortableContext } from '@dnd-kit/sortable';
 import DispatchCard from './DispatchCard';
 
-const DispatchList = ({ activity, cardType, handleDispatch }) => {
+const DispatchList = ({ activity, cardType }) => {
   if (activity.length === 0) return null;
   return (
     <>
-      <SortableContext items={activity.map((d) => d._id)}>
+      <SortableContext items={activity.map((d) => d._id)} strategy={() => null}>
         {activity.map((element, index) => (
-          <DispatchCard data={element} key={element?._id} id={element?._id} index={index} cardType={cardType} handleDispatch={handleDispatch} />
+          <DispatchCard data={element} key={element?._id} id={element?._id} index={index} cardType={cardType} />
         ))}
       </SortableContext>
     </>
