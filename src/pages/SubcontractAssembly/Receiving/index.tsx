cdd94deca6bb8bc64f916d753a2d1c7a1934647a
Rishabhsharawat15 <rishabhsharawat150@gmail.com>
2024-08-21 17:03:03 +0530

    feat: fix 3107

--- src/pages/SubcontractAssembly/Receiving/index.tsx
+++ src/pages/SubcontractAssembly/Receiving/index.tsx
@@ -38,7 +38,7 @@ const Receiving = ({ subcontractAssemblyData, stepFullScreen, fetchParentData, a
   const [columns, setColumns] = useState(null);
   const [costDialog, setCostDialog] = useState({ open: false, _id: null });
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [viewCost, setViewCost] = useState({ open: false, data: null });
+  const [viewCost, setViewCost] = useState({ open: false, data: null ,title: null });
   const [historyDialog, setHistoryDialog] = useState({ open: false, _id: '', product: '', productName: '' });
   const [showConformationReject, setShowConformationReject] = useState({ open: false, _id: null });
 
@@ -118,6 +118,7 @@ const Receiving = ({ subcontractAssemblyData, stepFullScreen, fetchParentData, a
         disableSortBy: true,
         canDrag: false,
         Cell: ({ row }) => {
+          console.log(row.original.productName);
           return (
             <div className="flex items-center gap-2">
               {row?.original?.receivedQty <= 0 && (
@@ -176,7 +177,7 @@ const Receiving = ({ subcontractAssemblyData, stepFullScreen, fetchParentData, a
                       size="small"
                       aria-label="cost"
                       onClick={() => {
-                        setViewCost({ open: true, data: row?.original?.cost });
+                        setViewCost({ open: true, data: row?.original?.cost ,title: row?.original?.productName});
                       }}
                     >
                       <Visibility fontSize="small" color={'primary'} />
@@ -313,8 +314,9 @@ const Receiving = ({ subcontractAssemblyData, stepFullScreen, fetchParentData, a
       {viewCost.open && (
         <ViewCost
           data={viewCost.data}
+          title = {viewCost.title}
           onClose={() => {
-            setViewCost({ open: false, data: null });
+            setViewCost({ open: false, data: null ,title : null});
           }}
           subcontractAssemblyData={subcontractAssemblyData}
         />
