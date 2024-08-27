5436d061b61cb4adf098cc65a86b9d1efd570684
Romit Sadaria <romit.sadaria@gmail.com>
2024-08-27 10:38:57 +0530

    fix: bug fix

--- src/pages/DynamicForm/index.tsx
+++ src/pages/DynamicForm/index.tsx
@@ -161,25 +161,21 @@ const DynamicForm = () => {
   const fetchData = async (cancelTokenSource?: CancelTokenSource) => {
     dispatch({ type: 'loading', loading: true });
     const queryString = getQueryString();
-    console.log(queryString);
-
-    axiosInstance()
-      .get(`dynamic-form/${queryString}`, {
-        headers: {
-          Resource: resource
-        },
-        cancelToken: cancelTokenSource?.token
-      })
-      .then(({ data: { data, count } }) => {
-        let rows = data.map((u) => {
-          let finalObject = prepareDataForGrid(u, user);
-          finalObject['isChecked'] = selectedRecords?.some((s) => s._id === u._id);
-          finalObject['allowedToEdit'] = permissions[renderedFrom]?.isUpdate;
-          finalObject['canDelete'] = permissions[renderedFrom]?.isDelete;
-          return finalObject;
-        });
-        dispatch({ type: 'initialize', data: rows, count: count });
-      })
+    axiosInstance().get(`dynamic-form/${queryString}`, {
+      headers: {
+        Resource: resource
+      },
+      cancelToken: cancelTokenSource?.token
+    }).then(({ data: { data, count } }) => {
+      let rows = data.map((u) => {
+        let finalObject = prepareDataForGrid(u, user);
+        finalObject['isChecked'] = selectedRecords?.some((s) => s._id === u._id);
+        finalObject['allowedToEdit'] = permissions[renderedFrom]?.isUpdate;
+        finalObject['canDelete'] = permissions[renderedFrom]?.isDelete;
+        return finalObject;
+      });
+      dispatch({ type: 'initialize', data: rows, count: count });
+    })
       .catch((error) => {
         toastConfig.setToastConfig(error);
       })
