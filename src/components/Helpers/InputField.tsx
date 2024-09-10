54cd299881264cfac6560bb47089215cbf56f1c1
Romit S <92029530+romit55@users.noreply.github.com>
2024-09-10 12:59:35 +0530

    Revert "fix: fix"

--- src/components/Helpers/InputField.tsx
+++ src/components/Helpers/InputField.tsx
@@ -18,7 +18,6 @@ const InputField = (props) => {
     resource = null,
     referenceId = null,
     collaborateTools = false,
-    onChange=null,
     ...rest
   } = props;
 
@@ -104,23 +103,19 @@ const InputField = (props) => {
                             required={field.required}
                             isTooltip={field.isTooltip}
                             tooltipMessage={field.tooltipMessage}
-                            onChange={(e, val) => {
-                              if (onChange) {
-                                onChange(field, e, val);
-                              } else {
-                                field.fieldName === 'currency'
-                                  ? (e, val) => {
-                                    if (val && val.currencyCode) {
-                                      setFieldValue(field.fieldName, val.currencyCode);
-                                      setCurrencySymbol(val.symbolNative);
-                                    } else {
-                                      setFieldValue(field.fieldName, '');
-                                      setCurrencySymbol(null);
-                                    }
+                            onChange={
+                              field.fieldName === 'currency'
+                                ? (e, val) => {
+                                  if (val && val.currencyCode) {
+                                    setFieldValue(field.fieldName, val.currencyCode);
+                                    setCurrencySymbol(val.symbolNative);
+                                  } else {
+                                    setFieldValue(field.fieldName, '');
+                                    setCurrencySymbol(null);
                                   }
-                                  : null;
-                              }
-                            }}
+                                }
+                                : null
+                            }
                             imageOrFileUploadCompletePercentage={
                               ['imageUpload', 'fileUpload'].some((s) => s === field.type)
                                 ? (completePercentage) => {
