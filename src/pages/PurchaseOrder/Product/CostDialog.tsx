fd8373cf1b130cd9d1be32a4e511d26677728207
Rishabhsharawat15 <rishabhsharawat150@gmail.com>
2024-09-17 12:51:03 +0530

    feat: ficx

--- src/pages/PurchaseOrder/Product/CostDialog.tsx
+++ src/pages/PurchaseOrder/Product/CostDialog.tsx
@@ -9,13 +9,17 @@ import { CustomDialogTransition } from '../../../constants/helpers';
 import { Formik, Form } from 'formik';
 import CommonSkeleton from '../../../components/Helpers/CommonSkeleton';
 import CustomButton from '../../../components/Helpers/CustomButton';
+import { FaDiceOne } from 'react-icons/fa';
+import FormTypes from '../../../components/Helpers/FormTypes';
+import { uniq, map, orderBy, isEqual } from 'lodash';
 import { autoCalculateSpecificFields } from 'src/constants/formulaUtility';
+import { fetchTaxRate } from './helper';
 import { fetch_child_resource_fields } from 'src/components/ChildResourceField';
 import { bulkUpdate } from 'src/components/RentalManagment/helper';
-import InputField from 'src/components/Helpers/InputField';
 
 const CostDialog = ({ onClose, purchaseOrderData, handleAddCost, handleUpdateCost, costData, bulkEdit, showSaveAndNext, loadingEdit }) => {
   const [initialData, setInitialData] = useState({ fields: [], values: {} });
+  const [fields, setFields] = useState([]);
   const [fullScreen, setFullScreen] = useState(isMobile || isTablet);
   const [allFields, setAllFields] = useState([]);
   const [saveAndNext, setSaveAndNext] = useState(false);
@@ -52,8 +56,28 @@ const CostDialog = ({ onClose, purchaseOrderData, handleAddCost, handleUpdateCos
         });
       }
     }
+    EvaluteproductFields(data);
   };
 
+  const EvaluteproductFields = async (fields) => {
+    const sections = uniq(map(fields, 'sectionName'));
+    const customData = sections.map((name) => {
+      let sectionFields = fields.filter((field) => field.sectionName === name);
+      sectionFields = orderBy(sectionFields, 'order', 'asc');
+      return { name, sectionFields };
+    });
+
+    if (purchaseOrderData?.taxCode) {
+      const taxCodeOptions = await fetchTaxRate(purchaseOrderData?.taxCode?.optionValue);
+      fields?.forEach((e: any) => {
+        if (e?.fieldName === 'taxCode') {
+          e.option = taxCodeOptions;
+        }
+      });
+    }
+
+    setFields(customData);
+  };
 
   const handleSubmit = (values) => {
     let returnData = [];
@@ -105,31 +129,86 @@ const CostDialog = ({ onClose, purchaseOrderData, handleAddCost, handleUpdateCos
               ></CustomDialogHeader>
               <CustomDialogContent>
                 <Form autoComplete="off" autoCorrect="off" noValidate>
-                <InputField
-                    errors={errors}
-                    values={values}
-                    setFieldValue={(name, value) => {
-                      setFieldValue(name, value);
-                      if (name === 'taxCode') {
-                        const taxCode = initialData?.fields?.find((e) => e?.fieldName === 'taxCode')?.option.find((d) => d.optionValue === value);
-                        setFieldValue('taxPercentage', taxCode?.taxRate || 0);
-                        const result = autoCalculateSpecificFields(
-                          { ['taxPercentage']: taxCode?.taxRate || 0 },
-                          values,
-                          initialData.fields
-                        );
-                        if (Object.keys(result).length >= 1) {
-                          for (var x in result) {
-                            setFieldValue(x, result[x]);
-                          }
-                        }
-                      }
-                    }}
-                    touched={touched}
-                    fieldsData={initialData.fields}
-                    size="small"
-                    fullWidth
-                  />
+                  {fields &&
+                    fields.map((section, i) => (
+                      <div key={i}>
+                        <div className={'detail-box-content detail-product-box'}>
+                          <div className={'product-form-layout'}>
+                            <FaDiceOne size={16} color={'var(--white)'} style={{ marginRight: '5px' }} />
+                            <h2 className={`${'form-label-style'} ${'form-label-product'}`}>{section.name}</h2>
+                          </div>
+                        </div>
+                        <Box marginY={2}>
+                          <Grid spacing={3} container>
+                            {section.sectionFields &&
+                              section.sectionFields.map((field) =>
+                                field.type === 'converter' || field.type === 'currencyAmount' || field.isConverter ? (
+                                  <FormTypes
+                                    fields={initialData.fields}
+                                    fieldData={{ ...field, hideConverter: true }}
+                                    values={values}
+                                    errors={errors}
+                                    touched={touched}
+                                    label={field.fieldLabel}
+                                    name={field.fieldName}
+                                    type={field.type}
+                                    options={field.option}
+                                    setFieldValue={(name, value) => {
+                                      setFieldValue(name, value);
+                                    }}
+                                    required={field.required}
+                                    fullWidth
+                                    isTooltip={field.isTooltip}
+                                    tooltipMessage={field.tooltipMessage}
+                                    size="small"
+                                  />
+                                ) : (
+                                  <Grid key={field.fieldName} item xs={12} sm={6} md={6}>
+                                    <Box display="flex">
+                                      <Box flexGrow={1}>
+                                        <FormTypes
+                                          {...field}
+                                          fields={initialData.fields}
+                                          fieldData={field}
+                                          values={values}
+                                          errors={errors}
+                                          touched={touched}
+                                          label={field.fieldLabel}
+                                          name={field.fieldName}
+                                          type={field.type}
+                                          options={field.option}
+                                          setFieldValue={(name, value) => {
+                                            setFieldValue(name, value);
+                                            if (name === 'taxCode') {
+                                              const taxCode = field.option?.find((d) => d.optionValue === value);
+                                              setFieldValue('taxPercentage', taxCode?.taxRate || 0);
+                                              const result = autoCalculateSpecificFields(
+                                                { ['taxPercentage']: taxCode?.taxRate || 0 },
+                                                values,
+                                                initialData.fields
+                                              );
+                                              if (Object.keys(result).length >= 1) {
+                                                for (var x in result) {
+                                                  setFieldValue(x, result[x]);
+                                                }
+                                              }
+                                            }
+                                          }}
+                                          required={field.required}
+                                          fullWidth
+                                          isTooltip={field.isTooltip}
+                                          tooltipMessage={field.tooltipMessage}
+                                          size="small"
+                                        />
+                                      </Box>
+                                    </Box>
+                                  </Grid>
+                                )
+                              )}
+                          </Grid>
+                        </Box>
+                      </div>
+                    ))}
                 </Form>
               </CustomDialogContent>
               <CustomDialogFooter>
