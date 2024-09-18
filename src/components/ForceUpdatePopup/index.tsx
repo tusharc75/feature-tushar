2aae578760f03ac0b4338f5293bc2ecc02893695
dhrubobarman <dhrubobarman847@gmail.com>
2024-09-18 17:34:35 +0530

    fix: fix

--- src/components/ForceUpdatePopup/index.tsx
+++ src/components/ForceUpdatePopup/index.tsx
@@ -25,7 +25,7 @@ const ForceUpdatePopup = ({ open, onClose }: ForceUpdatePopupProps) => {
         <div className="relative isolate flex min-h-[530px] items-end overflow-hidden bg-cover">
           <img src={bgImage} alt="" className=" absolute inset-0 -z-[1] w-full bg-cover" />
           <div className=" mx-auto w-full max-w-[442px] px-8 py-8 text-center">
-            <h4 className="mb-3 text-[20px] leading-[1.5]">New update is available</h4>
+            <h4 className="mb-3 text-[20px] leading-[1.5] text-[var(--primary)]">New update is available</h4>
             <p className="mb-3 leading-[1.5] text-gray-500">
               The current version of this application is no longer supported. We apologize for any inconvenience we may have caused you.
             </p>
