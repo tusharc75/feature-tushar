78b23dbb93dcdccbabe60322fa7f924a930a1b19
dhrubobarman <dhrubobarman847@gmail.com>
2024-09-23 15:42:28 +0530

    fix: jira EP-3297

--- src/components/Activity/History/index.tsx
+++ src/components/Activity/History/index.tsx
@@ -5,7 +5,9 @@ import ResourceLogsGrid from 'src/pages/ResourceLogs/ResourceLogsGrid';
 import CustomDialogHeader from 'src/components/CustomDialog/CustomDialogHeader';
 import CustomDialogContent from 'src/components/CustomDialog/CustomDialogContent';
 export default function HistoryDialog(props) {
-  const { onClose, open, resourceId, resource } = props;
+  const { onClose, open, resourceId, resource, resourceLabel = '' } = props;
+
+  console.log(resourceId);
 
   return (
     <Dialog
@@ -19,7 +21,7 @@ export default function HistoryDialog(props) {
       open={open}
       onClose={onClose}
     >
-      <CustomDialogHeader title={`History`} showManimizeMaximize={false} showRequiredLabel={false} onClose={onClose} />
+      <CustomDialogHeader title={<>History: {resourceLabel}</>} showManimizeMaximize={false} showRequiredLabel={false} onClose={onClose} />
       <CustomDialogContent isFooterPresent={false}>
         <ResourceLogsGrid selectedResource={sidebarResource[resource]} selectedOption={resourceId} hideResourceField={true} />
       </CustomDialogContent>
