ca0f4678b40f361929afd5da555ccc8796db6ef3
dhrubobarman <dhrubobarman847@gmail.com>
2024-09-23 15:03:54 +0530

    fix: notification changes modal

--- src/components/Header/Notification.tsx
+++ src/components/Header/Notification.tsx
@@ -1,5 +1,5 @@
 import { Badge, Box, Button, IconButton, List, ListItem, Menu, MenuItem, Popover, Typography, useMediaQuery } from '@material-ui/core';
-import { ClearAll, DoneAllOutlined, Settings } from '@material-ui/icons';
+import { ClearAll, DoneAllOutlined, Info, Settings } from '@material-ui/icons';
 import { useContext, useMemo, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Accepted, Assigned, Rejected, Changed, Created } from 'src/assets/notificationIcons';
@@ -14,6 +14,8 @@ import styles from './Header.module.scss';
 
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import HtmlTooltip from '../CustomTooltipTitle';
+import ChangesDialog from 'src/pages/ResourceLogs/ChangesDialog';
+import { BsEye } from 'react-icons/bs';
 
 const Notification = () => {
   const toastConfig = useContext(CustomToastContext);
@@ -36,7 +38,7 @@ const Notification = () => {
 
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleNotificationClose = () => {
+  const handleCloseNotification = () => {
     setAnchorEl(null);
   };
   const notificationId = isNotificationOpen ? 'full-screen-notification' : undefined;
@@ -56,8 +58,8 @@ const Notification = () => {
     id === selectedEntity
       ? history.push(resourceId ? `${resourcePath}/${resourceId}` : resourcePath)
       : hasAccessToEntity(id)
-      ? handleEntityChange(id) && history.push(resourceId ? `${resourcePath}/${resourceId}` : resourcePath)
-      : '';
+        ? handleEntityChange(id) && history.push(resourceId ? `${resourcePath}/${resourceId}` : resourcePath)
+        : '';
 
   const getAllNotifications = (event) => {
     setAnchorEl(event.currentTarget);
@@ -132,7 +134,7 @@ const Notification = () => {
         });
     }
 
-    handleNotificationClose();
+    handleCloseNotification();
     const resourcePath = returnResourcePath(d?.resourceId, d?.resourcePath);
     if (d?.entity) {
       handleRedirect(d?.entity, d?.resourceId, resourcePath);
@@ -195,7 +197,7 @@ const Notification = () => {
         id={notificationId}
         open={isNotificationOpen}
         anchorEl={anchorEl}
-        onClose={handleNotificationClose}
+        onClose={handleCloseNotification}
         anchorOrigin={{
           vertical: 'bottom',
           horizontal: 'center'
@@ -222,6 +224,7 @@ export default Notification;
 const NotificationContent = ({ isLoading, handleMarkAllRead, handleClearAll, handleReadSingle, data }) => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [tab, setTab] = useState<'all' | 'unread'>('all');
+  const [changesDialogData, setChangesDialogData] = useState<{ changes: any[]; updatedBy: string }>(null);
 
   const tabSetter = (cTab: 'all' | 'unread') => {
     if (tab === cTab) return;
@@ -276,7 +279,7 @@ const NotificationContent = ({ isLoading, handleMarkAllRead, handleClearAll, han
   return (
     <>
       <div className={``}>
-        <div className="[border-bottom:1px_solid_var(--common-border-color)] flex justify-between items-center px-[20px] py-[10px]">
+        <div className="flex items-center justify-between px-[20px] py-[10px] [border-bottom:1px_solid_var(--common-border-color)]">
           <h6 className="text-[16px] font-semibold ">Notifications</h6>
           <HtmlTooltip
             title={isLoading || data?.unread?.length === 0 ? 'No Unread notification' : 'Mark all as read'}
@@ -297,14 +300,14 @@ const NotificationContent = ({ isLoading, handleMarkAllRead, handleClearAll, han
             </span>
           </HtmlTooltip>
         </div>
-        <div className={`flex items-center gap-1 justify-between px-[20px] py-[8px] [border-bottom:1px_solid_var(--common-border-color)]`}>
+        <div className={`flex items-center justify-between gap-1 px-[20px] py-[8px] [border-bottom:1px_solid_var(--common-border-color)]`}>
           <div className="tabs flex gap-[24px]">
             <Button onClick={() => tabSetter('all')} size={'small'} disabled={isLoading} className={`relative`}>
               <div className={`${tab === 'all' ? 'bg-[var(--primary)]' : 'bg-[transparent]'} ${otherClasses}`} />
               All{' '}
               <span
-                className={`text-[#D3E0FF] px-2 py-[1px] block rounded-[5px] ml-2 bg-[#2A3042] text-[12px] font-semibold ${
-                  isLoading || tab === 'all' ? 'grayscale dark:opacity-50 opacity-70' : ''
+                className={`ml-2 block rounded-[5px] bg-[#2A3042] px-2 py-[1px] text-[12px] font-semibold text-[#D3E0FF] ${
+                  isLoading || tab === 'all' ? 'opacity-70 grayscale dark:opacity-50' : ''
                 }`}
               >
                 {data.all.length || 0}
@@ -314,8 +317,8 @@ const NotificationContent = ({ isLoading, handleMarkAllRead, handleClearAll, han
               <div className={`${tab === 'unread' ? 'bg-[var(--primary)]' : 'bg-[transparent]'} ${otherClasses}`} />
               Unread{' '}
               <span
-                className={`text-[#2A3042] px-2 py-[1px] block rounded-[5px] ml-2 bg-[#D3E0FF] text-[12px] font-semibold ${
-                  isLoading || tab === 'unread' ? 'grayscale dark:opacity-50 opacity-70' : ''
+                className={`ml-2 block rounded-[5px] bg-[#D3E0FF] px-2 py-[1px] text-[12px] font-semibold text-[#2A3042] ${
+                  isLoading || tab === 'unread' ? 'opacity-70 grayscale dark:opacity-50' : ''
                 }`}
               >
                 {data.unread.length || 0}
@@ -326,7 +329,7 @@ const NotificationContent = ({ isLoading, handleMarkAllRead, handleClearAll, han
             <Settings />
           </IconButton>
         </div>
-        <div className={`min-h-[300px] max-h-[calc(100vh-200px)] overflow-y-auto`}>
+        <div className={`max-h-[calc(100vh-200px)] min-h-[300px] overflow-y-auto`}>
           {isLoading ? (
             <Typography className="m-3">Loading Notifications...</Typography>
           ) : notificationList.length === 0 ? (
@@ -339,25 +342,44 @@ const NotificationContent = ({ isLoading, handleMarkAllRead, handleClearAll, han
                   <ListItem
                     button
                     aria-label={d.title}
-                    className={`p-0 dark:[border-bottom:1px_solid_var(--common-border-color)_!important] [border-bottom:1px_solid_#F4F4F4_!important] `}
+                    className={`p-0 [border-bottom:1px_solid_#F4F4F4_!important] dark:[border-bottom:1px_solid_var(--common-border-color)_!important] `}
                     key={d._id}
                     onClick={() => {
                       handleReadSingle(d);
                     }}
                   >
-                    <div className="flex items-center md:gap-[17px] gap-[15px] p-[20px] w-full">
+                    <div className="flex w-full items-center gap-[15px] p-[20px] md:gap-[17px]">
                       <div className=" basis-[38px]">
-                        <div className="rounded-full bg-[var(--dark-secondary,#F4F4F4)] w-[38px] h-[38px] relative">
-                          <Icon className="absolute top-1/2 left-1/2 [transform:translate(-50%,-50%)]" />
+                        <div className="relative h-[38px] w-[38px] rounded-full bg-[var(--dark-secondary,#F4F4F4)]">
+                          <Icon className="absolute left-1/2 top-1/2 [transform:translate(-50%,-50%)]" />
                         </div>
                       </div>
                       <div className="flex-grow">
-                        <h4
-                          className="text-[12px] font-medium mb-[8px] [&>strong]:font-semibold dark:[&>strong]:font-bold leading-[22px] text-[#6B6F77] dark:text-gray-300 [&>strong]:text-[var(--primary-text)]"
-                          dangerouslySetInnerHTML={{ __html: boldMatchPattern(d.title) }}
-                        ></h4>
-                        <h5 className="text-[var(--dark-secondary-text,_#718496)] text-[11px] font-normal mb-[2px]">{d.description}</h5>
-                        <p className="text-[var(--dark-secondary-text,_#718496)] text-[11px] font-normal">{displayCardDate(d?.date)}</p>
+                        <div className="flex items-center">
+                          <div className="text">
+                            <h4
+                              className="mb-[8px] text-[12px] font-medium leading-[22px] text-[#6B6F77] dark:text-gray-300 [&>strong]:font-semibold [&>strong]:text-[var(--primary-text)] dark:[&>strong]:font-bold"
+                              dangerouslySetInnerHTML={{ __html: boldMatchPattern(d.title) }}
+                            ></h4>
+                            <h5 className="mb-[2px] text-[11px] font-normal text-[var(--dark-secondary-text,_#718496)]">{d.description}</h5>
+                            <p className="text-[11px] font-normal text-[var(--dark-secondary-text,_#718496)]">{displayCardDate(d?.date)}</p>
+                          </div>
+
+                          {Array.isArray(d.changes) && d.changes?.length > 0 && (
+                            <HtmlTooltip title={'See changes'}>
+                              <IconButton
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                  setChangesDialogData({ changes: d.changes, updatedBy: d.userId });
+                                }}
+                                size="small"
+                                style={{ padding: 5 }}
+                              >
+                                <Info fontSize="small" />
+                              </IconButton>
+                            </HtmlTooltip>
+                          )}
+                        </div>
                       </div>
                     </div>
                   </ListItem>
@@ -378,6 +400,15 @@ const NotificationContent = ({ isLoading, handleMarkAllRead, handleClearAll, han
           Clear all
         </MenuItem>
       </Menu>
+      {changesDialogData && (
+        <ChangesDialog
+          open={Boolean(changesDialogData)}
+          changes={changesDialogData.changes}
+          updatedBy={changesDialogData.updatedBy}
+          operations={[]}
+          onClose={() => setChangesDialogData(null)}
+        />
+      )}
     </>
   );
 };
