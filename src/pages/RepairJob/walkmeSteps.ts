cf2251e431fb4e79ed4e5f0a19049bd2f325b2f9
Vivek Sharma <viveksh.701700@gmail.com>
2024-10-01 18:20:14 +0530

    fix: fix

--- src/pages/RepairJob/walkmeSteps.ts
+++ src/pages/RepairJob/walkmeSteps.ts
@@ -44,7 +44,7 @@ export const generateAddExistingSerialisedAsset = (waitForStepInsertion: boolean
         content: 'Click here to add existing SerialisedAsset.',
       },
       {
-        target: `#Serialised-Asset-table-checkbox-0`,
+        target: `#serializedAssets_assign-table-checkbox-0`,
         title: 'Select a SerialisedAsset'
       },
       {
