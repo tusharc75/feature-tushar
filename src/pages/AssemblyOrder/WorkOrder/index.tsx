a839b5a3ee537f25dcc2eadab014155ce120ea7b
ABHIMANYU KUMAR <manupatel18112000@gmail.com>
2024-10-10 14:19:44 +0530

    fix bugs

--- src/pages/AssemblyOrder/WorkOrder/index.tsx
+++ src/pages/AssemblyOrder/WorkOrder/index.tsx
@@ -1,5 +1,5 @@
 import { Box, IconButton, MenuItem, Typography } from '@material-ui/core';
-import { Delete } from '@material-ui/icons';
+import { CheckCircle, Delete } from '@material-ui/icons';
 import { startCase } from 'lodash';
 import { useContext, useEffect, useState } from 'react';
 import { isMobile } from 'react-device-detect';
@@ -12,11 +12,12 @@ import CommonSkeleton from 'src/components/Helpers/CommonSkeleton';
 import NoDataCell from 'src/components/Helpers/NoDataCell';
 import routes from 'src/components/Helpers/Routes';
 import { DetailsPageHeader } from 'src/components/PageHeaders';
-import { CHILD_RESOURCE, MATERIAL_TYPE, WORK_ORDER_STATUS, workOrder } from 'src/constants/helpers';
+import { CHILD_RESOURCE, MATERIAL_SUB_TYPE, MATERIAL_TYPE, WORK_ORDER_STATUS, workOrder } from 'src/constants/helpers';
 import { CustomToastContext } from 'src/StateProvider/CustomToastContext/CustomToastContext';
 import { useData } from 'src/StateProvider/Provider';
 import SyncIcon from '@material-ui/icons/Sync';
 import ConfirmationDialog from 'src/components/Helpers/ConfirmationDialog';
+import { AutoCompleteIcon } from 'src/assets/svg/svgIcons';
 
 const WorkOrder = ({ renderedFrom, assemblyOrderData, setNextStep, stepFullScreen, allowedToEdit, setCurrentStep }) => {
   const {
@@ -32,6 +33,10 @@ const WorkOrder = ({ renderedFrom, assemblyOrderData, setNextStep, stepFullScree
   const [deleteData, setDeleteData] = useState(null);
   const [showConfirmBox, setShowConfirmBox] = useState(false);
   const [isDeleting, setDeleting] = useState(false);
+  const [material, setMaterial] = useState([]);
+  const [autoCompleteData, setAutoCompleteData] = useState(null);
+  const [completeConfirmBox, setCompleteConfirmBox] = useState(false);
+  const [isCompleting, setCompleting] = useState(false);
 
   const { generateColumns } = useColumns();
 
@@ -123,30 +128,39 @@ const WorkOrder = ({ renderedFrom, assemblyOrderData, setNextStep, stepFullScree
         Cell: ({ row }) => {
           return row.original['description'] ? <h5 className="text-truncate">{row.original.description}</h5> : <NoDataCell />;
         }
+      },
+      {
+        accessor: 'workOrder',
+        Header: 'Work Order',
+        width: 200,
+        Cell: ({ row }) =>
+          row.original.workOrder ? (
+            <div className="flex items-center gap-2">
+              <h5 className="text-truncate">{row.original?.workOrderNumber}</h5>
+              <IconButton
+                size="small"
+                onClick={() => {
+                  window.open(`${routes.workOrderDetail.path}/${row.original?.workOrderId}`);
+                }}
+              >
+                <FiExternalLink size={16} className="-mt-[2px] text-gray-500 dark:text-gray-300" />
+              </IconButton>
+            </div>
+          ) : (
+            <NoDataCell />
+          )
+      },
+      {
+        accessor: 'workOrderStatus',
+        Header: 'Status',
+        width: 200,
+        show: false,
+        Cell: ({ row }) => {
+          return row.original['workOrderStatus'] ? <h5 className="text-truncate">{row.original.workOrderStatus}</h5> : <NoDataCell />;
+        }
       }
     ];
     coloum = [...coloum, ...newColumns];
-    coloum.push({
-      accessor: 'workOrder',
-      Header: 'Work Order',
-      width: 200,
-      Cell: ({ row }) =>
-        row.original.workOrder ? (
-          <div className="flex items-center gap-2">
-            <h5 className="text-truncate">{row.original?.workOrder?.optionLabel}</h5>
-            <IconButton
-              size="small"
-              onClick={() => {
-                window.open(`${routes.workOrderDetail.path}/${row.original?.workOrder?.optionValue}`);
-              }}
-            >
-              <FiExternalLink size={16} className="-mt-[2px] text-gray-500 dark:text-gray-300" />
-            </IconButton>
-          </div>
-        ) : (
-          <NoDataCell />
-        )
-    });
     coloum.push({
       accessor: 'action',
       Header: 'Actions',
@@ -156,21 +170,43 @@ const WorkOrder = ({ renderedFrom, assemblyOrderData, setNextStep, stepFullScree
       Cell: ({ row }) => {
         return (
           <>
-            <HtmlTooltip title="Delete">
-              <span>
+            {row?.original?.type === MATERIAL_TYPE.product && !row?.original?.subType && (
+              <HtmlTooltip title="Auto Complete Work Order">
                 <IconButton
-                  disabled={row?.original?.canDelete ? false : true}
                   size="small"
                   aria-label="Details"
                   onClick={() => {
-                    setDeleteData([row.original]);
-                    setShowConfirmBox(true);
+                    setAutoCompleteData([row.original]);
+                    setCompleteConfirmBox(true);
                   }}
+                  disabled={row?.original?.canAutoCompleteWorkOrder ? false : true}
                 >
-                  <Delete fontSize="small" color={row?.original?.canDelete ? 'error' : 'disabled'} />
+                  {row.original['workOrderStatus'] === 'Completed' ? (
+                    <CheckCircle className="text-[var(--chip-color-completed)] [font-size:19px_!important] dark:text-green-400" />
+                  ) : (
+                    <AutoCompleteIcon size={18} />
+                  )}
                 </IconButton>
-              </span>
-            </HtmlTooltip>
+              </HtmlTooltip>
+            )}
+
+            {row?.original?.type != MATERIAL_TYPE.package && (
+              <HtmlTooltip title="Delete">
+                <span>
+                  <IconButton
+                    disabled={row?.original?.canDelete ? false : true}
+                    size="small"
+                    aria-label="Delete"
+                    onClick={() => {
+                      setDeleteData([row.original]);
+                      setShowConfirmBox(true);
+                    }}
+                  >
+                    <Delete fontSize="small" color={row?.original?.canDelete ? 'error' : 'disabled'} />
+                  </IconButton>
+                </span>
+              </HtmlTooltip>
+            )}
           </>
         );
       }
@@ -181,33 +217,99 @@ const WorkOrder = ({ renderedFrom, assemblyOrderData, setNextStep, stepFullScree
   const fetchData = async () => {
     dispatch({ type: 'loading', loading: true });
     setNextStep(false);
-    const {
-      data: { data, count }
-    } = await axiosInstance().get(`${routes.assemblyOrder.path}/material/${assemblyOrderData._id}`);
 
-    let rows = data?.material?.filter((e) => !!e.parentId && e?.type === MATERIAL_TYPE.product);
+    const {
+      data: { data }
+    } = await axiosInstance().get(`${routes.assemblyOrder.path}/work-order/${assemblyOrderData._id}`);
+
+    setMaterial(JSON.parse(JSON.stringify(data)));
+    let rows = data?.filter((e) => e.parentId === null);
 
     rows.forEach((parent, i) => {
-      delete parent?.parentId;
       parent.index = i + 1;
-      parent.detail = parent.productDetail?.productName || '';
-      parent.description = parent?.productDetail?.productDescription || '';
+      parent.detail = parent.packageDetail?.packageName || '';
+      parent.description = parent?.packageDetail?.packageDescription || '';
       parent.qtyDisplay = parent.qty;
-      parent.isValid = true;
       parent.canDelete = false;
-      if (parent?.workOrder?.status !== WORK_ORDER_STATUS.completed) {
-        parent.canDelete = true;
+      parent.subRows = generateNestedData(data, parent);
+    });
+
+    dispatch({ type: 'initialize', data: rows, count: rows?.length });
+    dispatch({ type: 'loading', loading: false });
+  };
+
+  const generateNestedData = (material, parent) => {
+    const subRows: any = material.filter((e) => e.parentId === parent._id);
+    subRows.forEach((_subRow, index) => {
+      _subRow.index = parent.index + '.' + `${index + 1}`;
+      _subRow.detail = _subRow.type === MATERIAL_TYPE.product ? _subRow.productDetail?.productName : '';
+      _subRow.description = _subRow.type === MATERIAL_TYPE.product ? _subRow?.productDetail?.productDescription : '';
+      _subRow.qty = _subRow.qty;
+      _subRow.workOrderId = _subRow.subType === MATERIAL_SUB_TYPE.bom ? parent?.workOrderId : _subRow?.workOrder?._id;
+      _subRow.workOrderNumber = parent?.type === MATERIAL_SUB_TYPE.bom ? parent?.workOrderNumber : _subRow?.workOrder?.workOrderNumber;
+      _subRow.workOrderStatus = _subRow?.workOrder?.status || '';
+      if (_subRow?.workOrder?.status === WORK_ORDER_STATUS.new) {
+        _subRow.canAutoCompleteWorkOrder = true;
+      }
+      _subRow.subRows = generateNestedData(material, _subRow);
+
+      _subRow.canDelete = false;
+      if (_subRow?.status !== WORK_ORDER_STATUS.completed) {
+        if (_subRow.type === MATERIAL_TYPE.product) {
+          _subRow.canDelete = _subRow.subRows.length === 0 ? true : false;
+        }
+        if (_subRow.subRows?.length && _subRow.subRows?.find((e) => !e?.canDelete)) {
+          _subRow.canDelete = false;
+        }
       }
     });
 
-    dispatch({ type: 'initialize', data: rows, count: count });
-    dispatch({ type: 'loading', loading: false });
+    return subRows;
   };
 
   const handleDelete = async () => {
     setDeleting(true);
+    if (
+      deleteData?.some(
+        (d) =>
+          d?.type === MATERIAL_TYPE.product &&
+          d?.subType === MATERIAL_SUB_TYPE.bom &&
+          material?.find((r) => r?._id === d?.parentId)?.type === MATERIAL_TYPE.product
+      )
+    ) {
+      const records: any = [];
+      deleteData?.forEach((data) => {
+        const index = records?.findIndex((d) => d?.workOrder === data?.workOrderId);
+        if (index >= 0) {
+          records[index].ids = [...records[index].ids, data?._id];
+        } else {
+          records.push({
+            workOrder: data?.workOrderId,
+            ids: [data?._id]
+          });
+        }
+      });
+      await axiosInstance().put(`${workOrder.api}/${assemblyOrderData?._id}/material/remove`, records);
+      setDeleting(false);
+      setShowConfirmBox(false);
+      fetchData();
+      toastConfig.setToastConfig({
+        open: true,
+        type: 'success',
+        message: 'Deleted Successfully'
+      });
+    } else {
+      if (deleteData?.filter((e: any) => !e?.subRows?.length && e?.workOrderStatus !== WORK_ORDER_STATUS.completed)?.length) {
+        handleDeleteWorkOrder(
+          deleteData?.filter((e: any) => !e?.subRows?.length && e?.workOrderStatus !== WORK_ORDER_STATUS.completed)?.map((e) => e.workOrderId)
+        );
+      }
+    }
+  };
+
+  const handleDeleteWorkOrder = (ids) => {
     axiosInstance()
-      .put(`${workOrder.api}/remove`, { ids: deleteData?.map((d) => d?.workOrder?.optionValue) })
+      .put(`${workOrder.api}/remove`, { ids: ids })
       .then(({ data }) => {
         setDeleting(false);
         setShowConfirmBox(false);
@@ -228,6 +330,44 @@ const WorkOrder = ({ renderedFrom, assemblyOrderData, setNextStep, stepFullScree
       });
   };
 
+  const handleAutoComplete = () => {
+    let ids = [];
+    if (autoCompleteData && autoCompleteData.length > 0) {
+      autoCompleteData.forEach((d) => {
+        if (d?.canAutoCompleteWorkOrder && d?.workOrderId) {
+          if (!ids?.includes(d?.workOrderId)) ids.push(d?.workOrderId);
+        }
+      });
+    }
+    setCompleting(true);
+    if (ids.length) {
+      axiosInstance()
+        .put(`${routes.assemblyOrder.path}/work-order/${assemblyOrderData._id}/auto-complete`, {
+          workOrders: ids
+        })
+        .then(({ data }) => {
+          setCompleting(false);
+          setCompleteConfirmBox(false);
+          fetchData();
+          checkAllWorkOrderComplete();
+          toastConfig.setToastConfig({
+            open: true,
+            type: 'success',
+            message: data?.message
+          });
+        })
+        .catch((err) => {
+          setCompleteConfirmBox(false);
+          setCompleting(false);
+          toastConfig.setToastConfig(err);
+        });
+    } else {
+      setCompleting(false);
+      setCompleteConfirmBox(false);
+      fetchData();
+    }
+  };
+
   return (
     <>
       {isAutoCreating && (
@@ -243,7 +383,9 @@ const WorkOrder = ({ renderedFrom, assemblyOrderData, setNextStep, stepFullScree
             {...{
               selectedRecords,
               setDeleteData,
-              setShowConfirmBox
+              setShowConfirmBox,
+              setAutoCompleteData,
+              setCompleteConfirmBox
             }}
           />
         }
@@ -284,15 +426,36 @@ const WorkOrder = ({ renderedFrom, assemblyOrderData, setNextStep, stepFullScree
           onOk={handleDelete}
         />
       )}
+
+      {completeConfirmBox && (
+        <ConfirmationDialog
+          open={completeConfirmBox}
+          okBtnLoading={isCompleting}
+          message={`Are you sure you want to Auto Complete this Work Order(s)`}
+          onClose={() => {
+            setCompleteConfirmBox(false);
+          }}
+          onOk={handleAutoComplete}
+        />
+      )}
     </>
   );
 };
 
 export default WorkOrder;
 
-const ActionButtonMenuItems = ({ selectedRecords, setDeleteData, setShowConfirmBox }) => {
+const ActionButtonMenuItems = ({ selectedRecords, setDeleteData, setShowConfirmBox, setAutoCompleteData, setCompleteConfirmBox }) => {
   return (
     <>
+      <MenuItem
+        onClick={() => {
+          setAutoCompleteData(selectedRecords);
+          setCompleteConfirmBox(true);
+        }}
+        disabled={selectedRecords.some((e) => e?.canAutoCompleteWorkOrder) ? false : true}
+      >
+        Auto Complete Work Order(s)
+      </MenuItem>
       <MenuItem
         onClick={() => {
           setDeleteData(selectedRecords?.filter((e) => e?.canDelete));
