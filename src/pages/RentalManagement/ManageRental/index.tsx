7bd2f81d0f784a5ed776e0912a40b589fcdbc482
Romit Sadaria <romit.sadaria@gmail.com>
2024-10-11 17:22:41 +0530

    fix: bug fix

--- src/pages/RentalManagement/ManageRental/index.tsx
+++ src/pages/RentalManagement/ManageRental/index.tsx
@@ -430,7 +430,7 @@ const ManageRentalManagementDialog = ({
                   id="dialog-save-button"
                   variant="contained"
                   color="primary"
-                  disabled={uploadingImageOrFileProgress > 0 || loading || isEqual(rentalData.initialValues, values)}
+                  disabled={uploadingImageOrFileProgress > 0 || loading || (!isClone && isEqual(rentalData.initialValues, values))}
                   onClick={(e) => {
                     e.preventDefault();
                     handleScroll(errors);
