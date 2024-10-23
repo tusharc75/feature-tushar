ee0bc4fca9ce0956d1f3707d3dc92f5470c55480
harshit <hg25112002@gmail.com>
2024-10-23 15:50:14 +0530

    Workspace in managedPackages

--- src/constants/helpers.tsx
+++ src/constants/helpers.tsx
@@ -2376,7 +2376,8 @@ export const ACTIVITY_RESOURCE = {
   budget: 'budget',
   irtTicket: 'irtTicket',
   subcontractAssembly: 'subcontractAssembly',
-  assemblyOrder: 'assemblyOrder'
+  assemblyOrder: 'assemblyOrder',
+  managedPackages: 'managedPackages',
 };
 
 export const LOG_RESOURCE = {
