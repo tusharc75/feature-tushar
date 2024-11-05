71b2ea7838a85f032a8a1bfaf845dea187bed515
dhrubobarman <dhrubobarman847@gmail.com>
2024-11-05 12:02:15 +0530

    fix: fix

--- src/components/CustomReactTable/ArrangeView/ArrangeViewDialog.tsx
+++ src/components/CustomReactTable/ArrangeView/ArrangeViewDialog.tsx
@@ -341,7 +341,6 @@ const ArrangeViewDialog = ({
                 <div className="p-3 px-[20px] [border-bottom:1px_solid_var(--common-border-color)]">
                   <div className="flex items-center justify-between gap-2">
                     <SearchBox onChange={handeSearch} value={serchedValue} />
-                    {/* <SearchBox className="max-w-[350px] flex-grow" value={serchedValue} onChange={handeSearch} type="search" variant="outlined" /> */}
                     <span className="mr-[12px] flex-shrink-0">
                       <Switch size="small" checked={isAllChecked()} onChange={(e) => handleToggleAll(e, setFieldValue)} />
                     </span>
