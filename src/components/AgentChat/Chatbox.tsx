86ffcd05ab5e60333d654b677db94d6ba63261bb
dhrubobarman <dhrubobarman847@gmail.com>
2024-11-05 17:28:49 +0530

    fix: added markdown parser

--- src/components/AgentChat/Chatbox.tsx
+++ src/components/AgentChat/Chatbox.tsx
@@ -15,6 +15,7 @@ import { getRandomNumber, scrollToBottom } from 'src/components/AgentChat/utils'
 import HtmlTooltip from 'src/components/CustomTooltipTitle';
 import { cn } from 'src/constants/helpers';
 import { CustomToastContext } from 'src/StateProvider/CustomToastContext/CustomToastContext';
+import Markdown from 'react-markdown';
 
 type ChatboxProps = {
   isChatboxOpen: boolean;
@@ -171,9 +172,12 @@ const RenderSingleChat = ({ message, loading = false }: { message?: TMessage; lo
             {message.content}
           </Typography>
         ) : (
-          <Typography component={'pre'} variant="body2" className="whitespace-pre-wrap rounded-lg pt-[8px] text-[var(--primary)] dark:text-white">
+          <div className="whitespace-pre-wrap rounded-lg pt-[8px] text-[var(--primary)] dark:text-white">
+            <Markdown>{message.content}</Markdown>
+            {/* <Typography component={'pre'} variant="body2" >
             {message.content}
-          </Typography>
+          </Typography> */}
+          </div>
         )}
       </div>
     </>
