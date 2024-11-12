7f9cf6bf3b4cdae7372ad99df03d77f627682f75
dhrubobarman <dhrubobarman847@gmail.com>
2024-11-12 13:30:14 +0530

    feat: new UI

--- src/pages/Home/DisplaySideCard.tsx
+++ src/pages/Home/DisplaySideCard.tsx
@@ -1,14 +1,14 @@
-import { Box, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
-import { FiExternalLink } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { SVGImages } from '../../assets/dashboard_images';
+import adminSetupImage from 'src/assets/dashboard_images/sidebar/admin-setup.png';
+import userManualImage from 'src/assets/dashboard_images/sidebar/user-manual.png';
+import workspaceImage from 'src/assets/dashboard_images/sidebar/workspace.png';
 import styles from './Dashboard.module.scss';
-import './style.scss';
 
-import { HiArrowRight } from 'react-icons/hi';
 import DashboardModal from 'src/components/DashboardModal';
 import { userManual } from 'src/pages/Home';
+import SideCard, { SideCardProps } from 'src/pages/Home/SideCard';
 
 interface sidecardInterface extends React.HTMLAttributes<HTMLDivElement> {
   objBySectionName: any;
@@ -16,13 +16,56 @@ interface sidecardInterface extends React.HTMLAttributes<HTMLDivElement> {
   mode: 'Workspace' | 'Setups & Administration' | 'User Manual';
 }
 
+const getProps = (mode: 'Workspace' | 'Setups & Administration' | 'User Manual', handleClick: () => void): SideCardProps => {
+  switch (mode) {
+    case 'Workspace': {
+      return {
+        heading: 'Workspace',
+        description: 'By using Equipt Workspace, collaborate with your team members easily',
+        icon: (
+          <div className="max-w-[60px]">
+            <img src={workspaceImage} alt={'Workspace Logo'} className="max-w-full" />
+          </div>
+        ),
+        onClick: handleClick,
+        gradientColors: ['#925cb4', '#64b9fc']
+      };
+    }
+    case 'Setups & Administration': {
+      return {
+        heading: 'Setups & Administration',
+        description: 'List of all product and category setups',
+        icon: (
+          <div className="max-w-[60px]">
+            <img src={adminSetupImage} alt={'Setups & Administration Logo'} className="max-w-full" />
+          </div>
+        ),
+        onClick: handleClick,
+        gradientColors: ['#a274bf', '#feb237']
+      };
+    }
+    default: {
+      const token = localStorage.getItem('token');
+      const href = `${userManual.link}/?token=${encodeURIComponent(token)}`;
+      return {
+        heading: 'User Manual',
+        description: 'View our user manual in just a click.',
+        icon: (
+          <div className="max-w-[60px]">
+            <img src={userManualImage} className="max-w-full" alt={'User Manual Logo'} />
+          </div>
+        ),
+        href,
+        external: true,
+        gradientColors: ['#ffca6b', '#f6fafd']
+      };
+    }
+  }
+};
+
 const DisplaySideCard = ({ objBySectionName, handleRoutes, mode = 'Workspace', ...others }: sidecardInterface) => {
   const [modalContent, setModalContent] = useState(null);
   const [colabData, setColabData] = useState(null);
-  const style = { '--sideCardBg': '#FFFFFF' } as React.CSSProperties;
-  const description =
-    (mode === 'Workspace' && `By using Equipt Workspace, collaborate with your team members easily`) ||
-    (mode === 'Setups & Administration' && `List of all product and category setups`);
 
   useEffect(() => {
     if (objBySectionName) {
@@ -30,7 +73,13 @@ const DisplaySideCard = ({ objBySectionName, handleRoutes, mode = 'Workspace', .
         setColabData(objBySectionName['Collaboration Tools'] || objBySectionName['Activities'] || objBySectionName['Workspace'] || null);
       else setColabData(objBySectionName['Setups & Administration'] || objBySectionName['Setups'] || objBySectionName['Product Setup'] || null);
     }
-  }, [objBySectionName]);
+  }, [mode, objBySectionName]);
+
+  const handleClick = () => {
+    setModalContent({ items: colabData, title: mode, icon: propsForCard.icon });
+  };
+
+  const propsForCard = getProps(mode, handleClick);
 
   const handleClose = () => {
     setModalContent(null);
@@ -38,51 +87,9 @@ const DisplaySideCard = ({ objBySectionName, handleRoutes, mode = 'Workspace', .
   return (
     <>
       {colabData ? (
-        <div
-          role="button"
-          style={style}
-          className={`${styles.rightInner} mb-4`}
-          aria-label={`open ${mode}`}
-          onClick={() => {
-            if (mode === 'User Manual') {
-              const token = localStorage.getItem('token');
-              window.open(`${userManual.link}/?token=${encodeURIComponent(token)}`);
-            } else {
-              setModalContent({ items: colabData, title: mode, icon: <img src={SVGImages(mode)} alt={`${mode} Logo`} /> });
-            }
-          }}
-          {...others}
-        >
-          <img src={SVGImages(mode)} alt={`${mode} Logo`} className={styles.colabLogo} />
-          <Typography component={'h2'}>{mode}</Typography>
-          <Typography component={'p'}>{mode !== 'User Manual' ? description : userManual.description}</Typography>
-          {mode === 'Setups & Administration' && (
-            <>
-              <Box
-                pb={1}
-                pt={5}
-                className={styles.viewAll}
-                onClick={() => colabData.length > 0 && setModalContent({ items: colabData, title: mode })}
-              >
-                <Typography component="span">View All</Typography>
-                <HiArrowRight />
-              </Box>
-            </>
-          )}
-          {mode === 'Workspace' && (
-            <button className={styles.colabButton} onClick={() => setModalContent({ items: colabData, title: mode })}>
-              <Typography component="span">Start Collaborating</Typography>
-            </button>
-          )}
-          {mode === 'User Manual' && (
-            <Box pb={1} pt={5}>
-              <span title="open equipt documentation" className={styles.viewAll}>
-                <Typography component="span">Equipt - User Manual</Typography>
-                <FiExternalLink size={20} style={{ marginBottom: 4 }} />
-              </span>
-            </Box>
-          )}
-        </div>
+        <>
+          <SideCard {...propsForCard} />
+        </>
       ) : null}
       {mode !== 'User Manual' && (
         <DashboardModal
