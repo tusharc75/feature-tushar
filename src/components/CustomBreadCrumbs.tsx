c337eb8760d813686d41473cf65087699ce1300d
dhrubobarman <dhrubobarman847@gmail.com>
2024-12-09 16:03:25 +0530

    fix: fix

--- src/components/CustomBreadCrumbs.tsx
+++ src/components/CustomBreadCrumbs.tsx
@@ -1,5 +1,4 @@
-import { Breadcrumbs, Typography } from '@material-ui/core';
-import { Fragment } from 'react';
+import { Breadcrumbs } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { HomeIconBreadcrumb } from 'src/assets/newSvgs';
 
