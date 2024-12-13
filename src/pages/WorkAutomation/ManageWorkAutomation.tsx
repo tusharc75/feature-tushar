a38c2ae91a9c0b912edb3664b177578d0beddaed
VivekRaj0 <vivekraj152089@gmail.com>
2024-12-13 13:08:21 +0530

    fix: bug fix

--- src/pages/WorkAutomation/ManageWorkAutomation.tsx
+++ src/pages/WorkAutomation/ManageWorkAutomation.tsx
@@ -26,6 +26,10 @@ const ManageWorkAutomation = () => {
   const history = useHistory();
   const { id } = useParams();
 
+  const {
+    state: { resources }
+  }: any = useData();
+
   useEffect(() => {
     axiosInstance()
       .get(`${rentalManagement.api}/${id}`)
@@ -220,7 +224,7 @@ const AddServices = ({ rentalManagementData, setSelectedServices, selectedAssets
   );
 };
 
-const AddTechnicians = ({ rentalManagementData, setSelectedTechnicians, selectedServices, isExpand, setIsExpand, handleSave, loading, resources }) => {
+const AddTechnicians = ({ rentalManagementData, setSelectedTechnicians, selectedServices, isExpand, setIsExpand, handleSave, loading ,resources}) => {
   const [records, setRecords] = useState(null);
 
   const handleAdd = () => {
