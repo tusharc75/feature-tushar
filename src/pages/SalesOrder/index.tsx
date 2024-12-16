995b88cdb03af790edc7e2a6bab7d75886f8b7c7
Romit Sadaria <romit.sadaria@gmail.com>
2024-12-16 13:47:59 +0530

    fix: bug fox

--- src/pages/SalesOrder/index.tsx
+++ src/pages/SalesOrder/index.tsx
@@ -144,6 +144,7 @@ const SalesOrder = () => {
         type: 'success',
         message: data.message
       });
+      dispatch({ type: 'selection', selectedRecords: [] });
       fetchData();
       setShowDeleteConfirmBox(false);
       setDeleteRecord(null);
@@ -228,8 +229,13 @@ const SalesOrder = () => {
         <MenuItem
           disabled={selectedRecords.every((e) => e.canDelete) ? false : true}
           onClick={() => {
+            if (selectedRecords?.length === 1) {
+              setDeleteRecord(selectedRecords[0]);
+            }
+            else {
+              setDeleteRecord(null);
+            }
             setShowDeleteConfirmBox(true);
-            setDeleteRecord(null);
           }}
         >
           {`Delete (${selectedRecords?.length})`}
