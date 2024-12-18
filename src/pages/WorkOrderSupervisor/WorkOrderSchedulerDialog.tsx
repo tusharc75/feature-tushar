398ffa4abea3bbe5fecf227a148a42edef41e4c6
Rishabhsharawat15 <rishabhsharawat150@gmail.com>
2024-12-18 10:35:09 +0530

    feat: fix

--- src/pages/WorkOrderSupervisor/WorkOrderSchedulerDialog.tsx
+++ src/pages/WorkOrderSupervisor/WorkOrderSchedulerDialog.tsx
@@ -1,5 +1,11 @@
 import { Fragment, useContext, useEffect, useState } from 'react';
-import { ASSET_STATUS, CustomDialogTransition, serializedAsset, sidebarResource, workOrder } from '../../constants/helpers';
+import {
+  ASSET_STATUS,
+  CustomDialogTransition,
+  serializedAsset,
+  sidebarResource,
+  workOrder
+} from '../../constants/helpers';
 import { Dialog, TextField, Box, Grid, Button } from '@material-ui/core';
 import CustomDialogHeader from '../../components/CustomDialog/CustomDialogHeader';
 import CustomDialogContent from '../../components/CustomDialog/CustomDialogContent';
@@ -11,70 +17,39 @@ import { KeyboardDatePicker } from '@material-ui/pickers';
 import CustomDialogFooter from 'src/components/CustomDialog/CustomDialogFooter';
 import CustomButton from 'src/components/Helpers/CustomButton';
 import { isMobile, isTablet } from 'react-device-detect';
-import { useData } from 'src/StateProvider/Provider';
+import { Formik, Form, Field } from 'formik';
 
-export default function WorkOrderSchedulerDialog({ onClose, onSuccess }) {
+export default function ProductFrequencyDialog({ onClose, onSuccess }) {
   const toastConfig = useContext(CustomToastContext);
   const [productOptions, setProductOptions] = useState([]);
-  const [selectedProduct, setSelectedProduct] = useState(null);
   const [assetOptions, setAssetOptions] = useState([]);
-  const [selectedAsset, setSelectedAsset] = useState(null);
   const [servicesOptions, setServicesOptions] = useState([]);
-  const [selectedService, setSelectedService] = useState(null);
-  const [selectedDate, setSelectedDate] = useState(null);
   const [fullScreen, setFullScreen] = useState(isMobile || isTablet);
   const [loading, setLoading] = useState(false);
 
-  const {
-    state: { permissions, resources }
-  }: any = useData();
-
-
   useEffect(() => {
-    const deepFilter = [
-      {
-        field: 'serializedProduct',
-        term: { $in: [true] }
-      }
-    ];
+    const deepFilter = [{ field: 'serializedProduct', term: { $in: [true] } }];
     axiosInstance()
       .get(`/sa-formbuilder/lookup?lookupResource=${sidebarResource.product}&deepFilter=${JSON.stringify(deepFilter)}`)
       .then(({ data: { data: lookupResource } }) => {
         setProductOptions(lookupResource['Product'] || []);
       })
-      .catch((err) => {
-        toastConfig.setToastConfig(err);
-      });
+      .catch((err) => toastConfig.setToastConfig(err));
   }, []);
 
   useEffect(() => {
     setLoading(true);
     const deepFilter = [
-      {
-        field: 'status',
-        term: {
-          $in: [
-            ASSET_STATUS.new,
-            ASSET_STATUS.available,
-            ASSET_STATUS.underReview,
-            ASSET_STATUS.needRepair,
-            ASSET_STATUS.needRecert
-          ]
-        }
-      }
+      { field: 'status', term: { $in: Object.values(ASSET_STATUS) } }
     ];
     axiosInstance()
       .get(`/sa-formbuilder/lookup?lookupResource=${serializedAsset.resource}&deepFilter=${JSON.stringify(deepFilter)}`)
       .then(({ data: { data: lookupSerializedAssets } }) => {
-        let serializedAssets = lookupSerializedAssets['Serialized Asset'] || [];
-        const filteredAssets = serializedAssets?.filter(asset => asset?.product === selectedProduct);
-        setAssetOptions(filteredAssets || []);
+        setAssetOptions(lookupSerializedAssets['Serialized Asset'] || []);
         setLoading(false);
       })
-      .catch((err) => {
-        toastConfig.setToastConfig(err);
-      });
-  }, [selectedProduct]);
+      .catch((err) => toastConfig.setToastConfig(err));
+  }, []);
 
   useEffect(() => {
     axiosInstance()
@@ -84,157 +59,131 @@ export default function WorkOrderSchedulerDialog({ onClose, onSuccess }) {
       });
   }, []);
 
-  const handleSubmit = async () => {
-    let values = {
-      "asset": selectedAsset,
-      "product": selectedProduct,
-      "service": selectedService,
-      "date": selectedDate
-    }
+  const handleSubmit = (values) => {
     axiosInstance()
       .post(`${workOrder.api}/work-order-scheduler/scheduler`, values)
       .then(({ data }) => {
         onSuccess();
-        toastConfig.setToastConfig({
-          open: true,
-          type: 'success',
-          message: data.message
-        });
+        toastConfig.setToastConfig({ open: true, type: 'success', message: data.message });
       })
-      .catch((error) => {
-        toastConfig.setToastConfig(error);
-      });
+      .catch((error) => toastConfig.setToastConfig(error));
   };
 
   return (
     <Dialog
       TransitionComponent={CustomDialogTransition}
       open={true}
-      aria-labelledby="customized-dialog-title"
-      fullWidth
       fullScreen={fullScreen || isMobile || isTablet}
       maxWidth={'sm'}
       onClose={(e, reason) => {
-        if (reason !== 'backdropClick') {
-          onClose();
-        }
+        if (reason !== 'backdropClick') onClose();
       }}
     >
       {productOptions.length > 0 ? (
         <Fragment>
-          <CustomDialogHeader
-            isMinimized={!fullScreen}
-            onMinimizeMaximize={() => {
-              setFullScreen((prevState) => !prevState);
+          <Formik
+            initialValues={{
+              product: '',
+              asset: '',
+              service: [],
+              date: null
             }}
-            showManimizeMaximize={true}
-            onClose={() => {
-              onClose();
-            }}
-            showRequiredLabel={false}
-            title={`${resources?.workOrder?.titleSingular} Scheduler`}
-          />
-          <CustomDialogContent>
-            <div className="flex flex-col p-3">
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <Autocomplete
+            onSubmit={handleSubmit}
+          >
+            {({ values, setFieldValue }) => (
+              <Form style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+                <CustomDialogHeader
+                  isMinimized={!fullScreen}
+                  onMinimizeMaximize={() => setFullScreen((prevState) => !prevState)}
+                  showManimizeMaximize={true}
+                  onClose={onClose}
+                  title={`Work Order Scheduler`}
+                />
+                <CustomDialogContent style={{ flex: 1, overflowY: 'auto' }}>
+                  <div className="flex flex-col p-3">
+                    <Grid container spacing={3}>
+                      <Grid item xs={12}>
+                        <Autocomplete
+                          size="small"
+                          options={productOptions}
+                          value={productOptions.find((data) => data.optionValue === values.product) || null}
+                          getOptionLabel={(option) => option?.optionLabel || ''}
+                          onChange={(e, val) => setFieldValue('product', val?.optionValue || '')}
+                          renderInput={(params) => (
+                            <TextField {...params} label="Select Product" variant="outlined" required />
+                          )}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Autocomplete
+                          size="small"
+                          options={assetOptions}
+                          loading={loading}
+                          value={assetOptions.find((data) => data.optionValue === values.asset) || null}
+                          getOptionLabel={(option) => option?.optionLabel || ''}
+                          onChange={(e, val) => setFieldValue('asset', val?.optionValue || '')}
+                          renderInput={(params) => (
+                            <TextField {...params} label="Select Serialized Asset" variant="outlined" required />
+                          )}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Autocomplete
+                          multiple
+                          size="small"
+                          options={servicesOptions}
+                          value={servicesOptions.filter((option) =>
+                            values.service.includes(option.optionValue)
+                          )}
+                          getOptionLabel={(option) => option?.optionLabel || ''}
+                          onChange={(e, val) => setFieldValue('service', val.map((item) => item.optionValue))}
+                          renderInput={(params) => (
+                            <TextField
+                              {...params}
+                              label="Select Service"
+                              variant="outlined"
+                            />
+                          )}
+                        />
+
+                      </Grid>
+                      <Grid item xs={12}>
+                        <KeyboardDatePicker
+                          required
+                          fullWidth
+                          autoOk
+                          clearable
+                          size="small"
+                          inputVariant="outlined"
+                          value={values.date}
+                          label="Select Date"
+                          minDate={new Date()}
+                          onChange={(date) => setFieldValue('date', date)}
+                          format="MM/dd/yyyy"
+                        />
+                      </Grid>
+                    </Grid>
+                  </div>
+                </CustomDialogContent>
+                <CustomDialogFooter>
+                  <Button
                     size="small"
-                    style={{ width: '100%' }}
-                    options={productOptions}
-                    value={productOptions?.find((data) => data.optionValue === selectedProduct) || {}}
-                    getOptionLabel={(option) => option?.optionLabel || ''}
-                    onChange={(e, val) => setSelectedProduct(val?.optionValue)}
-                    renderInput={(params) => (
-                      <TextField {...params} label="Select Product" variant="outlined" required />
-                    )}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Autocomplete
-                    size="small"
-                    style={{ width: '100%' }}
-                    options={assetOptions}
-                    loading={loading}
-                    value={assetOptions?.find((data) => data.optionValue === selectedAsset) || {}}
-                    getOptionLabel={(option) => option?.optionLabel || ''}
-                    onChange={(e, val) => setSelectedAsset(val?.optionValue)}
-                    renderInput={(params) => (
-                      <TextField {...params} label="Select Serialized Asset" variant="outlined" required />
-                    )}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Autocomplete
-                    multiple
-                    size="small"
-                    style={{ width: '100%' }}
-                    options={servicesOptions}
-                    value={servicesOptions.filter((option) =>
-                      selectedService?.includes(option.optionValue)
-                    )}
-                    getOptionLabel={(option) => option?.optionLabel || ''}
-                    onChange={(e, val) =>
-                      setSelectedService(val.map((item) => item.optionValue))
-                    }
-                    renderInput={(params) => (
-                      <TextField {...params} label="Select Service" variant="outlined" required />
-                    )}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <KeyboardDatePicker
-                    required
-                    clearable
-                    autoOk
-                    fullWidth
-                    size="small"
-                    style={{ height: '40px' }}
-                    variant="inline"
-                    inputVariant="outlined"
-                    value={selectedDate}
-                    label="Select Date"
-                    minDate={new Date()}
-                    onChange={(date) => setSelectedDate(date)}
-                    format="MM/dd/yyyy"
-                    InputLabelProps={{
-                      shrink: true,
-                    }}
-                  />
-                </Grid>
-              </Grid>
-            </div>
-          </CustomDialogContent>
-          <CustomDialogFooter>
-            <Button
-              size="small"
-              color="primary"
-              id="dialog-cancel-button"
-              onClick={() => {
-                onClose();
-              }}
-            >
-              Cancel
-            </Button>
-            <CustomButton
-              variant="contained"
-              color="primary"
-              type="submit"
-              onClick={() => {
-                if (selectedAsset && selectedDate && selectedProduct && selectedService) {
-                  handleSubmit();
-                } else {
-                  toastConfig.setToastConfig({
-                    open: true,
-                    type: 'error',
-                    message: 'All fields are required',
-                  });
-                }
-              }}
-            >
-              Save
-            </CustomButton>
-          </CustomDialogFooter>
+                    color="primary"
+                    onClick={() => onClose()}
+                  >
+                    Cancel
+                  </Button>
+                  <CustomButton
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                  >
+                    Save
+                  </CustomButton>
+                </CustomDialogFooter>
+              </Form>
+            )}
+          </Formik>
         </Fragment>
       ) : (
         <Box p={2} height={500}>
