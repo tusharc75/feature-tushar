76489f9c9c282fdbe543725868ef5f95db3ff83c
ABHIMANYU KUMAR <manupatel18112000@gmail.com>
2024-12-19 14:15:55 +0530

    fix bugs

--- src/pages/WorkOrderSupervisor/index.tsx
+++ src/pages/WorkOrderSupervisor/index.tsx
@@ -354,12 +354,12 @@ const WorkOrderSupervisor = () => {
         },
         ...(['table-view', 'card-view']?.includes(viewType)
           ? [
-            {
-              disabled: selectedRecords?.length === 0,
-              label: 'Add Products/Consumables',
-              onClick: () => setConsumablesDialog(true)
-            }
-          ]
+              {
+                disabled: selectedRecords?.length === 0,
+                label: 'Add Products/Consumables',
+                onClick: () => setConsumablesDialog(true)
+              }
+            ]
           : [])
       ]
     };
@@ -395,13 +395,8 @@ const WorkOrderSupervisor = () => {
               </Button>
             )}
             <Box>
-              <HtmlTooltip title='More'>
-                <IconButton
-                  aria-haspopup="true"
-                  color="primary"
-                  size="small"
-                  title="More"
-                  onClick={(event) => setAnchorEl(event.currentTarget)}>
+              <HtmlTooltip title="More">
+                <IconButton aria-haspopup="true" color="primary" size="small" title="More" onClick={(event) => setAnchorEl(event.currentTarget)}>
                   <MoreVert />
                 </IconButton>
               </HtmlTooltip>
@@ -480,7 +475,7 @@ const WorkOrderSupervisor = () => {
                   </>
                 ) : (
                   <div className="flex">
-                    <ToggleButtonGroup size="small" exclusive value={resourceType} onChange={(e, newVal) => { }}>
+                    <ToggleButtonGroup size="small" exclusive value={resourceType} onChange={(e, newVal) => {}}>
                       <ToggleButton value={'workOrder'} onClick={() => setResourceType('workOrder')}>
                         {resources?.workOrder?.titleSingular}
                       </ToggleButton>
@@ -579,15 +574,15 @@ const WorkOrderSupervisor = () => {
             workOrderData={
               assignTechnicianDialog.multiple
                 ? selectedRecords?.map((r) => ({
-                  uniqueId: r?.uniqueId,
-                  workOrderId: r?.workOrder
-                }))
+                    uniqueId: r?.uniqueId,
+                    workOrderId: r?.workOrder
+                  }))
                 : [
-                  {
-                    uniqueId: selectedServiceData?.uniqueId,
-                    workOrderId: selectedServiceData?._id
-                  }
-                ]
+                    {
+                      uniqueId: selectedServiceData?.uniqueId,
+                      workOrderId: selectedServiceData?._id
+                    }
+                  ]
             }
             assignedUsers={
               assignTechnicianDialog.multiple
@@ -613,15 +608,15 @@ const WorkOrderSupervisor = () => {
             workOrderData={
               workStationAssignDialog.multiple
                 ? selectedRecords?.map((r) => ({
-                  uniqueId: r?.uniqueId,
-                  workOrderId: r?.workOrder
-                }))
+                    uniqueId: r?.uniqueId,
+                    workOrderId: r?.workOrder
+                  }))
                 : [
-                  {
-                    uniqueId: selectedServiceData?.uniqueId,
-                    workOrderId: selectedServiceData?._id
-                  }
-                ]
+                    {
+                      uniqueId: selectedServiceData?.uniqueId,
+                      workOrderId: selectedServiceData?._id
+                    }
+                  ]
             }
             workStations={workStationAssignDialog.multiple ? selectedRecords[0]?.assignedWorkStations : selectedServiceData?.assignedWorkStations}
             handleClose={() => {
