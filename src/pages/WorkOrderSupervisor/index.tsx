c080144cfc061af3307e66a8102db9aa6f663cac
Dhruv Khandelwal <dhruvkhandelwal@Dhruvs-MacBook-Air.local>
2024-12-24 17:43:46 +0530

    fix: bug fix

--- src/pages/WorkOrderSupervisor/index.tsx
+++ src/pages/WorkOrderSupervisor/index.tsx
@@ -107,7 +107,7 @@ const WorkOrderSupervisor = () => {
         fieldName: 'user',
         fieldLabel: resources?.employeeMaster?.titlePlural,
         lookup: true,
-        lookupResource: sidebarResource.employeeMaster,
+        lookupResource: sidebarResource.user,
         resource: sidebarResource.workOrderSupervisor,
         type: 'dropDown',
         order: 0,
@@ -732,7 +732,7 @@ const WorkOrderSupervisor = () => {
               : [
                 {
                   uniqueId: selectedServiceData?.uniqueId,
-                  workOrderId: selectedServiceData?._id
+                  workOrderId: selectedServiceData?.workOrder
                 }
               ]
           }
@@ -818,7 +818,6 @@ const WorkOrderSupervisor = () => {
         <Filter
           onClose={() => {
             setShowFilter(false);
-            dispatch({ type: 'setFilterQuery', filterQuery: '' });
           }}
           loading={false}
           filterTitle={resources?.workOrderSupervisor?.titleSingular}
