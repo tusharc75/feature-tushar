e85436e6b58d9002c999782514c514c9c522f1f3
dhrubobarman <dhrubobarman847@gmail.com>
2025-01-02 14:35:53 +0530

    fix: fix

--- src/components/CustomReactTable/hooks/useTableReducer.tsx
+++ src/components/CustomReactTable/hooks/useTableReducer.tsx
@@ -171,7 +171,7 @@ export type TActios =
   | { type: 'selection'; selectedRecords: any[] }
   | { type: 'update'; data: any[] }
   | { type: 'onlyFilter'; filters: any }
-  | { type: 'filter'; filters: any; loading?: boolean; filterTerm: any }
+  | { type: 'filter'; filters: any; loading?: boolean; filterTerm?: any }
   | { type: 'sort'; sorting: any[]; loading?: boolean }
   | { type: 'search'; search: string; loading?: boolean }
   | { type: 'pageChange'; page: number }
