b836bbdcb258305f2bbb1ca3999499ad1140e5a1
Vivek-0420 <Vivek.Raj@vebholic.com>
2025-01-20 08:24:24 +0530

    fix: fix

--- src/pages/ExpensesReport/ExpenseTable.tsx
+++ src/pages/ExpensesReport/ExpenseTable.tsx
@@ -5,40 +5,33 @@ const ExpenseTable = ({ selectedExpensesData, selectedExpenses, removeExpenseFie
 
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="spanning table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Description</TableCell>
-            <TableCell align="center">Currency</TableCell>
-            <TableCell align="center">Merchant</TableCell>
-            <TableCell align="center">Category</TableCell>
-            <TableCell align="center">Amount</TableCell>
-            <TableCell align="center">Action</TableCell>
+    <Table sx={{ minWidth: 700 }} aria-label="spanning table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Description</TableCell>
+          <TableCell align="center">Currency</TableCell>
+          <TableCell align="center">Merchant</TableCell>
+          <TableCell align="center">Category</TableCell>
+          <TableCell align="center">Action</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {selectedExpenses?.map((row, index) => (
+          <TableRow key={index}>
+            <TableCell>{row.expenseNumber}</TableCell>
+            <TableCell align="center">{row.currency}</TableCell>
+            <TableCell align="center">{row.merchant}</TableCell>
+            <TableCell align="center">{row.category}</TableCell>
+            <TableCell align="center">
+              <IconButton size="small" aria-label="Delete" onClick={() => {removeExpenseField(row.id)}}>
+                <DeleteIcon fontSize="small" color="error" />
+              </IconButton>
+            </TableCell>
           </TableRow>
-        </TableHead>
-        <TableBody>
-          {selectedExpensesData && (
-            <TableRow key={selectedExpensesData._id}>
-              <TableCell>{selectedExpensesData.expenseNumber}</TableCell>
-              <TableCell align="center">{selectedExpensesData.currency}</TableCell>
-              <TableCell align="center">{selectedExpensesData.merchant}</TableCell>
-              <TableCell align="center">{selectedExpensesData.category}</TableCell>
-              <TableCell align="center">{selectedExpensesData.totalAmount}</TableCell>
-              <TableCell align="center">
-                <IconButton size="small" aria-label="Delete" onClick={() => {removeExpenseField(selectedExpensesData.id)}}>
-                  <DeleteIcon fontSize="small" color="error" />
-                </IconButton>
-              </TableCell>
-            </TableRow>
-          )}
-          <TableRow>
-            <TableCell/>
-            <TableCell align="left">Total Amount</TableCell>
-            <TableCell align="right">{selectedExpensesData.totalAmount}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
   );
 };
 
