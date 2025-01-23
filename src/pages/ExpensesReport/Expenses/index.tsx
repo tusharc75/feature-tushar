01fefeb896cd7724fc0a5d1d8d400e8b4824b880
Vivek-0420 <Vivek.Raj@vebholic.com>
2025-01-23 17:30:41 +0530

    fix: fix

--- src/pages/ExpensesReport/Expenses/index.tsx
+++ src/pages/ExpensesReport/Expenses/index.tsx
@@ -107,53 +107,17 @@ const Expenses = (selectedExpenseData, removeExpense) => {
     )
   };
 
-  // const getQueryString = (isExport = false) => {
-  //   // let deepFilter = `?page=${page}&limit=${limit}`;
-  //    let deepFilter = `?`;
-  //    if (selectedEntity) {
-  //     deepFilter = `${deepFilter}&entity=${selectedEntity}`;
-  //   }
-  //   // const { filterByIds, deepFilters } = gridFilterParser(filters);
-  //   // const expenseNumbers = selectedExpenseData.map(item => item.expenseNumber);
-  //   // const expenseNumbers = selectedExpenseData.map(item => item._id);
-  //   const filterByIds = [{ field: 'selectedExpenses', term: selectedExpenseData?.selectedExpenseData?._id}];
-  //   if (filterByIds?.length) {
-  //     deepFilter = `${deepFilter}&filterById=${JSON.stringify(filterByIds)}&filterType=and`;
-  //   }
-  //   // if (deepFilters?.length) {
-  //   //   deepFilter = `${deepFilter}&deepFilter=${encodeURIComponent(JSON.stringify(deepFilters))}`;
-  //   // }
-
-  //   // if (filterByIds?.length || deepFilters?.length) {
-  //   //   deepFilter = `${deepFilter}&filterType=and`;
-  //   // }
-
-  //   // if (sorting.length > 0) {
-  //   //   deepFilter = `${deepFilter}&sortBy=${sorting[0].colId}&orderBy=${sorting[0].sort}`;
-  //   // }
-  //   // if (search) {
-  //   //   deepFilter = `${deepFilter}&search=${encodeURIComponent(search)}`;
-  //   // }
-  //   // if (showFilteredRecordsOnly) {
-  //   //   deepFilter = `${deepFilter}&getById=${JSON.stringify(selectedRecords.map((m) => m._id))}`;
-  //   // }
-  //   return deepFilter;
-  // };
-
   const getQueryString = () => {
     let queryString = `?`;
     if (selectedEntity) {
       queryString = `${queryString}&entity=${selectedEntity}`;
     }
   
-    // Extract selected expense IDs
     const filterByIds = selectedExpenseData?.selectedExpenses?.map((expense) => ({ field: '_id', term: expense._id }));
     if (filterByIds?.length) {
-      // Pass filterById as a simple array of IDs (assuming API expects this format)
-      queryString = `${queryString}&filterById=${encodeURIComponent(JSON.stringify(filterByIds))}`;
+      queryString = `${queryString}&filterById=${encodeURIComponent(JSON.stringify(filterByIds))}&filterType=and`;
     }
   
-    queryString = `${queryString}&filterType=and`; // Ensure filter type is added
     return queryString;
   };
 
