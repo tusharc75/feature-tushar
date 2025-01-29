97b122c5981d3f38f172c2e4191c3bc2d7b95e1f
dhrubobarman <dhrubobarman847@gmail.com>
2025-01-29 11:41:58 +0530

    fix: fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -61,7 +61,7 @@ function Dashboard() {
             <div className={'grid gap-5 max-[900px]:grid-cols-2 max-[600px]:grid-cols-1'}>
               {aiPermission && (
                 <SideCard
-                  heading={'Equipt Genie'}
+                  heading={'E Genie'}
                   href={routes.equiptAi.path}
                   icon={
                     <div className="max-w-[60px]">
