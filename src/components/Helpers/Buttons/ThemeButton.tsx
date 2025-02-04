70a3f39ee96fb4baa1e1e59d7747f5587e1d0b2d
Vivek-0420 <Vivek.Raj@vebholic.com>
2025-02-04 09:58:10 +0530

    fix: fix

--- src/components/Helpers/Buttons/ThemeButton.tsx
+++ src/components/Helpers/Buttons/ThemeButton.tsx
@@ -83,12 +83,7 @@ const getButtonStyle = ({ buttonType = 'default', mode = 'dark', iconForMobile =
           border: '1px solid var(--common-red-border-color)',
           color: '#d43e3e',
           opacity: 0.7
-        },
-        '&:hover': {
-          backgroundColor: 'var(--common-red-border-color)',
-          color: 'white'
-        },
-        fontWeight: '600'
+        }
       };
       break;
     }
