4ef36626dddb4461471955d389406558ac3b1479
ABHIMANYU KUMAR <manupatel18112000@gmail.com>
2025-02-06 17:54:42 +0530

    fix bugs

--- src/components/CustomReactTable/Cells/LookupCell.tsx
+++ src/components/CustomReactTable/Cells/LookupCell.tsx
@@ -19,7 +19,7 @@ const getText = ({ original, field }) => {
   const key = `rest${field.fieldName}`;
   let text = Array.isArray(original[field.fieldName])
     ? original[field.fieldName]?.map((e) => e?.optionLabel)?.toString()
-    : ['string', 'number']?.includes(typeof original[field.fieldName])
+    : original[field.fieldName]
       ? original[field.fieldName]
       : '';
 
