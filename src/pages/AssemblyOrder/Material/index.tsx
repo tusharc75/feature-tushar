08f7723f665006e6bc1dae00ede5b2930f076ca0
Romit Sadaria <romit.sadaria@gmail.com>
2025-02-20 16:58:04 +0530

    fix: bug fix

--- src/pages/AssemblyOrder/Material/index.tsx
+++ src/pages/AssemblyOrder/Material/index.tsx
@@ -96,7 +96,7 @@ const Material = ({ assemblyOrderData, setNextStep, renderedFrom, stepFullScreen
             ) : (
               <h5 className="text-truncate">{row.original?.detail}</h5>
             )}
-            {allowedToEdit && row.original.type === MATERIAL_TYPE.package && !row.original.parentId && (
+            {allowedToEdit && row.original.type === MATERIAL_TYPE.package && (
               <>
                 {row.original?.subRows?.length > 0 &&
                   <Box>
@@ -104,18 +104,16 @@ const Material = ({ assemblyOrderData, setNextStep, renderedFrom, stepFullScreen
                   </Box>
                 }
                 <Box>
-                  {!row?.original?.parentId && (
-                    <HtmlTooltip title={`Add Existing ${resources?.packages?.titlePlural}`}>
-                      <IconButton
-                        onClick={() => {
-                          setAddDialog({ open: true, parentId: row.original?._id });
-                        }}
-                        size="small"
-                      >
-                        <Add fontSize="small" color="primary" />
-                      </IconButton>
-                    </HtmlTooltip>
-                  )}
+                  <HtmlTooltip title={`Add Existing ${resources?.packages?.titlePlural}`}>
+                    <IconButton
+                      onClick={() => {
+                        setAddDialog({ open: true, parentId: row.original?._id });
+                      }}
+                      size="small"
+                    >
+                      <Add fontSize="small" color="primary" />
+                    </IconButton>
+                  </HtmlTooltip>
                 </Box>
               </>
             )}
