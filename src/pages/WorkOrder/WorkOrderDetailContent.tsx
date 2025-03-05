8ce6954b00c00290b1c0f7450f121336d9970e79
Romit Sadaria <romit.sadaria@gmail.com>
2025-03-05 17:04:18 +0530

    fix: bug fix

--- src/pages/WorkOrder/WorkOrderDetailContent.tsx
+++ src/pages/WorkOrder/WorkOrderDetailContent.tsx
@@ -74,7 +74,7 @@ type ToolbarButton = {
 
 type ToolbarComponents<T> = ToolbarElement<T> | ToolbarButton | ToolbarMenuItem;
 
-const WorkOrderDetailContent = ({ id, tab, resource }) => {
+const WorkOrderDetailContent = ({ id, tab, resource, sendWorkOrderData = null, defaultSelectedService = null, setDefaultSelectedService = null }) => {
   const toastConfig = useContext(CustomToastContext);
   const history = useHistory();
 
@@ -163,20 +163,24 @@ const WorkOrderDetailContent = ({ id, tab, resource }) => {
   }, []);
 
   const getWorkOrderCostFields = async () => {
-    let workOrderCost = await fetch_child_resource_fields(CHILD_RESOURCE.workOrderCost, workOrderData?.currency || user.user?.brandCurrency, true);
-    setWorkOrderCostFields(workOrderCost);
+    if (resource !== sidebarResource.workOrderTechnician) {
+      let workOrderCost = await fetch_child_resource_fields(CHILD_RESOURCE.workOrderCost, workOrderData?.currency || user.user?.brandCurrency, true);
+      setWorkOrderCostFields(workOrderCost);
+    }
   };
 
   const getResourceFields = () => {
-    axiosInstance()
-      .get(`/field?resource=${sidebarResource.workOrder}`)
-      .then(({ data: { data } }) => {
-        const adjustedData = [...data];
-        setWorkOrderFields(adjustedData);
-      })
-      .catch((err) => {
-        toastConfig.setToastConfig(err);
-      });
+    if (resource !== sidebarResource.workOrderTechnician) {
+      axiosInstance()
+        .get(`/field?resource=${sidebarResource.workOrder}`)
+        .then(({ data: { data } }) => {
+          const adjustedData = [...data];
+          setWorkOrderFields(adjustedData);
+        })
+        .catch((err) => {
+          toastConfig.setToastConfig(err);
+        });
+    }
   };
 
   const fetchWorkOrderData = () => {
@@ -186,6 +190,9 @@ const WorkOrderDetailContent = ({ id, tab, resource }) => {
         setAllowedToEdit(checkIsAllowedToEdit(user, sidebarResource.workOrder, data) && permissions?.workOrder?.isUpdate ? true : false);
         setCompleted(data?.status === WORK_ORDER_STATUS.completed || data?.status === WORK_ORDER_STATUS.onHold || data?.deleted ? true : false);
         setWorkOrderData({ ...data });
+        if (sendWorkOrderData) {
+          sendWorkOrderData(data)
+        }
       })
       .catch((err) => {
         toastConfig.setToastConfig(err);
@@ -219,14 +226,16 @@ const WorkOrderDetailContent = ({ id, tab, resource }) => {
   };
 
   const fetchTotalConsumablesCost = () => {
-    axiosInstance()
-      .get(`${routes?.workOrder?.path}/total-consumables-cost/${id}`)
-      .then(({ data: { data } }) => {
-        setTotalConsumablesCost(data?.totalConsumablesCost);
-      })
-      .catch((err) => {
-        toastConfig.setToastConfig(err);
-      });
+    if (resource !== sidebarResource.workOrderTechnician) {
+      axiosInstance()
+        .get(`${routes?.workOrder?.path}/total-consumables-cost/${id}`)
+        .then(({ data: { data } }) => {
+          setTotalConsumablesCost(data?.totalConsumablesCost);
+        })
+        .catch((err) => {
+          toastConfig.setToastConfig(err);
+        });
+    }
   };
 
   const handleDelete = () => {
@@ -552,7 +561,7 @@ const WorkOrderDetailContent = ({ id, tab, resource }) => {
 
   return (
     <Box className="main-container-v1">
-      <Box className="headerbox-v1">
+      <Box className={resource === sidebarResource.workOrderTechnician ? "" : "headerbox-v1"}>
         {resource === sidebarResource.workOrder && (
           <Box className="nav-v1">
             <CustomBreadCrumbs
@@ -560,35 +569,37 @@ const WorkOrderDetailContent = ({ id, tab, resource }) => {
             />
           </Box>
         )}
-        <Box className="controls-v1 ml-auto">
-          <Box className="control-buttons-v1 items-center">
-            {workOrderData ? (
-              <>{!workOrderData?.deleted && <RenderHeaderButtons buttonOptions={toolbarButtons} />}</>
-            ) : (
-              <Skeleton variant="text" width="150px" height="40px" />
-            )}
-            <ActivityButton
-              referenceId={workOrderData?._id}
-              resource={ACTIVITY_RESOURCE.workOrder}
-              resourceLabel={workOrderData?.workOrderNumber}
-              extraRelatedTo={{
-                referenceId: workOrderData?.repairOrder?.optionValue,
-                resource: ACTIVITY_RESOURCE.repairOrder
-              }}
-            />
+        {[sidebarResource.workOrder, sidebarResource.workOrderSupervisor]?.includes(resource) &&
+          <Box className="controls-v1 ml-auto">
+            <Box className="control-buttons-v1 items-center">
+              {workOrderData ? (
+                <>{!workOrderData?.deleted && <RenderHeaderButtons buttonOptions={toolbarButtons} />}</>
+              ) : (
+                <Skeleton variant="text" width="150px" height="40px" />
+              )}
+              <ActivityButton
+                referenceId={workOrderData?._id}
+                resource={ACTIVITY_RESOURCE.workOrder}
+                resourceLabel={workOrderData?.workOrderNumber}
+                extraRelatedTo={{
+                  referenceId: workOrderData?.repairOrder?.optionValue,
+                  resource: ACTIVITY_RESOURCE.repairOrder
+                }}
+              />
+            </Box>
           </Box>
-        </Box>
+        }
       </Box>
       <Box className={`detail-container-v1`}>
         <CustomTabs value={tabValue} onChange={handleMainTabChange}>
-          <CustomTab value={0}>Header</CustomTab>
+          {resource === sidebarResource.workOrder && <CustomTab value={0}>Header</CustomTab>}
           <CustomTab value={1}>Services</CustomTab>
           {!user?.user?.brandPolicy?.workOrderConsumableHide && <CustomTab value={2}>Products/Consumables</CustomTab>}
           {[WORK_ORDER_TYPE.productionOrder, WORK_ORDER_TYPE.assemblyOrder]?.includes(workOrderData?.type) && resourceData?.policy?.showBom && (
             <CustomTab value={3}>BOM</CustomTab>
           )}
           <CustomTab value={4}>Drawings</CustomTab>
-          {!(isMobile && !isTablet) && <CustomTab value={5}>Views</CustomTab>}
+          {!(isMobile && !isTablet) && resource === sidebarResource.workOrder && <CustomTab value={5}>Views</CustomTab>}
           {resourceData && resourceData?.tabs?.length && resourceData?.tabs?.map((tab, i) => <CustomTab value={i + 6}>{tab?.tabName}</CustomTab>)}
         </CustomTabs>
         <TabPanel value={tabValue} index={0}>
@@ -650,9 +661,9 @@ const WorkOrderDetailContent = ({ id, tab, resource }) => {
               allowedToEdit={allowedToEdit}
               completed={completed}
               fetchWorkOrderData={fetchWorkOrderData}
-              resource={sidebarResource.workOrder}
-              defaultSelectedService={null}
-              setDefaultSelectedService={null}
+              resource={resource === sidebarResource.workOrderTechnician ? resource : sidebarResource.workOrder}
+              defaultSelectedService={defaultSelectedService}
+              setDefaultSelectedService={setDefaultSelectedService}
             />
           )}
         </TabPanel>
@@ -836,7 +847,6 @@ const WorkOrderDetailContent = ({ id, tab, resource }) => {
           }}
         />
       )}
-
       {openSerializedPackageDialog && (
         <PackageNumberDialog
           onClose={() => {
