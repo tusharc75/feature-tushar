083bbc8ab292b6e540e5bf8e19c68c1855157d07
ABHIMANYU KUMAR <manupatel18112000@gmail.com>
2025-03-25 12:43:49 +0530

    fix bugs

--- src/pages/SerializedPackages/Assign/index.tsx
+++ src/pages/SerializedPackages/Assign/index.tsx
@@ -89,22 +89,20 @@ const Assign = ({ serializedPackagesData }) => {
           row?.original?.type ? (
             <div className="flex items-center gap-2">
               <p className="text-truncate">{row.original.detail}</p>
-              {row?.original?.type != 'serialNumber' && (
-                <IconButton
-                  size="small"
-                  onClick={() => {
-                    if (row?.original?.type === MATERIAL_TYPE.product) {
-                      window.open(`${routes.productDetail.path}/${row.original.materialId}`);
-                    } else if (row?.original?.type === MATERIAL_TYPE.package) {
-                      window.open(`${routes.packagesDetail.path}/${row.original.materialId}`);
-                    } else {
-                      window.open(`${routes.serializedAssetDetail.path}/${row.original.asset}`);
-                    }
-                  }}
-                >
-                  <FiExternalLink size={16} className="-mt-[2px] text-gray-500 dark:text-gray-300" />
-                </IconButton>
-              )}
+              <IconButton
+                size="small"
+                onClick={() => {
+                  if (row?.original?.type === MATERIAL_TYPE.product) {
+                    window.open(`${routes.productDetail.path}/${row.original.materialId}`);
+                  } else if (row?.original?.type === MATERIAL_TYPE.package) {
+                    window.open(`${routes.packagesDetail.path}/${row.original.materialId}`);
+                  } else {
+                    window.open(`${routes.serializedAssetDetail.path}/${row.original.asset}`);
+                  }
+                }}
+              >
+                <FiExternalLink size={16} className="-mt-[2px] text-gray-500 dark:text-gray-300" />
+              </IconButton>
             </div>
           ) : (
             <NoDataCell />
@@ -169,7 +167,7 @@ const Assign = ({ serializedPackagesData }) => {
         canDrag: false,
         Cell: ({ row }) => (
           <>
-            {permissions?.serializedPackages?.isUpdate && [MATERIAL_TYPE.serializedAsset, 'serialNumber']?.includes(row?.original?.type) && (
+            {permissions?.serializedPackages?.isUpdate && [MATERIAL_TYPE.serializedAsset]?.includes(row?.original?.type) && (
               <HtmlTooltip title="Delete">
                 <IconButton
                   size="small"
@@ -257,8 +255,8 @@ const Assign = ({ serializedPackagesData }) => {
       const subRowsLength = subRows?.length || 0;
       assetsSubRows.forEach((_subRow, j) => {
         _subRow.index = parent.index + '.' + (j + 1 + subRowsLength);
-        _subRow.type = _subRow?.assetDetail ? MATERIAL_TYPE.serializedAsset : 'serialNumber';
-        _subRow.detail = _subRow?.assetDetail?.assetNumber || _subRow?.serialNumberDetail?.serialNumber;
+        _subRow.type = MATERIAL_TYPE.serializedAsset;
+        _subRow.detail = _subRow?.assetDetail?.assetNumber;
         _subRow.parentId = _subRow?.product;
         _subRow.canDelete = serializedPackagesData?.status === SERIALIZED_PACKAGES_STATUS.available;
         subRows.push(_subRow);
@@ -271,20 +269,9 @@ const Assign = ({ serializedPackagesData }) => {
     setIsSubmitting(true);
     let ids = [];
     if (deleteRecord) {
-      if (deleteRecord?.asset) {
-        ids.push({ _id: deleteRecord._id, asset: deleteRecord.asset });
-      } else if (deleteRecord?.serialNumber) {
-        ids.push({ _id: deleteRecord._id, serialNumber: deleteRecord.serialNumber });
-      }
+      ids.push({ _id: deleteRecord._id, asset: deleteRecord.asset });
     } else {
-      ids = selectedRecords
-        ?.filter((r) => [MATERIAL_TYPE.serializedAsset, 'serialNumber']?.includes(r?.type))
-        ?.map((d) => {
-          if (d?.type === 'serialNumber') {
-            return { _id: d?._id, serialNumber: d?.serialNumber };
-          }
-          return { _id: d?._id, asset: d?.asset };
-        });
+      ids = selectedRecords?.filter((r) => r?.type === MATERIAL_TYPE.serializedAsset)?.map((d) => ({ _id: d?._id, asset: d?.asset }));
     }
 
     axiosInstance()
