07874097b2567a570300109b969d319bfc86e1d5
ROMIT <romit@Mac.bbrouter>
2025-03-31 14:37:43 +0530

    commit message

--- src/pages/WorkOrder/Versions/ServiceStepsData.tsx
+++ src/pages/WorkOrder/Versions/ServiceStepsData.tsx
@@ -95,7 +95,7 @@ const ServiceStepsData = ({ servicesData, stepsData }) => {
           }
         }}
         renderInput={(params) => (
-          <TextField {...params} margin="dense" size="small" variant="outlined" label="Select Service" placeholder="Select Service" name="service" />
+          <TextField {...params} margin="dense" size="small" variant="outlined" label="Service" placeholder="Service" name="service" />
         )}
       />
       <Box className="pt-3 " style={{ overflowY: 'auto', height: 'calc(100% - 70px)' }}>
