960fafaa271691ad6510eec8ee8e98bccf33bbcc
ROMIT <romit@Mac.bbrouter>
2025-04-01 16:21:16 +0530

    commit message

--- src/components/Activity/Event/CreateEvent.tsx
+++ src/components/Activity/Event/CreateEvent.tsx
@@ -415,13 +415,11 @@ export const CreateEvent = ({ relatedTo, eventId, handleClose, email, isMinimize
                       name="description"
                       variant="outlined"
                       onChange={(e) => setFieldValue('description', e.target.value.trimStart())}
-                      InputProps={{
-                        sx: {
-                          '& textarea': {
-                            resize: 'vertical',
-                            overflow: 'auto',
-                          }
-                        }
+                      sx={{
+                        '& .MuiInputBase-root textarea': {
+                          resize: 'vertical',
+                          overflow: 'auto',
+                        },
                       }}
                     />
                     {eventId && (
