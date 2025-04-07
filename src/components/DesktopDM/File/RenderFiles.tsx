f59a067afa408b37cedd2c4a1b0710d282fa9830
dhrubobarman <dhrubobarman847@gmail.com>
2025-04-07 16:32:26 +0530

    feat: attachment delete, cached file preview and downlaod, Edit message, some fixes

--- src/components/DesktopDM/File/RenderFiles.tsx
+++ src/components/DesktopDM/File/RenderFiles.tsx
@@ -1,10 +1,13 @@
-import { Close, Download } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
-import React from 'react';
+import { Delete, Download, MoreVert } from '@mui/icons-material';
+import { IconButton, ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material';
+import { useMemo, useState } from 'react';
 import { RenderFileProps } from 'src/components/DesktopDM/File/FilePreview';
+import { handleDownload } from 'src/components/DesktopDM/utils';
 import { cn } from 'src/constants/helpers';
 
-const RenderFiles = ({ files, getFileUrl, handleDownload, hasToDownload, onDelete, showDownloadButton }: RenderFileProps) => {
+const RenderFiles = ({ files, hasToDownload, onDelete, showDownloadButton }: RenderFileProps) => {
+  const [optionMenuAnchor, setOptionMenuAnchor] = useState<HTMLButtonElement | null>(null);
+  const isBothButtonVisible = useMemo(() => showDownloadButton && typeof onDelete === 'function', [showDownloadButton, onDelete]);
   return (
     <div className="grid gap-1">
       {files.map((file) => (
@@ -13,18 +16,48 @@ const RenderFiles = ({ files, getFileUrl, handleDownload, hasToDownload, onDelet
             <file.icon size={30} className="flex-shrink-0" />
             <p className="line-clamp-1 text-sm">{file.fileName}</p>
           </div>
-          <div className="flex items-center">
-            {showDownloadButton && (
-              <IconButton size="small" onClick={() => handleDownload(file)}>
-                <Download fontSize="small" />
+          {isBothButtonVisible ? (
+            <>
+              <IconButton size="small" color={'primary'} onClick={(e) => setOptionMenuAnchor(e.currentTarget)}>
+                <MoreVert fontSize="small" />
               </IconButton>
-            )}
-            {typeof onDelete === 'function' && (
-              <IconButton size="small" onClick={() => onDelete(file)} color={'error'}>
-                <Close fontSize="small" />
-              </IconButton>
-            )}
-          </div>
+              <Menu
+                anchorEl={optionMenuAnchor}
+                disableScrollLock
+                open={Boolean(optionMenuAnchor)}
+                onClose={() => setOptionMenuAnchor(null)}
+                slotProps={{ paper: { onClick: () => setOptionMenuAnchor(null), sx: { minWidth: '150px' } } }}
+              >
+                <MenuItem onClick={() => handleDownload(file, hasToDownload)}>
+                  <ListItemIcon>
+                    <Download color="primary" />
+                  </ListItemIcon>
+                  <ListItemText>Download</ListItemText>
+                </MenuItem>
+                <MenuItem onClick={() => onDelete(file)}>
+                  <ListItemIcon>
+                    <Delete color="error" />
+                  </ListItemIcon>
+                  <ListItemText>Delete</ListItemText>
+                </MenuItem>
+              </Menu>
+            </>
+          ) : (
+            <>
+              <div className="flex items-center">
+                {showDownloadButton && (
+                  <IconButton size="small" onClick={() => handleDownload(file, hasToDownload)}>
+                    <Download fontSize="small" />
+                  </IconButton>
+                )}
+                {typeof onDelete === 'function' && (
+                  <IconButton size="small" onClick={() => onDelete(file)} color={'error'}>
+                    <Delete fontSize="small" />
+                  </IconButton>
+                )}
+              </div>
+            </>
+          )}
         </div>
       ))}
     </div>
--- src/components/DesktopDM/ShowMessage/index.tsx
+++ src/components/DesktopDM/ShowMessage/index.tsx
@@ -24,15 +24,15 @@ export type ShowMessageRef = {
 let timeout: NodeJS.Timeout;
 
 const ShowMessages = React.forwardRef<ShowMessageRef, ShowMessagesProps>(({ data: panelData, state, openedChat }, ref) => {
-  const { toastConfig, socket, user, readMessage, checkIsUser } = state;
+  const { toastConfig, socket, user, readMessage, checkIsUser, currentlyEditingMessage } = state;
   const [messages, setMessages] = useState<{ [key: string]: Message[] }>(null);
   const isUserData = checkIsUser(panelData);
   const containerRef = useRef<HTMLDivElement>(null);
   const pinndedMessges = useMemo(() => {
     return messages
       ? Object.values(messages)
-        .flat()
-        ?.filter((message) => message['pinned'] === true)
+          .flat()
+          ?.filter((message) => message['pinned'] === true)
       : [];
   }, [messages]);
 
@@ -155,15 +155,23 @@ const ShowMessages = React.forwardRef<ShowMessageRef, ShowMessagesProps>(({ data
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [panelData?._id, isUserData]);
 
-  const focusMessage = (messageId: string) => {
+  useEffect(() => {
+    if (currentlyEditingMessage?._id) {
+      focusMessage(currentlyEditingMessage._id, false);
+    }
+  }, [currentlyEditingMessage?._id]);
+
+  const focusMessage = (messageId: string, showBorder = true) => {
     if (containerRef.current) {
       const messageElement = containerRef.current.querySelector(`#message-${messageId}`);
       if (messageElement) {
         messageElement.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-        messageElement.classList.add('outline', 'outline-2', 'outline-theme');
-        setTimeout(() => {
-          messageElement.classList.remove('outline', 'outline-2', 'outline-theme');
-        }, 2000);
+        if (showBorder) {
+          messageElement.classList.add('outline', 'outline-2', 'outline-theme');
+          setTimeout(() => {
+            messageElement.classList.remove('outline', 'outline-2', 'outline-theme');
+          }, 2000);
+        }
       }
     }
   };
@@ -176,7 +184,8 @@ const ShowMessages = React.forwardRef<ShowMessageRef, ShowMessagesProps>(({ data
   }));
 
   return (
-    <div ref={containerRef} className="flex-grow overflow-y-auto scroll-smooth">
+    <div ref={containerRef} className={cn('flex-grow scroll-smooth', currentlyEditingMessage ? 'overflow-hidden' : 'overflow-y-auto')}>
+      {/* Pinned messages */}
       {pinndedMessges.length > 0 && (
         <div className="sticky top-0 z-10 flex w-full items-center justify-between border-b bg-white p-2 text-sm font-semibold text-gray-900 dark:bg-slate-800 dark:text-white">
           <span>Pinned Messages</span>
@@ -191,8 +200,11 @@ const ShowMessages = React.forwardRef<ShowMessageRef, ShowMessagesProps>(({ data
           </span>
         </div>
       )}
+      {/* End of Pinned messages */}
+      {/* Start of Message content */}
       {messages ? (
-        <>
+        <div className={cn('messages relative')}>
+          {currentlyEditingMessage && <div className="absolute inset-0 z-[1] bg-black/50" />}
           {Object.keys(messages).map((date) => {
             const messagesInThatDate = messages[date];
             return (
@@ -203,7 +215,13 @@ const ShowMessages = React.forwardRef<ShowMessageRef, ShowMessagesProps>(({ data
                     return (
                       <div className={cn('flex  items-start gap-2.5', isUserMessage ? 'flex-row-reverse' : '')} key={message._id}>
                         <RenderAvatar message={message} />
-                        <RenderContent message={message} isUserMessage={isUserMessage} className="scroll-m-[60px]" id={`message-${message._id}`} />
+                        <RenderContent
+                          message={message}
+                          state={state}
+                          isUserMessage={isUserMessage}
+                          className={cn('scroll-m-[60px]', currentlyEditingMessage?._id === message._id ? 'z-[2] bg-[--dark-primary,white]' : '')}
+                          id={`message-${message._id}`}
+                        />
                         <RenderButton message={message} isUserMessage={isUserMessage} state={state} />
                       </div>
                     );
@@ -212,7 +230,7 @@ const ShowMessages = React.forwardRef<ShowMessageRef, ShowMessagesProps>(({ data
               </Fragment>
             );
           })}
-        </>
+        </div>
       ) : isUserData ? (
         <div className="flex h-full items-center justify-center">
           <p>Send Message</p>
@@ -222,6 +240,7 @@ const ShowMessages = React.forwardRef<ShowMessageRef, ShowMessagesProps>(({ data
           <CommonSkeleton />
         </div>
       )}
+      {/* End of Message content */}
     </div>
   );
 });
