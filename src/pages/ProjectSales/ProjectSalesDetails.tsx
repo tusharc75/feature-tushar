ecd70cc276ae081889b8f779f680250f49e3dccb
dhrubobarman <dhrubobarman847@gmail.com>
2025-04-14 18:15:41 +0530

    fix: technician scheduler

--- src/pages/ProjectSales/ProjectSalesDetails.tsx
+++ src/pages/ProjectSales/ProjectSalesDetails.tsx
@@ -1,12 +1,10 @@
-import { Box, IconButton, Paper, Typography } from '@mui/material';
-import Grid from '@mui/material/Grid2';
 import { ControlPoint } from '@mui/icons-material';
-import { Skeleton } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import { Box, IconButton, Paper, Skeleton, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import queryString from 'query-string';
 import React, { useContext, useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import EditIcon from '@mui/icons-material/Edit';
-import queryString from 'query-string';
-import { isMobile, isTablet } from 'react-device-detect';
 import ActivityButton from 'src/components/Activity/ActivityButton';
 import CustomTabs, { CustomTab, TabPanel } from 'src/components/CustomTabs';
 import { DeleteButton, ThemeButton } from 'src/components/Helpers/Buttons';
