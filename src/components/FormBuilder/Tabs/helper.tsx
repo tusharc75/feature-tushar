a3ce5b311e9a689cb84476eb01cb87def7ba0501
ROMIT <romit@Mac.bbrouter>
2025-04-15 17:14:23 +0530

    commit message

--- src/components/FormBuilder/Tabs/helper.tsx
+++ src/components/FormBuilder/Tabs/helper.tsx
@@ -237,6 +237,12 @@ export const resourcePolicy = [
         defaultValue: false
       },
       {
+        fieldName: 'nonSerializedProductReceivingTicket',
+        fieldLabel: 'Non Serialized Product Receiving Ticket',
+        type: 'checkBox',
+        defaultValue: false
+      },
+      {
         fieldName: 'loadingReceivingDefaultView',
         fieldLabel: 'Loading Receiving Default View',
         type: 'dropDown',
@@ -245,7 +251,7 @@ export const resourcePolicy = [
           { optionValue: 'parentChild', optionLabel: 'Parent Child', order: 1 }
         ],
         defaultValue: ''
-      }
+      },
     ]
   },
   {
