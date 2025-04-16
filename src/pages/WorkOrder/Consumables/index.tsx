b8753c66374c5a8bd24abe2b343d42fbd1a2e22d
ROMIT <romit@Mac.bbrouter>
2025-04-16 19:22:39 +0530

    commit message

--- src/pages/WorkOrder/Consumables/index.tsx
+++ src/pages/WorkOrder/Consumables/index.tsx
@@ -54,6 +54,7 @@ const Consumables = ({
   defaultServiceUniqueId = null,
   tableHeight = null
 }) => {
+
   let renderedFrom = `${camelCase(sidebarResource?.workOrder)}_consumable`;
 
   const workOrderId = workOrderData?._id;
@@ -103,7 +104,7 @@ const Consumables = ({
 
   useEffect(() => {
     fetchData();
-  }, [allowedToEdit, workOrderId, selectedService]);
+  }, [allowedToEdit, workOrderId, selectedService, uniqueId]);
 
   const handleUpdate = async (row: any) => {
     setUpdating(true);
@@ -158,8 +159,8 @@ const Consumables = ({
             return row.original[e?.fieldName] ? (
               <div className="flex items-center gap-2">
                 {hasChildFields &&
-                allowedToEdit &&
-                ![MATERIAL_TYPE.serializedAsset, OTHER_MATERIAL_TYPE.serialNumber]?.includes(row?.original?.type) ? (
+                  allowedToEdit &&
+                  ![MATERIAL_TYPE.serializedAsset, OTHER_MATERIAL_TYPE.serialNumber]?.includes(row?.original?.type) ? (
                   <p
                     className={'link text-truncate'}
                     onClick={() => {
@@ -264,24 +265,24 @@ const Consumables = ({
       },
       ...(user?.user?.brandPolicy?.workOrderConsumableRequest && !user?.user?.brandPolicy?.workOrderConsumableConsumeHide
         ? [
-            {
-              accessor: 'requestedQty',
-              Header: 'Requested Qty',
-              width: 150,
-              Cell: ({ row }) => <p className="text-truncate">{row?.original?.requestedQty || <NoDataCell />}</p>
-            }
-          ]
+          {
+            accessor: 'requestedQty',
+            Header: 'Requested Qty',
+            width: 150,
+            Cell: ({ row }) => <p className="text-truncate">{row?.original?.requestedQty || <NoDataCell />}</p>
+          }
+        ]
         : []),
       ...(!user?.user?.brandPolicy?.workOrderConsumableConsumeHide
         ? [
-            {
-              accessor: 'consumedQty',
-              Header: 'Consumed Qty',
-              primaryField: true,
-              width: 150,
-              Cell: ({ row }) => <p className="text-truncate">{row?.original?.consumedQty || <NoDataCell />}</p>
-            }
-          ]
+          {
+            accessor: 'consumedQty',
+            Header: 'Consumed Qty',
+            primaryField: true,
+            width: 150,
+            Cell: ({ row }) => <p className="text-truncate">{row?.original?.consumedQty || <NoDataCell />}</p>
+          }
+        ]
         : [])
     ];
     extracolumns.push({
@@ -506,7 +507,7 @@ const Consumables = ({
   const createNewVersionQuote = async (quoteId, quoteVersionId) => {
     axiosInstance()
       .post(`/quotation/clone-version/${quoteId}/${quoteVersionId}`)
-      .then(() => {})
+      .then(() => { })
       .catch((error) => {
         toastConfig.setToastConfig(error);
       });
@@ -615,7 +616,7 @@ const Consumables = ({
           <ThemeButton
             disabled={
               selectedRecords?.length &&
-              selectedRecords?.every((r) => !r?.serializedProduct && !r?.hideSelection && r?.type === MATERIAL_TYPE.product)
+                selectedRecords?.every((r) => !r?.serializedProduct && !r?.hideSelection && r?.type === MATERIAL_TYPE.product)
                 ? false
                 : true
             }
