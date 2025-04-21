8b7f2a3445eb2c92d3f51660e3ae6af6980584f3
dhrubobarman <dhrubobarman847@gmail.com>
2025-04-21 15:01:54 +0530

    fix: fix

--- src/pages/WorkOrder/Diagram/ImageEditor/RightSidebar.tsx
+++ src/pages/WorkOrder/Diagram/ImageEditor/RightSidebar.tsx
@@ -28,7 +28,7 @@ const RightSidebar = ({ imageEditor }: RightSidebarPorps) => {
 
   return (
     <div className="pointer-events-none absolute bottom-0 right-1 top-0 flex h-full w-0 items-center justify-end">
-      <div className="pointer-events-auto w-fit min-w-[55px] rounded-[55px] bg-[--dark-secondary,white] px-2 py-4">
+      <div className="pointer-events-auto w-fit min-w-[55px] rounded-[55px] border  bg-[--dark-secondary,white] px-2 py-4">
         <ul className="flex list-none flex-col items-center justify-center space-y-2">
           {/* <li>
             <HtmlTooltip title={'Zoom in'} placement="right">
