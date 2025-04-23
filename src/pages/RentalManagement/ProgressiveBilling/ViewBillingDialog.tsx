229c93f51b2366d1e517e8362b22ac23c1a6994d
harshit <harshit@vebholic.com>
2025-04-23 12:43:19 +0530

    EP-5028

--- src/pages/RentalManagement/ProgressiveBilling/ViewBillingDialog.tsx
+++ src/pages/RentalManagement/ProgressiveBilling/ViewBillingDialog.tsx
@@ -362,7 +362,7 @@ const ViewBillingDialog = ({ rentalManagementData, invoiceId, onClose, onSuccess
   const handleDeleteData = async (rows) => {
     const data = {
       invoiceId: invoiceData?._id,
-      materialIds: rows?.map((e) => e.materialId) || []
+      _id: rows?.map((e) => e._id) || []
     };
     axiosInstance()
       .put(`${rentalManagement.api}/${rentalManagementData._id}/progressive-billing/remove`, data)
