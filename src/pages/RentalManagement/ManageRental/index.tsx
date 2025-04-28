099e64731496839ace0cdd0eb5583e7fe2f78a53
harshit <harshit@vebholic.com>
2025-04-28 17:35:03 +0530

    EP-103

--- src/pages/RentalManagement/ManageRental/index.tsx
+++ src/pages/RentalManagement/ManageRental/index.tsx
@@ -204,7 +204,10 @@ const ManageRentalManagementDialog = ({
     const errors = {};
     let estimateStartDate = dayjs(values?.estimateStartDate);
     let estimateEndDate = dayjs(values?.estimateEndDate);
-    if (estimateEndDate.diff(estimateStartDate, 'day') < 0) {
+    if (!estimateStartDate?.isValid()) {
+      errors['estimateStartDate'] = 'Please enter valid estimate start date';
+    } 
+    if (!estimateEndDate?.isValid() || estimateEndDate.diff(estimateStartDate, 'day') < 0) {
       errors['estimateEndDate'] = 'Please enter valid estimate end date';
     }
     // let actualStartDate = dayjs(values?.actualStartDate);
