74da3773a24d1fd034a56aec32cffbd0667e8837
dhrubobarman <dhrubobarman847@gmail.com>
2025-04-29 16:51:20 +0530

    fix: always visible thumbnail actions

--- src/components/AttachmentThumbnail/index.tsx
+++ src/components/AttachmentThumbnail/index.tsx
@@ -1,23 +1,21 @@
-import { IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import GetAppIcon from '@mui/icons-material/GetApp';
 import PreviewIcon from '@mui/icons-material/Visibility';
+import { IconButton } from '@mui/material';
 import axios from 'axios';
-import _ from 'lodash';
 import mimeDb from 'mime-db';
-import { Fragment, useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import { CustomToastContext } from 'src/StateProvider/CustomToastContext/CustomToastContext';
 import { FileIcon, fileIcons } from 'src/assets/fileIcons';
 import axiosInstance from 'src/axios/axiosInstance';
 import ConfirmationDialog from 'src/components/Helpers/ConfirmationDialog';
-import HtmlTooltip from '../CustomTooltipTitle';
 import { deleteDisable } from 'src/constants/messageHelpers';
+import HtmlTooltip from '../CustomTooltipTitle';
 
 const imageExtensions = ['tif', 'tiff', 'bmp', 'jpg', 'jpeg', 'gif', 'png', 'eps', 'raw', 'cr2', 'nef', 'orf', 'sr2'];
 const pdfExtensions = ['pdf'];
 
 const AttachmentThumbnail = ({ attachments, handleDeleteAttachment, allowedToEdit }) => {
-
   const toastConfig = useContext(CustomToastContext);
   const [, setDownloadProgress] = useState(0);
   const [, setIsDownloading] = useState(false);
@@ -182,90 +180,72 @@ const AttachmentThumbnail = ({ attachments, handleDeleteAttachment, allowedToEdi
             {attachments.map((attachment, i) => {
               const Icon = getFileIconSrc(attachment?.contentType ? attachment?.contentType : attachment.url ? attachment.url : attachment);
               return (
-                <Fragment key={i}>
-                  <div className="group relative min-h-[153px] w-[138px] max-w-[138px] flex-grow basis-[138px] rounded-[4px] border border-[var(--common-border-color)] p-[var(--gutter)] [--gutter:18px]">
-                    <div className="front  group-hover:hidden">
-                      <div className="mx-auto mb-[11px] h-[79px] text-center">
-                        <Icon size={79} className="mx-auto" />
-                      </div>
-                      <p className=" line-clamp-1 text-[14px] text-[var(--text-primary)]">
-                        {attachment
-                          ? attachment?.name
-                            ? attachment?.name
-                            : attachment?.url?.substring(attachment.url.lastIndexOf('/') + 1)
-                              ? attachment?.url?.substring(attachment.url.lastIndexOf('/') + 1)
-                              : attachment.substring(attachment.lastIndexOf('/') + 1)
-                          : 'attachment'}
-                      </p>
-                    </div>
-                    <div className="back absolute inset-0 flex flex-col justify-between p-[var(--gutter)] opacity-0 group-hover:opacity-100">
-                      <p
-                        className=" line-clamp-4 text-[14px] text-[var(--text-primary)]"
-                        title={
-                          attachment
-                            ? attachment?.name
-                              ? attachment?.name
-                              : attachment?.url?.substring(attachment.url.lastIndexOf('/') + 1)
-                                ? attachment?.url?.substring(attachment.url.lastIndexOf('/') + 1)
-                                : attachment?.substring(attachment.lastIndexOf('/') + 1)
-                            : 'attachment'
-                        }
-                      >
-                        {attachment
-                          ? attachment?.name
-                            ? attachment?.name
-                            : attachment?.url?.substring(attachment.url.lastIndexOf('/') + 1)
-                              ? attachment?.url?.substring(attachment.url.lastIndexOf('/') + 1)
-                              : attachment?.substring(attachment.lastIndexOf('/') + 1)
-                          : 'attachment'}
-                      </p>
-                      <div className="flex justify-between">
-                        <HtmlTooltip title="Download" placement="top" enterTouchDelay={0}>
-                          <IconButton
-                            size={'small'}
-                            onClick={(event) => downloadFile(event, attachment)}
-                            style={{ paddingBottom: 3, width: 30, height: 30 }}
-                          >
-                            <GetAppIcon color="primary" />
-                          </IconButton>
-                        </HtmlTooltip>
-                        {[...imageExtensions, ...pdfExtensions]?.includes(attachment?.url?.split('.')?.pop()?.toLowerCase()) && (
-                          <HtmlTooltip title="Preview" placement="top" enterTouchDelay={0}>
-                            <IconButton
-                              size={'small'}
-                              onClick={(e) => {
-                                viewAttachment(e, attachment.url);
-                              }}
-                              style={{ paddingBottom: 3, width: 30, height: 30 }}
-                            >
-                              <PreviewIcon color="primary" />
-                            </IconButton>
-                          </HtmlTooltip>
-                        )}
-                        {allowedToEdit ? (
-                          <HtmlTooltip title="Delete" placement="top" enterTouchDelay={0}>
-                            <IconButton
-                              size={'small'}
-                              onClick={() => {
-                                setShowConfirmationDialog(true);
-                                setAttachemnetToDelete(attachment);
-                              }}
-                              style={{ paddingBottom: 3, width: 30, height: 30 }}
-                            >
-                              {<DeleteIcon color="error" fontSize="small" />}
-                            </IconButton>
-                          </HtmlTooltip>
-                        ) : (
-                          <HtmlTooltip className="cursor-stop" title={deleteDisable} enterTouchDelay={0}>
-                            <IconButton size={'small'} style={{ paddingBottom: 3, width: 30, height: 30 }}>
-                              <DeleteIcon color="disabled" fontSize="small" />
-                            </IconButton>
-                          </HtmlTooltip>
-                        )}
-                      </div>
-                    </div>
+                <div
+                  key={i}
+                  title={
+                    attachment
+                      ? attachment?.name
+                        ? attachment?.name
+                        : attachment?.url?.substring(attachment.url.lastIndexOf('/') + 1)
+                          ? attachment?.url?.substring(attachment.url.lastIndexOf('/') + 1)
+                          : attachment.substring(attachment.lastIndexOf('/') + 1)
+                      : 'attachment'
+                  }
+                  className="group relative min-h-[153px] w-[138px] max-w-[138px] flex-grow basis-[138px] rounded-[4px] border border-[var(--common-border-color)] p-[var(--gutter)] [--gutter:8px]"
+                >
+                  <div className="mx-auto mb-[11px] h-[79px] text-center">
+                    <Icon size={79} className="mx-auto" />
                   </div>
-                </Fragment>
+                  <p className="line-clamp-1 text-[14px] text-[var(--text-primary)]">
+                    {attachment
+                      ? attachment?.name
+                        ? attachment?.name
+                        : attachment?.url?.substring(attachment.url.lastIndexOf('/') + 1)
+                          ? attachment?.url?.substring(attachment.url.lastIndexOf('/') + 1)
+                          : attachment.substring(attachment.lastIndexOf('/') + 1)
+                      : 'attachment'}
+                  </p>
+                  <div className="flex justify-between">
+                    <HtmlTooltip title="Download" placement="top" enterTouchDelay={0}>
+                      <IconButton
+                        size={'small'}
+                        onClick={(event) => downloadFile(event, attachment)}
+                        style={{ paddingBottom: 3, width: 30, height: 30 }}
+                        color="primary"
+                      >
+                        <GetAppIcon fontSize="small" />
+                      </IconButton>
+                    </HtmlTooltip>
+                    {[...imageExtensions, ...pdfExtensions]?.includes(attachment?.url?.split('.')?.pop()?.toLowerCase()) && (
+                      <HtmlTooltip title="Preview" placement="top" enterTouchDelay={0}>
+                        <IconButton
+                          size={'small'}
+                          onClick={(e) => {
+                            viewAttachment(e, attachment.url);
+                          }}
+                          style={{ paddingBottom: 3, width: 30, height: 30 }}
+                          color="primary"
+                        >
+                          <PreviewIcon fontSize="small" />
+                        </IconButton>
+                      </HtmlTooltip>
+                    )}
+                    <HtmlTooltip title={allowedToEdit ? 'Delete' : deleteDisable} placement="top" enterTouchDelay={0}>
+                      <IconButton
+                        size={'small'}
+                        disabled={!allowedToEdit}
+                        onClick={() => {
+                          setShowConfirmationDialog(true);
+                          setAttachemnetToDelete(attachment);
+                        }}
+                        style={{ paddingBottom: 3, width: 30, height: 30 }}
+                        color="error"
+                      >
+                        <DeleteIcon fontSize="small" />
+                      </IconButton>
+                    </HtmlTooltip>
+                  </div>
+                </div>
               );
             })}
           </>
