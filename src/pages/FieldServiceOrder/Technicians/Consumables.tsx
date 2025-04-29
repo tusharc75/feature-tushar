2bae8a3ff1e947a0eabf05f9355cff06e8b633f5
ROMIT <romit@Mac.bbrouter>
2025-04-29 19:18:46 +0530

    commit message

--- src/pages/FieldServiceOrder/Technicians/Consumables.tsx
+++ src/pages/FieldServiceOrder/Technicians/Consumables.tsx
@@ -251,6 +251,7 @@ const Consumables = ({
         const calValues = autoCalculateSpecificFields(values, element, allFields);
         Object.assign(element, calValues);
         element = { materialId: e.materialId, type: MATERIAL_TYPE.product, ...getObjKeysWithValues(element, allFields) };
+        element.technician = selectedTechnician?.technicianId === 'All' ? null : selectedTechnician?.technicianId;
         material.push(element);
       });
       AddMaterial(material, null);
