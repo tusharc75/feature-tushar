6fcc1241058264e0a2bcd732f415c3ea69b86bd8
ROMIT <romit@Mac.bbrouter>
2025-05-08 17:43:27 +0530

    commit message

--- src/constants/messageHelpers.tsx
+++ src/constants/messageHelpers.tsx
@@ -12,6 +12,7 @@ export const actionDisable = "You don't have permission to perform this action";
 export const actionSelection = 'Select at least one line item to enable this button';
 
 export const cloneDisable = 'You do not have permission to Clone/Create';
+export const createDisable = 'You do not have permission to Create';
 export const deleteDisable = 'You do not have permission to Delete';
 export const editDisable = 'You do not have permission to Edit';
 export const addDisable = 'You do not have permission to Add';
