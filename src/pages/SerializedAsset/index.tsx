14daf661c95e68c9ec55938e46fd13a05377d9d3
Vivek <10Vivekkumaryadav@gmail.com>
2025-05-13 18:29:14 +0530

    fix: add tooltip to "Change Status" in SerializedAsset

--- src/pages/SerializedAsset/index.tsx
+++ src/pages/SerializedAsset/index.tsx
@@ -784,7 +784,7 @@ const SerializedAsset = () => {
           searchValue={search}
           onSearch={handleSearch}
           isActionButtonVisible={true}
-          actionButtonProps={{ disabled: selectedRecords?.length ? false : true }}
+          actionButtonProps={{ disabled: selectedRecords?.length ? false : true, tooltip: "Change Status" }}
           actionMenuItems={<ActionMenuItems />}
           addButtonProps={{ disabled: !permissions?.serializedAsset?.isCreate }}
           addButtonOnclick={() => {
