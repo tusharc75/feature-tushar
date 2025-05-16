8a3cffbab7805131771a8be1e303e38c7b1ae322
dhrubobarman <dhrubobarman847@gmail.com>
2025-05-16 17:46:57 +0530

    fix: fix

--- src/pages/PlanningView/Calendar/index.tsx
+++ src/pages/PlanningView/Calendar/index.tsx
@@ -664,7 +664,7 @@ function CalendarView({ resourceList, selectedResource, setSelectedResource, set
 
   const handleClick = useCallback(
     (args: EventClickArg) => {
-      const data = { ...args.event, ...args.event.extendedProps } as any;
+      const data = { ...args.event, ...args.event._def, ...args.event.extendedProps, start: args.event.start, end: args.event.end } as any;
       if (selectedResource.resource === sidebarResource.product) {
         if (data?.type === 'assetStatus') {
           let query = `?assetStatus=${data?.status}`;
@@ -812,7 +812,6 @@ function CalendarView({ resourceList, selectedResource, setSelectedResource, set
   }, [selectedResource]);
 
   const dragAndDropOnSelectEvent = useCallback((args: EventClickArg) => {
-    console.log(args.event._def);
     setShowDetail({ open: true, data: args.event, anchor: args.el });
   }, []);
 
