3384602cfd118730af12de31655acca797a9775f
dhrubobarman <dhrubobarman847@gmail.com>
2025-05-20 15:48:53 +0530

    fix: calendar mobile view

--- src/components/CustomCalendar/utils.ts
+++ src/components/CustomCalendar/utils.ts
@@ -1,8 +1,10 @@
 import dayjs from 'dayjs';
 
 type ParsedEvents<D> = {
-  start: string;
-  end: string;
+  start: string | Date;
+  end: string | Date;
+  id?: string;
+  _id?: string;
 } & D;
 
 export const parseEventForMobile = <D>(events: ParsedEvents<D>[]): D[] => {
@@ -19,5 +21,39 @@ export const parseEventForMobile = <D>(events: ParsedEvents<D>[]): D[] => {
       date = date.add(1, 'day');
     }
   });
+  return Object.values(parsedEventsMap);
+};
+
+export const parseEventForMobile1 = <D>(events: ParsedEvents<D>[], startRange: Date, endRange: Date): D[] => {
+  // const parsedEvents: ParsedEvents<D>[] = [];
+  if (!events || events.length === 0 || !endRange) {
+    return [];
+  }
+  const parsedEventsMap: { [key: string]: ParsedEvents<D> } = {};
+  const startR = dayjs(startRange);
+  const endR = dayjs(endRange);
+  for (const event of events) {
+    const startDate = dayjs(event.start);
+    const endDate = dayjs(event.end);
+    for (let date = startDate.clone(); date.isSameOrBefore(endDate); ) {
+      if (date.isBefore(startR.subtract(1, 'day'))) {
+        date = date.add(1, 'day');
+        continue;
+      }
+      const formattedDate = date.toDate();
+      const formatOnlyDate = date.format('YYYY-MM-DD');
+      const newEvent = { ...event, start: formattedDate, end: formattedDate };
+      if (event.id || event._id) {
+        newEvent['_id'] = event._id;
+        newEvent['id'] = `${event.id}-${formatOnlyDate}`;
+      }
+      parsedEventsMap[formatOnlyDate] = newEvent;
+      if (date.isSameOrAfter(endR)) {
+        break;
+      }
+      date = date.add(1, 'day');
+    }
+  }
+
   return Object.values(parsedEventsMap);
 };
