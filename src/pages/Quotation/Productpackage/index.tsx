b61f9f24073415a1185cd2517905001a955efbc9
ROMIT <romit@Mac.bbrouter>
2025-05-26 19:16:19 +0530

    commit message

--- src/pages/Quotation/Productpackage/index.tsx
+++ src/pages/Quotation/Productpackage/index.tsx
@@ -209,14 +209,13 @@ const Productpackage = ({
                 size="small"
                 onClick={() => {
                   window.open(
-                    `${
-                      row.original.type === MATERIAL_TYPE.serializedAsset
-                        ? routes.serializedAssetDetail.path
-                        : row.original.type === MATERIAL_TYPE.product
-                          ? routes.productDetail.path
-                          : row.original.type === MATERIAL_TYPE.package
-                            ? routes.packagesDetail.path
-                            : routes.serviceMasterDetail.path
+                    `${row.original.type === MATERIAL_TYPE.serializedAsset
+                      ? routes.serializedAssetDetail.path
+                      : row.original.type === MATERIAL_TYPE.product
+                        ? routes.productDetail.path
+                        : row.original.type === MATERIAL_TYPE.package
+                          ? routes.packagesDetail.path
+                          : routes.serviceMasterDetail.path
                     }/${row.original.materialId}`
                   );
                 }}
@@ -229,19 +228,19 @@ const Productpackage = ({
       },
       ...(user?.user?.brandPolicy?.leadTime
         ? [
-            {
-              accessor: 'leadTime',
-              Header: 'Lead Time (Days)',
-              Cell: ({ row }) => <div>{row.original['leadTime'] ? <p>{row.original['leadTime']}</p> : 0}</div>,
-              Footer: (info) => {
-                let rows = info.table.getExpandedRowModel().rows;
-                const total = rows
-                  ?.filter((f) => f.original.hasOwnProperty('leadTime') && !isNaN(f.original['leadTime']))
-                  .reduce((sum, row) => parseInt(row.original['leadTime']) + sum, 0);
-                return <>{total}</>;
-              }
+          {
+            accessor: 'leadTime',
+            Header: 'Lead Time (Days)',
+            Cell: ({ row }) => <div>{row.original['leadTime'] ? <p>{row.original['leadTime']}</p> : 0}</div>,
+            Footer: (info) => {
+              let rows = info.table.getExpandedRowModel().rows;
+              const total = rows
+                ?.filter((f) => f.original.hasOwnProperty('leadTime') && !isNaN(f.original['leadTime']))
+                .reduce((sum, row) => parseInt(row.original['leadTime']) + sum, 0);
+              return <>{total}</>;
             }
-          ]
+          }
+        ]
         : []),
       {
         accessor: 'description',
@@ -338,17 +337,16 @@ const Productpackage = ({
     let rows = data.material.filter((e) => e.parentId === null);
     rows.forEach((parent, i) => {
       parent.index = i + 1;
-      parent.detail = `${
-        parent.type === MATERIAL_TYPE.serializedAsset
-          ? parent.serializedAssetDetail?.assetNumber
-          : parent.type === MATERIAL_TYPE.product
-            ? parent.productDetail?.productName
-            : parent.type === MATERIAL_TYPE.service
-              ? parent.serviceDetail?.serviceName
-              : parent.type === MATERIAL_TYPE.package
-                ? parent.packageDetail?.packageName
-                : parent.detail
-      }`;
+      parent.detail = `${parent.type === MATERIAL_TYPE.serializedAsset
+        ? parent.serializedAssetDetail?.assetNumber
+        : parent.type === MATERIAL_TYPE.product
+          ? parent.productDetail?.productName
+          : parent.type === MATERIAL_TYPE.service
+            ? parent.serviceDetail?.serviceName
+            : parent.type === MATERIAL_TYPE.package
+              ? parent.packageDetail?.packageName
+              : parent.detail
+        }`;
       parent.description =
         parent.type === MATERIAL_TYPE.service
           ? parent?.serviceDetail?.serviceDescription || ''
@@ -378,15 +376,14 @@ const Productpackage = ({
     const subRows: any = material.filter((e) => e.parentId === parent._id);
     subRows.forEach((_subRow, index) => {
       _subRow.index = parent.index + '.' + `${index + 1}`;
-      _subRow.detail = `${
-        _subRow.type === MATERIAL_TYPE.serializedAsset
-          ? _subRow.serializedAssetDetail?.assetNumber
-          : _subRow.type === MATERIAL_TYPE.product
-            ? _subRow.productDetail?.productName
-            : _subRow.type === MATERIAL_TYPE.service
-              ? _subRow.serviceDetail?.serviceName
-              : _subRow.packageDetail?.packageName
-      }`;
+      _subRow.detail = `${_subRow.type === MATERIAL_TYPE.serializedAsset
+        ? _subRow.serializedAssetDetail?.assetNumber
+        : _subRow.type === MATERIAL_TYPE.product
+          ? _subRow.productDetail?.productName
+          : _subRow.type === MATERIAL_TYPE.service
+            ? _subRow.serviceDetail?.serviceName
+            : _subRow.packageDetail?.packageName
+        }`;
       _subRow.description =
         _subRow.type === MATERIAL_TYPE.service
           ? _subRow?.serviceDetail?.serviceDescription || ''
@@ -723,8 +720,8 @@ const Productpackage = ({
           </MenuItem>
         )}
         {(quotationData?.type === QUOTATION_TYPE.rentalJob && !user?.user?.brandPolicy?.rentalService) ||
-        quotationData?.type === QUOTATION_TYPE.assemblyOrder ||
-        quotationData?.type === QUOTATION_TYPE.repairOrder ? null : (
+          quotationData?.type === QUOTATION_TYPE.assemblyOrder ||
+          quotationData?.type === QUOTATION_TYPE.repairOrder ? null : (
           <MenuItem
             onClick={() => {
               setAddDialog({ open: true, type: MATERIAL_TYPE.service, parentId: null });
@@ -807,8 +804,8 @@ const Productpackage = ({
           disabled={
             !Boolean(
               selectedRecords &&
-                selectedRecords.filter((e) => !e.hideSelection).length &&
-                !selectedRecords.some((e) => e.type === MATERIAL_TYPE.manualEntry)
+              selectedRecords.filter((e) => !e.hideSelection).length &&
+              !selectedRecords.some((e) => e.type === MATERIAL_TYPE.manualEntry)
             )
           }
           onClick={() => {
@@ -872,7 +869,9 @@ const Productpackage = ({
 
   const RightSideContents = () => {
     return (
-      <ThemeButton buttonType="theme" onClick={() => setAiPriceSuggestionDialog(true)}>
+      <ThemeButton
+        buttonType="theme"
+        onClick={() => setAiPriceSuggestionDialog(true)}>
         AI Price Suggestions
       </ThemeButton>
     );
@@ -886,7 +885,7 @@ const Productpackage = ({
         isActionButtonVisible={allowedToEdit}
         actionButtonMenuItems={actionButtonMenuItems()}
         actionButtonProps={{ disabled: dataRows?.length > 0 ? false : true }}
-        rightSideContents={<RightSideContents />}
+        rightSideContents={dataRows?.length > 0 ? <RightSideContents /> : null}
         hasXpadding
       />
       {columns ? (
@@ -1055,9 +1054,10 @@ const Productpackage = ({
       )}
       {aiPriceSuggestionDialog && (
         <AiPriceSuggestionsDialog
-          setAiPriceSuggestionDialog={setAiPriceSuggestionDialog}
-          aiPriceSuggestionDialog={aiPriceSuggestionDialog}
-          quoteData={quotationData}
+          handleClose={() => {
+            setAiPriceSuggestionDialog(false)
+          }}
+          quotationData={quotationData}
         />
       )}
       {leadTimeDialog.open && (
@@ -1119,8 +1119,8 @@ const Productpackage = ({
               Add Existing Products
             </MenuItem>
             {quotationData?.type === QUOTATION_TYPE.fieldJob && !fieldTicketPolicyData?.policy?.showAddPackages ? null : [
-                QUOTATION_TYPE.repairOrder
-              ]?.includes(quotationData?.type) ? null : (
+              QUOTATION_TYPE.repairOrder
+            ]?.includes(quotationData?.type) ? null : (
               <MenuItem
                 onClick={() => {
                   setAddDialog({ open: true, type: 'package', parentId: addchildDialog.parentId });
@@ -1131,9 +1131,9 @@ const Productpackage = ({
               </MenuItem>
             )}
             {quotationData?.type === QUOTATION_TYPE.rentalJob && !user?.user?.brandPolicy?.rentalService ? null : [
-                QUOTATION_TYPE.fieldJob,
-                QUOTATION_TYPE.repairOrder
-              ]?.includes(quotationData?.type) ? null : (
+              QUOTATION_TYPE.fieldJob,
+              QUOTATION_TYPE.repairOrder
+            ]?.includes(quotationData?.type) ? null : (
               <MenuItem
                 onClick={() => {
                   setAddDialog({ open: true, type: 'service', parentId: addchildDialog.parentId });
