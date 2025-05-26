aede09ec2680008522bd368f01e50ede04eacdc4
Rishabhsharawat15 <rishabhsharawat150@gmail.com>
2025-05-26 15:52:28 +0530

    fix

--- src/components/Helpers/FormTypes/AsyncDropdown.tsx
+++ src/components/Helpers/FormTypes/AsyncDropdown.tsx
@@ -73,7 +73,7 @@ const AsyncDropDown = ({ resource, multiple, errors, touched, value, fieldLabel,
         loading={loading}
         options={options}
         autoHighlight
-        value={value ? value : multiple ? [] : ''}
+        value={value ? value : multiple ? [] : null}
         getOptionLabel={(option: any) => option?.optionLabel}
         isOptionEqualToValue={(option, val) => option?.optionValue === val?.optionValue}
         onChange={onChange}
