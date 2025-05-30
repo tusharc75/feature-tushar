468af4b1fa0b638efbdf8da72e8588745b13f66b
Abhi76317 <manupatel18112000@gmail.com>
2025-05-30 16:17:24 +0530

    fix bugs

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -65,7 +65,7 @@ const Dashboard = () => {
           businessUnit: businessUnitOptions,
           frequency: frequencyData
         });
-      } catch (error) {}
+      } catch (error) { }
     })();
     fetchDashboards();
   }, [selectedEntity]);
