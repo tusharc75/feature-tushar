2a07d7c32e77b02d40153e6711504a938207f198
dhrubobarman <dhrubobarman847@gmail.com>
2025-06-06 12:04:19 +0530

    fix: fix

--- src/pages/FieldView/MapView/index.tsx
+++ src/pages/FieldView/MapView/index.tsx
@@ -1,6 +1,7 @@
-import { GoogleMap, InfoWindow, Marker, InfoBox } from '@react-google-maps/api';
-import { useEffect, useState } from 'react';
-import { cn } from 'src/constants/helpers';
+import { GoogleMap, InfoWindow, Marker } from '@react-google-maps/api';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { useAppTheme } from 'src/constants/AppConfig';
+import { cn, mapDarkTheme, mapLightTheme } from 'src/constants/helpers';
 import { generateData, getAddressPayload } from 'src/pages/FieldView/CardView/utils';
 import { FieldViewResource, TData } from 'src/pages/FieldView/types';
 import { useFieldStore } from 'src/pages/FieldView/useFieldStore';
@@ -9,11 +10,6 @@ type MapProps = {
   data: TData[];
   resource: FieldViewResource;
 };
-
-// const hasLatLng = typeof item?.address?.latitude === 'number' && typeof item?.address?.longitude === 'number';
-// const payload: google.maps.GeocoderRequest = hasLatLng
-//   ? { location: { lat: item.address.latitude, lng: item.address.longitude } }
-//   : { address: item?.address?.optionLabel as string };
 
 type LocationData = { latitude: number; longitude: number; _id: string } & TData;
 
@@ -25,7 +21,9 @@ const MapView = ({ data, resource }: MapProps) => {
     minWidth: '100%'
   };
   const [locations, setLocations] = useState<LocationData[]>([]);
-  const [, stStore] = useFieldStore((store) => store.activeItem);
+  const [, setStore] = useFieldStore((store) => store.activeItem);
+  const [thmeColor] = useAppTheme();
+  const mapRef = useRef<GoogleMap>(null);
 
   useEffect(() => {
     if (data) {
@@ -49,6 +47,14 @@ const MapView = ({ data, resource }: MapProps) => {
             .filter((result): result is PromiseFulfilledResult<LocationData> => result.status === 'fulfilled')
             .map((result) => result.value);
 
+          const availableLocations = successfulLocations?.reduce(
+            (a, c) => {
+              a[c._id] = { lat: c.latitude, lng: c.longitude };
+              return a; // Return the accumulator
+            },
+            {} as Record<string, { lat: number; lng: number }>
+          );
+          setStore({ availableLocations });
           setLocations(successfulLocations);
         })
         .catch((error) => {
@@ -59,66 +65,60 @@ const MapView = ({ data, resource }: MapProps) => {
 
   return (
     <GoogleMap
+      ref={mapRef}
       options={{
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         mapTypeControlOptions: {
           style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
         },
 
-        styles: [
-          {
-            featureType: 'water',
-            stylers: [{ color: '#46bcec' }, { visibility: 'on' }]
-          },
-          { featureType: 'landscape', stylers: [{ color: '#f2f2f2' }] },
-          {
-            featureType: 'road',
-            stylers: [{ saturation: -100 }, { lightness: 45 }]
-          },
-          {
-            featureType: 'road.highway',
-            stylers: [{ visibility: 'simplified' }]
-          },
-
-          { featureType: 'transit', stylers: [{ visibility: 'off' }] },
-          { featureType: 'poi', stylers: [{ visibility: 'off' }] }
-        ],
+        styles: thmeColor === 'dark' ? mapDarkTheme : mapLightTheme,
         // gestureHandling: 'cooperative',
         gestureHandling: 'greedy'
       }}
       mapContainerStyle={containerStyle}
       center={{ lat: locations[0]?.latitude ? locations[0].latitude : 31.9686, lng: locations[0]?.longitude ? locations[0]?.longitude : 99.9018 }}
-      zoom={4}
+      zoom={8}
     >
       {locations?.map((item) => {
-        const { type, label, value, className, ...rest } = generateData(item, resource)[0];
-
-        return (
-          <>
-            <InfoWindow position={new google.maps.LatLng(item?.latitude, item?.longitude)} options={{ minWidth: 200, maxWidth: 400 }}>
-              <>
-                <button
-                  onClick={() => stStore({ activeItem: item._id })}
-                  className="flex w-[200px] cursor-pointer flex-col justify-center bg-transparent px-2 py-4 text-left"
-                >
-                  <div key={label} className={cn('items-start', className)} {...rest}>
-                    {/* <h6 className="text-sm font-semibold text-[--primary-text]">{label}: </h6> */}
-                    <p
-                      className=" line-clamp-2 text-center font-bold text-[#6B7280] dark:text-gray-300"
-                      title={typeof value === 'string' ? value : ''}
-                    >
-                      {value}
-                    </p>
-                  </div>
-                </button>
-              </>
-            </InfoWindow>
-          </>
-        );
-        // <Marker key={item?._id} position={new google.maps.LatLng(item?.latitude, item?.longitude)} />;
+        return <RenderPoint item={item} resource={resource} setStore={setStore} />;
       })}
     </GoogleMap>
   );
 };
 
 export default MapView;
+
+const RenderPoint = ({ item, setStore, resource }) => {
+  const { type, label, value, className, ...rest } = generateData(item, resource)[0];
+  const position = new google.maps.LatLng(item?.latitude, item?.longitude);
+
+  const [infowindowOpen, setInfowindowOpen] = useState(true);
+
+  return (
+    <>
+      <Marker position={position} onClick={() => setInfowindowOpen((prev) => true)} />
+      {infowindowOpen && (
+        <InfoWindow
+          options={{ minWidth: 200, maxWidth: 400, headerDisabled: false }}
+          position={position}
+          onCloseClick={() => setInfowindowOpen(false)}
+        >
+          <div className="min-h-[47px]">
+            <button
+              onClick={() => setStore({ activeItem: item._id })}
+              className="absolute inset-0 right-6 top-6 flex w-[200px] cursor-pointer flex-col justify-center bg-transparent px-2 py-4 text-left"
+            >
+              <div key={label} className={cn('items-start', className)} {...rest}>
+                {/* <h6 className="text-sm font-semibold text-[--primary-text]">{label}: </h6> */}
+                <p className=" line-clamp-2 text-center font-bold text-[#6B7280] dark:text-gray-300" title={typeof value === 'string' ? value : ''}>
+                  {value}
+                </p>
+              </div>
+            </button>
+          </div>
+        </InfoWindow>
+      )}
+    </>
+  );
+};
