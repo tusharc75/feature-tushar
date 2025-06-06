99e265ecbc78cccc0a797502168ef8b5a64d8ba6
dhrubobarman <dhrubobarman847@gmail.com>
2025-06-06 15:21:13 +0530

    fix: fix

--- src/components/GenerativeAiDialog/index.tsx
+++ src/components/GenerativeAiDialog/index.tsx
@@ -446,8 +446,8 @@ const GenerativeAiDialog: React.FC<any> = ({ handleInsert, handleClose, anchorEl
               >
                 <div
                   className={cn(
-                    'rounded-md border-2 [&_fieldset]:hidden',
-                    '![--animation-duration:10s] ![--glow-intensity:0.25] ![--spread:4px]',
+                    'rounded-md border-[--border-w] [&_fieldset]:hidden',
+                    '![--animation-duration:10s] ![--border-w:2px] ![--glow-intensity:0.25] ![--spread:4px]',
                     isInputFocused ? '' : `ai-ring border-transparent`
                   )}
                 >
