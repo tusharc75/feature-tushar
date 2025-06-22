746a823dfafb32834cb0f2dddef3a38615612499
Rishabhsharawat15 <rishabhsharawat150@gmail.com>
2025-06-23 00:03:38 +0530

    integration fix

--- src/components/DigitalSignature/SendForSignatureDialog.tsx
+++ src/components/DigitalSignature/SendForSignatureDialog.tsx
@@ -4,6 +4,7 @@ import {
   Autocomplete,
   TextField,
   Box,
+  MenuItem,
 } from '@mui/material';
 import axiosInstance from '../../axios/axiosInstance';
 import { CustomToastContext } from '../../StateProvider/CustomToastContext/CustomToastContext';
@@ -13,9 +14,11 @@ import CustomDialogFooter from '../../components/CustomDialog/CustomDialogFooter
 import MultiLine from 'src/components/Helpers/FormTypes/MultiLine';
 import { ThemeButton } from 'src/components/Helpers/Buttons';
 
+const providers = ['AdobeSign', 'DocuSign'];
+
 const SendForSignatureDialog = ({ attachmentId, allAttachments, onClose }: any) => {
 
-  const [initialData, setInitialData] = useState({ emails: [] as string[], comment: '' });
+  const [initialData, setInitialData] = useState({ emails: [] as string[], comment: '', provider: '' });
   const [loading, setLoading] = useState(false);
   const toastConfig = useContext(CustomToastContext);
 
@@ -39,6 +42,7 @@ const SendForSignatureDialog = ({ attachmentId, allAttachments, onClose }: any) 
         emails: initialData.emails,
         comment: initialData.comment,
         selectedAttachments: allAttachments,
+        provider: initialData.provider,
       });
       toastConfig.setToastConfig({
         open: true,
@@ -91,6 +95,27 @@ const SendForSignatureDialog = ({ attachmentId, allAttachments, onClose }: any) 
             />
           )}
         />
+        <Box mt={2}>
+          <TextField
+            select
+            fullWidth
+            label="Service Provider"
+            value={initialData.provider}
+            onChange={(e) =>
+              setInitialData({ ...initialData, provider: e.target.value })
+            }
+            variant="outlined"
+            size="small"
+            margin="normal"
+            required
+          >
+            {providers.map((option) => (
+              <MenuItem key={option} value={option}>
+                {option}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
         <Box mt={2}>
           <MultiLine
             label="Comment"
