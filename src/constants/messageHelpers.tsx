6ad7e24a4b3776dd8a949a2f09d02b3b22356c1e
Rishabhsharawat15 <rishabhsharawat150@gmail.com>
2025-06-30 13:15:47 +0530

    fix

--- src/constants/messageHelpers.tsx
+++ src/constants/messageHelpers.tsx
@@ -178,5 +178,8 @@ export const repairJobMessage = {
 export const repairOrderMessage = {
   workOrderComplete: 'Complete work order to proceed',
   deliverLoadingTicketStep: 'Create and Deliver at least one Loading Ticket to proceed',
+  assignAssets: 'Assign Assets to proceed',
+  addProductAndAssetsInPackage :' Add Product and Assign Asset in Pakage to Proceed',
+  addAssetsInPackage :' Add Asset in Package to Proceed',
 };
 
