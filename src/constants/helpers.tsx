7df4fc0db43124c632ba873b95709d805a38495c
harshit <hg25112002@gmail.com>
2025-06-30 18:13:19 +0530

    combinedValidation in Fields

--- src/constants/helpers.tsx
+++ src/constants/helpers.tsx
@@ -1321,97 +1321,84 @@ export const yupSchema = (fields: any[], validEmail = true) => {
         schema[input.fieldName] = input.required ? string().required(message) : string();
       }
     } else if (input.type === 'name') {
-      schema[input.fieldName] = input.required
-        ? validationFields?.length && validation
-          ? string().when(
-              validationFields?.map((f) => f?.fieldName),
-              {
-                is: validation,
-                then: string().matches(nameRegex, "Numbers aren't allowed").required(message),
-                otherwise: string().matches(nameRegex, "Numbers aren't allowed")
-              }
-            )
-          : string().matches(nameRegex, "Numbers aren't allowed").required(message)
-        : string().matches(nameRegex, "Numbers aren't allowed");
+      if (uniqueDependentFields.length > 0) {
+        schema[input.fieldName] = string().when(uniqueDependentFields, {
+          is: (...args) => combinedValidation(...args),
+          then: string().matches(nameRegex, "Numbers aren't allowed").required(message),
+          otherwise: string().matches(nameRegex, "Numbers aren't allowed")
+        });
+      } else {
+        schema[input.fieldName] = input.required
+          ? string().matches(nameRegex, "Numbers aren't allowed").required(message)
+          : string().matches(nameRegex, "Numbers aren't allowed");
+      }
     } else if (input.type === 'url') {
-      schema[input.fieldName] = input.required
-        ? validationFields?.length && validation
-          ? string().when(
-              validationFields?.map((f) => f?.fieldName),
-              {
-                is: validation,
-                then: string().matches(urlRegex, 'Enter valid URL').required(message),
-                otherwise: string().matches(urlRegex, 'Enter valid URL')
-              }
-            )
-          : string().matches(urlRegex, 'Enter valid URL').required(message)
-        : string().matches(urlRegex, 'Enter valid URL');
+      if (uniqueDependentFields.length > 0) {
+        schema[input.fieldName] = string().when(uniqueDependentFields, {
+          is: (...args) => combinedValidation(...args),
+          then: string().matches(urlRegex, 'Enter valid URL').required(message),
+          otherwise: string().matches(urlRegex, 'Enter valid URL')
+        });
+      } else {
+        schema[input.fieldName] = input.required
+          ? string().matches(urlRegex, 'Enter valid URL').required(message)
+          : string().matches(urlRegex, 'Enter valid URL');
+      }
     } else if (input.type === 'mobileNumber') {
-      schema[input.fieldName] = input.required
-        ? validationFields?.length && validation
-          ? string().when(
-              validationFields?.map((f) => f?.fieldName),
-              {
-                is: validation,
-                then: string().min(10, 'Mobile number is too short').required(message),
-                otherwise: string().min(10, 'Mobile number is too short')
-              }
-            )
-          : string().min(10, 'Mobile number is too short').required(message)
-        : string().min(10, 'Mobile number is too short');
+      if (uniqueDependentFields.length > 0) {
+        schema[input.fieldName] = string().when(uniqueDependentFields, {
+          is: (...args) => combinedValidation(...args),
+          then: string().min(10, 'Mobile number is too short').required(message),
+          otherwise: string().min(10, 'Mobile number is too short')
+        });
+      } else {
+        schema[input.fieldName] = input.required
+          ? string().min(10, 'Mobile number is too short').required(message)
+          : string().min(10, 'Mobile number is too short');
+      }
     } else if (input.type === 'multiSelect' || input?.type === 'freeStyleMultiSelect') {
-      schema[input.fieldName] = input.required
-        ? validationFields?.length && validation
-          ? array().when(
-              validationFields?.map((f) => f?.fieldName),
-              {
-                is: validation,
-                then: array().min(1, message),
-                otherwise: array()
-              }
-            )
-          : array().min(1, message)
-        : array();
+      if (uniqueDependentFields.length > 0) {
+        schema[input.fieldName] = array().when(uniqueDependentFields, {
+          is: (...args) => combinedValidation(...args),
+          then: array().min(1, message),
+          otherwise: array()
+        });
+      } else {
+        schema[input.fieldName] = input.required ? array().min(1, message) : array();
+      }
     } else if (input.type === 'percent' || input.type === 'number' || input.type === 'decimal' || input.type === 'formula') {
-      schema[input.fieldName] = input.required
-        ? validationFields?.length && validation
-          ? number().when(
-              validationFields?.map((f) => f?.fieldName),
-              {
-                is: validation,
-                then: number().required(message).moreThan(0, `${input.fieldLabel} must be greater than 0`).nullable(),
-                otherwise: number().nullable()
-              }
-            )
-          : number().required(message).moreThan(0, `${input.fieldLabel} must be greater than 0`).nullable()
-        : number().nullable();
+      if (uniqueDependentFields.length > 0) {
+        schema[input.fieldName] = number().when(uniqueDependentFields, {
+          is: (...args) => combinedValidation(...args),
+          then: number().required(message).moreThan(0, `${input.fieldLabel} must be greater than 0`).nullable(),
+          otherwise: number().nullable()
+        });
+      } else {
+        schema[input.fieldName] = input.required
+          ? number().required(message).moreThan(0, `${input.fieldLabel} must be greater than 0`).nullable()
+          : number().nullable();
+      }
     } else if (input.type === 'email') {
-      schema[input.fieldName] =
-        input.required && validEmail
-          ? validationFields?.length && validation
-            ? string().when(
-                validationFields?.map((f) => f?.fieldName),
-                {
-                  is: validation,
-                  then: string().email().required(message),
-                  otherwise: string().email(`${input.fieldLabel} must be a valid email`)
-                }
-              )
-            : string().email().required(message)
-          : string().email(`${input.fieldLabel} must be a valid email`);
+      if (uniqueDependentFields.length > 0) {
+        schema[input.fieldName] = string().when(uniqueDependentFields, {
+          is: (...args) => combinedValidation(...args),
+          then: string().email().required(message),
+          otherwise: string().email(`${input.fieldLabel} must be a valid email`)
+        });
+      } else {
+        schema[input.fieldName] =
+          input.required && validEmail ? string().email().required(message) : string().email(`${input.fieldLabel} must be a valid email`);
+      }
     } else if (input.type === 'switch' || input.type === 'checkBox') {
-      schema[input.fieldName] = input.required
-        ? validationFields?.length && validation
-          ? boolean().when(
-              validationFields?.map((f) => f?.fieldName),
-              {
-                is: validation,
-                then: boolean().required(message),
-                otherwise: boolean()
-              }
-            )
-          : boolean().required(message)
-        : boolean();
+      if (uniqueDependentFields.length > 0) {
+        schema[input.fieldName] = boolean().when(uniqueDependentFields, {
+          is: (...args) => combinedValidation(...args),
+          then: boolean().required(message),
+          otherwise: boolean()
+        });
+      } else {
+        schema[input.fieldName] = input.required ? boolean().required(message) : boolean();
+      }
     } else if (input.type !== 'currencyAmount' && (input.type === 'converter' || input.isConverter === true)) {
       input.displayUnits &&
         input.displayUnits.forEach((_unit) => {
@@ -1435,18 +1422,15 @@ export const yupSchema = (fields: any[], validEmail = true) => {
           }
         });
     } else if (input.type === 'date' || input?.type === 'dateTime') {
-      schema[input.fieldName] = input.required
-        ? validationFields?.length && validation
-          ? string().when(
-              validationFields?.map((f) => f?.fieldName),
-              {
-                is: validation,
-                then: string().required(`${input.fieldLabel} is required`).nullable(),
-                otherwise: string().nullable()
-              }
-            )
-          : dateValidation
-        : dateValidation;
+      if (uniqueDependentFields.length > 0) {
+        schema[input.fieldName] = string().when(uniqueDependentFields, {
+          is: (...args) => combinedValidation(...args),
+          then: string().required(`${input.fieldLabel} is required`).nullable(),
+          otherwise: dateValidation
+        });
+      } else {
+        schema[input.fieldName] = dateValidation;
+      }
     } else if (input.type === 'colorPicker') {
       schema[input.fieldName] = input.required ? string().required(`${input.fieldLabel} is required`).nullable() : string().nullable();
     } else if (input.type === 'multiImageUpload') {
@@ -1462,18 +1446,15 @@ export const yupSchema = (fields: any[], validEmail = true) => {
     } else if (input.type === 'gpsLocation') {
       schema[input.fieldName] = input.required ? object().required(`${input.fieldLabel} is required`) : object();
     } else {
-      schema[input.fieldName] = input.required
-        ? validationFields?.length && validation
-          ? string().when(
-              validationFields?.map((f) => f?.fieldName),
-              {
-                is: validation,
-                then: string().required(message),
-                otherwise: string()
-              }
-            )
-          : string().required(message)
-        : string();
+      if (uniqueDependentFields.length > 0) {
+        schema[input.fieldName] = string().when(uniqueDependentFields, {
+          is: (...args) => combinedValidation(...args),
+          then: string().required(message),
+          otherwise: string()
+        });
+      } else {
+        schema[input.fieldName] = input.required ? string().required(message) : string();
+      }
     }
   });
 
