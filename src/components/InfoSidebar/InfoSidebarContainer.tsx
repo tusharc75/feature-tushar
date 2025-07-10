ec221a22f7523e8b4e22733f52e4b3adbb7a0025
dhrubojyoti barman <dhrubobarman847@gmail.com>
2025-07-10 12:07:26 +0530

    resource info data fetching issue

--- src/components/InfoSidebar/InfoSidebarContainer.tsx
+++ src/components/InfoSidebar/InfoSidebarContainer.tsx
@@ -1,12 +1,41 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
+import axiosInstance from 'src/axios/axiosInstance';
 import InfoSidebar from 'src/components/InfoSidebar/InfoSidebar';
 import { useInforSidebar } from 'src/components/InfoSidebar/store';
 import { cn } from 'src/constants/helpers';
+import { CustomToastContext } from 'src/StateProvider/CustomToastContext/CustomToastContext';
 
 const InfoSidebarContainer = () => {
-  const [item] = useInforSidebar((state) => state.item);
+  const [item, setStore] = useInforSidebar((state) => state.item);
+  const cache = useRef<Record<string, string>>({});
+  const toastConfig = useContext(CustomToastContext);
+
   const [fullScreen, setFullScreen] = useState(false);
+
+  useEffect(() => {
+    if (item) {
+      const api = `/resource-information/actions?resourceId=${item.resourceId}&actionId=${item._id}`;
+      if (cache.current[api]) {
+        setStore({ content: cache.current[api] });
+      } else {
+        axiosInstance()
+          .get(api)
+          .then(({ data: { data } }) => {
+            if (data?.content) {
+              cache.current[api] = data?.content;
+            } else {
+              cache.current[api] = 'No information available';
+            }
+            setStore({ content: cache.current[api] });
+          })
+          .catch((err) => {
+            toastConfig.setToastConfig(err);
+          });
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [item?._id]);
 
   if (fullScreen && item) {
     return createPortal(
