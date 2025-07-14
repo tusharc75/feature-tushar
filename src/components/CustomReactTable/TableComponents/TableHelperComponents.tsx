70d9bbbc552e4bcee56a8ff9054f1eef62650558
dhrubojyoti barman <dhrubobarman847@gmail.com>
2025-07-14 15:49:10 +0530

    fix

--- src/components/CustomReactTable/TableComponents/TableHelperComponents.tsx
+++ src/components/CustomReactTable/TableComponents/TableHelperComponents.tsx
@@ -200,8 +200,6 @@ export function Filter({ column, table }: { column: Column<any, unknown>; table:
     }
   }, [isOpen]);
 
-  if (isOpen) console.log(column.getFilterValue(), column);
-
   return (
     <div className="search-container">
       <IconButton
