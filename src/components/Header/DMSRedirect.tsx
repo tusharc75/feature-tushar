af8b27ad6e53997eaee17d32bd844cfa6a004e18
dhrubojyoti barman <dhrubobarman847@gmail.com>
2025-07-17 15:52:44 +0530

    fix Dark

--- src/components/Header/DMSRedirect.tsx
+++ src/components/Header/DMSRedirect.tsx
@@ -19,13 +19,10 @@ const DMSRedirect = () => {
   };
 
   return (
-    <HtmlTooltip
-      title={`Open DMS`}
-      className="inline-block"
-    >
-      <span className="relative mx-2 inline-block rounded-full border border-blue-400 bg-blue-500/10">
+    <HtmlTooltip title={`Open DMS`} className="inline-block">
+      <span className="relative mx-2 inline-block rounded-full border border-blue-400 bg-blue-500/10 dark:border-blue-800">
         <IconButton sx={{ padding: '5px', width: '30px', height: '30px' }} onClick={handleRedirect}>
-          <FiExternalLink className="absolute -right-[5px] -top-[5px] text-[#0000FF]" size={14} />
+          <FiExternalLink className="absolute -right-[5px] -top-[5px] text-[#0000FF] dark:text-blue-600" size={14} />
           <PiDatabaseBold size={18} className="text-[#2563EB]" />
         </IconButton>
       </span>
