7261b3b5fd3b2f27eb8dae8d87585aded84b60f1
Rishabhsharawat15 <rishabhsharawat150@gmail.com>
2025-07-29 13:38:51 +0530

    fix

--- src/pages/ProfilePage/components/ResourceWiseNotificationPreference.tsx
+++ src/pages/ProfilePage/components/ResourceWiseNotificationPreference.tsx
@@ -119,12 +119,7 @@ export default function ResourceWiseNotificationPreference() {
   };
 
   const handleExpand = (resource) => {
-    const container = document.querySelector('.notification-table-container');
-    const scrollTop = container?.scrollTop || 0;
     setExpanded((prev) => ({ ...prev, [resource]: !prev[resource] }));
-    setTimeout(() => {
-      if (container) container.scrollTop = scrollTop;
-    }, 0);
   };
 
   return (
@@ -141,7 +136,7 @@ export default function ResourceWiseNotificationPreference() {
         )}
       </Box>
       <TableContainer
-        className={cn('notification-table-container min-h-[300px] rounded-md border')}
+        className={cn('min-h-[300px] rounded-md border')}
         style={{ maxHeight: 'calc(100vh - 300px)', overflowY: 'auto' }}
       >
         <Table
