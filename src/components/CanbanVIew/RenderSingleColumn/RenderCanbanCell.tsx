8a7e495e5294c716b34dab8e853809e76c0e13bc
dhrubojyoti barman <dhrubobarman847@gmail.com>
2025-08-05 11:29:33 +0530

    fix

--- src/components/CanbanVIew/RenderSingleColumn/RenderCanbanCell.tsx
+++ src/components/CanbanVIew/RenderSingleColumn/RenderCanbanCell.tsx
@@ -69,7 +69,7 @@ const RenderCanbanCellImpl = <D,>({
       id: column?.id,
       key: column?.id,
       className: cn(
-        `!text-[12px] [&>*]:[font-size:12px_!important]  [&_*]:!font-semibold [&_*]:[white-space:unset_!important] [&_h5]:[font-size:12px_!important]`,
+        `!text-[12px] [&>*]:!text-[12px] [&_span]:!text-[12px] [&_p]:!text-[12px] [&_*]:!font-medium [&_*]:[white-space:unset_!important] [&_h5]:[font-size:12px_!important]`,
         hideHeader ? 'flex-shrink-0 flex-nowrap flex' : ' line-clamp-1 [&>div]:[flex-wrap:wrap_!important]'
       ),
       onClick: () => {
@@ -96,7 +96,7 @@ const RenderCanbanCellImpl = <D,>({
       );
     case isEditable:
       return (
-        <h6 className=" grid max-w-full text-[12px]">
+        <h6 className=" grid max-w-full !text-[12px]">
           {!hideHeader && <span className={combinedHeaderClass}>{renderedHead}: </span>}
           <span {...props}>
             <div className="w-fit">
@@ -112,7 +112,7 @@ const RenderCanbanCellImpl = <D,>({
       );
     default:
       return (
-        <h6 className=" grid max-w-full text-[12px]">
+        <h6 className=" grid max-w-full !text-[12px]">
           {!hideHeader && <span className={combinedHeaderClass}>{renderedHead}: </span>}
           <span {...props}>{renderedCell}</span>
         </h6>
