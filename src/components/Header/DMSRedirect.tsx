7998f847bdc5273ef3ebc81e2721773689e15dad
ROMIT <romit@ip-192-168-1-14.us-east-2.compute.internal>
2025-08-08 16:44:21 +0530

    commit message

--- src/components/Header/DMSRedirect.tsx
+++ src/components/Header/DMSRedirect.tsx
@@ -2,24 +2,23 @@ import { IconButton } from '@mui/material';
 import { FiExternalLink } from 'react-icons/fi';
 import { PiDatabaseBold } from 'react-icons/pi';
 import HtmlTooltip from 'src/components/CustomTooltipTitle';
-
-const envLink = import.meta.env.VITE_APP_DMS_URL;
+import { VITE_APP_DMS_URL, VITE_APP_ENV } from 'src/config';
+import { useData } from 'src/StateProvider/Provider';
 
 const DMSRedirect = () => {
-  if (!envLink) return null;
+
+  const { state: { user } }: any = useData();
 
   const handleRedirect = () => {
     const token = localStorage.getItem('token');
-    let redirectUrl = envLink;
-
+    let redirectUrl = VITE_APP_DMS_URL;
     if (token) {
-      redirectUrl = `${envLink}/oauth?token=${encodeURIComponent(token)}`;
+      redirectUrl = `${VITE_APP_DMS_URL}/oauth?token=${encodeURIComponent(token)}`;
     }
-
     window.open(redirectUrl, '_blank');
   };
 
-  return (
+  return (VITE_APP_DMS_URL && ((user?.brand === '66c81f4780d6ec9b112302ef' && VITE_APP_ENV === 'production') || VITE_APP_ENV !== 'production') ?
     <HtmlTooltip title={`Open EDrive`} className="inline-block">
       <span className="relative mx-2 inline-block rounded-full border border-blue-400 bg-blue-500/10 dark:border-blue-800">
         <IconButton sx={{ padding: '5px', width: '30px', height: '30px' }} onClick={handleRedirect}>
@@ -27,7 +26,7 @@ const DMSRedirect = () => {
           <PiDatabaseBold size={18} className="text-[#2563EB]" />
         </IconButton>
       </span>
-    </HtmlTooltip>
+    </HtmlTooltip> : null
   );
 };
 
