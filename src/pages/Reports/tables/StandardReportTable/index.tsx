e786de3e614081f4be798742207f1374dcca0695
harshit <hg25112002@gmail.com>
2025-08-12 13:26:15 +0530

    dutyLogs report

--- src/pages/Reports/tables/StandardReportTable/index.tsx
+++ src/pages/Reports/tables/StandardReportTable/index.tsx
@@ -461,6 +461,11 @@ const StandardReportsTable = ({ state: reportState, isMobile, isSidebarOpen }: T
 
         data = data?.map((u: any) => {
           let finalObject: any = prepareDataForGrid(u);
+          if (resourceCamelCase === 'dutyLogs') {
+            finalObject['location.name'] = u?.location?.name;
+            finalObject['createdBy.date'] = u?.createdBy?.date;
+            finalObject['createdBy.user'] = u?.createdBy?.user?.concatedName;
+          }
           return finalObject;
         });
 
