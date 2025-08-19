bbae6ddef2283f31deba457835cba1da18ae9326
vivekyadav5750 <10vivekkumaryadav@gmail.com>
2025-08-19 11:25:58 +0530

    fix

--- src/pages/ProfilePage/components/MyProfile.tsx
+++ src/pages/ProfilePage/components/MyProfile.tsx
@@ -5,7 +5,7 @@ import { Box } from '@mui/material';
 const MyProfile = ({ userData, onFetchUserData, otherDetails, proxyBy, userFields, loading, userLoading }) => {
   return (
     <>
-      <Box p={{ xs: 0, md: 2 }}>
+      <Box>
         <Grid container spacing={3}>
           <Grid size={{ sm: 12, md: 4, lg: 3 }}>
             <ManageProfile
