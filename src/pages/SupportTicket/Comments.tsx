a82d884c6c8a73bb3cf8b5047525bb6c57c8d4ad
ROMIT <romit@Mac.bbrouter>
2025-09-02 16:07:44 +0530

    commit message

--- src/pages/SupportTicket/Comments.tsx
+++ src/pages/SupportTicket/Comments.tsx
@@ -11,6 +11,7 @@ import { ThemeButton } from 'src/components/Helpers/Buttons';
 import { useData } from 'src/StateProvider/Provider';
 import AddCommentDialog from 'src/pages/SupportTicket/AddCommentDialog';
 import AttachmentThumbnail from 'src/components/AttachmentThumbnail';
+import AddIcon from '@mui/icons-material/Add';
 
 const Comments = ({ uniqueId, supportTicketData }) => {
   const toastConfig = useContext(CustomToastContext);
@@ -19,7 +20,7 @@ const Comments = ({ uniqueId, supportTicketData }) => {
     state: { permissions },
   }: any = useData();
 
-  const [addComment, setAddComment] = useState(false);
+  const [showAddCommentDialog, setShowAddCommentDialog] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -46,58 +47,62 @@ const Comments = ({ uniqueId, supportTicketData }) => {
   return (
     <div className="relative">
       <div className='my-4'>
-        <ThemeButton buttonType='theme' onClick={() => setAddComment(true)}>
-          Add
-        </ThemeButton>
-        <span className="absolute right-0 top-0 z-[1] flex size-[40px] items-center justify-center rounded-full bg-[var(--dark-secondary,white)] shadow-md">
-          <HtmlTooltip className="size-[30px]" title={'Refresh'}>
-            <IconButton size="small" onClick={() => fetchData()} style={{ marginRight: '16px' }}>
+        {permissions?.brandSupportTickets?.isUpdate &&
+          <ThemeButton
+            buttonType='theme'
+            onClick={() => setShowAddCommentDialog(true)}
+            startIcon={<AddIcon fontSize='small' />}
+          >
+            Add
+          </ThemeButton>
+        }
+        <span className="absolute right-0 top-0 z-[1] flex size-[40px] items-center justify-center">
+          <HtmlTooltip title={'Refresh'}>
+            <IconButton size="small" onClick={() => fetchData()}>
               <RefreshIcon fontSize="small" />
             </IconButton>
           </HtmlTooltip>
         </span>
       </div>
-
-
-      {data ? (
-        <>
-          <div className="[scrollbar-gutter: stable] relative mb-2 max-h-[calc(100vh-200px)] min-h-[300px] space-y-2 overflow-y-auto px-2">
-            {data.length > 0 ? (
-              data.map((item: any) => (
-                <div key={item._id} className="rounded-md border p-2">
-                  <p className="mb-2 flex flex-wrap gap-[10px] text-[13px] text-[var(--primary-text)] ">
-                    <span className="font-semibold">{item?.user?.optionLabel}</span>
-                    <span className="ml-2 text-[#969696] dark:text-gray-400">{displayDateTime(item.date)}</span>
-                  </p>
-                  <div
-                    className="max-image [&_*:last-child]:mb-0"
-                    dangerouslySetInnerHTML={{
-                      __html: item?.comment
-                    }}
-                  />
-                  {item?.attachments && item.attachments.length > 0 && (
-                    <div className='mt-3'>
-                      <AttachmentThumbnail
-                        attachments={item.attachments}
-                        handleDeleteAttachment={() => { }}
-                        allowedToEdit={false}
-                      />
-                    </div>
-                  )}
-                </div>
-              ))
-            ) : (
-              <div className="absolute left-1/2 top-1/2 select-none text-gray-500 [transform:translate(-50%,-50%)]">No Data Found</div>
-            )}
-          </div>
-          {permissions?.brandSupportTickets?.isUpdate && addComment && (
-            <AddCommentDialog
-              onClose={() => setAddComment(false)}
-              uniqueId={uniqueId}
-              supportTicketData={supportTicketData}
-              fetchData={fetchData}
-            />)}
-        </>
+      {data ? (<>
+        <div className="[scrollbar-gutter: stable] relative mb-2 max-h-[calc(100vh-250px)] min-h-[300px] space-y-2 overflow-y-auto ">
+          {data.length > 0 ? (
+            data.map((item: any) => (
+              <div key={item._id} className="rounded-md border p-2">
+                <p className="mb-2 flex flex-wrap gap-[10px] text-[13px] text-[var(--primary-text)] ">
+                  <span className="font-semibold">{item?.user?.optionLabel}</span>
+                  <span className="ml-2 text-[#969696] dark:text-gray-400">{displayDateTime(item.date)}</span>
+                </p>
+                <div className="max-image [&_*:last-child]:mb-0"
+                  dangerouslySetInnerHTML={{
+                    __html: item?.comment
+                  }}
+                />
+                {item?.attachments && item.attachments.length > 0 && (
+                  <div className='mt-3'>
+                    <AttachmentThumbnail
+                      attachments={item.attachments}
+                      handleDeleteAttachment={() => { }}
+                      allowedToEdit={false}
+                    />
+                  </div>
+                )}
+              </div>
+            ))
+          ) : (
+            <div className="absolute left-1/2 top-1/2 select-none text-gray-500 [transform:translate(-50%,-50%)]">There are no comments yet</div>
+          )}
+        </div>
+        {showAddCommentDialog && (
+          <AddCommentDialog
+            onClose={() => setShowAddCommentDialog(false)}
+            uniqueId={uniqueId}
+            supportTicketData={supportTicketData}
+            fetchData={fetchData}
+          />
+        )
+        }
+      </>
       ) : (
         <Box p={2} height={500}>
           <CommonSkeleton lenArray={[...Array(10).keys()]} />
