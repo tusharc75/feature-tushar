5be39d95622b58a6248c76d620e3c26f172315b6
Abhi76317 <manupatel18112000@gmail.com>
2025-09-23 19:14:57 +0530

    fix bugs

--- src/pages/FieldServiceTechnician/FieldTicketDetailView.tsx
+++ src/pages/FieldServiceTechnician/FieldTicketDetailView.tsx
@@ -100,8 +100,7 @@ const FieldTicketDetailView = ({ id }) => {
             resourceLabel={fieldTicketData?.fieldTicketNumber}
             extraRelatedTo={{
               referenceId: fieldTicketData?.fieldServiceOrder?.optionValue,
-              resource: ACTIVITY_RESOURCE.fieldServiceOrder,
-              label: fieldTicketData?.fieldServiceOrder?.optionLabel
+              resource: ACTIVITY_RESOURCE.fieldServiceOrder
             }}
             resourceData={fieldTicketData}
           />
