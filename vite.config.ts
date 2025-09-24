fd12f820f6d65043fa843777d99ee611d433ab53
dhrubojyoti barman <dhrubobarman847@gmail.com>
2025-09-24 11:57:53 +0530

    enabled source map for error tracking

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       workbox: {
         globPatterns: ['**/*'],
-        globIgnores: ['**/*pdfme*.js'],
+        globIgnores: ['**/*.map', '**/*pdfme*.js'],
         maximumFileSizeToCacheInBytes: 26 * 1024 * 1024, // 26 MB
         importScripts: ['/firebase-messaging-sw.js']
       },
@@ -46,7 +46,8 @@ export default defineConfig({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
   },
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    sourcemap: true
   },
   server: {
     open: true,
