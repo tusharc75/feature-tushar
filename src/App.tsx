08adba0176c5585c7be8c57efb128f9bd974379e
dhrubojyoti barman <dhrubobarman847@gmail.com>
2025-09-24 14:15:37 +0530

    error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import queryString from 'query-string';
 import { useContext, useEffect, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import CustomIntro from 'src/components/CustomIntro';
-import DesktopDM from 'src/components/DesktopDM';
+// import DesktopDM from 'src/components/DesktopDM';
 import ForceUpdatePopup from 'src/components/ForceUpdatePopup';
 import RenderAllInfoButtons from 'src/components/InfoSidebar/RenderAllInfoButtons';
 import RenderInfoInspector from 'src/components/InfoSidebar/RenderInfoInspector';
