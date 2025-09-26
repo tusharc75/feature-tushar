8eca70165aa7cc1e71b4acdbdadac5be2a013d57
Vivek-0420 <Vivek.Raj@vebholic.com>
2025-09-26 18:20:23 +0530

    fix: 6174

--- src/components/AssignRolesDialog/AssignSerialNumbersDialog.tsx
+++ src/components/AssignRolesDialog/AssignSerialNumbersDialog.tsx
@@ -167,6 +167,12 @@ const AssignSerialNumbersDialog = ({
   }, [page, limit, filters, sorting, search, selectedEntity, showFilteredRecordsOnly, selectedProduct, selectedWarehouse, selectedStorageLocation]);
 
   const fetchData = () => {
+    if (!selectedProduct) {
+      dispatch({ type: 'initialize', data: [], count: 0 });
+      dispatch({ type: 'loading', loading: false });
+      return;
+    }
+
     dispatch({ type: 'loading', loading: true });
     let queryString = getQueryString();
     axiosInstance()
@@ -195,7 +201,7 @@ const AssignSerialNumbersDialog = ({
 
   const getQueryString = () => {
     const ignoreIds = ids && ids?.length > 0 ? ids : [];
-    let deepFilter = `?page=${page}&limit=${limit}&ignoreIds=${JSON.stringify(ignoreIds)}&activeStorageLocation=true`;
+    let deepFilter = `?page=${page}&limit=${limit}&ignoreIds=${JSON.stringify(ignoreIds)}`;
     if (!selectedProduct) {
       deepFilter = `${deepFilter}&products=${selectedProducts?.map((p) => p?.id).join(',')}`;
     } else {
