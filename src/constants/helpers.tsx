3bd7ee68b1e4e8a200326f4f6c036fac767863f3
rishabhsharawat15 <rishabhsharawat150@gmail.com>
2025-10-01 15:21:09 +0530

    added plugin

--- src/constants/helpers.tsx
+++ src/constants/helpers.tsx
@@ -2991,6 +2991,10 @@ export const WORK_ORDER_TYPE_LABEL = {
   [WORK_ORDER_TYPE.disassemblyOrder]: 'Disassembly'
 };
 
+export const PLUGIN = {
+  CUSTOM_TABLE: 'CUSTOM_TABLE'
+}
+
 export const IRT_APPROVER_STATUS = {
   send: 'Email Sent',
   approved: 'Approved',
