7aab9e6260e35f901c51e7d6fb707c3fc7ad4049
Abhi76317 <manupatel18112000@gmail.com>
2025-10-03 18:59:17 +0530

    fix bugs

--- src/pages/RepairJob/AddSerializedAsset/index.tsx
+++ src/pages/RepairJob/AddSerializedAsset/index.tsx
@@ -508,7 +508,6 @@ const SerializedAsset = ({
             setAddMaterialDialog({ open: false, type: '' })
           }}
           extraDeepFilter={[{ field: 'status', term: SERIALIZED_PACKAGE_STATUS.available }]}
-          referenceData={{ warehouse: repairJobData?.warehouse }}
           isSubmitting={isSubmitting}
         />
       )}
