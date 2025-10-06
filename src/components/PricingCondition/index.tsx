5cf660ad72c8562567abaef25d2d8949fa80b043
vivekyadav5750 <10vivekkumaryadav@gmail.com>
2025-10-06 15:50:49 +0530

    fix

--- src/components/PricingCondition/index.tsx
+++ src/components/PricingCondition/index.tsx
@@ -164,7 +164,9 @@ export const getTaxById = async (taxCode: any) => {
 }
 
 export const getCostPriceConditions = async (material: any[], type: string) => {
-  const ids = material?.map((e) => type === sidebarResource.employeeMaster ? e?.technician : e?.materialId);
+  let ids = [];
+  ids = material?.map((e) => type === sidebarResource.employeeMaster ? e?.technician : e?.materialId);
+  ids = material?.map((e) => type === sidebarResource.competencies ? e?.competence : e?.materialId);
   const {
     data: { data }
   } = await axiosInstance().post(`cost-price/get`, {
