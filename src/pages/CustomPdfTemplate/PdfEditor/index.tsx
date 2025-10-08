23d697e1facb2b90068fa96120029d17cbf0bc04
dhrubojyoti barman <dhrubobarman847@gmail.com>
2025-10-08 11:49:19 +0530

    fix

--- src/pages/CustomPdfTemplate/PdfEditor/index.tsx
+++ src/pages/CustomPdfTemplate/PdfEditor/index.tsx
@@ -72,10 +72,11 @@ const PdfEditor = ({ template, onTemplateChange, disabled, noOfPages, variables,
         const autoCompleteHeight = 56;
         const autoCompleteWidth = 320;
 
-        const left = rect.x + window.scrollX;
-        const centerY = rect.y + window.scrollY - autoCompleteHeight - padding;
-        const centerX = left - autoCompleteWidth * 0.5 + rect.width * 0.5;
         if (rect) {
+          const left = rect.x + window.scrollX;
+          const centerY = rect.y + window.scrollY - autoCompleteHeight - padding;
+          const centerX = left - autoCompleteWidth * 0.5 + rect.width * 0.5;
+
           setDropdownPos({ x: centerX, y: centerY });
           const sel = window.getSelection();
           const activeGrid = sel?.anchorNode?.parentElement?.closest(`[plugin-type=${PLUGIN.CUSTOM_TABLE}]`);
