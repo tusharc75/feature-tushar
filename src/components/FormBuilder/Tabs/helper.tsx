f1c466cec1c7be9948cc28a4d09f1191085752a7
Abhi76317 <manupatel18112000@gmail.com>
2025-10-15 10:12:36 +0530

    add Field Color in form builder and remove statusColor

--- src/components/FormBuilder/Tabs/helper.tsx
+++ src/components/FormBuilder/Tabs/helper.tsx
@@ -635,27 +635,6 @@ export const RESOURCE_ACTION_TYPE = {
   triggers: 'Triggers'
 }
 
-export const statusColor = {
-  fieldName: 'statusColor',
-  fieldLabel: 'Status Color',
-  type: 'multipleFields',
-  fields: [
-    {
-      fieldName: 'status',
-      fieldLabel: 'Status',
-      type: 'multiSelect',
-      required: true
-    },
-    {
-      fieldName: 'colorCode',
-      fieldLabel: 'Color Code',
-      type: 'colorPicker',
-      required: true
-    }
-  ],
-  defaultValue: []
-}
-
 export const fieldColor = {
   fieldName: 'fieldColor',
   fieldLabel: 'Field Color',
