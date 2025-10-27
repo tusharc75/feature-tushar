24df989b8aa8557af67514140abe58866d83d28d
ROMIT <romit@Mac.bbrouter>
2025-10-27 19:25:46 +0530

    commit message

--- src/constants/helpers.tsx
+++ src/constants/helpers.tsx
@@ -1284,7 +1284,7 @@ export const yupSchema = (fields: any[], validEmail = true) => {
 
       validation = (...args) => {
         let validate = false;
-        for (let i = 0; i < validationFields?.length; ) {
+        for (let i = 0; i < validationFields?.length;) {
           const field = validationFields[i];
           const condition =
             field?.type === 'section'
@@ -1691,7 +1691,7 @@ export const getPermissions = (user, selectedEntity = undefined): IGetPermission
         });
       }
       return { permissions, resources };
-    } catch (e) {}
+    } catch (e) { }
   }
 };
 
@@ -2169,6 +2169,11 @@ export const SYSTEM_ASSET_STATUS = [
   ASSET_STATUS.scrapRequested,
   ASSET_STATUS.onPO
 ];
+
+export const ASSET_SUB_STATUS = {
+  operational: 'Operational',
+  inUse: 'In-Use',
+};
 
 export const ASSET_NUMBER_TYPE = {
   auto: 'Auto',
@@ -3778,8 +3783,8 @@ function fallbackCopyTextToClipboard(text: string, callBack: (text: string) => v
   document.body.removeChild(textArea);
 }
 
-export function copyTextToClipboard(text: string, callBack: (text: string) => void = () => {}) {
-  if (typeof callBack !== 'function') callBack = (text) => {};
+export function copyTextToClipboard(text: string, callBack: (text: string) => void = () => { }) {
+  if (typeof callBack !== 'function') callBack = (text) => { };
 
   if (!navigator.clipboard) {
     fallbackCopyTextToClipboard(text, callBack);
