3f72bf75ec434c40444be3683b741e579c4e1ae8
dhrubojyoti barman <dhrubobarman847@gmail.com>
2025-10-30 15:11:46 +0530

    optimized virtualization, new Work order technician UI

---
+++ src/components/CardColTimeline1/components/ColumnWrapper.tsx
@@ -0,0 +1,163 @@
+import { CheckCircle, CheckCircleOutline, RadioButtonUnchecked } from '@mui/icons-material';
+import { Checkbox, Skeleton } from '@mui/material';
+import axios, { CancelToken } from 'axios';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { getRandomNumber } from 'src/components/AiChatbox/utils';
+import CardColTimelineLoader from 'src/components/CardColTimeline1/CardColTimelineLoader';
+import { cn } from 'src/constants/helpers';
+import { CommonProps } from './types';
+import Column from './Column';
+
+const ColumnWrapper = <D, C extends readonly string[]>({ state, getColColors, column, ...rest }: CommonProps<D, C>) => {
+  const [data, setData] = useState<D[] | null>(null);
+  const [count, setCount] = useState(0);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [initialLoading, setInitialLoading] = useState(true);
+  const { keyGetter, fetchSingleColumn, resource, refreshSignal, limit, filterQuery, columnDef, resetSelectionSignal, selectedRecordObj, setState } =
+    state;
+  const [selectedRecordMap, setSelectedRecordmap] = useState<Map<string, boolean>>(new Map());
+
+  const colors = getColColors(column);
+  const isDataLoading = loading;
+  const isInitialLoading = initialLoading || columnDef?.length === 0;
+
+  const handleFetchSingleColumnWrapper = useCallback(
+    async (page = 0, pushData = false, cancelToken?: CancelToken, resource?: string) => {
+      try {
+        setLoading(true);
+        const { count, data } = await fetchSingleColumn({ column, filterQuery, limit, page, resource, cancelToken });
+        if (pushData) {
+          setData((prev) => [...prev, ...data]);
+        } else {
+          setData(data);
+        }
+        setCount(count);
+        setPage(page);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [column, fetchSingleColumn, filterQuery, limit]
+  );
+
+  const isAllSelected = useMemo(() => {
+    return selectedRecordMap.size === data?.length;
+  }, [data?.length, selectedRecordMap]);
+
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
+
+  const handleSelectAll = () => {
+    if (isAllSelected) {
+      setState({ type: 'setSelectedRecordObj', payload: { ...selectedRecordObj, [column]: [] } });
+      setSelectedRecordmap(new Map());
+    } else {
+      const newSelectedMap: Map<string, boolean> = new Map();
+      data?.forEach((d) => {
+        newSelectedMap.set(keyGetter(d), true);
+      });
+      setState({ type: 'setSelectedRecordObj', payload: { ...selectedRecordObj, [column]: data } });
+      setSelectedRecordmap(newSelectedMap);
+    }
+  };
+
+  const handleSelectSingle = (data: D) => {
+    setSelectedRecordmap((prev) => {
+      const newData = new Map(prev);
+      if (prev.has(keyGetter(data))) {
+        newData.delete(keyGetter(data));
+        setState({
+          type: 'setSelectedRecordObj',
+          payload: { ...selectedRecordObj, [column]: selectedRecordObj[column].filter((d) => keyGetter(d) !== keyGetter(data)) }
+        });
+      } else {
+        newData.set(keyGetter(data), true);
+        if (selectedRecordObj[column]) {
+          setState({ type: 'setSelectedRecordObj', payload: { ...selectedRecordObj, [column]: [...selectedRecordObj[column], data] } });
+        } else {
+          setState({ type: 'setSelectedRecordObj', payload: { ...selectedRecordObj, [column]: [data] } });
+        }
+      }
+
+      return newData;
+    });
+  };
+
+  useEffect(() => {
+    const cancelToken = axios.CancelToken.source();
+    const fetchInitialData = async () => {
+      setInitialLoading(true);
+      await handleFetchSingleColumnWrapper(0, false, cancelToken.token, resource);
+      setInitialLoading(false);
+    };
+    fetchInitialData();
+
+    return () => {
+      cancelToken.cancel();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [refreshSignal, filterQuery, resource]);
+
+  useEffect(() => {
+    setSelectedRecordmap(new Map());
+  }, [resetSelectionSignal]);
+
+  useEffect(() => {
+    if (selectedRecordObj[column]?.length) {
+      const newSelectedMap: Map<string, boolean> = new Map();
+      selectedRecordObj[column]?.forEach((d) => {
+        newSelectedMap.set(keyGetter(d), true);
+      });
+      setSelectedRecordmap(newSelectedMap);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  return (
+    <div className="min-w-[min(90%,400px)] max-w-[400px] flex-shrink-0 snap-start ">
+      <div className={cn('head mb-2 flex items-center rounded-md px-[11px] py-[5px]', colors.background, colors.color)}>
+        <Checkbox
+          size="small"
+          id={`${column}-select-all`}
+          checked={isAllSelected && data?.length > 0}
+          onClick={(e) => {
+            e.stopPropagation();
+            handleSelectAll();
+          }}
+          indeterminate={!isAllSelected && Object.keys(selectedRecordMap).length > 0}
+          icon={<RadioButtonUnchecked />}
+          indeterminateIcon={<CheckCircleOutline />}
+          checkedIcon={<CheckCircle />}
+          disabled={isDataLoading || data?.length === 0}
+        />
+        <label htmlFor={`${column}-select-all`} className="flex cursor-pointer text-[15px] font-bold">
+          {column} ({count || count === 0 ? count : <Skeleton width={getRandomNumber(20, 50)} />})
+        </label>
+      </div>
+      <div className={cn('h-[calc(100vh-270px)] min-h-[400px] flex-grow ', !isInitialLoading ? '' : 'overflow-hidden')} ref={setContainer}>
+        {!isInitialLoading ? (
+          <Column
+            page={page}
+            count={count}
+            data={data}
+            handleFetchSingleColumnWrapper={handleFetchSingleColumnWrapper}
+            loading={loading}
+            column={column}
+            container={container}
+            getColColors={getColColors}
+            state={state}
+            handleSelectSingle={handleSelectSingle}
+            selectedRecordMap={selectedRecordMap}
+            colors={colors}
+            {...rest}
+          />
+        ) : (
+          <CardColTimelineLoader />
+        )}
+      </div>
+    </div>
+  );
+};
+export default ColumnWrapper;
---
+++ src/pages/WorkOrderTechnician/GridView/GridCustomComponent.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import CustomReactTable, { useTableReducer } from 'src/components/CustomReactTable';
+import { TColType } from 'src/components/CustomReactTable/TableComponents/TableHelperComponents';
+import CommonSkeleton from 'src/components/Helpers/CommonSkeleton';
+import { useEffectEvent } from 'src/hooks/useEffectEvent';
+
+type CustomContentProps = {
+  row: any;
+  height: number;
+  renderedFrom: string;
+  columns: TColType[];
+};
+
+const GridCustomComponent = React.memo(
+  ({ row, height, renderedFrom, columns }: CustomContentProps) => {
+    const { state, dispatch } = useTableReducer({ renderedFrom });
+
+    const getData = useEffectEvent(() => {
+      dispatch({ type: 'initialize', data: row.services, count: row?.services?.length });
+      dispatch({ type: 'loading', loading: false });
+    });
+
+    console.log(row);
+
+    useEffect(() => {
+      getData();
+    }, [getData]);
+
+    return (
+      <>
+        <div className="max-w-full">
+          {columns ? (
+            <>
+              <CustomReactTable
+                showTableHead={false}
+                height={`${height}px`}
+                columns={columns}
+                state={state}
+                dispatch={dispatch}
+                renderedFrom={renderedFrom}
+                isClientSideGrid={true}
+                hideSelection={false}
+                expander={false}
+              />
+            </>
+          ) : (
+            <div>
+              <CommonSkeleton lenArray={[...Array(3).keys()]} xs={12} sm={12} md={12} lg={12} />
+            </div>
+          )}
+        </div>
+      </>
+    );
+  },
+  (prev, next) => prev.row?._id === next.row?._id
+);
+
+export default GridCustomComponent;
