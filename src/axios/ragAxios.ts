f1d65d77cc17ae0661f4447884713ece59366b76
tusharc75 <90505752+tusharc75@users.noreply.github.com>
2025-11-19 14:32:34 +0530

    feat: fix:bullet point issue

--- src/axios/ragAxios.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const ragAxios = axios.create({
-  baseURL: "http://localhost:8000",
-  headers: { "Content-Type": "application/json" }
-});
-
-export default ragAxios;
